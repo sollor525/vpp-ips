FDio/vpp

Adding Sequence Number - Per Packet Counter(PPC) support for iOAM6.
- Added support in classifier session to identify a flow to be iOAM6 encap/decap
- Sequence number as part of iOAM6 E2E header is created as a plugin.

Change-Id: Ib7605de45aecff25d684d099b525f8dc96ee7038
Signed-off-by: AkshayaNadahalli <anadahal@cisco.com>

vpp/vpp-api/api.c
@@ -7219,7 +7219,8 @@ vl_api_ioam_enable_t_handler (vl_api_ioam_enable_t * mp)
 
   /* Ignoring the profile id as currently a single profile
    * is supported */
-  error = ip6_ioam_enable (mp->trace_enable, mp->pow_enable, mp->trace_ppc);
+  error = ip6_ioam_enable (mp->trace_enable, mp->pow_enable,
+			   mp->seqno, mp->analyse);
   if (error)
     {
       clib_error_report (error);

sollor525/vpp-ips

vpp/vpp-api/api.c
@@ -7233,7 +7233,8 @@ vl_api_ioam_enable_t_handler (vl_api_ioam_enable_t * mp)
 
   /* Ignoring the profile id as currently a single profile
    * is supported */
-  error = ip6_ioam_enable (mp->trace_enable, mp->pow_enable, mp->trace_ppc);
+  error = ip6_ioam_enable (mp->trace_enable, mp->pot_enable,
+			   mp->seqno, mp->analyse);
   if (error)
     {
       clib_error_report (error);