FDio/vpp

VPP changes to support iOAM over NSH-MD2. Separate trace data structure definitions into two files to share code with NSH plugin (iOAM)
Change-Id: I0192551f71678e4f814bc6a7d25200a1580f3033
Signed-off-by: Vengada <venggovi@cisco.com>

src/plugins/ioam/analyse/ioam_analyse.h
@@ -21,6 +21,7 @@
 #include <vppinfra/types.h>
 #include <ioam/lib-e2e/e2e_util.h>
 #include <ioam/lib-trace/trace_util.h>
+#include <ioam/lib-trace/trace_internal_util.h>
 
 #define IOAM_FLOW_TEMPLATE_ID    260
 #define IOAM_TRACE_MAX_NODES      10

src/plugins/ioam/encap/ip6_ioam_trace.c
@@ -27,6 +27,8 @@
 #include <vppinfra/elog.h>
 #include <vnet/plugin/plugin.h>
 
+#include <ioam/lib-trace/trace_util.h>
+#include <ioam/lib-trace/trace_internal_util.h>
 #include <ioam/encap/ip6_ioam_trace.h>
 #include <ioam/udp-ping/udp_ping.h>
 #include <ioam/udp-ping/udp_ping_packet.h>

src/plugins/ioam/lib-trace/trace_api.c
@@ -22,6 +22,7 @@
 #include <vnet/vnet.h>
 #include <vnet/plugin/plugin.h>
 #include <ioam/lib-trace/trace_util.h>
+#include <ioam/lib-trace/trace_internal_util.h>
 
 #include <vlibapi/api.h>
 #include <vlibmemory/api.h>

src/plugins/ioam/lib-trace/trace_util.c
@@ -18,6 +18,7 @@
 #include <string.h>
 #include <vppinfra/mem.h>
 #include "trace_util.h"
+#include "trace_internal_util.h"
 
 trace_main_t trace_main;
 

src/plugins/ioam/lib-vxlan-gpe/vxlan_gpe_ioam_trace.c
@@ -25,6 +25,7 @@
 #include <vppinfra/elog.h>
 
 #include <ioam/lib-trace/trace_util.h>
+#include <ioam/lib-trace/trace_internal_util.h>
 #include <ioam/lib-vxlan-gpe/vxlan_gpe_ioam.h>
 
 /* Timestamp precision multipliers for seconds, milliseconds, microseconds

sollor525/vpp-ips

src/plugins/ioam/analyse/ioam_analyse.h
@@ -21,6 +21,7 @@
 #include <vppinfra/types.h>
 #include <ioam/lib-e2e/e2e_util.h>
 #include <ioam/lib-trace/trace_util.h>
+#include <ioam/lib-trace/trace_config.h>
 
 #define IOAM_FLOW_TEMPLATE_ID    260
 #define IOAM_TRACE_MAX_NODES      10

src/plugins/ioam/encap/ip6_ioam_trace.c
@@ -27,6 +27,8 @@
 #include <vppinfra/elog.h>
 #include <vnet/plugin/plugin.h>
 
+#include <ioam/lib-trace/trace_util.h>
+#include <ioam/lib-trace/trace_config.h>
 #include <ioam/encap/ip6_ioam_trace.h>
 #include <ioam/udp-ping/udp_ping.h>
 #include <ioam/udp-ping/udp_ping_packet.h>

src/plugins/ioam/lib-trace/trace_api.c
@@ -22,6 +22,7 @@
 #include <vnet/vnet.h>
 #include <vnet/plugin/plugin.h>
 #include <ioam/lib-trace/trace_util.h>
+#include <ioam/lib-trace/trace_config.h>
 
 #include <vlibapi/api.h>
 #include <vlibmemory/api.h>

src/plugins/ioam/lib-trace/trace_util.c
@@ -18,6 +18,7 @@
 #include <string.h>
 #include <vppinfra/mem.h>
 #include "trace_util.h"
+#include "trace_config.h"
 
 trace_main_t trace_main;
 

src/plugins/ioam/lib-vxlan-gpe/vxlan_gpe_ioam_trace.c
@@ -25,6 +25,7 @@
 #include <vppinfra/elog.h>
 
 #include <ioam/lib-trace/trace_util.h>
+#include <ioam/lib-trace/trace_config.h>
 #include <ioam/lib-vxlan-gpe/vxlan_gpe_ioam.h>
 
 /* Timestamp precision multipliers for seconds, milliseconds, microseconds