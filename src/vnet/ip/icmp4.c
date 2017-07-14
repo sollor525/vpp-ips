FDio/vpp

vnet_buffer_t flags cleanup
Change-Id: I123eccea98abafeb31f25d2a162501e2eded60d4
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vnet/ip/icmp4.c
@@ -336,8 +336,8 @@ ip4_icmp_echo_request (vlib_main_t * vm,
 	  ASSERT (ip0->checksum == ip4_header_checksum (ip0));
 	  ASSERT (ip1->checksum == ip4_header_checksum (ip1));
 
-	  p0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
-	  p1->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+	  p0->flags |= VNET_BUFFER_FLAG_LOCALLY_ORIGINATED;
+	  p1->flags |= VNET_BUFFER_FLAG_LOCALLY_ORIGINATED;
 	}
 
       while (n_left_from > 0 && n_left_to_next > 0)
@@ -392,7 +392,7 @@ ip4_icmp_echo_request (vlib_main_t * vm,
 
 	  ASSERT (ip0->checksum == ip4_header_checksum (ip0));
 
-	  p0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+	  p0->flags |= VNET_BUFFER_FLAG_LOCALLY_ORIGINATED;
 	}
 
       vlib_put_next_frame (vm, node, next, n_left_to_next);

src/vnet/ip/ip6_neighbor.c
@@ -1649,7 +1649,7 @@ icmp6_router_solicitation (vlib_main_t * vm,
 				adj_index0;
 			    }
 			}
-		      p0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+		      p0->flags |= VNET_BUFFER_FLAG_LOCALLY_ORIGINATED;
 
 		      radv_info->n_solicitations_dropped += is_dropped;
 		      radv_info->n_solicitations_rcvd += is_solicitation;
@@ -2348,7 +2348,7 @@ ip6_neighbor_send_mldpv2_report (u32 sw_if_index)
     vnet_main.local_interface_sw_if_index;
 
   vnet_buffer (b0)->ip.adj_index[VLIB_TX] = radv_info->mcast_adj_index;
-  b0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+  b0->flags |= VNET_BUFFER_FLAG_LOCALLY_ORIGINATED;
 
   vlib_node_t *node = vlib_get_node_by_name (vm, (u8 *) "ip6-rewrite-mcast");
 

src/vnet/tcp/tcp_output.c
@@ -585,7 +585,7 @@ tcp_enqueue_to_ip_lookup (vlib_main_t * vm, vlib_buffer_t * b, u32 bi,
   u32 *to_next, next_index;
   vlib_frame_t *f;
 
-  b->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+  b->flags |= VNET_BUFFER_FLAG_LOCALLY_ORIGINATED;
   b->error = 0;
 
   /* Default FIB for now */
@@ -847,7 +847,7 @@ tcp_enqueue_to_output (vlib_main_t * vm, vlib_buffer_t * b, u32 bi, u8 is_ip4)
   u32 *to_next, next_index;
   vlib_frame_t *f;
 
-  b->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+  b->flags |= VNET_BUFFER_FLAG_LOCALLY_ORIGINATED;
   b->error = 0;
 
   /* Decide where to send the packet */
@@ -1563,7 +1563,7 @@ tcp46_output_inline (vlib_main_t * vm,
 	  vnet_buffer (b0)->sw_if_index[VLIB_RX] = 0;
 	  vnet_buffer (b0)->sw_if_index[VLIB_TX] = ~0;
 
-	  b0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+	  b0->flags |= VNET_BUFFER_FLAG_LOCALLY_ORIGINATED;
 	done:
 	  b0->error = node->errors[error0];
 	  if (PREDICT_FALSE (b0->flags & VLIB_BUFFER_IS_TRACED))
@@ -1728,7 +1728,7 @@ tcp46_send_reset_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 
 	done:
 	  b0->error = node->errors[error0];
-	  b0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+	  b0->flags |= VNET_BUFFER_FLAG_LOCALLY_ORIGINATED;
 	  if (PREDICT_FALSE (b0->flags & VLIB_BUFFER_IS_TRACED))
 	    {
 	      th0 = vlib_buffer_get_current (b0);

src/vnet/vxlan-gpe/decap.c
@@ -857,7 +857,7 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags0 = b0->flags;
-	  good_udp0 = (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp0 = (flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp0 |= udp0->checksum == 0;
@@ -873,14 +873,14 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp0))
 	    {
-	      if ((flags0 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags0 = ip4_tcp_udp_validate_checksum (vm, b0);
 		  else
 		    flags0 = ip6_tcp_udp_icmp_validate_checksum (vm, b0);
 		  good_udp0 =
-		    (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 
@@ -939,7 +939,7 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags1 = b1->flags;
-	  good_udp1 = (flags1 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp1 = (flags1 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp1 |= udp1->checksum == 0;
@@ -955,14 +955,14 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp1))
 	    {
-	      if ((flags1 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags1 & VNET_BUFFER_FLAG_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags1 = ip4_tcp_udp_validate_checksum (vm, b1);
 		  else
 		    flags1 = ip6_tcp_udp_icmp_validate_checksum (vm, b1);
 		  good_udp1 =
-		    (flags1 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags1 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 
@@ -1055,7 +1055,7 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags0 = b0->flags;
-	  good_udp0 = (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp0 = (flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp0 |= udp0->checksum == 0;
@@ -1071,14 +1071,14 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp0))
 	    {
-	      if ((flags0 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags0 = ip4_tcp_udp_validate_checksum (vm, b0);
 		  else
 		    flags0 = ip6_tcp_udp_icmp_validate_checksum (vm, b0);
 		  good_udp0 =
-		    (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 

src/vnet/vxlan/decap.c
@@ -916,7 +916,7 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags0 = b0->flags;
-	  good_udp0 = (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp0 = (flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp0 |= udp0->checksum == 0;
@@ -932,14 +932,14 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp0))
 	    {
-	      if ((flags0 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags0 = ip4_tcp_udp_validate_checksum (vm, b0);
 		  else
 		    flags0 = ip6_tcp_udp_icmp_validate_checksum (vm, b0);
 		  good_udp0 =
-		    (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 
@@ -998,7 +998,7 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags1 = b1->flags;
-	  good_udp1 = (flags1 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp1 = (flags1 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp1 |= udp1->checksum == 0;
@@ -1014,14 +1014,14 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp1))
 	    {
-	      if ((flags1 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags1 & VNET_BUFFER_FLAG_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags1 = ip4_tcp_udp_validate_checksum (vm, b1);
 		  else
 		    flags1 = ip6_tcp_udp_icmp_validate_checksum (vm, b1);
 		  good_udp1 =
-		    (flags1 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags1 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 
@@ -1116,7 +1116,7 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags0 = b0->flags;
-	  good_udp0 = (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp0 = (flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp0 |= udp0->checksum == 0;
@@ -1132,14 +1132,14 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp0))
 	    {
-	      if ((flags0 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags0 = ip4_tcp_udp_validate_checksum (vm, b0);
 		  else
 		    flags0 = ip6_tcp_udp_icmp_validate_checksum (vm, b0);
 		  good_udp0 =
-		    (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags0 & VNET_BUFFER_FLAG_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 

sollor525/vpp-ips

src/vnet/ip/icmp4.c
@@ -336,8 +336,8 @@ ip4_icmp_echo_request (vlib_main_t * vm,
 	  ASSERT (ip0->checksum == ip4_header_checksum (ip0));
 	  ASSERT (ip1->checksum == ip4_header_checksum (ip1));
 
-	  p0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
-	  p1->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+	  p0->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
+	  p1->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
 	}
 
       while (n_left_from > 0 && n_left_to_next > 0)
@@ -392,7 +392,7 @@ ip4_icmp_echo_request (vlib_main_t * vm,
 
 	  ASSERT (ip0->checksum == ip4_header_checksum (ip0));
 
-	  p0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+	  p0->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
 	}
 
       vlib_put_next_frame (vm, node, next, n_left_to_next);

src/vnet/ip/ip6_neighbor.c
@@ -1649,7 +1649,7 @@ icmp6_router_solicitation (vlib_main_t * vm,
 				adj_index0;
 			    }
 			}
-		      p0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+		      p0->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
 
 		      radv_info->n_solicitations_dropped += is_dropped;
 		      radv_info->n_solicitations_rcvd += is_solicitation;
@@ -2348,7 +2348,7 @@ ip6_neighbor_send_mldpv2_report (u32 sw_if_index)
     vnet_main.local_interface_sw_if_index;
 
   vnet_buffer (b0)->ip.adj_index[VLIB_TX] = radv_info->mcast_adj_index;
-  b0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+  b0->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
 
   vlib_node_t *node = vlib_get_node_by_name (vm, (u8 *) "ip6-rewrite-mcast");
 

src/vnet/tcp/tcp_output.c
@@ -585,7 +585,7 @@ tcp_enqueue_to_ip_lookup (vlib_main_t * vm, vlib_buffer_t * b, u32 bi,
   u32 *to_next, next_index;
   vlib_frame_t *f;
 
-  b->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+  b->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
   b->error = 0;
 
   /* Default FIB for now */
@@ -847,7 +847,7 @@ tcp_enqueue_to_output (vlib_main_t * vm, vlib_buffer_t * b, u32 bi, u8 is_ip4)
   u32 *to_next, next_index;
   vlib_frame_t *f;
 
-  b->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+  b->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
   b->error = 0;
 
   /* Decide where to send the packet */
@@ -1563,7 +1563,7 @@ tcp46_output_inline (vlib_main_t * vm,
 	  vnet_buffer (b0)->sw_if_index[VLIB_RX] = 0;
 	  vnet_buffer (b0)->sw_if_index[VLIB_TX] = ~0;
 
-	  b0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+	  b0->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
 	done:
 	  b0->error = node->errors[error0];
 	  if (PREDICT_FALSE (b0->flags & VLIB_BUFFER_IS_TRACED))
@@ -1728,7 +1728,7 @@ tcp46_send_reset_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 
 	done:
 	  b0->error = node->errors[error0];
-	  b0->flags |= VNET_BUFFER_LOCALLY_ORIGINATED;
+	  b0->flags |= VNET_BUFFER_F_LOCALLY_ORIGINATED;
 	  if (PREDICT_FALSE (b0->flags & VLIB_BUFFER_IS_TRACED))
 	    {
 	      th0 = vlib_buffer_get_current (b0);

src/vnet/vxlan-gpe/decap.c
@@ -857,7 +857,7 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags0 = b0->flags;
-	  good_udp0 = (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp0 = (flags0 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp0 |= udp0->checksum == 0;
@@ -873,14 +873,14 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp0))
 	    {
-	      if ((flags0 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags0 & VNET_BUFFER_F_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags0 = ip4_tcp_udp_validate_checksum (vm, b0);
 		  else
 		    flags0 = ip6_tcp_udp_icmp_validate_checksum (vm, b0);
 		  good_udp0 =
-		    (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags0 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 
@@ -939,7 +939,7 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags1 = b1->flags;
-	  good_udp1 = (flags1 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp1 = (flags1 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp1 |= udp1->checksum == 0;
@@ -955,14 +955,14 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp1))
 	    {
-	      if ((flags1 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags1 & VNET_BUFFER_F_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags1 = ip4_tcp_udp_validate_checksum (vm, b1);
 		  else
 		    flags1 = ip6_tcp_udp_icmp_validate_checksum (vm, b1);
 		  good_udp1 =
-		    (flags1 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags1 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 
@@ -1055,7 +1055,7 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags0 = b0->flags;
-	  good_udp0 = (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp0 = (flags0 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp0 |= udp0->checksum == 0;
@@ -1071,14 +1071,14 @@ ip_vxlan_gpe_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp0))
 	    {
-	      if ((flags0 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags0 & VNET_BUFFER_F_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags0 = ip4_tcp_udp_validate_checksum (vm, b0);
 		  else
 		    flags0 = ip6_tcp_udp_icmp_validate_checksum (vm, b0);
 		  good_udp0 =
-		    (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags0 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 

src/vnet/vxlan/decap.c
@@ -916,7 +916,7 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags0 = b0->flags;
-	  good_udp0 = (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp0 = (flags0 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp0 |= udp0->checksum == 0;
@@ -932,14 +932,14 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp0))
 	    {
-	      if ((flags0 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags0 & VNET_BUFFER_F_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags0 = ip4_tcp_udp_validate_checksum (vm, b0);
 		  else
 		    flags0 = ip6_tcp_udp_icmp_validate_checksum (vm, b0);
 		  good_udp0 =
-		    (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags0 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 
@@ -998,7 +998,7 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags1 = b1->flags;
-	  good_udp1 = (flags1 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp1 = (flags1 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp1 |= udp1->checksum == 0;
@@ -1014,14 +1014,14 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp1))
 	    {
-	      if ((flags1 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags1 & VNET_BUFFER_F_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags1 = ip4_tcp_udp_validate_checksum (vm, b1);
 		  else
 		    flags1 = ip6_tcp_udp_icmp_validate_checksum (vm, b1);
 		  good_udp1 =
-		    (flags1 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags1 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 
@@ -1116,7 +1116,7 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	    }
 
 	  flags0 = b0->flags;
-	  good_udp0 = (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+	  good_udp0 = (flags0 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 
 	  /* Don't verify UDP checksum for packets with explicit zero checksum. */
 	  good_udp0 |= udp0->checksum == 0;
@@ -1132,14 +1132,14 @@ ip_vxlan_bypass_inline (vlib_main_t * vm,
 	  /* Verify UDP checksum */
 	  if (PREDICT_FALSE (!good_udp0))
 	    {
-	      if ((flags0 & IP_BUFFER_L4_CHECKSUM_COMPUTED) == 0)
+	      if ((flags0 & VNET_BUFFER_F_L4_CHECKSUM_COMPUTED) == 0)
 	        {
 		  if (is_ip4)
 		    flags0 = ip4_tcp_udp_validate_checksum (vm, b0);
 		  else
 		    flags0 = ip6_tcp_udp_icmp_validate_checksum (vm, b0);
 		  good_udp0 =
-		    (flags0 & IP_BUFFER_L4_CHECKSUM_CORRECT) != 0;
+		    (flags0 & VNET_BUFFER_F_L4_CHECKSUM_CORRECT) != 0;
 	        }
 	    }
 