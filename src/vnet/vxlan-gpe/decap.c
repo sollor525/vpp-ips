FDio/vpp

make "test-all" target pass again
The "test-all" target is still never called as part of any continuous
test (as it probably should) but at least it can now be expected to
succeed.

VXLAN-GPE:
* decapsulate Ethernet to "l2-input" instead of "ethernet-input"
  otherwise the inner mac address get checked against the interface one
  (external) and packet gets dropped (mac mismatch)
* set packet input sw_if_index to unicast vxlan tunnel for learning

TEST:
* VXLAN:
  * reduce the number of share tunnels:
    => reduce test duration by half
    => no functional change
* VXLAN-GPE:
  * fix test TearDown() cli: command is "show vxlan-gpe" only
  * remove vxlan-gpe specific tests as the were a duplicated of the
    BridgeDomain one and already inherited.
  * disable test_mcast_rcv() and test_mcast_flood() tests
* P2PEthernetAPI:
  * remove test: "create 100k of p2p subifs"
    there already is a "create 1k p2p subifs" so this one is a load test
    and not a unit test.
    See: lists.fd.io/pipermail/vpp-dev/2017-November/007280.html

Change-Id: Icafb83769eb560cbdeb3dc6d1f1d3c23c0901cd9
Signed-off-by: Gabriel Ganne <gabriel.ganne@enea.com>

src/vnet/vxlan-gpe/decap.c
@@ -326,6 +326,9 @@ vxlan_gpe_input (vlib_main_t * vm,
       /* Required to make the l2 tag push / pop code work on l2 subifs */
       vnet_update_l2_len (b0);
 
+      /* Set packet input sw_if_index to unicast VXLAN tunnel for learning */
+      vnet_buffer(b0)->sw_if_index[VLIB_RX] = t0->sw_if_index;
+
       /**
        * ip[46] lookup in the configured FIB
        */
@@ -408,6 +411,9 @@ vxlan_gpe_input (vlib_main_t * vm,
 
       /* Required to make the l2 tag push / pop code work on l2 subifs */
       vnet_update_l2_len (b1);
+
+      /* Set packet input sw_if_index to unicast VXLAN tunnel for learning */
+      vnet_buffer(b1)->sw_if_index[VLIB_RX] = t1->sw_if_index;
 
       /*
        * ip[46] lookup in the configured FIB
@@ -569,6 +575,9 @@ vxlan_gpe_input (vlib_main_t * vm,
 
       /* Required to make the l2 tag push / pop code work on l2 subifs */
       vnet_update_l2_len (b0);
+
+      /* Set packet input sw_if_index to unicast VXLAN tunnel for learning */
+      vnet_buffer(b0)->sw_if_index[VLIB_RX] = t0->sw_if_index;
 
       /*
        * ip[46] lookup in the configured FIB

sollor525/vpp-ips

src/vnet/vxlan-gpe/decap.c
@@ -338,6 +338,9 @@ vxlan_gpe_input (vlib_main_t * vm,
 	  /* Required to make the l2 tag push / pop code work on l2 subifs */
 	  vnet_update_l2_len (b0);
 
+	  /* Set packet input sw_if_index to unicast VXLAN tunnel for learning */
+	  vnet_buffer (b0)->sw_if_index[VLIB_RX] = t0->sw_if_index;
+
       /**
        * ip[46] lookup in the configured FIB
        */
@@ -425,6 +428,9 @@ vxlan_gpe_input (vlib_main_t * vm,
 
 	  /* Required to make the l2 tag push / pop code work on l2 subifs */
 	  vnet_update_l2_len (b1);
+
+	  /* Set packet input sw_if_index to unicast VXLAN tunnel for learning */
+	  vnet_buffer (b1)->sw_if_index[VLIB_RX] = t1->sw_if_index;
 
 	  /*
 	   * ip[46] lookup in the configured FIB
@@ -594,6 +600,9 @@ vxlan_gpe_input (vlib_main_t * vm,
 
 	  /* Required to make the l2 tag push / pop code work on l2 subifs */
 	  vnet_update_l2_len (b0);
+
+	  /* Set packet input sw_if_index to unicast VXLAN tunnel for learning */
+	  vnet_buffer (b0)->sw_if_index[VLIB_RX] = t0->sw_if_index;
 
 	  /*
 	   * ip[46] lookup in the configured FIB