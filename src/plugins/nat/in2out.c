FDio/vpp

NAT64: IPFix (VPP-1106)
Change-Id: Ib90dc5613c9fdac0344b3bd7f163e2f7163c64d8
Signed-off-by: Matus Fabian <matfabia@cisco.com>

src/plugins/nat/in2out.c
@@ -270,6 +270,7 @@ static u32 slow_path (snat_main_t *sm, vlib_buffer_t *b0,
   if (PREDICT_FALSE (maximum_sessions_exceeded(sm, thread_index)))
     {
       b0->error = node->errors[SNAT_IN2OUT_ERROR_MAX_SESSIONS_EXCEEDED];
+      nat_ipfix_logging_max_sesssions(sm->max_translations);
       return SNAT_IN2OUT_NEXT_DROP;
     }
 
@@ -291,13 +292,6 @@ static u32 slow_path (snat_main_t *sm, vlib_buffer_t *b0,
       return SNAT_IN2OUT_NEXT_DROP;
     }
 
-  s = nat_session_alloc_or_recycle (sm, u, thread_index);
-  if (!s)
-    {
-      clib_warning ("create NAT session failed");
-      return SNAT_IN2OUT_NEXT_DROP;
-    }
-
   /* First try to match static mapping by local address and port */
   if (snat_static_mapping_match (sm, *key0, &key1, 0, 0, 0))
     {
@@ -316,9 +310,17 @@ static u32 slow_path (snat_main_t *sm, vlib_buffer_t *b0,
   else
     {
       u->nstaticsessions++;
-      s->flags |= SNAT_SESSION_FLAG_STATIC_MAPPING;
     }
 
+  s = nat_session_alloc_or_recycle (sm, u, thread_index);
+  if (!s)
+    {
+      clib_warning ("create NAT session failed");
+      return SNAT_IN2OUT_NEXT_DROP;
+    }
+
+  if (address_index == ~0)
+    s->flags |= SNAT_SESSION_FLAG_STATIC_MAPPING;
   s->outside_address_index = address_index;
   s->in2out = *key0;
   s->out2in = key1;
@@ -999,6 +1001,7 @@ snat_in2out_unknown_proto (snat_main_t *sm,
       if (PREDICT_FALSE (maximum_sessions_exceeded(sm, thread_index)))
         {
           b->error = node->errors[SNAT_IN2OUT_ERROR_MAX_SESSIONS_EXCEEDED];
+          nat_ipfix_logging_max_sesssions(sm->max_translations);
           return 0;
         }
 
@@ -1186,6 +1189,7 @@ snat_in2out_lb (snat_main_t *sm,
       if (PREDICT_FALSE (maximum_sessions_exceeded (sm, thread_index)))
         {
           b->error = node->errors[SNAT_IN2OUT_ERROR_MAX_SESSIONS_EXCEEDED];
+          nat_ipfix_logging_max_sesssions(sm->max_translations);
           return 0;
         }
 

src/plugins/nat/nat64_db.c
@@ -17,6 +17,8 @@
  * @brief NAT64 DB
  */
 #include <nat/nat64_db.h>
+#include <nat/nat_ipfix_logging.h>
+#include <vnet/fib/fib_table.h>
 
 int
 nat64_db_init (nat64_db_t * db, u32 bib_buckets, u32 bib_memory_size,
@@ -36,6 +38,12 @@ nat64_db_init (nat64_db_t * db, u32 bib_buckets, u32 bib_memory_size,
 			 st_memory_size);
 
   db->free_addr_port_cb = free_addr_port_cb;
+  db->bib.limit = 10 * bib_buckets;
+  db->bib.bib_entries_num = 0;
+  db->st.limit = 10 * st_buckets;
+  db->st.st_entries_num = 0;
+  db->addr_free = 0;
+
   return 0;
 }
 
@@ -48,6 +56,14 @@ nat64_db_bib_entry_create (nat64_db_t * db, ip6_address_t * in_addr,
   nat64_db_bib_entry_t *bibe;
   nat64_db_bib_entry_key_t bibe_key;
   clib_bihash_kv_24_8_t kv;
+  fib_table_t *fib;
+
+  if (db->bib.bib_entries_num >= db->bib.limit)
+    {
+      db->free_addr_port_cb (db, out_addr, out_port, proto);
+      nat_ipfix_logging_max_bibs (db->bib.limit);
+      return 0;
+    }
 
   /* create pool entry */
   switch (ip_proto_to_snat_proto (proto))
@@ -66,6 +82,9 @@ nat64_db_bib_entry_create (nat64_db_t * db, ip6_address_t * in_addr,
       kv.value = bibe - db->bib._unk_proto_bib;
       break;
     }
+
+  db->bib.bib_entries_num++;
+
   memset (bibe, 0, sizeof (*bibe));
   bibe->in_addr.as_u64[0] = in_addr->as_u64[0];
   bibe->in_addr.as_u64[1] = in_addr->as_u64[1];
@@ -97,6 +116,9 @@ nat64_db_bib_entry_create (nat64_db_t * db, ip6_address_t * in_addr,
   kv.key[2] = bibe_key.as_u64[2];
   clib_bihash_add_del_24_8 (&db->bib.out2in, &kv, 1);
 
+  fib = fib_table_get (bibe->fib_index, FIB_PROTOCOL_IP6);
+  nat_ipfix_logging_nat64_bib (in_addr, out_addr, proto, in_port, out_port,
+			       fib->ft_table_id, 1);
   return bibe;
 }
 
@@ -108,6 +130,7 @@ nat64_db_bib_entry_free (nat64_db_t * db, nat64_db_bib_entry_t * bibe)
   nat64_db_bib_entry_t *bib;
   u32 *ste_to_be_free = 0, *ste_index, bibe_index;
   nat64_db_st_entry_t *st, *ste;
+  fib_table_t *fib;
 
   switch (ip_proto_to_snat_proto (bibe->proto))
     {
@@ -125,6 +148,8 @@ nat64_db_bib_entry_free (nat64_db_t * db, nat64_db_bib_entry_t * bibe)
       st = db->st._unk_proto_st;
       break;
     }
+
+  db->bib.bib_entries_num--;
 
   bibe_index = bibe - bib;
 
@@ -162,7 +187,14 @@ nat64_db_bib_entry_free (nat64_db_t * db, nat64_db_bib_entry_t * bibe)
   kv.key[2] = bibe_key.as_u64[2];
   clib_bihash_add_del_24_8 (&db->bib.out2in, &kv, 0);
 
-  db->free_addr_port_cb (db, &bibe->out_addr, bibe->out_port, bibe->proto);
+  if (!db->addr_free)
+    db->free_addr_port_cb (db, &bibe->out_addr, bibe->out_port, bibe->proto);
+
+  fib = fib_table_get (bibe->fib_index, FIB_PROTOCOL_IP6);
+  nat_ipfix_logging_nat64_bib (&bibe->in_addr, &bibe->out_addr, bibe->proto,
+			       bibe->in_port, bibe->out_port,
+			       fib->ft_table_id, 0);
+
   /* delete from pool */
   pool_put (bib, bibe);
 
@@ -344,6 +376,13 @@ nat64_db_st_entry_create (nat64_db_t * db, nat64_db_bib_entry_t * bibe,
   nat64_db_bib_entry_t *bib;
   nat64_db_st_entry_key_t ste_key;
   clib_bihash_kv_48_8_t kv;
+  fib_table_t *fib;
+
+  if (db->st.st_entries_num >= db->st.limit)
+    {
+      nat_ipfix_logging_max_sesssions (db->st.limit);
+      return 0;
+    }
 
   /* create pool entry */
   switch (ip_proto_to_snat_proto (bibe->proto))
@@ -364,6 +403,9 @@ nat64_db_st_entry_create (nat64_db_t * db, nat64_db_bib_entry_t * bibe,
       bib = db->bib._unk_proto_bib;
       break;
     }
+
+  db->st.st_entries_num++;
+
   memset (ste, 0, sizeof (*ste));
   ste->in_r_addr.as_u64[0] = in_r_addr->as_u64[0];
   ste->in_r_addr.as_u64[1] = in_r_addr->as_u64[1];
@@ -407,6 +449,13 @@ nat64_db_st_entry_create (nat64_db_t * db, nat64_db_bib_entry_t * bibe,
   kv.key[5] = ste_key.as_u64[5];
   clib_bihash_add_del_48_8 (&db->st.out2in, &kv, 1);
 
+  fib = fib_table_get (bibe->fib_index, FIB_PROTOCOL_IP6);
+  nat_ipfix_logging_nat64_session (&bibe->in_addr, &bibe->out_addr,
+				   bibe->proto, bibe->in_port, bibe->out_port,
+				   &ste->in_r_addr, &ste->out_r_addr,
+				   ste->r_port, ste->r_port, fib->ft_table_id,
+				   1);
+
   return ste;
 }
 
@@ -417,6 +466,7 @@ nat64_db_st_entry_free (nat64_db_t * db, nat64_db_st_entry_t * ste)
   nat64_db_bib_entry_t *bib, *bibe;
   nat64_db_st_entry_key_t ste_key;
   clib_bihash_kv_48_8_t kv;
+  fib_table_t *fib;
 
   switch (ip_proto_to_snat_proto (ste->proto))
     {
@@ -436,6 +486,8 @@ nat64_db_st_entry_free (nat64_db_t * db, nat64_db_st_entry_t * ste)
     }
 
   bibe = pool_elt_at_index (bib, ste->bibe_index);
+
+  db->st.st_entries_num--;
 
   /* delete hash lookup */
   memset (&ste_key, 0, sizeof (ste_key));
@@ -468,6 +520,13 @@ nat64_db_st_entry_free (nat64_db_t * db, nat64_db_st_entry_t * ste)
   kv.key[4] = ste_key.as_u64[4];
   kv.key[5] = ste_key.as_u64[5];
   clib_bihash_add_del_48_8 (&db->st.out2in, &kv, 0);
+
+  fib = fib_table_get (bibe->fib_index, FIB_PROTOCOL_IP6);
+  nat_ipfix_logging_nat64_session (&bibe->in_addr, &bibe->out_addr,
+				   bibe->proto, bibe->in_port, bibe->out_port,
+				   &ste->in_r_addr, &ste->out_r_addr,
+				   ste->r_port, ste->r_port, fib->ft_table_id,
+				   0);
 
   /* delete from pool */
   pool_put (st, ste);
@@ -613,6 +672,7 @@ nat64_db_free_out_addr (nat64_db_t * db, ip4_address_t * out_addr)
   nat64_db_st_entry_t *st, *ste;
   nat64_db_bib_entry_t *bibe;
 
+  db->addr_free = 1;
 /* *INDENT-OFF* */
 #define _(N, i, n, s) \
   st = db->st._##n##_st; \
@@ -636,6 +696,7 @@ nat64_db_free_out_addr (nat64_db_t * db, ip4_address_t * out_addr)
   vec_foreach (ste_index, ste_to_be_free)
     nat64_db_st_entry_free (db, pool_elt_at_index(st, ste_index[0]));
   vec_free (ste_to_be_free);
+  db->addr_free = 0;
 /* *INDENT-ON* */
 }
 

src/plugins/nat/nat_ipfix_logging.h
@@ -17,16 +17,26 @@
 #ifndef __included_nat_ipfix_logging_h__
 #define __included_nat_ipfix_logging_h__
 
+#include <nat/nat.h>
+
 typedef enum {
   NAT_ADDRESSES_EXHAUTED = 3,
   NAT44_SESSION_CREATE = 4,
   NAT44_SESSION_DELETE = 5,
+  NAT64_SESSION_CREATE = 6,
+  NAT64_SESSION_DELETE = 7,
+  NAT64_BIB_CREATE = 10,
+  NAT64_BIB_DELETE = 11,
   NAT_PORTS_EXHAUSTED = 12,
   QUOTA_EXCEEDED = 13,
 } nat_event_t;
 
 typedef enum {
+  MAX_SESSION_ENTRIES = 1,
+  MAX_BIB_ENTRIES = 2,
   MAX_ENTRIES_PER_USER = 3,
+  MAX_FRAGMENTS_PENDING_REASSEMBLY = 5,
+  MAX_FRAGMENTS_PENDING_REASSEMBLY_IP6,
 } quota_exceed_event_t;
 
 typedef struct {
@@ -37,16 +47,34 @@ typedef struct {
   vlib_buffer_t *nat44_session_buffer;
   vlib_buffer_t *addr_exhausted_buffer;
   vlib_buffer_t *max_entries_per_user_buffer;
+  vlib_buffer_t *max_sessions_buffer;
+  vlib_buffer_t *max_bibs_buffer;
+  vlib_buffer_t *max_frags_ip4_buffer;
+  vlib_buffer_t *max_frags_ip6_buffer;
+  vlib_buffer_t *nat64_bib_buffer;
+  vlib_buffer_t *nat64_ses_buffer;
 
   /** frames containing ipfix buffers */
   vlib_frame_t *nat44_session_frame;
   vlib_frame_t *addr_exhausted_frame;
   vlib_frame_t *max_entries_per_user_frame;
+  vlib_frame_t *max_sessions_frame;
+  vlib_frame_t *max_bibs_frame;
+  vlib_frame_t *max_frags_ip4_frame;
+  vlib_frame_t *max_frags_ip6_frame;
+  vlib_frame_t *nat64_bib_frame;
+  vlib_frame_t *nat64_ses_frame;
 
   /** next record offset */
   u32 nat44_session_next_record_offset;
   u32 addr_exhausted_next_record_offset;
   u32 max_entries_per_user_next_record_offset;
+  u32 max_sessions_next_record_offset;
+  u32 max_bibs_next_record_offset;
+  u32 max_frags_ip4_next_record_offset;
+  u32 max_frags_ip6_next_record_offset;
+  u32 nat64_bib_next_record_offset;
+  u32 nat64_ses_next_record_offset;
 
   /** Time reference pair */
   u64 milisecond_time_0;
@@ -56,6 +84,12 @@ typedef struct {
   u16 nat44_session_template_id;
   u16 addr_exhausted_template_id;
   u16 max_entries_per_user_template_id;
+  u16 max_sessions_template_id;
+  u16 max_bibs_template_id;
+  u16 max_frags_ip4_template_id;
+  u16 max_frags_ip6_template_id;
+  u16 nat64_bib_template_id;
+  u16 nat64_ses_template_id;
 
   /** stream index */
   u32 stream_index;
@@ -74,6 +108,21 @@ void snat_ipfix_logging_nat44_ses_delete (u32 src_ip, u32 nat_src_ip,
                                           u16 src_port, u16 nat_src_port,
                                           u32 vrf_id);
 void snat_ipfix_logging_addresses_exhausted(u32 pool_id);
-void snat_ipfix_logging_max_entries_per_user(u32 src_ip);
+void snat_ipfix_logging_max_entries_per_user(u32 limit, u32 src_ip);
+void nat_ipfix_logging_max_sesssions(u32 limit);
+void nat_ipfix_logging_max_bibs(u32 limit);
+void nat_ipfix_logging_max_fragments_ip4(u32 limit, ip4_address_t * src);
+void nat_ipfix_logging_max_fragments_ip6(u32 limit, ip6_address_t * src);
+void nat_ipfix_logging_nat64_session(ip6_address_t * src_ip,
+                                     ip4_address_t * nat_src_ip, u8 proto,
+                                     u16 src_port, u16 nat_src_port,
+                                     ip6_address_t * dst_ip,
+                                     ip4_address_t * nat_dst_ip,
+                                     u16 dst_port, u16 nat_dst_port,
+                                     u32 vrf_id, u8 is_create);
+void nat_ipfix_logging_nat64_bib(ip6_address_t * src_ip,
+                                 ip4_address_t * nat_src_ip, u8 proto,
+                                 u16 src_port, u16 nat_src_port,
+                                 u32 vrf_id, u8 is_create);
 
 #endif /* __included_nat_ipfix_logging_h__ */

sollor525/vpp-ips

src/plugins/nat/in2out.c
@@ -162,7 +162,7 @@ typedef enum {
 /**
  * @brief Check if packet should be translated
  *
- * Packets aimed at outside interface and external addresss with active session
+ * Packets aimed at outside interface and external address with active session
  * should be translated.
  *
  * @param sm            NAT main
@@ -273,6 +273,7 @@ static u32 slow_path (snat_main_t *sm, vlib_buffer_t *b0,
   if (PREDICT_FALSE (maximum_sessions_exceeded(sm, thread_index)))
     {
       b0->error = node->errors[SNAT_IN2OUT_ERROR_MAX_SESSIONS_EXCEEDED];
+      nat_ipfix_logging_max_sessions(sm->max_translations);
       return SNAT_IN2OUT_NEXT_DROP;
     }
 
@@ -294,13 +295,6 @@ static u32 slow_path (snat_main_t *sm, vlib_buffer_t *b0,
       return SNAT_IN2OUT_NEXT_DROP;
     }
 
-  s = nat_session_alloc_or_recycle (sm, u, thread_index);
-  if (!s)
-    {
-      clib_warning ("create NAT session failed");
-      return SNAT_IN2OUT_NEXT_DROP;
-    }
-
   /* First try to match static mapping by local address and port */
   if (snat_static_mapping_match (sm, *key0, &key1, 0, 0, 0))
     {
@@ -319,9 +313,17 @@ static u32 slow_path (snat_main_t *sm, vlib_buffer_t *b0,
   else
     {
       u->nstaticsessions++;
-      s->flags |= SNAT_SESSION_FLAG_STATIC_MAPPING;
     }
 
+  s = nat_session_alloc_or_recycle (sm, u, thread_index);
+  if (!s)
+    {
+      clib_warning ("create NAT session failed");
+      return SNAT_IN2OUT_NEXT_DROP;
+    }
+
+  if (address_index == ~0)
+    s->flags |= SNAT_SESSION_FLAG_STATIC_MAPPING;
   s->outside_address_index = address_index;
   s->in2out = *key0;
   s->out2in = key1;
@@ -1002,6 +1004,7 @@ snat_in2out_unknown_proto (snat_main_t *sm,
       if (PREDICT_FALSE (maximum_sessions_exceeded(sm, thread_index)))
         {
           b->error = node->errors[SNAT_IN2OUT_ERROR_MAX_SESSIONS_EXCEEDED];
+          nat_ipfix_logging_max_sessions(sm->max_translations);
           return 0;
         }
 
@@ -1189,6 +1192,7 @@ snat_in2out_lb (snat_main_t *sm,
       if (PREDICT_FALSE (maximum_sessions_exceeded (sm, thread_index)))
         {
           b->error = node->errors[SNAT_IN2OUT_ERROR_MAX_SESSIONS_EXCEEDED];
+          nat_ipfix_logging_max_sessions(sm->max_translations);
           return 0;
         }
 

src/plugins/nat/nat64_db.c
@@ -17,6 +17,8 @@
  * @brief NAT64 DB
  */
 #include <nat/nat64_db.h>
+#include <nat/nat_ipfix_logging.h>
+#include <vnet/fib/fib_table.h>
 
 int
 nat64_db_init (nat64_db_t * db, u32 bib_buckets, u32 bib_memory_size,
@@ -36,6 +38,12 @@ nat64_db_init (nat64_db_t * db, u32 bib_buckets, u32 bib_memory_size,
 			 st_memory_size);
 
   db->free_addr_port_cb = free_addr_port_cb;
+  db->bib.limit = 10 * bib_buckets;
+  db->bib.bib_entries_num = 0;
+  db->st.limit = 10 * st_buckets;
+  db->st.st_entries_num = 0;
+  db->addr_free = 0;
+
   return 0;
 }
 
@@ -48,6 +56,14 @@ nat64_db_bib_entry_create (nat64_db_t * db, ip6_address_t * in_addr,
   nat64_db_bib_entry_t *bibe;
   nat64_db_bib_entry_key_t bibe_key;
   clib_bihash_kv_24_8_t kv;
+  fib_table_t *fib;
+
+  if (db->bib.bib_entries_num >= db->bib.limit)
+    {
+      db->free_addr_port_cb (db, out_addr, out_port, proto);
+      nat_ipfix_logging_max_bibs (db->bib.limit);
+      return 0;
+    }
 
   /* create pool entry */
   switch (ip_proto_to_snat_proto (proto))
@@ -66,6 +82,9 @@ nat64_db_bib_entry_create (nat64_db_t * db, ip6_address_t * in_addr,
       kv.value = bibe - db->bib._unk_proto_bib;
       break;
     }
+
+  db->bib.bib_entries_num++;
+
   memset (bibe, 0, sizeof (*bibe));
   bibe->in_addr.as_u64[0] = in_addr->as_u64[0];
   bibe->in_addr.as_u64[1] = in_addr->as_u64[1];
@@ -97,6 +116,9 @@ nat64_db_bib_entry_create (nat64_db_t * db, ip6_address_t * in_addr,
   kv.key[2] = bibe_key.as_u64[2];
   clib_bihash_add_del_24_8 (&db->bib.out2in, &kv, 1);
 
+  fib = fib_table_get (bibe->fib_index, FIB_PROTOCOL_IP6);
+  nat_ipfix_logging_nat64_bib (in_addr, out_addr, proto, in_port, out_port,
+			       fib->ft_table_id, 1);
   return bibe;
 }
 
@@ -108,6 +130,7 @@ nat64_db_bib_entry_free (nat64_db_t * db, nat64_db_bib_entry_t * bibe)
   nat64_db_bib_entry_t *bib;
   u32 *ste_to_be_free = 0, *ste_index, bibe_index;
   nat64_db_st_entry_t *st, *ste;
+  fib_table_t *fib;
 
   switch (ip_proto_to_snat_proto (bibe->proto))
     {
@@ -125,6 +148,8 @@ nat64_db_bib_entry_free (nat64_db_t * db, nat64_db_bib_entry_t * bibe)
       st = db->st._unk_proto_st;
       break;
     }
+
+  db->bib.bib_entries_num--;
 
   bibe_index = bibe - bib;
 
@@ -162,7 +187,14 @@ nat64_db_bib_entry_free (nat64_db_t * db, nat64_db_bib_entry_t * bibe)
   kv.key[2] = bibe_key.as_u64[2];
   clib_bihash_add_del_24_8 (&db->bib.out2in, &kv, 0);
 
-  db->free_addr_port_cb (db, &bibe->out_addr, bibe->out_port, bibe->proto);
+  if (!db->addr_free)
+    db->free_addr_port_cb (db, &bibe->out_addr, bibe->out_port, bibe->proto);
+
+  fib = fib_table_get (bibe->fib_index, FIB_PROTOCOL_IP6);
+  nat_ipfix_logging_nat64_bib (&bibe->in_addr, &bibe->out_addr, bibe->proto,
+			       bibe->in_port, bibe->out_port,
+			       fib->ft_table_id, 0);
+
   /* delete from pool */
   pool_put (bib, bibe);
 
@@ -344,6 +376,13 @@ nat64_db_st_entry_create (nat64_db_t * db, nat64_db_bib_entry_t * bibe,
   nat64_db_bib_entry_t *bib;
   nat64_db_st_entry_key_t ste_key;
   clib_bihash_kv_48_8_t kv;
+  fib_table_t *fib;
+
+  if (db->st.st_entries_num >= db->st.limit)
+    {
+      nat_ipfix_logging_max_sessions (db->st.limit);
+      return 0;
+    }
 
   /* create pool entry */
   switch (ip_proto_to_snat_proto (bibe->proto))
@@ -364,6 +403,9 @@ nat64_db_st_entry_create (nat64_db_t * db, nat64_db_bib_entry_t * bibe,
       bib = db->bib._unk_proto_bib;
       break;
     }
+
+  db->st.st_entries_num++;
+
   memset (ste, 0, sizeof (*ste));
   ste->in_r_addr.as_u64[0] = in_r_addr->as_u64[0];
   ste->in_r_addr.as_u64[1] = in_r_addr->as_u64[1];
@@ -407,6 +449,13 @@ nat64_db_st_entry_create (nat64_db_t * db, nat64_db_bib_entry_t * bibe,
   kv.key[5] = ste_key.as_u64[5];
   clib_bihash_add_del_48_8 (&db->st.out2in, &kv, 1);
 
+  fib = fib_table_get (bibe->fib_index, FIB_PROTOCOL_IP6);
+  nat_ipfix_logging_nat64_session (&bibe->in_addr, &bibe->out_addr,
+				   bibe->proto, bibe->in_port, bibe->out_port,
+				   &ste->in_r_addr, &ste->out_r_addr,
+				   ste->r_port, ste->r_port, fib->ft_table_id,
+				   1);
+
   return ste;
 }
 
@@ -417,6 +466,7 @@ nat64_db_st_entry_free (nat64_db_t * db, nat64_db_st_entry_t * ste)
   nat64_db_bib_entry_t *bib, *bibe;
   nat64_db_st_entry_key_t ste_key;
   clib_bihash_kv_48_8_t kv;
+  fib_table_t *fib;
 
   switch (ip_proto_to_snat_proto (ste->proto))
     {
@@ -436,6 +486,8 @@ nat64_db_st_entry_free (nat64_db_t * db, nat64_db_st_entry_t * ste)
     }
 
   bibe = pool_elt_at_index (bib, ste->bibe_index);
+
+  db->st.st_entries_num--;
 
   /* delete hash lookup */
   memset (&ste_key, 0, sizeof (ste_key));
@@ -468,6 +520,13 @@ nat64_db_st_entry_free (nat64_db_t * db, nat64_db_st_entry_t * ste)
   kv.key[4] = ste_key.as_u64[4];
   kv.key[5] = ste_key.as_u64[5];
   clib_bihash_add_del_48_8 (&db->st.out2in, &kv, 0);
+
+  fib = fib_table_get (bibe->fib_index, FIB_PROTOCOL_IP6);
+  nat_ipfix_logging_nat64_session (&bibe->in_addr, &bibe->out_addr,
+				   bibe->proto, bibe->in_port, bibe->out_port,
+				   &ste->in_r_addr, &ste->out_r_addr,
+				   ste->r_port, ste->r_port, fib->ft_table_id,
+				   0);
 
   /* delete from pool */
   pool_put (st, ste);
@@ -613,6 +672,7 @@ nat64_db_free_out_addr (nat64_db_t * db, ip4_address_t * out_addr)
   nat64_db_st_entry_t *st, *ste;
   nat64_db_bib_entry_t *bibe;
 
+  db->addr_free = 1;
 /* *INDENT-OFF* */
 #define _(N, i, n, s) \
   st = db->st._##n##_st; \
@@ -636,6 +696,7 @@ nat64_db_free_out_addr (nat64_db_t * db, ip4_address_t * out_addr)
   vec_foreach (ste_index, ste_to_be_free)
     nat64_db_st_entry_free (db, pool_elt_at_index(st, ste_index[0]));
   vec_free (ste_to_be_free);
+  db->addr_free = 0;
 /* *INDENT-ON* */
 }
 

src/plugins/nat/nat_ipfix_logging.h
@@ -17,16 +17,26 @@
 #ifndef __included_nat_ipfix_logging_h__
 #define __included_nat_ipfix_logging_h__
 
+#include <nat/nat.h>
+
 typedef enum {
   NAT_ADDRESSES_EXHAUTED = 3,
   NAT44_SESSION_CREATE = 4,
   NAT44_SESSION_DELETE = 5,
+  NAT64_SESSION_CREATE = 6,
+  NAT64_SESSION_DELETE = 7,
+  NAT64_BIB_CREATE = 10,
+  NAT64_BIB_DELETE = 11,
   NAT_PORTS_EXHAUSTED = 12,
   QUOTA_EXCEEDED = 13,
 } nat_event_t;
 
 typedef enum {
+  MAX_SESSION_ENTRIES = 1,
+  MAX_BIB_ENTRIES = 2,
   MAX_ENTRIES_PER_USER = 3,
+  MAX_FRAGMENTS_PENDING_REASSEMBLY = 5,
+  MAX_FRAGMENTS_PENDING_REASSEMBLY_IP6,
 } quota_exceed_event_t;
 
 typedef struct {
@@ -37,16 +47,34 @@ typedef struct {
   vlib_buffer_t *nat44_session_buffer;
   vlib_buffer_t *addr_exhausted_buffer;
   vlib_buffer_t *max_entries_per_user_buffer;
+  vlib_buffer_t *max_sessions_buffer;
+  vlib_buffer_t *max_bibs_buffer;
+  vlib_buffer_t *max_frags_ip4_buffer;
+  vlib_buffer_t *max_frags_ip6_buffer;
+  vlib_buffer_t *nat64_bib_buffer;
+  vlib_buffer_t *nat64_ses_buffer;
 
   /** frames containing ipfix buffers */
   vlib_frame_t *nat44_session_frame;
   vlib_frame_t *addr_exhausted_frame;
   vlib_frame_t *max_entries_per_user_frame;
+  vlib_frame_t *max_sessions_frame;
+  vlib_frame_t *max_bibs_frame;
+  vlib_frame_t *max_frags_ip4_frame;
+  vlib_frame_t *max_frags_ip6_frame;
+  vlib_frame_t *nat64_bib_frame;
+  vlib_frame_t *nat64_ses_frame;
 
   /** next record offset */
   u32 nat44_session_next_record_offset;
   u32 addr_exhausted_next_record_offset;
   u32 max_entries_per_user_next_record_offset;
+  u32 max_sessions_next_record_offset;
+  u32 max_bibs_next_record_offset;
+  u32 max_frags_ip4_next_record_offset;
+  u32 max_frags_ip6_next_record_offset;
+  u32 nat64_bib_next_record_offset;
+  u32 nat64_ses_next_record_offset;
 
   /** Time reference pair */
   u64 milisecond_time_0;
@@ -56,6 +84,12 @@ typedef struct {
   u16 nat44_session_template_id;
   u16 addr_exhausted_template_id;
   u16 max_entries_per_user_template_id;
+  u16 max_sessions_template_id;
+  u16 max_bibs_template_id;
+  u16 max_frags_ip4_template_id;
+  u16 max_frags_ip6_template_id;
+  u16 nat64_bib_template_id;
+  u16 nat64_ses_template_id;
 
   /** stream index */
   u32 stream_index;
@@ -74,6 +108,21 @@ void snat_ipfix_logging_nat44_ses_delete (u32 src_ip, u32 nat_src_ip,
                                           u16 src_port, u16 nat_src_port,
                                           u32 vrf_id);
 void snat_ipfix_logging_addresses_exhausted(u32 pool_id);
-void snat_ipfix_logging_max_entries_per_user(u32 src_ip);
+void snat_ipfix_logging_max_entries_per_user(u32 limit, u32 src_ip);
+void nat_ipfix_logging_max_sessions(u32 limit);
+void nat_ipfix_logging_max_bibs(u32 limit);
+void nat_ipfix_logging_max_fragments_ip4(u32 limit, ip4_address_t * src);
+void nat_ipfix_logging_max_fragments_ip6(u32 limit, ip6_address_t * src);
+void nat_ipfix_logging_nat64_session(ip6_address_t * src_ip,
+                                     ip4_address_t * nat_src_ip, u8 proto,
+                                     u16 src_port, u16 nat_src_port,
+                                     ip6_address_t * dst_ip,
+                                     ip4_address_t * nat_dst_ip,
+                                     u16 dst_port, u16 nat_dst_port,
+                                     u32 vrf_id, u8 is_create);
+void nat_ipfix_logging_nat64_bib(ip6_address_t * src_ip,
+                                 ip4_address_t * nat_src_ip, u8 proto,
+                                 u16 src_port, u16 nat_src_port,
+                                 u32 vrf_id, u8 is_create);
 
 #endif /* __included_nat_ipfix_logging_h__ */