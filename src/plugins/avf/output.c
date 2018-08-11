FDio/vpp

Multiversioning: Device (tx) function constructor
Change-Id: I39f87ca161c891fb22462a23188982fef7c3243f
Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

src/plugins/avf/output.c
@@ -34,10 +34,9 @@ avf_tx_desc_get_dtyp (avf_tx_desc_t * d)
   return d->qword[1] & 0x0f;
 }
 
-uword
-CLIB_MULTIARCH_FN (avf_interface_tx) (vlib_main_t * vm,
-				      vlib_node_runtime_t * node,
-				      vlib_frame_t * frame)
+VNET_DEVICE_CLASS_TX_FN (avf_tx) (vlib_main_t * vm,
+				  vlib_node_runtime_t * node,
+				  vlib_frame_t * frame)
 {
   avf_main_t *am = &avf_main;
   vnet_interface_output_runtime_t *rd = (void *) node->runtime_data;
@@ -158,21 +157,6 @@ CLIB_MULTIARCH_FN (avf_interface_tx) (vlib_main_t * vm,
 
   return frame->n_vectors - n_left;
 }
-
-#ifndef CLIB_MARCH_VARIANT
-#if __x86_64__
-vlib_node_function_t __clib_weak avf_interface_tx_avx512;
-vlib_node_function_t __clib_weak avf_interface_tx_avx2;
-static void __clib_constructor
-avf_interface_tx_multiarch_select (void)
-{
-  if (avf_interface_tx_avx512 && clib_cpu_supports_avx512f ())
-    avf_device_class.tx_function = avf_interface_tx_avx512;
-  else if (avf_interface_tx_avx2 && clib_cpu_supports_avx2 ())
-    avf_device_class.tx_function = avf_interface_tx_avx2;
-}
-#endif
-#endif
 
 /*
  * fd.io coding-style-patch-verification: ON

src/plugins/dpdk/device/device.c
@@ -327,10 +327,9 @@ dpdk_buffer_tx_offload (dpdk_device_t * xd, vlib_buffer_t * b,
  * node. It first copies packets on the frame to a per-thread arrays
  * containing the rte_mbuf pointers.
  */
-uword
-CLIB_MULTIARCH_FN (dpdk_interface_tx) (vlib_main_t * vm,
-				       vlib_node_runtime_t * node,
-				       vlib_frame_t * f)
+VNET_DEVICE_CLASS_TX_FN (dpdk_tx) (vlib_main_t * vm,
+				   vlib_node_runtime_t * node,
+				   vlib_frame_t * f)
 {
   dpdk_main_t *dm = &dpdk_main;
   vnet_interface_output_runtime_t *rd = (void *) node->runtime_data;
@@ -661,9 +660,8 @@ done:
 }
 
 /* *INDENT-OFF* */
-VNET_DEVICE_CLASS (dpdk_device_class) = {
+VNET_DEVICE_CLASS (dpdk_tx_device_class) = {
   .name = "dpdk",
-  .tx_function = dpdk_interface_tx,
   .tx_function_n_errors = DPDK_TX_FUNC_N_ERROR,
   .tx_function_error_strings = dpdk_tx_func_error_strings,
   .format_device_name = format_dpdk_device_name,
@@ -679,18 +677,6 @@ VNET_DEVICE_CLASS (dpdk_device_class) = {
 };
 /* *INDENT-ON* */
 
-#if __x86_64__
-vlib_node_function_t __clib_weak dpdk_interface_tx_avx512;
-vlib_node_function_t __clib_weak dpdk_interface_tx_avx2;
-static void __clib_constructor
-dpdk_interface_tx_multiarch_select (void)
-{
-  if (dpdk_interface_tx_avx512 && clib_cpu_supports_avx512f ())
-    dpdk_device_class.tx_function = dpdk_interface_tx_avx512;
-  else if (dpdk_interface_tx_avx2 && clib_cpu_supports_avx2 ())
-    dpdk_device_class.tx_function = dpdk_interface_tx_avx2;
-}
-#endif
 #endif
 
 #define UP_DOWN_FLAG_EVENT 1

src/plugins/memif/device.c
@@ -400,10 +400,9 @@ no_free_slots:
   return frame->n_vectors;
 }
 
-uword
-CLIB_MULTIARCH_FN (memif_interface_tx) (vlib_main_t * vm,
-					vlib_node_runtime_t * node,
-					vlib_frame_t * frame)
+VNET_DEVICE_CLASS_TX_FN (memif_tx) (vlib_main_t * vm,
+				    vlib_node_runtime_t * node,
+				    vlib_frame_t * frame)
 {
   memif_main_t *nm = &memif_main;
   vnet_interface_output_runtime_t *rund = (void *) node->runtime_data;
@@ -502,9 +501,8 @@ memif_subif_add_del_function (vnet_main_t * vnm,
 
 #ifndef CLIB_MARCH_VARIANT
 /* *INDENT-OFF* */
-VNET_DEVICE_CLASS (memif_device_class) = {
+VNET_DEVICE_CLASS (memif_tx_device_class) = {
   .name = "memif",
-  .tx_function = memif_interface_tx,
   .format_device_name = format_memif_device_name,
   .format_device = format_memif_device,
   .format_tx_trace = format_memif_tx_trace,
@@ -517,18 +515,6 @@ VNET_DEVICE_CLASS (memif_device_class) = {
   .rx_mode_change_function = memif_interface_rx_mode_change,
 };
 
-#if __x86_64__
-vlib_node_function_t __clib_weak memif_interface_tx_avx512;
-vlib_node_function_t __clib_weak memif_interface_tx_avx2;
-static void __clib_constructor
-memif_interface_tx_multiarch_select (void)
-{
-  if (memif_interface_tx_avx512 && clib_cpu_supports_avx512f ())
-    memif_device_class.tx_function = memif_interface_tx_avx512;
-  else if (memif_interface_tx_avx2 && clib_cpu_supports_avx2 ())
-    memif_device_class.tx_function = memif_interface_tx_avx2;
-}
-#endif
 #endif
 
 /* *INDENT-ON* */

sollor525/vpp-ips

src/plugins/avf/output.c
@@ -34,10 +34,9 @@ avf_tx_desc_get_dtyp (avf_tx_desc_t * d)
   return d->qword[1] & 0x0f;
 }
 
-uword
-CLIB_MULTIARCH_FN (avf_interface_tx) (vlib_main_t * vm,
-				      vlib_node_runtime_t * node,
-				      vlib_frame_t * frame)
+VNET_DEVICE_CLASS_TX_FN (avf_device_class) (vlib_main_t * vm,
+					    vlib_node_runtime_t * node,
+					    vlib_frame_t * frame)
 {
   avf_main_t *am = &avf_main;
   vnet_interface_output_runtime_t *rd = (void *) node->runtime_data;
@@ -158,21 +157,6 @@ CLIB_MULTIARCH_FN (avf_interface_tx) (vlib_main_t * vm,
 
   return frame->n_vectors - n_left;
 }
-
-#ifndef CLIB_MARCH_VARIANT
-#if __x86_64__
-vlib_node_function_t __clib_weak avf_interface_tx_avx512;
-vlib_node_function_t __clib_weak avf_interface_tx_avx2;
-static void __clib_constructor
-avf_interface_tx_multiarch_select (void)
-{
-  if (avf_interface_tx_avx512 && clib_cpu_supports_avx512f ())
-    avf_device_class.tx_function = avf_interface_tx_avx512;
-  else if (avf_interface_tx_avx2 && clib_cpu_supports_avx2 ())
-    avf_device_class.tx_function = avf_interface_tx_avx2;
-}
-#endif
-#endif
 
 /*
  * fd.io coding-style-patch-verification: ON

src/plugins/dpdk/device/device.c
@@ -327,10 +327,9 @@ dpdk_buffer_tx_offload (dpdk_device_t * xd, vlib_buffer_t * b,
  * node. It first copies packets on the frame to a per-thread arrays
  * containing the rte_mbuf pointers.
  */
-uword
-CLIB_MULTIARCH_FN (dpdk_interface_tx) (vlib_main_t * vm,
-				       vlib_node_runtime_t * node,
-				       vlib_frame_t * f)
+VNET_DEVICE_CLASS_TX_FN (dpdk_device_class) (vlib_main_t * vm,
+					     vlib_node_runtime_t * node,
+					     vlib_frame_t * f)
 {
   dpdk_main_t *dm = &dpdk_main;
   vnet_interface_output_runtime_t *rd = (void *) node->runtime_data;
@@ -663,7 +662,6 @@ done:
 /* *INDENT-OFF* */
 VNET_DEVICE_CLASS (dpdk_device_class) = {
   .name = "dpdk",
-  .tx_function = dpdk_interface_tx,
   .tx_function_n_errors = DPDK_TX_FUNC_N_ERROR,
   .tx_function_error_strings = dpdk_tx_func_error_strings,
   .format_device_name = format_dpdk_device_name,
@@ -679,18 +677,6 @@ VNET_DEVICE_CLASS (dpdk_device_class) = {
 };
 /* *INDENT-ON* */
 
-#if __x86_64__
-vlib_node_function_t __clib_weak dpdk_interface_tx_avx512;
-vlib_node_function_t __clib_weak dpdk_interface_tx_avx2;
-static void __clib_constructor
-dpdk_interface_tx_multiarch_select (void)
-{
-  if (dpdk_interface_tx_avx512 && clib_cpu_supports_avx512f ())
-    dpdk_device_class.tx_function = dpdk_interface_tx_avx512;
-  else if (dpdk_interface_tx_avx2 && clib_cpu_supports_avx2 ())
-    dpdk_device_class.tx_function = dpdk_interface_tx_avx2;
-}
-#endif
 #endif
 
 #define UP_DOWN_FLAG_EVENT 1

src/plugins/memif/device.c
@@ -400,10 +400,9 @@ no_free_slots:
   return frame->n_vectors;
 }
 
-uword
-CLIB_MULTIARCH_FN (memif_interface_tx) (vlib_main_t * vm,
-					vlib_node_runtime_t * node,
-					vlib_frame_t * frame)
+VNET_DEVICE_CLASS_TX_FN (memif_device_class) (vlib_main_t * vm,
+					      vlib_node_runtime_t * node,
+					      vlib_frame_t * frame)
 {
   memif_main_t *nm = &memif_main;
   vnet_interface_output_runtime_t *rund = (void *) node->runtime_data;
@@ -504,7 +503,6 @@ memif_subif_add_del_function (vnet_main_t * vnm,
 /* *INDENT-OFF* */
 VNET_DEVICE_CLASS (memif_device_class) = {
   .name = "memif",
-  .tx_function = memif_interface_tx,
   .format_device_name = format_memif_device_name,
   .format_device = format_memif_device,
   .format_tx_trace = format_memif_tx_trace,
@@ -517,18 +515,6 @@ VNET_DEVICE_CLASS (memif_device_class) = {
   .rx_mode_change_function = memif_interface_rx_mode_change,
 };
 
-#if __x86_64__
-vlib_node_function_t __clib_weak memif_interface_tx_avx512;
-vlib_node_function_t __clib_weak memif_interface_tx_avx2;
-static void __clib_constructor
-memif_interface_tx_multiarch_select (void)
-{
-  if (memif_interface_tx_avx512 && clib_cpu_supports_avx512f ())
-    memif_device_class.tx_function = memif_interface_tx_avx512;
-  else if (memif_interface_tx_avx2 && clib_cpu_supports_avx2 ())
-    memif_device_class.tx_function = memif_interface_tx_avx2;
-}
-#endif
 #endif
 
 /* *INDENT-ON* */