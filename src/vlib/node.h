FDio/vpp

Multiarch handling in different constructor macros
This significantly reduces need for

...

in multiarch code. Simply constructor macros will jost create static unused
entry if CLIB_MARCH_VARIANT is defined and that will be optimized out by
compiler.

Change-Id: I17d1c4ac0c903adcfadaa4a07de1b854c7ab14ac
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vlib/node.h
@@ -150,6 +150,7 @@ typedef struct _vlib_node_registration
 
 } vlib_node_registration_t;
 
+#ifndef CLIB_MARCH_VARIANT
 #define VLIB_REGISTER_NODE(x,...)                                       \
     __VA_ARGS__ vlib_node_registration_t x;                             \
 static void __vlib_add_node_registration_##x (void)                     \
@@ -169,6 +170,10 @@ static void __vlib_rm_node_registration_##x (void)                      \
                                   &x, next_registration);               \
 }                                                                       \
 __VA_ARGS__ vlib_node_registration_t x
+#else
+#define VLIB_REGISTER_NODE(x,...)                                       \
+static __clib_unused vlib_node_registration_t unused_##x
+#endif
 
 #define VLIB_NODE_FN(node)						\
 uword CLIB_MARCH_SFX (node##_fn)();					\

src/vnet/feature/feature.h
@@ -99,6 +99,7 @@ typedef struct
 
 extern vnet_feature_main_t feature_main;
 
+#ifndef CLIB_MARCH_VARIANT
 #define VNET_FEATURE_ARC_INIT(x,...)				\
   __VA_ARGS__ vnet_feature_arc_registration_t vnet_feat_arc_##x;\
 static void __vnet_add_feature_arc_registration_##x (void)	\
@@ -138,6 +139,14 @@ static void __vnet_rm_feature_registration_##x (void)		\
   VLIB_REMOVE_FROM_LINKED_LIST (fm->next_feature, r, next);	\
 }								\
 __VA_ARGS__ vnet_feature_registration_t vnet_feat_##x
+#else
+#define VNET_FEATURE_ARC_INIT(x,...)				\
+extern vnet_feature_arc_registration_t __clib_unused vnet_feat_arc_##x; \
+static vnet_feature_arc_registration_t __clib_unused vnet_feat_arc_unused_##x
+#define VNET_FEATURE_INIT(x,...)				\
+extern vnet_feature_registration_t __clib_unused vnet_feat_##x; \
+static vnet_feature_registration_t __clib_unused vnet_feat_unused_##x
+#endif
 
 void
 vnet_config_update_feature_count (vnet_feature_main_t * fm, u8 arc,

sollor525/vpp-ips

src/vlib/node.h
@@ -150,6 +150,7 @@ typedef struct _vlib_node_registration
 
 } vlib_node_registration_t;
 
+#ifndef CLIB_MARCH_VARIANT
 #define VLIB_REGISTER_NODE(x,...)                                       \
     __VA_ARGS__ vlib_node_registration_t x;                             \
 static void __vlib_add_node_registration_##x (void)                     \
@@ -169,6 +170,10 @@ static void __vlib_rm_node_registration_##x (void)                      \
                                   &x, next_registration);               \
 }                                                                       \
 __VA_ARGS__ vlib_node_registration_t x
+#else
+#define VLIB_REGISTER_NODE(x,...)                                       \
+static __clib_unused vlib_node_registration_t __clib_unused_##x
+#endif
 
 #define VLIB_NODE_FN(node)						\
 uword CLIB_MARCH_SFX (node##_fn)();					\

src/vnet/feature/feature.h
@@ -99,6 +99,7 @@ typedef struct
 
 extern vnet_feature_main_t feature_main;
 
+#ifndef CLIB_MARCH_VARIANT
 #define VNET_FEATURE_ARC_INIT(x,...)				\
   __VA_ARGS__ vnet_feature_arc_registration_t vnet_feat_arc_##x;\
 static void __vnet_add_feature_arc_registration_##x (void)	\
@@ -138,6 +139,14 @@ static void __vnet_rm_feature_registration_##x (void)		\
   VLIB_REMOVE_FROM_LINKED_LIST (fm->next_feature, r, next);	\
 }								\
 __VA_ARGS__ vnet_feature_registration_t vnet_feat_##x
+#else
+#define VNET_FEATURE_ARC_INIT(x,...)				\
+extern vnet_feature_arc_registration_t __clib_unused vnet_feat_arc_##x; \
+static vnet_feature_arc_registration_t __clib_unused __clib_unused_vnet_feat_arc_##x
+#define VNET_FEATURE_INIT(x,...)				\
+extern vnet_feature_registration_t __clib_unused vnet_feat_##x; \
+static vnet_feature_registration_t __clib_unused __clib_unused_vnet_feat_##x
+#endif
 
 void
 vnet_config_update_feature_count (vnet_feature_main_t * fm, u8 arc,