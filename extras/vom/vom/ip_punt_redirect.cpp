FDio/vpp

vom: Add ip punt redirect
Change-Id: I5c7b925b22b53f18b0c45374883a6a4bff580c75
Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

extras/vom/vom/ip_punt_redirect.cpp
@@ -1,0 +1,148 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "vom/ip_punt_redirect.hpp"
+#include "vom/ip_punt_redirect_cmds.hpp"
+#include "vom/singular_db_funcs.hpp"
+
+namespace VOM {
+/**
+ * A DB of all IP Punt configs
+ */
+singular_db<ip_punt_redirect::key_t, ip_punt_redirect> ip_punt_redirect::m_db;
+
+ip_punt_redirect::event_handler ip_punt_redirect::m_evh;
+
+ip_punt_redirect::ip_punt_redirect(const interface& rx_itf,
+                                   const interface& tx_itf,
+                                   route::prefix_t prefix)
+  : m_rx_itf(rx_itf.singular())
+  , m_tx_itf(tx_itf.singular())
+  , m_prefix(prefix)
+{
+}
+
+ip_punt_redirect::ip_punt_redirect(const ip_punt_redirect& o)
+  : m_rx_itf(o.m_rx_itf)
+  , m_tx_itf(o.m_tx_itf)
+  , m_prefix(o.m_prefix)
+  , m_config(o.m_config)
+{
+}
+
+ip_punt_redirect::~ip_punt_redirect()
+{
+  sweep();
+
+  // not in the DB anymore.
+  m_db.release(m_rx_itf->key(), this);
+}
+
+void
+ip_punt_redirect::sweep()
+{
+  if (m_config) {
+    HW::enqueue(new ip_punt_redirect_cmds::unconfig_cmd(
+      m_config, m_rx_itf->handle(), m_tx_itf->handle(), m_prefix));
+  }
+  HW::write();
+}
+
+void
+ip_punt_redirect::dump(std::ostream& os)
+{
+  db_dump(m_db, os);
+}
+
+void
+ip_punt_redirect::replay()
+{
+  if (m_config) {
+    HW::enqueue(new ip_punt_redirect_cmds::config_cmd(
+      m_config, m_rx_itf->handle(), m_tx_itf->handle(), m_prefix));
+  }
+}
+
+std::string
+ip_punt_redirect::to_string() const
+{
+  std::ostringstream s;
+  s << "IP-punt-redirect-config:"
+    << " rx-itf:" << m_rx_itf->to_string()
+    << " tx-itf:" << m_tx_itf->to_string()
+    << " next-hop:" << m_prefix.to_string();
+
+  return (s.str());
+}
+
+void
+ip_punt_redirect::update(const ip_punt_redirect& desired)
+{
+  if (!m_config) {
+    HW::enqueue(new ip_punt_redirect_cmds::config_cmd(
+      m_config, m_rx_itf->handle(), m_tx_itf->handle(), m_prefix));
+  }
+}
+
+std::shared_ptr<ip_punt_redirect>
+ip_punt_redirect::find_or_add(const ip_punt_redirect& temp)
+{
+  return (m_db.find_or_add(temp.m_rx_itf->key(), temp));
+}
+
+std::shared_ptr<ip_punt_redirect>
+ip_punt_redirect::singular() const
+{
+  return find_or_add(*this);
+}
+
+ip_punt_redirect::event_handler::event_handler()
+{
+  OM::register_listener(this);
+  inspect::register_handler({ "ip-punt-redirect" },
+                            "IP punt redirect configurations", this);
+}
+
+void
+ip_punt_redirect::event_handler::handle_replay()
+{
+  m_db.replay();
+}
+
+void
+ip_punt_redirect::event_handler::handle_populate(const client_db::key_t& key)
+{
+}
+
+dependency_t
+ip_punt_redirect::event_handler::order() const
+{
+  return (dependency_t::BINDING);
+}
+
+void
+ip_punt_redirect::event_handler::show(std::ostream& os)
+{
+  db_dump(m_db, os);
+}
+}
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "mozilla")
+ * End:
+ */

test/ext/vom_test.cpp
@@ -56,6 +56,8 @@
 #include "vom/arp_proxy_binding.hpp"
 #include "vom/arp_proxy_config_cmds.hpp"
 #include "vom/arp_proxy_binding_cmds.hpp"
+#include "vom/ip_punt_redirect.hpp"
+#include "vom/ip_punt_redirect_cmds.hpp"
 #include "vom/ip_unnumbered.hpp"
 #include "vom/ip_unnumbered_cmds.hpp"
 #include "vom/interface_ip6_nd.hpp"
@@ -376,6 +378,14 @@ public:
                     else if (typeid(*f_exp) == typeid(arp_proxy_config_cmds::unconfig_cmd))
                     {
                         rc = handle_derived<arp_proxy_config_cmds::unconfig_cmd>(f_exp, f_act);
+                    }
+                    else if (typeid(*f_exp) == typeid(ip_punt_redirect_cmds::config_cmd))
+                    {
+                        rc = handle_derived<ip_punt_redirect_cmds::config_cmd>(f_exp, f_act);
+                    }
+                    else if (typeid(*f_exp) == typeid(ip_punt_redirect_cmds::unconfig_cmd))
+                    {
+                        rc = handle_derived<ip_punt_redirect_cmds::unconfig_cmd>(f_exp, f_act);
                     }
                     else if (typeid(*f_exp) == typeid(ip_unnumbered_cmds::config_cmd))
                     {
@@ -1327,6 +1337,58 @@ BOOST_AUTO_TEST_CASE(test_arp_proxy) {
     ADD_EXPECT(arp_proxy_config_cmds::unconfig_cmd(hw_ap_cfg, low, high));
 
     TRY_CHECK(OM::remove(kurt));
+}
+
+BOOST_AUTO_TEST_CASE(test_ip_punt_redirect) {
+    VppInit vi;
+    const std::string eliot = "EliotReed";
+    rc_t rc = rc_t::OK;
+
+    /*
+     * Interface 1 is the tx interface
+     */
+    std::string itf1_name = "tx-itf";
+    interface itf1(itf1_name,
+                   interface::type_t::AFPACKET,
+                   interface::admin_state_t::UP);
+    HW::item<handle_t> hw_ifh(2, rc_t::OK);
+    HW::item<interface::admin_state_t> hw_as_up(interface::admin_state_t::UP, rc_t::OK);
+    ADD_EXPECT(interface_cmds::af_packet_create_cmd(hw_ifh, itf1_name));
+    ADD_EXPECT(interface_cmds::state_change_cmd(hw_as_up, hw_ifh));
+    TRY_CHECK_RC(OM::write(eliot, itf1));
+
+    route::prefix_t pfx_10("10.10.10.10", 24);
+
+    /*
+     * Interface 2 is the rx interface
+     */
+    std::string itf2_name = "rx-itf";
+    interface itf2(itf2_name,
+                   interface::type_t::AFPACKET,
+                   interface::admin_state_t::UP);
+
+    HW::item<handle_t> hw_ifh2(4, rc_t::OK);
+    ADD_EXPECT(interface_cmds::af_packet_create_cmd(hw_ifh2, itf2_name));
+    ADD_EXPECT(interface_cmds::state_change_cmd(hw_as_up, hw_ifh2));
+    TRY_CHECK_RC(OM::write(eliot, itf2));
+
+    ip_punt_redirect *ip_punt = new ip_punt_redirect(itf2, itf1, pfx_10);
+    HW::item<bool> hw_ip_cfg(true, rc_t::OK);
+    HW::item<bool> hw_ip_uncfg(false, rc_t::OK);
+    ADD_EXPECT(ip_punt_redirect_cmds::config_cmd(hw_ip_cfg, hw_ifh2.data(), hw_ifh.data(), pfx_10));
+    TRY_CHECK_RC(OM::write(eliot, *ip_punt));
+
+    delete ip_punt;
+
+    HW::item<interface::admin_state_t> hw_as_down(interface::admin_state_t::DOWN, rc_t::OK);
+    STRICT_ORDER_OFF();
+    ADD_EXPECT(ip_punt_redirect_cmds::unconfig_cmd(hw_ip_uncfg, hw_ifh2.data(), hw_ifh.data(), pfx_10));
+    ADD_EXPECT(interface_cmds::state_change_cmd(hw_as_down, hw_ifh));
+    ADD_EXPECT(interface_cmds::af_packet_delete_cmd(hw_ifh, itf1_name));
+    ADD_EXPECT(interface_cmds::state_change_cmd(hw_as_down, hw_ifh2));
+    ADD_EXPECT(interface_cmds::af_packet_delete_cmd(hw_ifh2, itf2_name));
+
+    TRY_CHECK(OM::remove(eliot));
 }
 
 BOOST_AUTO_TEST_CASE(test_ip_unnumbered) {

sollor525/vpp-ips

extras/vom/vom/ip_punt_redirect.cpp
@@ -1,0 +1,147 @@
+/*
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "vom/ip_punt_redirect.hpp"
+#include "vom/ip_punt_redirect_cmds.hpp"
+#include "vom/singular_db_funcs.hpp"
+
+namespace VOM {
+/**
+ * A DB of all IP Punt configs
+ */
+singular_db<ip_punt_redirect::key_t, ip_punt_redirect> ip_punt_redirect::m_db;
+
+ip_punt_redirect::event_handler ip_punt_redirect::m_evh;
+
+ip_punt_redirect::ip_punt_redirect(const interface& rx_itf,
+                                   const interface& tx_itf,
+                                   const boost::asio::ip::address& addr)
+  : m_rx_itf(rx_itf.singular())
+  , m_tx_itf(tx_itf.singular())
+  , m_addr(addr)
+{
+}
+
+ip_punt_redirect::ip_punt_redirect(const ip_punt_redirect& o)
+  : m_rx_itf(o.m_rx_itf)
+  , m_tx_itf(o.m_tx_itf)
+  , m_addr(o.m_addr)
+  , m_config(o.m_config)
+{
+}
+
+ip_punt_redirect::~ip_punt_redirect()
+{
+  sweep();
+
+  // not in the DB anymore.
+  m_db.release(m_rx_itf->key(), this);
+}
+
+void
+ip_punt_redirect::sweep()
+{
+  if (m_config) {
+    HW::enqueue(new ip_punt_redirect_cmds::unconfig_cmd(
+      m_config, m_rx_itf->handle(), m_tx_itf->handle(), m_addr));
+  }
+  HW::write();
+}
+
+void
+ip_punt_redirect::dump(std::ostream& os)
+{
+  db_dump(m_db, os);
+}
+
+void
+ip_punt_redirect::replay()
+{
+  if (m_config) {
+    HW::enqueue(new ip_punt_redirect_cmds::config_cmd(
+      m_config, m_rx_itf->handle(), m_tx_itf->handle(), m_addr));
+  }
+}
+
+std::string
+ip_punt_redirect::to_string() const
+{
+  std::ostringstream s;
+  s << "IP-punt-redirect-config:"
+    << " rx-itf:" << m_rx_itf->to_string()
+    << " tx-itf:" << m_tx_itf->to_string() << " next-hop:" << m_addr;
+
+  return (s.str());
+}
+
+void
+ip_punt_redirect::update(const ip_punt_redirect& desired)
+{
+  if (!m_config) {
+    HW::enqueue(new ip_punt_redirect_cmds::config_cmd(
+      m_config, m_rx_itf->handle(), m_tx_itf->handle(), m_addr));
+  }
+}
+
+std::shared_ptr<ip_punt_redirect>
+ip_punt_redirect::find_or_add(const ip_punt_redirect& temp)
+{
+  return (m_db.find_or_add(temp.m_rx_itf->key(), temp));
+}
+
+std::shared_ptr<ip_punt_redirect>
+ip_punt_redirect::singular() const
+{
+  return find_or_add(*this);
+}
+
+ip_punt_redirect::event_handler::event_handler()
+{
+  OM::register_listener(this);
+  inspect::register_handler({ "ip-punt-redirect" },
+                            "IP punt redirect configurations", this);
+}
+
+void
+ip_punt_redirect::event_handler::handle_replay()
+{
+  m_db.replay();
+}
+
+void
+ip_punt_redirect::event_handler::handle_populate(const client_db::key_t& key)
+{
+}
+
+dependency_t
+ip_punt_redirect::event_handler::order() const
+{
+  return (dependency_t::BINDING);
+}
+
+void
+ip_punt_redirect::event_handler::show(std::ostream& os)
+{
+  db_dump(m_db, os);
+}
+}
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "mozilla")
+ * End:
+ */

test/ext/vom_test.cpp
@@ -56,6 +56,8 @@
 #include "vom/arp_proxy_binding.hpp"
 #include "vom/arp_proxy_config_cmds.hpp"
 #include "vom/arp_proxy_binding_cmds.hpp"
+#include "vom/ip_punt_redirect.hpp"
+#include "vom/ip_punt_redirect_cmds.hpp"
 #include "vom/ip_unnumbered.hpp"
 #include "vom/ip_unnumbered_cmds.hpp"
 #include "vom/interface_ip6_nd.hpp"
@@ -376,6 +378,14 @@ public:
                     else if (typeid(*f_exp) == typeid(arp_proxy_config_cmds::unconfig_cmd))
                     {
                         rc = handle_derived<arp_proxy_config_cmds::unconfig_cmd>(f_exp, f_act);
+                    }
+                    else if (typeid(*f_exp) == typeid(ip_punt_redirect_cmds::config_cmd))
+                    {
+                        rc = handle_derived<ip_punt_redirect_cmds::config_cmd>(f_exp, f_act);
+                    }
+                    else if (typeid(*f_exp) == typeid(ip_punt_redirect_cmds::unconfig_cmd))
+                    {
+                        rc = handle_derived<ip_punt_redirect_cmds::unconfig_cmd>(f_exp, f_act);
                     }
                     else if (typeid(*f_exp) == typeid(ip_unnumbered_cmds::config_cmd))
                     {
@@ -1327,6 +1337,58 @@ BOOST_AUTO_TEST_CASE(test_arp_proxy) {
     ADD_EXPECT(arp_proxy_config_cmds::unconfig_cmd(hw_ap_cfg, low, high));
 
     TRY_CHECK(OM::remove(kurt));
+}
+
+BOOST_AUTO_TEST_CASE(test_ip_punt_redirect) {
+    VppInit vi;
+    const std::string eliot = "EliotReed";
+    rc_t rc = rc_t::OK;
+
+    /*
+     * Interface 1 is the tx interface
+     */
+    std::string itf1_name = "tx-itf";
+    interface itf1(itf1_name,
+                   interface::type_t::AFPACKET,
+                   interface::admin_state_t::UP);
+    HW::item<handle_t> hw_ifh(2, rc_t::OK);
+    HW::item<interface::admin_state_t> hw_as_up(interface::admin_state_t::UP, rc_t::OK);
+    ADD_EXPECT(interface_cmds::af_packet_create_cmd(hw_ifh, itf1_name));
+    ADD_EXPECT(interface_cmds::state_change_cmd(hw_as_up, hw_ifh));
+    TRY_CHECK_RC(OM::write(eliot, itf1));
+
+    boost::asio::ip::address addr = boost::asio::ip::address::from_string("192.168.0.20");
+
+    /*
+     * Interface 2 is the rx interface
+     */
+    std::string itf2_name = "rx-itf";
+    interface itf2(itf2_name,
+                   interface::type_t::AFPACKET,
+                   interface::admin_state_t::UP);
+
+    HW::item<handle_t> hw_ifh2(4, rc_t::OK);
+    ADD_EXPECT(interface_cmds::af_packet_create_cmd(hw_ifh2, itf2_name));
+    ADD_EXPECT(interface_cmds::state_change_cmd(hw_as_up, hw_ifh2));
+    TRY_CHECK_RC(OM::write(eliot, itf2));
+
+    ip_punt_redirect *ip_punt = new ip_punt_redirect(itf2, itf1, addr);
+    HW::item<bool> hw_ip_cfg(true, rc_t::OK);
+    HW::item<bool> hw_ip_uncfg(false, rc_t::OK);
+    ADD_EXPECT(ip_punt_redirect_cmds::config_cmd(hw_ip_cfg, hw_ifh2.data(), hw_ifh.data(), addr));
+    TRY_CHECK_RC(OM::write(eliot, *ip_punt));
+
+    delete ip_punt;
+
+    HW::item<interface::admin_state_t> hw_as_down(interface::admin_state_t::DOWN, rc_t::OK);
+    STRICT_ORDER_OFF();
+    ADD_EXPECT(ip_punt_redirect_cmds::unconfig_cmd(hw_ip_uncfg, hw_ifh2.data(), hw_ifh.data(), addr));
+    ADD_EXPECT(interface_cmds::state_change_cmd(hw_as_down, hw_ifh));
+    ADD_EXPECT(interface_cmds::af_packet_delete_cmd(hw_ifh, itf1_name));
+    ADD_EXPECT(interface_cmds::state_change_cmd(hw_as_down, hw_ifh2));
+    ADD_EXPECT(interface_cmds::af_packet_delete_cmd(hw_ifh2, itf2_name));
+
+    TRY_CHECK(OM::remove(eliot));
 }
 
 BOOST_AUTO_TEST_CASE(test_ip_unnumbered) {