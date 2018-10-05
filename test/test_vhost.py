FDio/vpp

vhost_user: Add test for interface states and events
Change-Id: I2c330945bb0b07f649f574a055bfbea455e5d0b3
Signed-off-by: Juraj Sloboda <jsloboda@cisco.com>

test/test_vhost.py
@@ -12,6 +12,13 @@ class TesVhostInterface(VppTestCase):
 
     """
 
+    def tearDown(self):
+        super(TesVhostInterface, self).tearDown()
+        if not self.vpp_dead:
+            if_dump = self.vapi.sw_interface_vhost_user_dump()
+            for ifc in if_dump:
+                self.vapi.delete_vhost_user_if(ifc.sw_if_index)
+
     def test_vhost(self):
         """ Vhost User add/delete interface test """
         self.logger.info("Vhost User add interfaces")
@@ -69,6 +76,41 @@ class TesVhostInterface(VppTestCase):
         # verify VirtualEthernet0/0/0 is not in the dump
         if_dump = self.vapi.sw_interface_vhost_user_dump()
         self.assertFalse(vhost_if1.is_interface_config_in_dump(if_dump))
+
+    def test_vhost_interface_state(self):
+        """ Vhost User interface states and events test """
+
+        self.vapi.want_interface_events()
+
+        # clear outstanding events
+        # (like delete interface events from other tests)
+        self.vapi.collect_events()
+
+        vhost_if = VppVhostInterface(self, sock_filename='/tmp/sock1')
+
+        # create vhost interface
+        vhost_if.add_vpp_config()
+        self.sleep(0.1)
+        events = self.vapi.collect_events()
+        # creating interface doesn't currently create events
+        self.assert_equal(len(events), 0, "number of events")
+
+        vhost_if.admin_up()
+        vhost_if.verify_interface_state(1, 0, expect_event=True)
+
+        vhost_if.admin_down()
+        vhost_if.verify_interface_state(0, 0, expect_event=True)
+
+        # delete vhost interface
+        vhost_if.remove_vpp_config()
+        event = self.vapi.wait_for_event(timeout=1)
+        self.assert_equal(event.sw_if_index, vhost_if.sw_if_index,
+                          "sw_if_index")
+        self.assert_equal(event.deleted, 1, "deleted flag")
+
+        # verify there are no more events
+        events = self.vapi.collect_events()
+        self.assert_equal(len(events), 0, "number of events")
 
 if __name__ == '__main__':
     unittest.main(testRunner=VppTestRunner)

test/vpp_interface.py
@@ -426,10 +426,31 @@ class VppInterface(object):
         dump = self.test.vapi.sw_interface_dump()
         return self.is_interface_config_in_dump(dump)
 
-    def is_interface_config_in_dump(self, dump):
+    def get_interface_config_from_dump(self, dump):
         for i in dump:
             if i.interface_name.rstrip(' \t\r\n\0') == self.name and \
                i.sw_if_index == self.sw_if_index:
-                return True
+                return i
         else:
-            return False
+            return None
+
+    def is_interface_config_in_dump(self, dump):
+        return self.get_interface_config_from_dump(dump) is not None
+
+    def verify_interface_state(self, admin_up_down, link_up_down,
+                               expect_event=False):
+        if expect_event:
+            event = self.test.vapi.wait_for_event(timeout=1,
+                                                  name='sw_interface_event')
+            self.test.assert_equal(event.sw_if_index, self.sw_if_index,
+                                   "sw_if_index")
+            self.test.assert_equal(event.admin_up_down, admin_up_down,
+                                   "admin state")
+            self.test.assert_equal(event.link_up_down, link_up_down,
+                                   "link state")
+        dump = self.test.vapi.sw_interface_dump()
+        if_state = self.get_interface_config_from_dump(dump)
+        self.test.assert_equal(if_state.admin_up_down, admin_up_down,
+                               "admin state")
+        self.test.assert_equal(if_state.link_up_down, link_up_down,
+                               "link state")

sollor525/vpp-ips

test/test_vhost.py
@@ -12,6 +12,13 @@ class TesVhostInterface(VppTestCase):
 
     """
 
+    def tearDown(self):
+        super(TesVhostInterface, self).tearDown()
+        if not self.vpp_dead:
+            if_dump = self.vapi.sw_interface_vhost_user_dump()
+            for ifc in if_dump:
+                self.vapi.delete_vhost_user_if(ifc.sw_if_index)
+
     def test_vhost(self):
         """ Vhost User add/delete interface test """
         self.logger.info("Vhost User add interfaces")
@@ -69,6 +76,41 @@ class TesVhostInterface(VppTestCase):
         # verify VirtualEthernet0/0/0 is not in the dump
         if_dump = self.vapi.sw_interface_vhost_user_dump()
         self.assertFalse(vhost_if1.is_interface_config_in_dump(if_dump))
+
+    def test_vhost_interface_state(self):
+        """ Vhost User interface states and events test """
+
+        self.vapi.want_interface_events()
+
+        # clear outstanding events
+        # (like delete interface events from other tests)
+        self.vapi.collect_events()
+
+        vhost_if = VppVhostInterface(self, sock_filename='/tmp/sock1')
+
+        # create vhost interface
+        vhost_if.add_vpp_config()
+        self.sleep(0.1)
+        events = self.vapi.collect_events()
+        # creating interface doesn't currently create events
+        self.assert_equal(len(events), 0, "number of events")
+
+        vhost_if.admin_up()
+        vhost_if.assert_interface_state(1, 0, expect_event=True)
+
+        vhost_if.admin_down()
+        vhost_if.assert_interface_state(0, 0, expect_event=True)
+
+        # delete vhost interface
+        vhost_if.remove_vpp_config()
+        event = self.vapi.wait_for_event(timeout=1)
+        self.assert_equal(event.sw_if_index, vhost_if.sw_if_index,
+                          "sw_if_index")
+        self.assert_equal(event.deleted, 1, "deleted flag")
+
+        # verify there are no more events
+        events = self.vapi.collect_events()
+        self.assert_equal(len(events), 0, "number of events")
 
 if __name__ == '__main__':
     unittest.main(testRunner=VppTestRunner)

test/vpp_interface.py
@@ -426,10 +426,31 @@ class VppInterface(object):
         dump = self.test.vapi.sw_interface_dump()
         return self.is_interface_config_in_dump(dump)
 
-    def is_interface_config_in_dump(self, dump):
+    def get_interface_config_from_dump(self, dump):
         for i in dump:
             if i.interface_name.rstrip(' \t\r\n\0') == self.name and \
                i.sw_if_index == self.sw_if_index:
-                return True
+                return i
         else:
-            return False
+            return None
+
+    def is_interface_config_in_dump(self, dump):
+        return self.get_interface_config_from_dump(dump) is not None
+
+    def assert_interface_state(self, admin_up_down, link_up_down,
+                               expect_event=False):
+        if expect_event:
+            event = self.test.vapi.wait_for_event(timeout=1,
+                                                  name='sw_interface_event')
+            self.test.assert_equal(event.sw_if_index, self.sw_if_index,
+                                   "sw_if_index")
+            self.test.assert_equal(event.admin_up_down, admin_up_down,
+                                   "admin state")
+            self.test.assert_equal(event.link_up_down, link_up_down,
+                                   "link state")
+        dump = self.test.vapi.sw_interface_dump()
+        if_state = self.get_interface_config_from_dump(dump)
+        self.test.assert_equal(if_state.admin_up_down, admin_up_down,
+                               "admin state")
+        self.test.assert_equal(if_state.link_up_down, link_up_down,
+                               "link state")