FDio/vpp

Remove c-11 memcpy checks from perf-critical code
Change-Id: Id4f37f5d4a03160572954a416efa1ef9b3d79ad1
Signed-off-by: Dave Barach <dave@barachs.net>

src/plugins/dpdk/device/flow.c
@@ -84,10 +84,10 @@ dpdk_flow_add (dpdk_device_t * xd, vnet_flow_t * f, dpdk_flow_entry_t * fe)
   if (f->type == VNET_FLOW_TYPE_IP6_N_TUPLE)
     {
       vnet_flow_ip6_n_tuple_t *t6 = &f->ip6_n_tuple;
-      clib_memcpy (ip6[0].hdr.src_addr, &t6->src_addr.addr, 16);
-      clib_memcpy (ip6[1].hdr.src_addr, &t6->src_addr.mask, 16);
-      clib_memcpy (ip6[0].hdr.dst_addr, &t6->dst_addr.addr, 16);
-      clib_memcpy (ip6[1].hdr.dst_addr, &t6->dst_addr.mask, 16);
+      _clib_memcpy (ip6[0].hdr.src_addr, &t6->src_addr.addr, 16);
+      _clib_memcpy (ip6[1].hdr.src_addr, &t6->src_addr.mask, 16);
+      _clib_memcpy (ip6[0].hdr.dst_addr, &t6->dst_addr.addr, 16);
+      _clib_memcpy (ip6[1].hdr.dst_addr, &t6->dst_addr.mask, 16);
       item->type = RTE_FLOW_ITEM_TYPE_IPV6;
       item->spec = ip6;
       item->mask = ip6 + 1;
@@ -183,9 +183,9 @@ dpdk_flow_add (dpdk_device_t * xd, vnet_flow_t * f, dpdk_flow_entry_t * fe)
       raw[0].item.relative = 1;
       raw[0].item.length = vxlan_hdr_sz;
 
-      clib_memcpy (raw[0].val + raw_sz, &spec_hdr, vxlan_hdr_sz);
+      _clib_memcpy (raw[0].val + raw_sz, &spec_hdr, vxlan_hdr_sz);
       raw[0].item.pattern = raw[0].val + raw_sz;
-      clib_memcpy (raw[1].val + raw_sz, &mask_hdr, vxlan_hdr_sz);
+      _clib_memcpy (raw[1].val + raw_sz, &mask_hdr, vxlan_hdr_sz);
       raw[1].item.pattern = raw[1].val + raw_sz;
 
       vec_add2 (items, item, 1);

src/vnet/classify/vnet_classify.c
@@ -131,7 +131,7 @@ vnet_classify_new_table (vnet_classify_main_t * cm,
   clib_memset (t, 0, sizeof (*t));
 
   vec_validate_aligned (t->mask, match_n_vectors - 1, sizeof (u32x4));
-  clib_memcpy (t->mask, mask, match_n_vectors * sizeof (u32x4));
+  _clib_memcpy (t->mask, mask, match_n_vectors * sizeof (u32x4));
 
   t->next_table_index = ~0;
   t->nbuckets = nbuckets;
@@ -273,7 +273,7 @@ static inline void make_working_copy
 
   v = vnet_classify_get_entry (t, b->offset);
 
-  clib_memcpy (working_copy, v, required_length);
+  _clib_memcpy (working_copy, v, required_length);
 
   working_bucket.as_u64 = b->as_u64;
   working_bucket.offset = vnet_classify_get_offset (t, working_copy);
@@ -317,8 +317,8 @@ split_and_rehash (vnet_classify_table_t * t,
 
 	      if (vnet_classify_entry_is_free (new_v))
 		{
-		  clib_memcpy (new_v, v, sizeof (vnet_classify_entry_t)
-			       + (t->match_n_vectors * sizeof (u32x4)));
+		  _clib_memcpy (new_v, v, sizeof (vnet_classify_entry_t)
+				+ (t->match_n_vectors * sizeof (u32x4)));
 		  new_v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
 		  goto doublebreak;
 		}
@@ -361,8 +361,8 @@ split_and_rehash_linear (vnet_classify_table_t * t,
 		  clib_warning ("BUG: linear rehash new entry not free!");
 		  continue;
 		}
-	      clib_memcpy (new_v, v, sizeof (vnet_classify_entry_t)
-			   + (t->match_n_vectors * sizeof (u32x4)));
+	      _clib_memcpy (new_v, v, sizeof (vnet_classify_entry_t)
+			    + (t->match_n_vectors * sizeof (u32x4)));
 	      new_v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
 	      j++;
 	      goto doublebreak;
@@ -457,8 +457,8 @@ vnet_classify_add_del (vnet_classify_table_t * t,
 	}
 
       v = vnet_classify_entry_alloc (t, 0 /* new_log2_pages */ );
-      clib_memcpy (v, add_v, sizeof (vnet_classify_entry_t) +
-		   t->match_n_vectors * sizeof (u32x4));
+      _clib_memcpy (v, add_v, sizeof (vnet_classify_entry_t) +
+		    t->match_n_vectors * sizeof (u32x4));
       v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
       vnet_classify_entry_claim_resource (v);
 
@@ -496,8 +496,8 @@ vnet_classify_add_del (vnet_classify_table_t * t,
 	  if (!memcmp
 	      (v->key, add_v->key, t->match_n_vectors * sizeof (u32x4)))
 	    {
-	      clib_memcpy (v, add_v, sizeof (vnet_classify_entry_t) +
-			   t->match_n_vectors * sizeof (u32x4));
+	      _clib_memcpy (v, add_v, sizeof (vnet_classify_entry_t) +
+			    t->match_n_vectors * sizeof (u32x4));
 	      v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
 	      vnet_classify_entry_claim_resource (v);
 
@@ -513,8 +513,8 @@ vnet_classify_add_del (vnet_classify_table_t * t,
 
 	  if (vnet_classify_entry_is_free (v))
 	    {
-	      clib_memcpy (v, add_v, sizeof (vnet_classify_entry_t) +
-			   t->match_n_vectors * sizeof (u32x4));
+	      _clib_memcpy (v, add_v, sizeof (vnet_classify_entry_t) +
+			    t->match_n_vectors * sizeof (u32x4));
 	      v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
 	      vnet_classify_entry_claim_resource (v);
 
@@ -609,8 +609,8 @@ vnet_classify_add_del (vnet_classify_table_t * t,
 
       if (vnet_classify_entry_is_free (new_v))
 	{
-	  clib_memcpy (new_v, add_v, sizeof (vnet_classify_entry_t) +
-		       t->match_n_vectors * sizeof (u32x4));
+	  _clib_memcpy (new_v, add_v, sizeof (vnet_classify_entry_t) +
+			t->match_n_vectors * sizeof (u32x4));
 	  new_v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
 	  vnet_classify_entry_claim_resource (new_v);
 
@@ -1885,10 +1885,10 @@ unformat_ip6_match (unformat_input_t * input, va_list * args)
   ip = (ip6_header_t *) match;
 
   if (src)
-    clib_memcpy (&ip->src_address, &src_val, sizeof (ip->src_address));
+    _clib_memcpy (&ip->src_address, &src_val, sizeof (ip->src_address));
 
   if (dst)
-    clib_memcpy (&ip->dst_address, &dst_val, sizeof (ip->dst_address));
+    _clib_memcpy (&ip->dst_address, &dst_val, sizeof (ip->dst_address));
 
   if (proto)
     ip->protocol = proto_val;
@@ -2012,10 +2012,10 @@ unformat_l2_match (unformat_input_t * input, va_list * args)
   vec_validate_aligned (match, len - 1, sizeof (u32x4));
 
   if (dst)
-    clib_memcpy (match, dst_val, 6);
+    _clib_memcpy (match, dst_val, 6);
 
   if (src)
-    clib_memcpy (match + 6, src_val, 6);
+    _clib_memcpy (match + 6, src_val, 6);
 
   if (tag2)
     {
@@ -2188,8 +2188,8 @@ vnet_classify_add_del_session (vnet_classify_main_t * cm,
     e->metadata = 0;
 
   /* Copy key data, honoring skip_n_vectors */
-  clib_memcpy (&e->key, match + t->skip_n_vectors * sizeof (u32x4),
-	       t->match_n_vectors * sizeof (u32x4));
+  _clib_memcpy (&e->key, match + t->skip_n_vectors * sizeof (u32x4),
+		t->match_n_vectors * sizeof (u32x4));
 
   /* Clear don't-care bits; likely when dynamically creating sessions */
   for (i = 0; i < t->match_n_vectors; i++)

src/vnet/session/mma_template.c
@@ -238,7 +238,7 @@ RT (mma_rules_table_del_rule) (RTT (mma_rules_table) * srt,
 	  if (i != 0)
 	    {
 	      vec_add2 (next_indices, new_elts, i);
-	      clib_memcpy (new_elts, rp->next_indices, i * sizeof (u32));
+	      _clib_memcpy (new_elts, rp->next_indices, i * sizeof (u32));
 	    }
 	  if (vec_len (child->next_indices))
 	    vec_append (next_indices, child->next_indices);
@@ -246,8 +246,8 @@ RT (mma_rules_table_del_rule) (RTT (mma_rules_table) * srt,
 	  if (left_to_add)
 	    {
 	      vec_add2 (next_indices, new_elts, left_to_add);
-	      clib_memcpy (new_elts, &rp->next_indices[i + 1],
-			   left_to_add * sizeof (u32));
+	      _clib_memcpy (new_elts, &rp->next_indices[i + 1],
+			    left_to_add * sizeof (u32));
 	    }
 	  RT (mma_rule_free) (srt, child);
 	  vec_free (rp->next_indices);

src/vppinfra/fifo.c
@@ -112,11 +112,11 @@ _clib_fifo_resize (void *v_old, uword n_new_elts, uword elt_bytes)
       if (head + n_copy_bytes >= end)
 	{
 	  uword n = end - head;
-	  clib_memcpy (v_new, head, n);
-	  clib_memcpy (v_new + n, v_old, n_copy_bytes - n);
+	  _clib_memcpy (v_new, head, n);
+	  _clib_memcpy (v_new + n, v_old, n_copy_bytes - n);
 	}
       else
-	clib_memcpy (v_new, head, n_copy_bytes);
+	_clib_memcpy (v_new, head, n_copy_bytes);
     }
 
   /* Zero empty space. */

sollor525/vpp-ips

src/plugins/dpdk/device/flow.c
@@ -84,10 +84,10 @@ dpdk_flow_add (dpdk_device_t * xd, vnet_flow_t * f, dpdk_flow_entry_t * fe)
   if (f->type == VNET_FLOW_TYPE_IP6_N_TUPLE)
     {
       vnet_flow_ip6_n_tuple_t *t6 = &f->ip6_n_tuple;
-      clib_memcpy (ip6[0].hdr.src_addr, &t6->src_addr.addr, 16);
-      clib_memcpy (ip6[1].hdr.src_addr, &t6->src_addr.mask, 16);
-      clib_memcpy (ip6[0].hdr.dst_addr, &t6->dst_addr.addr, 16);
-      clib_memcpy (ip6[1].hdr.dst_addr, &t6->dst_addr.mask, 16);
+      clib_memcpy_fast (ip6[0].hdr.src_addr, &t6->src_addr.addr, 16);
+      clib_memcpy_fast (ip6[1].hdr.src_addr, &t6->src_addr.mask, 16);
+      clib_memcpy_fast (ip6[0].hdr.dst_addr, &t6->dst_addr.addr, 16);
+      clib_memcpy_fast (ip6[1].hdr.dst_addr, &t6->dst_addr.mask, 16);
       item->type = RTE_FLOW_ITEM_TYPE_IPV6;
       item->spec = ip6;
       item->mask = ip6 + 1;
@@ -183,9 +183,9 @@ dpdk_flow_add (dpdk_device_t * xd, vnet_flow_t * f, dpdk_flow_entry_t * fe)
       raw[0].item.relative = 1;
       raw[0].item.length = vxlan_hdr_sz;
 
-      clib_memcpy (raw[0].val + raw_sz, &spec_hdr, vxlan_hdr_sz);
+      clib_memcpy_fast (raw[0].val + raw_sz, &spec_hdr, vxlan_hdr_sz);
       raw[0].item.pattern = raw[0].val + raw_sz;
-      clib_memcpy (raw[1].val + raw_sz, &mask_hdr, vxlan_hdr_sz);
+      clib_memcpy_fast (raw[1].val + raw_sz, &mask_hdr, vxlan_hdr_sz);
       raw[1].item.pattern = raw[1].val + raw_sz;
 
       vec_add2 (items, item, 1);

src/vnet/classify/vnet_classify.c
@@ -131,7 +131,7 @@ vnet_classify_new_table (vnet_classify_main_t * cm,
   clib_memset (t, 0, sizeof (*t));
 
   vec_validate_aligned (t->mask, match_n_vectors - 1, sizeof (u32x4));
-  clib_memcpy (t->mask, mask, match_n_vectors * sizeof (u32x4));
+  clib_memcpy_fast (t->mask, mask, match_n_vectors * sizeof (u32x4));
 
   t->next_table_index = ~0;
   t->nbuckets = nbuckets;
@@ -273,7 +273,7 @@ static inline void make_working_copy
 
   v = vnet_classify_get_entry (t, b->offset);
 
-  clib_memcpy (working_copy, v, required_length);
+  clib_memcpy_fast (working_copy, v, required_length);
 
   working_bucket.as_u64 = b->as_u64;
   working_bucket.offset = vnet_classify_get_offset (t, working_copy);
@@ -317,8 +317,8 @@ split_and_rehash (vnet_classify_table_t * t,
 
 	      if (vnet_classify_entry_is_free (new_v))
 		{
-		  clib_memcpy (new_v, v, sizeof (vnet_classify_entry_t)
-			       + (t->match_n_vectors * sizeof (u32x4)));
+		  clib_memcpy_fast (new_v, v, sizeof (vnet_classify_entry_t)
+				    + (t->match_n_vectors * sizeof (u32x4)));
 		  new_v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
 		  goto doublebreak;
 		}
@@ -361,8 +361,8 @@ split_and_rehash_linear (vnet_classify_table_t * t,
 		  clib_warning ("BUG: linear rehash new entry not free!");
 		  continue;
 		}
-	      clib_memcpy (new_v, v, sizeof (vnet_classify_entry_t)
-			   + (t->match_n_vectors * sizeof (u32x4)));
+	      clib_memcpy_fast (new_v, v, sizeof (vnet_classify_entry_t)
+				+ (t->match_n_vectors * sizeof (u32x4)));
 	      new_v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
 	      j++;
 	      goto doublebreak;
@@ -457,8 +457,8 @@ vnet_classify_add_del (vnet_classify_table_t * t,
 	}
 
       v = vnet_classify_entry_alloc (t, 0 /* new_log2_pages */ );
-      clib_memcpy (v, add_v, sizeof (vnet_classify_entry_t) +
-		   t->match_n_vectors * sizeof (u32x4));
+      clib_memcpy_fast (v, add_v, sizeof (vnet_classify_entry_t) +
+			t->match_n_vectors * sizeof (u32x4));
       v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
       vnet_classify_entry_claim_resource (v);
 
@@ -496,8 +496,8 @@ vnet_classify_add_del (vnet_classify_table_t * t,
 	  if (!memcmp
 	      (v->key, add_v->key, t->match_n_vectors * sizeof (u32x4)))
 	    {
-	      clib_memcpy (v, add_v, sizeof (vnet_classify_entry_t) +
-			   t->match_n_vectors * sizeof (u32x4));
+	      clib_memcpy_fast (v, add_v, sizeof (vnet_classify_entry_t) +
+				t->match_n_vectors * sizeof (u32x4));
 	      v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
 	      vnet_classify_entry_claim_resource (v);
 
@@ -513,8 +513,8 @@ vnet_classify_add_del (vnet_classify_table_t * t,
 
 	  if (vnet_classify_entry_is_free (v))
 	    {
-	      clib_memcpy (v, add_v, sizeof (vnet_classify_entry_t) +
-			   t->match_n_vectors * sizeof (u32x4));
+	      clib_memcpy_fast (v, add_v, sizeof (vnet_classify_entry_t) +
+				t->match_n_vectors * sizeof (u32x4));
 	      v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
 	      vnet_classify_entry_claim_resource (v);
 
@@ -609,8 +609,8 @@ vnet_classify_add_del (vnet_classify_table_t * t,
 
       if (vnet_classify_entry_is_free (new_v))
 	{
-	  clib_memcpy (new_v, add_v, sizeof (vnet_classify_entry_t) +
-		       t->match_n_vectors * sizeof (u32x4));
+	  clib_memcpy_fast (new_v, add_v, sizeof (vnet_classify_entry_t) +
+			    t->match_n_vectors * sizeof (u32x4));
 	  new_v->flags &= ~(VNET_CLASSIFY_ENTRY_FREE);
 	  vnet_classify_entry_claim_resource (new_v);
 
@@ -1885,10 +1885,10 @@ unformat_ip6_match (unformat_input_t * input, va_list * args)
   ip = (ip6_header_t *) match;
 
   if (src)
-    clib_memcpy (&ip->src_address, &src_val, sizeof (ip->src_address));
+    clib_memcpy_fast (&ip->src_address, &src_val, sizeof (ip->src_address));
 
   if (dst)
-    clib_memcpy (&ip->dst_address, &dst_val, sizeof (ip->dst_address));
+    clib_memcpy_fast (&ip->dst_address, &dst_val, sizeof (ip->dst_address));
 
   if (proto)
     ip->protocol = proto_val;
@@ -2012,10 +2012,10 @@ unformat_l2_match (unformat_input_t * input, va_list * args)
   vec_validate_aligned (match, len - 1, sizeof (u32x4));
 
   if (dst)
-    clib_memcpy (match, dst_val, 6);
+    clib_memcpy_fast (match, dst_val, 6);
 
   if (src)
-    clib_memcpy (match + 6, src_val, 6);
+    clib_memcpy_fast (match + 6, src_val, 6);
 
   if (tag2)
     {
@@ -2188,8 +2188,8 @@ vnet_classify_add_del_session (vnet_classify_main_t * cm,
     e->metadata = 0;
 
   /* Copy key data, honoring skip_n_vectors */
-  clib_memcpy (&e->key, match + t->skip_n_vectors * sizeof (u32x4),
-	       t->match_n_vectors * sizeof (u32x4));
+  clib_memcpy_fast (&e->key, match + t->skip_n_vectors * sizeof (u32x4),
+		    t->match_n_vectors * sizeof (u32x4));
 
   /* Clear don't-care bits; likely when dynamically creating sessions */
   for (i = 0; i < t->match_n_vectors; i++)

src/vnet/session/mma_template.c
@@ -238,7 +238,7 @@ RT (mma_rules_table_del_rule) (RTT (mma_rules_table) * srt,
 	  if (i != 0)
 	    {
 	      vec_add2 (next_indices, new_elts, i);
-	      clib_memcpy (new_elts, rp->next_indices, i * sizeof (u32));
+	      clib_memcpy_fast (new_elts, rp->next_indices, i * sizeof (u32));
 	    }
 	  if (vec_len (child->next_indices))
 	    vec_append (next_indices, child->next_indices);
@@ -246,8 +246,8 @@ RT (mma_rules_table_del_rule) (RTT (mma_rules_table) * srt,
 	  if (left_to_add)
 	    {
 	      vec_add2 (next_indices, new_elts, left_to_add);
-	      clib_memcpy (new_elts, &rp->next_indices[i + 1],
-			   left_to_add * sizeof (u32));
+	      clib_memcpy_fast (new_elts, &rp->next_indices[i + 1],
+				left_to_add * sizeof (u32));
 	    }
 	  RT (mma_rule_free) (srt, child);
 	  vec_free (rp->next_indices);

src/vppinfra/fifo.c
@@ -112,11 +112,11 @@ _clib_fifo_resize (void *v_old, uword n_new_elts, uword elt_bytes)
       if (head + n_copy_bytes >= end)
 	{
 	  uword n = end - head;
-	  clib_memcpy (v_new, head, n);
-	  clib_memcpy (v_new + n, v_old, n_copy_bytes - n);
+	  clib_memcpy_fast (v_new, head, n);
+	  clib_memcpy_fast (v_new + n, v_old, n_copy_bytes - n);
 	}
       else
-	clib_memcpy (v_new, head, n_copy_bytes);
+	clib_memcpy_fast (v_new, head, n_copy_bytes);
     }
 
   /* Zero empty space. */