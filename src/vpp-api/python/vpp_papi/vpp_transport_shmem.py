FDio/vpp

vpp_papi: Reserved keywords.
'async' and 'await' are reserved keywords starting with Python 3.7.

This change is necessary to support VPP-1508.

Change-Id: Iba2b3aef98a0ecaabc1622719b364f8f4ab5d5a3
Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

src/vpp-api/python/vpp_papi/vpp_transport_shmem.py
@@ -83,8 +83,8 @@ class VppTransport(object):
     def resume(self):
         vpp_api.vac_rx_resume()
 
-    def get_callback(self, async):
-        return vac_callback_sync if not async else vac_callback_async
+    def get_callback(self, async_):
+        return vac_callback_sync if not async_ else vac_callback_async
 
     def get_msg_index(self, name):
         return vpp_api.vac_get_msg_index(name)

src/vpp-api/python/vpp_papi/vpp_transport_socket.py
@@ -118,7 +118,7 @@ class VppTransport(object):
     def callback(self):
         raise NotImplemented
 
-    def get_callback(self, async):
+    def get_callback(self, async_):
         return self.callback
 
     def get_msg_index(self, name):

sollor525/vpp-ips

src/vpp-api/python/vpp_papi/vpp_transport_shmem.py
@@ -83,8 +83,8 @@ class VppTransport(object):
     def resume(self):
         vpp_api.vac_rx_resume()
 
-    def get_callback(self, async):
-        return vac_callback_sync if not async else vac_callback_async
+    def get_callback(self, do_async):
+        return vac_callback_sync if not do_async else vac_callback_async
 
     def get_msg_index(self, name):
         return vpp_api.vac_get_msg_index(name)

src/vpp-api/python/vpp_papi/vpp_transport_socket.py
@@ -118,7 +118,7 @@ class VppTransport(object):
     def callback(self):
         raise NotImplemented
 
-    def get_callback(self, async):
+    def get_callback(self, do_async):
         return self.callback
 
     def get_msg_index(self, name):