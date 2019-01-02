FDio/vpp

Fixes for buliding for 32bit targets:   * u32/u64/uword mismatches   * pointer-to-int fixes   * printf formatting issues   * issues with incorrect "ULL" and related suffixes   * structure alignment and padding issues
Change-Id: I70b989007758755fe8211c074f651150680f60b4
Signed-off-by: David Johnson <davijoh3@cisco.com>

src/vnet/classify/vnet_classify.h
@@ -188,6 +188,7 @@ typedef struct
   /* Writer (only) lock for this table */
   volatile u32 *writer_lock;
 
+  CLIB_CACHE_LINE_ALIGN_MARK(pad);
 } vnet_classify_table_t;
 
 struct _vnet_classify_main

sollor525/vpp-ips

src/vnet/classify/vnet_classify.h
@@ -149,6 +149,7 @@ typedef struct
 
 typedef struct
 {
+  CLIB_CACHE_LINE_ALIGN_MARK (cacheline0);
   /* Mask to apply after skipping N vectors */
   u32x4 *mask;
   /* Buckets and entries */