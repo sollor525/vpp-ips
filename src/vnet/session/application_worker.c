FDio/vpp

session: refactor listen logic
Make app-listener the handle for app listens. Consequently transport and
local listen sessions are now associated to the app-listener.

Change-Id: I9397a26d42cccb100970b6b4794c15bac2e11465
Signed-off-by: Florin Coras <fcoras@cisco.com>

src/vnet/session/application_worker.c
@@ -164,15 +164,18 @@ app_worker_free (app_worker_t * app_wrk)
   segment_manager_t *sm;
   u32 sm_index;
   int i;
+  app_listener_t *al;
+  session_t *ls;
 
   /*
    *  Listener cleanup
    */
 
   /* *INDENT-OFF* */
-  hash_foreach (handle, sm_index, app_wrk->listeners_table,
-  ({
-    vec_add1 (handles, handle);
+  hash_foreach (handle, sm_index, app_wrk->listeners_table, ({
+    ls = listen_session_get_from_handle (handle);
+    al = app_listener_get (app, ls->listener_db_index);
+    vec_add1 (handles, app_listener_handle (al));
     sm = segment_manager_get (sm_index);
     sm->app_wrk_index = SEGMENT_MANAGER_INVALID_APP_INDEX;
   }));
@@ -184,7 +187,7 @@ app_worker_free (app_worker_t * app_wrk)
       a->wrk_map_index = app_wrk->wrk_map_index;
       a->handle = handles[i];
       /* seg manager is removed when unbind completes */
-      vnet_unbind (a);
+      vnet_unlisten (a);
     }
 
   /*
@@ -252,54 +255,94 @@ app_worker_alloc_segment_manager (app_worker_t * app_wrk)
 }
 
 int
-app_worker_start_listen (app_worker_t * app_wrk, session_t * ls)
+app_worker_start_listen (app_worker_t * app_wrk,
+			 app_listener_t * app_listener)
 {
   segment_manager_t *sm;
+  session_t *ls;
+
+  app_listener->workers = clib_bitmap_set (app_listener->workers,
+					   app_wrk->wrk_map_index, 1);
+
+  if (app_listener->session_index == SESSION_INVALID_INDEX)
+    return 0;
+
+  ls = session_get (app_listener->session_index, 0);
 
   /* Allocate segment manager. All sessions derived out of a listen session
    * have fifos allocated by the same segment manager. */
   if (!(sm = app_worker_alloc_segment_manager (app_wrk)))
     return -1;
 
-  /* Add to app's listener table. Useful to find all child listeners
-   * when app goes down, although, just for unbinding this is not needed */
+  /* Keep track of the segment manager for the listener or this worker */
   hash_set (app_wrk->listeners_table, listen_session_get_handle (ls),
 	    segment_manager_index (sm));
 
-  if (!ls->rx_fifo
-      && session_transport_service_type (ls) == TRANSPORT_SERVICE_CL)
+  if (session_transport_service_type (ls) == TRANSPORT_SERVICE_CL)
     {
-      if (session_alloc_fifos (sm, ls))
+      if (!ls->rx_fifo && session_alloc_fifos (sm, ls))
 	return -1;
     }
   return 0;
 }
 
 int
-app_worker_stop_listen (app_worker_t * app_wrk, session_handle_t handle)
+app_worker_stop_listen (app_worker_t * app_wrk, app_listener_t * al)
 {
+  session_handle_t handle;
   segment_manager_t *sm;
   uword *sm_indexp;
 
-  sm_indexp = hash_get (app_wrk->listeners_table, handle);
-  if (PREDICT_FALSE (!sm_indexp))
+  if (!clib_bitmap_get (al->workers, app_wrk->wrk_map_index))
+    return 0;
+
+  if (al->session_index != SESSION_INVALID_INDEX)
     {
-      clib_warning ("listener handle was removed %llu!", handle);
-      return -1;
+      session_t *ls;
+
+      ls = listen_session_get (al->session_index);
+      handle = listen_session_get_handle (ls);
+
+      sm_indexp = hash_get (app_wrk->listeners_table, handle);
+      if (PREDICT_FALSE (!sm_indexp))
+	{
+	  clib_warning ("listener handle was removed %llu!", handle);
+	  return -1;
+	}
+
+      sm = segment_manager_get (*sm_indexp);
+      if (app_wrk->first_segment_manager == *sm_indexp)
+	{
+	  /* Delete sessions but don't remove segment manager */
+	  app_wrk->first_segment_manager_in_use = 0;
+	  segment_manager_del_sessions (sm);
+	}
+      else
+	{
+	  segment_manager_init_del (sm);
+	}
+      hash_unset (app_wrk->listeners_table, handle);
     }
 
-  sm = segment_manager_get (*sm_indexp);
-  if (app_wrk->first_segment_manager == *sm_indexp)
+  if (al->local_index != SESSION_INVALID_INDEX)
     {
-      /* Delete sessions but don't remove segment manager */
-      app_wrk->first_segment_manager_in_use = 0;
-      segment_manager_del_sessions (sm);
+      local_session_t *ll, *ls;
+      application_t *app;
+
+      app = application_get (app_wrk->app_index);
+      ll = application_get_local_listen_session (app, al->local_index);
+
+      /* *INDENT-OFF* */
+      pool_foreach (ls, app_wrk->local_sessions, ({
+        if (ls->listener_index == ll->session_index)
+          app_worker_local_session_disconnect (app_wrk->app_index, ls);
+      }));
+      /* *INDENT-ON* */
     }
-  else
-    {
-      segment_manager_init_del (sm);
-    }
-  hash_unset (app_wrk->listeners_table, handle);
+
+  clib_bitmap_set_no_check (al->workers, app_wrk->wrk_map_index, 0);
+  if (clib_bitmap_is_zero (al->workers))
+    app_listener_cleanup (al);
 
   return 0;
 }
@@ -350,8 +393,8 @@ app_worker_own_session (app_worker_t * app_wrk, session_t * s)
 }
 
 int
-app_worker_open_session (app_worker_t * app, session_endpoint_t * sep,
-			 u32 api_context)
+app_worker_connect_session (app_worker_t * app, session_endpoint_t * sep,
+			    u32 api_context)
 {
   int rv;
 
@@ -1040,6 +1083,7 @@ void
 app_worker_format_local_sessions (app_worker_t * app_wrk, int verbose)
 {
   vlib_main_t *vm = vlib_get_main ();
+  app_worker_t *client_wrk;
   local_session_t *ls;
   transport_proto_t tp;
   u8 *conn = 0;
@@ -1061,8 +1105,9 @@ app_worker_format_local_sessions (app_worker_t * app_wrk, int verbose)
     tp = session_type_transport_proto(ls->listener_session_type);
     conn = format (0, "[L][%U] *:%u", format_transport_proto_short, tp,
                    ls->port);
-    vlib_cli_output (vm, "%-40v%-15u%-20u", conn, ls->app_wrk_index,
-                     ls->client_wrk_index);
+    client_wrk = app_worker_get (ls->client_wrk_index);
+    vlib_cli_output (vm, "%-40v%-15u%-20u", conn, ls->app_index,
+                     client_wrk->app_index);
     vec_reset_length (conn);
   }));
   /* *INDENT-ON* */

src/vnet/session/session_api.c
@@ -19,6 +19,7 @@
 #include <vnet/session/application_interface.h>
 #include <vnet/session/session_rules_table.h>
 #include <vnet/session/session_table.h>
+#include <vnet/session/session.h>
 
 #include <vnet/vnet_msg_enum.h>
 
@@ -207,6 +208,7 @@ send_session_accept_callback (session_t * s)
   session_t *listener;
   svm_msg_q_t *vpp_queue;
   application_t *server;
+  app_listener_t *al;
 
   server = application_get (server_wrk->app_index);
   reg =
@@ -228,7 +230,8 @@ send_session_accept_callback (session_t * s)
   if (session_has_transport (s))
     {
       listener = listen_session_get (s->listener_index);
-      mp->listener_handle = listen_session_get_handle (listener);
+      al = app_listener_get (server, listener->listener_db_index);
+      mp->listener_handle = app_listener_handle (al);
       if (application_is_proxy (server))
 	{
 	  listener =
@@ -253,22 +256,16 @@ send_session_accept_callback (session_t * s)
       if (application_local_session_listener_has_transport (ls))
 	{
 	  listener = listen_session_get (ls->listener_index);
-	  mp->listener_handle = listen_session_get_handle (listener);
+	  al = app_listener_get (server, listener->listener_db_index);
+	  mp->listener_handle = app_listener_handle (al);
 	  mp->is_ip4 = session_type_is_ip4 (listener->session_type);
 	}
       else
 	{
 	  ll = application_get_local_listen_session (server,
 						     ls->listener_index);
-	  if (ll->transport_listener_index != ~0)
-	    {
-	      listener = listen_session_get (ll->transport_listener_index);
-	      mp->listener_handle = listen_session_get_handle (listener);
-	    }
-	  else
-	    {
-	      mp->listener_handle = application_local_session_handle (ll);
-	    }
+	  al = app_listener_get (server, ll->listener_db_index);
+	  mp->listener_handle = app_listener_handle (al);
 	  mp->is_ip4 = session_type_is_ip4 (ll->listener_session_type);
 	}
       mp->handle = application_local_session_handle (ls);
@@ -424,6 +421,7 @@ mq_send_session_accepted_cb (session_t * s)
   session_accepted_msg_t *mp;
   session_event_t *evt;
   application_t *app;
+  app_listener_t *al;
 
   app = application_get (app_wrk->app_index);
   app_mq = app_wrk->event_queue;
@@ -443,7 +441,8 @@ mq_send_session_accepted_cb (session_t * s)
   if (session_has_transport (s))
     {
       listener = listen_session_get (s->listener_index);
-      mp->listener_handle = listen_session_get_handle (listener);
+      al = app_listener_get (app, listener->listener_db_index);
+      mp->listener_handle = app_listener_handle (al);
       if (application_is_proxy (app))
 	{
 	  listener =
@@ -475,21 +474,15 @@ mq_send_session_accepted_cb (session_t * s)
       if (application_local_session_listener_has_transport (ls))
 	{
 	  listener = listen_session_get (ls->listener_index);
-	  mp->listener_handle = listen_session_get_handle (listener);
+	  al = app_listener_get (app, listener->listener_db_index);
+	  mp->listener_handle = app_listener_handle (al);
 	  mp->is_ip4 = session_type_is_ip4 (listener->session_type);
 	}
       else
 	{
 	  ll = application_get_local_listen_session (app, ls->listener_index);
-	  if (ll->transport_listener_index != ~0)
-	    {
-	      listener = listen_session_get (ll->transport_listener_index);
-	      mp->listener_handle = listen_session_get_handle (listener);
-	    }
-	  else
-	    {
-	      mp->listener_handle = application_local_session_handle (ll);
-	    }
+	  al = app_listener_get (app, ll->listener_db_index);
+	  mp->listener_handle = app_listener_handle (al);
 	  mp->is_ip4 = session_type_is_ip4 (ll->listener_session_type);
 	}
       mp->handle = application_local_session_handle (ls);
@@ -674,11 +667,12 @@ mq_send_session_bound_cb (u32 app_wrk_index, u32 api_context,
   svm_msg_q_msg_t _msg, *msg = &_msg;
   svm_msg_q_t *app_mq, *vpp_evt_q;
   transport_connection_t *tc;
-  session_t *ls = 0;
   session_bound_msg_t *mp;
   app_worker_t *app_wrk;
   session_event_t *evt;
   application_t *app;
+  app_listener_t *al;
+  session_t *ls = 0;
 
   app_wrk = app_worker_get (app_wrk_index);
   app = application_get (app_wrk->app_index);
@@ -705,7 +699,8 @@ mq_send_session_bound_cb (u32 app_wrk_index, u32 api_context,
   mp->handle = handle;
   if (application_has_global_scope (app))
     {
-      ls = listen_session_get_from_handle (handle);
+      al = app_listener_get_w_handle (handle);
+      ls = app_listener_get_session (al);
       tc = listen_session_get_transport (ls);
       mp->lcl_port = tc->lcl_port;
       mp->lcl_is_ip4 = tc->is_ip4;
@@ -888,7 +883,7 @@ static void
 vl_api_bind_uri_t_handler (vl_api_bind_uri_t * mp)
 {
   transport_connection_t *tc = 0;
-  vnet_bind_args_t _a, *a = &_a;
+  vnet_listen_args_t _a, *a = &_a;
   vl_api_bind_uri_reply_t *rmp;
   session_t *s;
   application_t *app = 0;
@@ -1180,16 +1175,17 @@ vl_api_map_another_segment_reply_t_handler (vl_api_map_another_segment_reply_t
 static void
 vl_api_bind_sock_t_handler (vl_api_bind_sock_t * mp)
 {
+  vnet_listen_args_t _a, *a = &_a;
+  transport_connection_t *tc = 0;
   vl_api_bind_sock_reply_t *rmp;
-  vnet_bind_args_t _a, *a = &_a;
-  int rv = 0;
-  clib_error_t *error;
+  svm_msg_q_t *vpp_evt_q;
   application_t *app = 0;
   app_worker_t *app_wrk;
-  session_t *s;
-  transport_connection_t *tc = 0;
   ip46_address_t *ip46;
-  svm_msg_q_t *vpp_evt_q;
+  clib_error_t *error;
+  app_listener_t *al;
+  session_t *s;
+  int rv = 0;
 
   if (session_manager_is_enabled () == 0)
     {
@@ -1215,7 +1211,7 @@ vl_api_bind_sock_t_handler (vl_api_bind_sock_t * mp)
   a->app_index = app->app_index;
   a->wrk_map_index = mp->wrk_index;
 
-  if ((error = vnet_bind (a)))
+  if ((error = vnet_listen (a)))
     {
       rv = clib_error_get_code (error);
       clib_error_report (error);
@@ -1231,7 +1227,8 @@ done:
 	rmp->lcl_is_ip4 = mp->is_ip4;
 	if (app && application_has_global_scope (app))
 	  {
-	    s = listen_session_get_from_handle (a->handle);
+	    al = app_listener_get_w_handle (a->handle);
+	    s = app_listener_get_session (al);
 	    tc = listen_session_get_transport (s);
             clib_memcpy_fast (rmp->lcl_ip, &tc->lcl_ip, sizeof (tc->lcl_ip));
             if (session_transport_service_type (s) == TRANSPORT_SERVICE_CL)
@@ -1276,7 +1273,7 @@ vl_api_unbind_sock_t_handler (vl_api_unbind_sock_t * mp)
       a->app_index = app->app_index;
       a->handle = mp->handle;
       a->wrk_map_index = mp->wrk_index;
-      if ((error = vnet_unbind (a)))
+      if ((error = vnet_unlisten (a)))
 	{
 	  rv = clib_error_get_code (error);
 	  clib_error_report (error);

sollor525/vpp-ips

src/vnet/session/application_worker.c
@@ -164,15 +164,18 @@ app_worker_free (app_worker_t * app_wrk)
   segment_manager_t *sm;
   u32 sm_index;
   int i;
+  app_listener_t *al;
+  session_t *ls;
 
   /*
    *  Listener cleanup
    */
 
   /* *INDENT-OFF* */
-  hash_foreach (handle, sm_index, app_wrk->listeners_table,
-  ({
-    vec_add1 (handles, handle);
+  hash_foreach (handle, sm_index, app_wrk->listeners_table, ({
+    ls = listen_session_get_from_handle (handle);
+    al = app_listener_get (app, ls->al_index);
+    vec_add1 (handles, app_listener_handle (al));
     sm = segment_manager_get (sm_index);
     sm->app_wrk_index = SEGMENT_MANAGER_INVALID_APP_INDEX;
   }));
@@ -184,7 +187,7 @@ app_worker_free (app_worker_t * app_wrk)
       a->wrk_map_index = app_wrk->wrk_map_index;
       a->handle = handles[i];
       /* seg manager is removed when unbind completes */
-      vnet_unbind (a);
+      vnet_unlisten (a);
     }
 
   /*
@@ -252,54 +255,97 @@ app_worker_alloc_segment_manager (app_worker_t * app_wrk)
 }
 
 int
-app_worker_start_listen (app_worker_t * app_wrk, session_t * ls)
+app_worker_start_listen (app_worker_t * app_wrk,
+			 app_listener_t * app_listener)
 {
   segment_manager_t *sm;
+  session_t *ls;
+
+  if (clib_bitmap_get (app_listener->workers, app_wrk->wrk_map_index))
+    return VNET_API_ERROR_ADDRESS_IN_USE;
+
+  app_listener->workers = clib_bitmap_set (app_listener->workers,
+					   app_wrk->wrk_map_index, 1);
+
+  if (app_listener->session_index == SESSION_INVALID_INDEX)
+    return 0;
+
+  ls = session_get (app_listener->session_index, 0);
 
   /* Allocate segment manager. All sessions derived out of a listen session
    * have fifos allocated by the same segment manager. */
   if (!(sm = app_worker_alloc_segment_manager (app_wrk)))
     return -1;
 
-  /* Add to app's listener table. Useful to find all child listeners
-   * when app goes down, although, just for unbinding this is not needed */
+  /* Keep track of the segment manager for the listener or this worker */
   hash_set (app_wrk->listeners_table, listen_session_get_handle (ls),
 	    segment_manager_index (sm));
 
-  if (!ls->rx_fifo
-      && session_transport_service_type (ls) == TRANSPORT_SERVICE_CL)
+  if (session_transport_service_type (ls) == TRANSPORT_SERVICE_CL)
     {
-      if (session_alloc_fifos (sm, ls))
+      if (!ls->rx_fifo && session_alloc_fifos (sm, ls))
 	return -1;
     }
   return 0;
 }
 
 int
-app_worker_stop_listen (app_worker_t * app_wrk, session_handle_t handle)
+app_worker_stop_listen (app_worker_t * app_wrk, app_listener_t * al)
 {
+  session_handle_t handle;
   segment_manager_t *sm;
   uword *sm_indexp;
 
-  sm_indexp = hash_get (app_wrk->listeners_table, handle);
-  if (PREDICT_FALSE (!sm_indexp))
+  if (!clib_bitmap_get (al->workers, app_wrk->wrk_map_index))
+    return 0;
+
+  if (al->session_index != SESSION_INVALID_INDEX)
     {
-      clib_warning ("listener handle was removed %llu!", handle);
-      return -1;
+      session_t *ls;
+
+      ls = listen_session_get (al->session_index);
+      handle = listen_session_get_handle (ls);
+
+      sm_indexp = hash_get (app_wrk->listeners_table, handle);
+      if (PREDICT_FALSE (!sm_indexp))
+	{
+	  clib_warning ("listener handle was removed %llu!", handle);
+	  return -1;
+	}
+
+      sm = segment_manager_get (*sm_indexp);
+      if (app_wrk->first_segment_manager == *sm_indexp)
+	{
+	  /* Delete sessions but don't remove segment manager */
+	  app_wrk->first_segment_manager_in_use = 0;
+	  segment_manager_del_sessions (sm);
+	}
+      else
+	{
+	  segment_manager_init_del (sm);
+	}
+      hash_unset (app_wrk->listeners_table, handle);
     }
 
-  sm = segment_manager_get (*sm_indexp);
-  if (app_wrk->first_segment_manager == *sm_indexp)
+  if (al->local_index != SESSION_INVALID_INDEX)
     {
-      /* Delete sessions but don't remove segment manager */
-      app_wrk->first_segment_manager_in_use = 0;
-      segment_manager_del_sessions (sm);
+      local_session_t *ll, *ls;
+      application_t *app;
+
+      app = application_get (app_wrk->app_index);
+      ll = application_get_local_listen_session (app, al->local_index);
+
+      /* *INDENT-OFF* */
+      pool_foreach (ls, app_wrk->local_sessions, ({
+        if (ls->listener_index == ll->session_index)
+          app_worker_local_session_disconnect (app_wrk->app_index, ls);
+      }));
+      /* *INDENT-ON* */
     }
-  else
-    {
-      segment_manager_init_del (sm);
-    }
-  hash_unset (app_wrk->listeners_table, handle);
+
+  clib_bitmap_set_no_check (al->workers, app_wrk->wrk_map_index, 0);
+  if (clib_bitmap_is_zero (al->workers))
+    app_listener_cleanup (al);
 
   return 0;
 }
@@ -350,8 +396,8 @@ app_worker_own_session (app_worker_t * app_wrk, session_t * s)
 }
 
 int
-app_worker_open_session (app_worker_t * app, session_endpoint_t * sep,
-			 u32 api_context)
+app_worker_connect_session (app_worker_t * app, session_endpoint_t * sep,
+			    u32 api_context)
 {
   int rv;
 
@@ -405,7 +451,7 @@ app_worker_get_listen_segment_manager (app_worker_t * app,
 }
 
 session_t *
-app_worker_first_listener (app_worker_t * app, u8 fib_proto,
+app_worker_first_listener (app_worker_t * app_wrk, u8 fib_proto,
 			   u8 transport_proto)
 {
   session_t *listener;
@@ -417,7 +463,7 @@ app_worker_first_listener (app_worker_t * app, u8 fib_proto,
 					fib_proto == FIB_PROTOCOL_IP4);
 
   /* *INDENT-OFF* */
-   hash_foreach (handle, sm_index, app->listeners_table, ({
+   hash_foreach (handle, sm_index, app_wrk->listeners_table, ({
      listener = listen_session_get_from_handle (handle);
      if (listener->session_type == sst
 	 && listener->enqueue_epoch != SESSION_PROXY_LISTENER_INDEX)
@@ -429,7 +475,7 @@ app_worker_first_listener (app_worker_t * app, u8 fib_proto,
 }
 
 session_t *
-app_worker_proxy_listener (app_worker_t * app, u8 fib_proto,
+app_worker_proxy_listener (app_worker_t * app_wrk, u8 fib_proto,
 			   u8 transport_proto)
 {
   session_t *listener;
@@ -441,7 +487,7 @@ app_worker_proxy_listener (app_worker_t * app, u8 fib_proto,
 					fib_proto == FIB_PROTOCOL_IP4);
 
   /* *INDENT-OFF* */
-   hash_foreach (handle, sm_index, app->listeners_table, ({
+   hash_foreach (handle, sm_index, app_wrk->listeners_table, ({
      listener = listen_session_get_from_handle (handle);
      if (listener->session_type == sst
 	 && listener->enqueue_epoch == SESSION_PROXY_LISTENER_INDEX)
@@ -1040,6 +1086,7 @@ void
 app_worker_format_local_sessions (app_worker_t * app_wrk, int verbose)
 {
   vlib_main_t *vm = vlib_get_main ();
+  app_worker_t *client_wrk;
   local_session_t *ls;
   transport_proto_t tp;
   u8 *conn = 0;
@@ -1061,8 +1108,9 @@ app_worker_format_local_sessions (app_worker_t * app_wrk, int verbose)
     tp = session_type_transport_proto(ls->listener_session_type);
     conn = format (0, "[L][%U] *:%u", format_transport_proto_short, tp,
                    ls->port);
-    vlib_cli_output (vm, "%-40v%-15u%-20u", conn, ls->app_wrk_index,
-                     ls->client_wrk_index);
+    client_wrk = app_worker_get (ls->client_wrk_index);
+    vlib_cli_output (vm, "%-40v%-15u%-20u", conn, ls->app_index,
+                     client_wrk->app_index);
     vec_reset_length (conn);
   }));
   /* *INDENT-ON* */

src/vnet/session/session_api.c
@@ -19,6 +19,7 @@
 #include <vnet/session/application_interface.h>
 #include <vnet/session/session_rules_table.h>
 #include <vnet/session/session_table.h>
+#include <vnet/session/session.h>
 
 #include <vnet/vnet_msg_enum.h>
 
@@ -207,6 +208,7 @@ send_session_accept_callback (session_t * s)
   session_t *listener;
   svm_msg_q_t *vpp_queue;
   application_t *server;
+  app_listener_t *al;
 
   server = application_get (server_wrk->app_index);
   reg =
@@ -228,7 +230,8 @@ send_session_accept_callback (session_t * s)
   if (session_has_transport (s))
     {
       listener = listen_session_get (s->listener_index);
-      mp->listener_handle = listen_session_get_handle (listener);
+      al = app_listener_get (server, listener->al_index);
+      mp->listener_handle = app_listener_handle (al);
       if (application_is_proxy (server))
 	{
 	  listener =
@@ -253,22 +256,16 @@ send_session_accept_callback (session_t * s)
       if (application_local_session_listener_has_transport (ls))
 	{
 	  listener = listen_session_get (ls->listener_index);
-	  mp->listener_handle = listen_session_get_handle (listener);
+	  al = app_listener_get (server, listener->al_index);
+	  mp->listener_handle = app_listener_handle (al);
 	  mp->is_ip4 = session_type_is_ip4 (listener->session_type);
 	}
       else
 	{
 	  ll = application_get_local_listen_session (server,
 						     ls->listener_index);
-	  if (ll->transport_listener_index != ~0)
-	    {
-	      listener = listen_session_get (ll->transport_listener_index);
-	      mp->listener_handle = listen_session_get_handle (listener);
-	    }
-	  else
-	    {
-	      mp->listener_handle = application_local_session_handle (ll);
-	    }
+	  al = app_listener_get (server, ll->al_index);
+	  mp->listener_handle = app_listener_handle (al);
 	  mp->is_ip4 = session_type_is_ip4 (ll->listener_session_type);
 	}
       mp->handle = application_local_session_handle (ls);
@@ -424,6 +421,7 @@ mq_send_session_accepted_cb (session_t * s)
   session_accepted_msg_t *mp;
   session_event_t *evt;
   application_t *app;
+  app_listener_t *al;
 
   app = application_get (app_wrk->app_index);
   app_mq = app_wrk->event_queue;
@@ -443,7 +441,8 @@ mq_send_session_accepted_cb (session_t * s)
   if (session_has_transport (s))
     {
       listener = listen_session_get (s->listener_index);
-      mp->listener_handle = listen_session_get_handle (listener);
+      al = app_listener_get (app, listener->al_index);
+      mp->listener_handle = app_listener_handle (al);
       if (application_is_proxy (app))
 	{
 	  listener =
@@ -475,21 +474,15 @@ mq_send_session_accepted_cb (session_t * s)
       if (application_local_session_listener_has_transport (ls))
 	{
 	  listener = listen_session_get (ls->listener_index);
-	  mp->listener_handle = listen_session_get_handle (listener);
+	  al = app_listener_get (app, listener->al_index);
+	  mp->listener_handle = app_listener_handle (al);
 	  mp->is_ip4 = session_type_is_ip4 (listener->session_type);
 	}
       else
 	{
 	  ll = application_get_local_listen_session (app, ls->listener_index);
-	  if (ll->transport_listener_index != ~0)
-	    {
-	      listener = listen_session_get (ll->transport_listener_index);
-	      mp->listener_handle = listen_session_get_handle (listener);
-	    }
-	  else
-	    {
-	      mp->listener_handle = application_local_session_handle (ll);
-	    }
+	  al = app_listener_get (app, ll->al_index);
+	  mp->listener_handle = app_listener_handle (al);
 	  mp->is_ip4 = session_type_is_ip4 (ll->listener_session_type);
 	}
       mp->handle = application_local_session_handle (ls);
@@ -674,11 +667,12 @@ mq_send_session_bound_cb (u32 app_wrk_index, u32 api_context,
   svm_msg_q_msg_t _msg, *msg = &_msg;
   svm_msg_q_t *app_mq, *vpp_evt_q;
   transport_connection_t *tc;
-  session_t *ls = 0;
   session_bound_msg_t *mp;
   app_worker_t *app_wrk;
   session_event_t *evt;
   application_t *app;
+  app_listener_t *al;
+  session_t *ls = 0;
 
   app_wrk = app_worker_get (app_wrk_index);
   app = application_get (app_wrk->app_index);
@@ -705,7 +699,8 @@ mq_send_session_bound_cb (u32 app_wrk_index, u32 api_context,
   mp->handle = handle;
   if (application_has_global_scope (app))
     {
-      ls = listen_session_get_from_handle (handle);
+      al = app_listener_get_w_handle (handle);
+      ls = app_listener_get_session (al);
       tc = listen_session_get_transport (ls);
       mp->lcl_port = tc->lcl_port;
       mp->lcl_is_ip4 = tc->is_ip4;
@@ -888,7 +883,7 @@ static void
 vl_api_bind_uri_t_handler (vl_api_bind_uri_t * mp)
 {
   transport_connection_t *tc = 0;
-  vnet_bind_args_t _a, *a = &_a;
+  vnet_listen_args_t _a, *a = &_a;
   vl_api_bind_uri_reply_t *rmp;
   session_t *s;
   application_t *app = 0;
@@ -1180,16 +1175,17 @@ vl_api_map_another_segment_reply_t_handler (vl_api_map_another_segment_reply_t
 static void
 vl_api_bind_sock_t_handler (vl_api_bind_sock_t * mp)
 {
+  vnet_listen_args_t _a, *a = &_a;
+  transport_connection_t *tc = 0;
   vl_api_bind_sock_reply_t *rmp;
-  vnet_bind_args_t _a, *a = &_a;
-  int rv = 0;
-  clib_error_t *error;
+  svm_msg_q_t *vpp_evt_q;
   application_t *app = 0;
   app_worker_t *app_wrk;
-  session_t *s;
-  transport_connection_t *tc = 0;
   ip46_address_t *ip46;
-  svm_msg_q_t *vpp_evt_q;
+  clib_error_t *error;
+  app_listener_t *al;
+  session_t *s;
+  int rv = 0;
 
   if (session_manager_is_enabled () == 0)
     {
@@ -1215,7 +1211,7 @@ vl_api_bind_sock_t_handler (vl_api_bind_sock_t * mp)
   a->app_index = app->app_index;
   a->wrk_map_index = mp->wrk_index;
 
-  if ((error = vnet_bind (a)))
+  if ((error = vnet_listen (a)))
     {
       rv = clib_error_get_code (error);
       clib_error_report (error);
@@ -1231,7 +1227,8 @@ done:
 	rmp->lcl_is_ip4 = mp->is_ip4;
 	if (app && application_has_global_scope (app))
 	  {
-	    s = listen_session_get_from_handle (a->handle);
+	    al = app_listener_get_w_handle (a->handle);
+	    s = app_listener_get_session (al);
 	    tc = listen_session_get_transport (s);
             clib_memcpy_fast (rmp->lcl_ip, &tc->lcl_ip, sizeof (tc->lcl_ip));
             if (session_transport_service_type (s) == TRANSPORT_SERVICE_CL)
@@ -1276,7 +1273,7 @@ vl_api_unbind_sock_t_handler (vl_api_unbind_sock_t * mp)
       a->app_index = app->app_index;
       a->handle = mp->handle;
       a->wrk_map_index = mp->wrk_index;
-      if ((error = vnet_unbind (a)))
+      if ((error = vnet_unlisten (a)))
 	{
 	  rv = clib_error_get_code (error);
 	  clib_error_report (error);