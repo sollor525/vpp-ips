FDio/vpp

GBP: drop and contract counters
Change-Id: Ib436512a26e53f70f7b5e47bf34224ab73e5244e
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/plugins/gbp/gbp_policy_node.c
@@ -18,12 +18,9 @@
 
 #include <vnet/vxlan-gbp/vxlan_gbp_packet.h>
 
-#define foreach_gbp_policy                      \
-  _(DENY,    "deny")
-
 typedef enum
 {
-#define _(sym,str) GBP_ERROR_##sym,
+#define _(sym,str) GBP_POLICY_ERROR_##sym,
   foreach_gbp_policy
 #undef _
     GBP_POLICY_N_ERROR,
@@ -37,10 +34,8 @@ static char *gbp_policy_error_strings[] = {
 
 typedef enum
 {
-#define _(sym,str) GBP_POLICY_NEXT_##sym,
-  foreach_gbp_policy
-#undef _
-    GBP_POLICY_N_NEXT,
+  GBP_POLICY_NEXT_DENY,
+  GBP_POLICY_N_NEXT,
 } gbp_policy_next_t;
 
 /**
@@ -114,11 +109,14 @@ gbp_policy_inline (vlib_main_t * vm,
   gbp_main_t *gm = &gbp_main;
   gbp_policy_main_t *gpm = &gbp_policy_main;
   u32 n_left_from, *from, *to_next;
-  u32 next_index;
+  u32 next_index, thread_index;
+  u32 n_allow_intra, n_allow_a_bit;
 
   next_index = 0;
   n_left_from = frame->n_vectors;
   from = vlib_frame_vector_args (frame);
+  thread_index = vm->thread_index;
+  n_allow_intra = n_allow_a_bit = 0;
 
   while (n_left_from > 0)
     {
@@ -162,6 +160,7 @@ gbp_policy_inline (vlib_main_t * vm,
 					    (is_port_based ?
 					     L2OUTPUT_FEAT_GBP_POLICY_PORT :
 					     L2OUTPUT_FEAT_GBP_POLICY_MAC));
+	      n_allow_a_bit++;
 	      key0.as_u32 = ~0;
 	      goto trace;
 	    }
@@ -177,9 +176,11 @@ gbp_policy_inline (vlib_main_t * vm,
 	  if (NULL != ge0)
 	    key0.gck_dst = ge0->ge_fwd.gef_sclass;
 	  else
-	    /* If you cannot determine the destination EP then drop */
-	    goto trace;
-
+	    {
+	      /* If you cannot determine the destination EP then drop */
+	      b0->error = node->errors[GBP_POLICY_ERROR_DROP_NO_DCLASS];
+	      goto trace;
+	    }
 	  key0.gck_src = vnet_buffer2 (b0)->gbp.sclass;
 
 	  if (SCLASS_INVALID != key0.gck_src)
@@ -197,6 +198,7 @@ gbp_policy_inline (vlib_main_t * vm,
 					   L2OUTPUT_FEAT_GBP_POLICY_PORT :
 					   L2OUTPUT_FEAT_GBP_POLICY_MAC));
 		  vnet_buffer2 (b0)->gbp.flags |= VXLAN_GBP_GPFLAGS_A;
+		  n_allow_intra++;
 		}
 	      else
 		{
@@ -212,6 +214,11 @@ gbp_policy_inline (vlib_main_t * vm,
 		      u16 ether_type0;
 		      const u8 *h0;
 
+		      vlib_prefetch_combined_counter
+			(&gbp_contract_drop_counters, thread_index, gci0);
+		      vlib_prefetch_combined_counter
+			(&gbp_contract_permit_counters, thread_index, gci0);
+
 		      action0 = 0;
 		      gc0 = gbp_contract_get (gci0);
 		      l2_len0 = vnet_buffer (b0)->l2.l2_len;
@@ -224,6 +231,14 @@ gbp_policy_inline (vlib_main_t * vm,
 			  /*
 			   * black list model so drop
 			   */
+			  b0->error =
+			    node->errors[GBP_POLICY_ERROR_DROP_ETHER_TYPE];
+
+			  vlib_increment_combined_counter
+			    (&gbp_contract_drop_counters,
+			     thread_index,
+			     gci0, 1, vlib_buffer_length_in_chain (vm, b0));
+
 			  goto trace;
 			}
 
@@ -275,7 +290,7 @@ gbp_policy_inline (vlib_main_t * vm,
 				      L2OUTPUT_FEAT_GBP_POLICY_MAC));
 				  break;
 				case GBP_RULE_DENY:
-				  next0 = 0;
+				  next0 = GBP_POLICY_NEXT_DENY;
 				  break;
 				case GBP_RULE_REDIRECT:
 				  next0 = gbp_rule_l2_redirect (gu, b0);
@@ -283,6 +298,27 @@ gbp_policy_inline (vlib_main_t * vm,
 				}
 			    }
 			}
+		      if (next0 == GBP_POLICY_NEXT_DENY)
+			{
+			  vlib_increment_combined_counter
+			    (&gbp_contract_drop_counters,
+			     thread_index,
+			     gci0, 1, vlib_buffer_length_in_chain (vm, b0));
+			  b0->error =
+			    node->errors[GBP_POLICY_ERROR_DROP_CONTRACT];
+			}
+		      else
+			{
+			  vlib_increment_combined_counter
+			    (&gbp_contract_permit_counters,
+			     thread_index,
+			     gci0, 1, vlib_buffer_length_in_chain (vm, b0));
+			}
+		    }
+		  else
+		    {
+		      b0->error =
+			node->errors[GBP_POLICY_ERROR_DROP_NO_CONTRACT];
 		    }
 		}
 	    }
@@ -307,8 +343,8 @@ gbp_policy_inline (vlib_main_t * vm,
 		vlib_add_trace (vm, node, b0, sizeof (*t));
 	      t->sclass = key0.gck_src;
 	      t->dst_epg = key0.gck_dst;
-	      t->acl_index = (gc0 ? gc0->gc_acl_index : ~0),
-		t->allowed = (next0 != GBP_POLICY_NEXT_DENY);
+	      t->acl_index = (gc0 ? gc0->gc_acl_index : ~0);
+	      t->allowed = (next0 != GBP_POLICY_NEXT_DENY);
 	    }
 
 	  /* verify speculative enqueue, maybe switch current next frame */
@@ -319,6 +355,11 @@ gbp_policy_inline (vlib_main_t * vm,
 
       vlib_put_next_frame (vm, node, next_index, n_left_to_next);
     }
+
+  vlib_node_increment_counter (vm, node->node_index,
+			       GBP_POLICY_ERROR_ALLOW_INTRA, n_allow_intra);
+  vlib_node_increment_counter (vm, node->node_index,
+			       GBP_POLICY_ERROR_ALLOW_A_BIT, n_allow_a_bit);
 
   return frame->n_vectors;
 }
@@ -375,6 +416,8 @@ VLIB_REGISTER_NODE (gbp_policy_mac_node) = {
   .format_trace = format_gbp_policy_trace,
   .type = VLIB_NODE_TYPE_INTERNAL,
   .sibling_of = "gbp-policy-port",
+  .n_errors = ARRAY_LEN(gbp_policy_error_strings),
+  .error_strings = gbp_policy_error_strings,
 };
 
 /* *INDENT-ON* */

test/test_gbp.py
@@ -465,13 +465,14 @@ class VppGbpContract(VppObject):
         rules = []
         for r in self.rules:
             rules.append(r.encode())
-        self._test.vapi.gbp_contract_add_del(
+        r = self._test.vapi.gbp_contract_add_del(
             1,
             self.sclass,
             self.dclass,
             self.acl_index,
             rules,
             self.allowed_ethertypes)
+        self.stats_index = r.stats_index
         self._test.registry.register(self, self._test.logger)
 
     def remove_vpp_config(self):
@@ -498,6 +499,14 @@ class VppGbpContract(VppObject):
                and c.contract.dclass == self.dclass:
                 return True
         return False
+
+    def get_drop_stats(self):
+        c = self._test.statistics.get_counter("/net/gbp-contract-drop")
+        return c[0][self.stats_index]
+
+    def get_permit_stats(self):
+        c = self._test.statistics.get_counter("/net/gbp-contract-permit")
+        return c[0][self.stats_index]
 
 
 class VppGbpVxlanTunnel(VppInterface):
@@ -1181,6 +1190,11 @@ class TestGBP(VppTestCase):
                                      pkt_inter_epg_221_to_220 * 65,
                                      eps[0].itf)
 
+        ds = c2.get_drop_stats()
+        self.assertEqual(ds['packets'], 0)
+        ps = c2.get_permit_stats()
+        self.assertEqual(ps['packets'], 65)
+
         #
         # the contract does not allow non-IP
         #
@@ -1452,6 +1466,8 @@ class TestGBP(VppTestCase):
     def test_gbp_learn_l2(self):
         """ GBP L2 Endpoint Learning """
 
+        self.vapi.cli("clear errors")
+        self.logger.error(self.vapi.cli("show error"))
         ep_flags = VppEnum.vl_api_gbp_endpoint_flags_t
         learnt = [{'mac': '00:00:11:11:11:01',
                    'ip': '10.0.0.1',
@@ -1560,6 +1576,9 @@ class TestGBP(VppTestCase):
 
         self.send_and_assert_no_replies(self.pg2, p)
 
+        self.assert_packet_counter_equal(
+            '/err/gbp-policy-port/drop-no-contract', 1)
+
         #
         # we should not have learnt a new tunnel endpoint, since
         # the EPG was not learnt.
@@ -1610,6 +1629,9 @@ class TestGBP(VppTestCase):
             self.assertTrue(find_gbp_endpoint(self,
                                               vx_tun_l2_1.sw_if_index,
                                               ip=l['ip']))
+
+        self.assert_packet_counter_equal(
+            '/err/gbp-policy-port/allow-intra-sclass', 2)
 
         self.logger.info(self.vapi.cli("show gbp endpoint"))
         self.logger.info(self.vapi.cli("show gbp vxlan"))

sollor525/vpp-ips

src/plugins/gbp/gbp_policy_node.c
@@ -25,15 +25,15 @@
 
 typedef enum
 {
-#define _(sym,str) GBP_ERROR_##sym,
-  foreach_gbp_policy
+#define _(sym,str) GBP_POLICY_ERROR_##sym,
+  foreach_gbp_policy_error
 #undef _
     GBP_POLICY_N_ERROR,
 } gbp_policy_error_t;
 
 static char *gbp_policy_error_strings[] = {
 #define _(sym,string) string,
-  foreach_gbp_policy
+  foreach_gbp_policy_error
 #undef _
 };
 
@@ -115,11 +115,14 @@ gbp_policy_inline (vlib_main_t * vm,
   gbp_main_t *gm = &gbp_main;
   gbp_policy_main_t *gpm = &gbp_policy_main;
   u32 n_left_from, *from, *to_next;
-  u32 next_index;
+  u32 next_index, thread_index;
+  u32 n_allow_intra, n_allow_a_bit;
 
   next_index = 0;
   n_left_from = frame->n_vectors;
   from = vlib_frame_vector_args (frame);
+  thread_index = vm->thread_index;
+  n_allow_intra = n_allow_a_bit = 0;
 
   while (n_left_from > 0)
     {
@@ -172,6 +175,7 @@ gbp_policy_inline (vlib_main_t * vm,
 					    (is_port_based ?
 					     L2OUTPUT_FEAT_GBP_POLICY_PORT :
 					     L2OUTPUT_FEAT_GBP_POLICY_MAC));
+	      n_allow_a_bit++;
 	      key0.as_u32 = ~0;
 	      goto trace;
 	    }
@@ -188,9 +192,11 @@ gbp_policy_inline (vlib_main_t * vm,
 	  if (NULL != ge0)
 	    key0.gck_dst = ge0->ge_fwd.gef_sclass;
 	  else
-	    /* If you cannot determine the destination EP then drop */
-	    goto trace;
-
+	    {
+	      /* If you cannot determine the destination EP then drop */
+	      b0->error = node->errors[GBP_POLICY_ERROR_DROP_NO_DCLASS];
+	      goto trace;
+	    }
 	  key0.gck_src = vnet_buffer2 (b0)->gbp.sclass;
 
 	  if (SCLASS_INVALID != key0.gck_src)
@@ -208,6 +214,7 @@ gbp_policy_inline (vlib_main_t * vm,
 					   L2OUTPUT_FEAT_GBP_POLICY_PORT :
 					   L2OUTPUT_FEAT_GBP_POLICY_MAC));
 		  vnet_buffer2 (b0)->gbp.flags |= VXLAN_GBP_GPFLAGS_A;
+		  n_allow_intra++;
 		}
 	      else
 		{
@@ -223,6 +230,11 @@ gbp_policy_inline (vlib_main_t * vm,
 		      u16 ether_type0;
 		      const u8 *h0;
 
+		      vlib_prefetch_combined_counter
+			(&gbp_contract_drop_counters, thread_index, gci0);
+		      vlib_prefetch_combined_counter
+			(&gbp_contract_permit_counters, thread_index, gci0);
+
 		      action0 = 0;
 		      gc0 = gbp_contract_get (gci0);
 		      l2_len0 = vnet_buffer (b0)->l2.l2_len;
@@ -235,6 +247,14 @@ gbp_policy_inline (vlib_main_t * vm,
 			  /*
 			   * black list model so drop
 			   */
+			  b0->error =
+			    node->errors[GBP_POLICY_ERROR_DROP_ETHER_TYPE];
+
+			  vlib_increment_combined_counter
+			    (&gbp_contract_drop_counters,
+			     thread_index,
+			     gci0, 1, vlib_buffer_length_in_chain (vm, b0));
+
 			  goto trace;
 			}
 
@@ -286,7 +306,7 @@ gbp_policy_inline (vlib_main_t * vm,
 				      L2OUTPUT_FEAT_GBP_POLICY_MAC));
 				  break;
 				case GBP_RULE_DENY:
-				  next0 = 0;
+				  next0 = GBP_POLICY_NEXT_DROP;
 				  break;
 				case GBP_RULE_REDIRECT:
 				  next0 = gbp_rule_l2_redirect (gu, b0);
@@ -294,6 +314,27 @@ gbp_policy_inline (vlib_main_t * vm,
 				}
 			    }
 			}
+		      if (next0 == GBP_POLICY_NEXT_DROP)
+			{
+			  vlib_increment_combined_counter
+			    (&gbp_contract_drop_counters,
+			     thread_index,
+			     gci0, 1, vlib_buffer_length_in_chain (vm, b0));
+			  b0->error =
+			    node->errors[GBP_POLICY_ERROR_DROP_CONTRACT];
+			}
+		      else
+			{
+			  vlib_increment_combined_counter
+			    (&gbp_contract_permit_counters,
+			     thread_index,
+			     gci0, 1, vlib_buffer_length_in_chain (vm, b0));
+			}
+		    }
+		  else
+		    {
+		      b0->error =
+			node->errors[GBP_POLICY_ERROR_DROP_NO_CONTRACT];
 		    }
 		}
 	    }
@@ -331,6 +372,11 @@ gbp_policy_inline (vlib_main_t * vm,
 
       vlib_put_next_frame (vm, node, next_index, n_left_to_next);
     }
+
+  vlib_node_increment_counter (vm, node->node_index,
+			       GBP_POLICY_ERROR_ALLOW_INTRA, n_allow_intra);
+  vlib_node_increment_counter (vm, node->node_index,
+			       GBP_POLICY_ERROR_ALLOW_A_BIT, n_allow_a_bit);
 
   return frame->n_vectors;
 }
@@ -376,7 +422,6 @@ VLIB_REGISTER_NODE (gbp_policy_port_node) = {
   .error_strings = gbp_policy_error_strings,
 
   .n_next_nodes = GBP_POLICY_N_NEXT,
-
   .next_nodes = {
     [GBP_POLICY_NEXT_DROP] = "error-drop",
   },
@@ -387,7 +432,14 @@ VLIB_REGISTER_NODE (gbp_policy_mac_node) = {
   .vector_size = sizeof (u32),
   .format_trace = format_gbp_policy_trace,
   .type = VLIB_NODE_TYPE_INTERNAL,
-  .sibling_of = "gbp-policy-port",
+
+  .n_errors = ARRAY_LEN(gbp_policy_error_strings),
+  .error_strings = gbp_policy_error_strings,
+
+  .n_next_nodes = GBP_POLICY_N_NEXT,
+  .next_nodes = {
+    [GBP_POLICY_NEXT_DROP] = "error-drop",
+  },
 };
 
 /* *INDENT-ON* */

test/test_gbp.py
@@ -447,13 +447,14 @@ class VppGbpContract(VppObject):
         rules = []
         for r in self.rules:
             rules.append(r.encode())
-        self._test.vapi.gbp_contract_add_del(
+        r = self._test.vapi.gbp_contract_add_del(
             1,
             self.sclass,
             self.dclass,
             self.acl_index,
             rules,
             self.allowed_ethertypes)
+        self.stats_index = r.stats_index
         self._test.registry.register(self, self._test.logger)
 
     def remove_vpp_config(self):
@@ -477,6 +478,14 @@ class VppGbpContract(VppObject):
                and c.contract.dclass == self.dclass:
                 return True
         return False
+
+    def get_drop_stats(self):
+        c = self._test.statistics.get_counter("/net/gbp/contract/drop")
+        return c[0][self.stats_index]
+
+    def get_permit_stats(self):
+        c = self._test.statistics.get_counter("/net/gbp/contract/permit")
+        return c[0][self.stats_index]
 
 
 class VppGbpVxlanTunnel(VppInterface):
@@ -1154,6 +1163,11 @@ class TestGBP(VppTestCase):
                                      pkt_inter_epg_221_to_220 * 65,
                                      eps[0].itf)
 
+        ds = c2.get_drop_stats()
+        self.assertEqual(ds['packets'], 0)
+        ps = c2.get_permit_stats()
+        self.assertEqual(ps['packets'], 65)
+
         #
         # the contract does not allow non-IP
         #
@@ -1425,6 +1439,8 @@ class TestGBP(VppTestCase):
     def test_gbp_learn_l2(self):
         """ GBP L2 Endpoint Learning """
 
+        self.vapi.cli("clear errors")
+
         ep_flags = VppEnum.vl_api_gbp_endpoint_flags_t
         learnt = [{'mac': '00:00:11:11:11:01',
                    'ip': '10.0.0.1',
@@ -1533,6 +1549,10 @@ class TestGBP(VppTestCase):
 
         self.send_and_assert_no_replies(self.pg2, p)
 
+        self.logger.info(self.vapi.cli("sh error"))
+        # self.assert_packet_counter_equal(
+        #    '/err/gbp-policy-port/drop-no-contract', 1)
+
         #
         # we should not have learnt a new tunnel endpoint, since
         # the EPG was not learnt.
@@ -1583,6 +1603,9 @@ class TestGBP(VppTestCase):
             self.assertTrue(find_gbp_endpoint(self,
                                               vx_tun_l2_1.sw_if_index,
                                               ip=l['ip']))
+
+        # self.assert_packet_counter_equal(
+        #    '/err/gbp-policy-port/allow-intra-sclass', 2)
 
         self.logger.info(self.vapi.cli("show gbp endpoint"))
         self.logger.info(self.vapi.cli("show gbp vxlan"))