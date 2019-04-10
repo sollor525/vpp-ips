FDio/vpp

Tests Cleanup: Fix missing calls to setUpClass/tearDownClass.
Continuation/Part 2 of https://gerrit.fd.io/r/#/c/17092/

Change-Id: Id0122d84eaf2c05d29e5be63a594d5e528ee7c9a
Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

test/test_ipsec_tun_if_esp.py
@@ -12,6 +12,14 @@ class TemplateIpsecTunIfEsp(TemplateIpsec):
 
     encryption_type = ESP
 
+    @classmethod
+    def setUpClass(cls):
+        super(TemplateIpsecTunIfEsp, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TemplateIpsecTunIfEsp, cls).tearDownClass()
+
     def setUp(self):
         super(TemplateIpsecTunIfEsp, self).setUp()
 

sollor525/vpp-ips

test/test_ipsec_tun_if_esp.py
@@ -20,6 +20,14 @@ class TemplateIpsec4TunIfEsp(TemplateIpsec):
 
     encryption_type = ESP
 
+    @classmethod
+    def setUpClass(cls):
+        super(TemplateIpsec4TunIfEsp, cls).setUpClass()
+
+    @classmethod
+    def tearDownClass(cls):
+        super(TemplateIpsec4TunIfEsp, cls).tearDownClass()
+
     def setUp(self):
         super(TemplateIpsec4TunIfEsp, self).setUp()
 