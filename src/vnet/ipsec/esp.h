sollor525/vpp-ips

commit 23747b499d60a8ef713a43efa8e52865fd9cdf42

IPSEC: support GCM in ESP
Change-Id: Id2ddb77b4ec3dd543d6e638bc882923f2bac011d
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/ipsec/esp.h
@@ -54,6 +54,20 @@ typedef CLIB_PACKED (struct {
 }) ip6_and_esp_header_t;
 /* *INDENT-ON* */
 
+/**
+ * AES GCM Additional Authentication data
+ */
+typedef struct esp_aead_t_
+{
+  /**
+   * for GCM: when using ESN it's:
+   *   SPI, seq-hi, seg-low
+   * else
+   *   SPI, seq-low
+   */
+  u32 data[3];
+} __clib_packed esp_aead_t;
+
 #define ESP_SEQ_MAX		(4294967295UL)
 #define ESP_MAX_BLOCK_SIZE	(16)
 #define ESP_MAX_IV_SIZE		(16)
@@ -117,6 +131,26 @@ hmac_calc (vlib_main_t * vm, ipsec_sa_t * sa, u8 * data, int data_len,
   return sa->integ_icv_size;
 }
 
+always_inline void
+esp_aad_fill (vnet_crypto_op_t * op,
+	      const esp_header_t * esp, const ipsec_sa_t * sa)
+{
+  esp_aead_t *aad;
+
+  aad = (esp_aead_t *) op->aad;
+  clib_memcpy_fast (aad, esp, 8);
+
+  if (ipsec_sa_is_set_USE_ESN (sa))
+    {
+      /* SPI, seq-hi, seq-low */
+      aad->data[2] = aad->data[1];
+      aad->data[1] = clib_host_to_net_u32 (sa->seq_hi);
+      op->aad_len = 12;
+    }
+  else
+    /* SPI, seq-low */
+    op->aad_len = 8;
+}
 #endif /* __ESP_H__ */
 
 /*

src/plugins/crypto_openssl/main.c
@@ -115,14 +115,18 @@ openssl_ops_enc_gcm (vlib_main_t * vm, vnet_crypto_op_t * ops[], u32 n_ops,
   for (i = 0; i < n_ops; i++)
     {
       vnet_crypto_op_t *op = ops[i];
+      u32 nonce[3];
       int len;
 
       if (op->flags & VNET_CRYPTO_OP_FLAG_INIT_IV)
 	RAND_bytes (op->iv, 8);
 
+      nonce[0] = op->salt;
+      clib_memcpy_fast (nonce + 1, op->iv, 8);
+
       EVP_EncryptInit_ex (ctx, cipher, 0, 0, 0);
-      EVP_CIPHER_CTX_ctrl (ctx, EVP_CTRL_GCM_SET_IVLEN, 8, NULL);
-      EVP_EncryptInit_ex (ctx, 0, 0, op->key, op->iv);
+      EVP_CIPHER_CTX_ctrl (ctx, EVP_CTRL_GCM_SET_IVLEN, 12, NULL);
+      EVP_EncryptInit_ex (ctx, 0, 0, op->key, (u8 *) nonce);
       if (op->aad_len)
 	EVP_EncryptUpdate (ctx, NULL, &len, op->aad, op->aad_len);
       EVP_EncryptUpdate (ctx, op->dst, &len, op->src, op->len);

src/vnet/crypto/crypto.h
@@ -115,7 +115,7 @@ typedef struct
   u8 flags;
 #define VNET_CRYPTO_OP_FLAG_INIT_IV (1 << 0)
 #define VNET_CRYPTO_OP_FLAG_HMAC_CHECK (1 << 1)
-  u32 len;
+  u32 len, salt;
   u16 aad_len;
   u8 key_len, iv_len, digest_len, tag_len;
   u8 *key;

src/vnet/ipsec/esp_decrypt.c
@@ -197,7 +197,7 @@ esp_decrypt_inline (vlib_main_t * vm,
       current_sa_pkts += 1;
       current_sa_bytes += pd->current_length;
 
-      if (PREDICT_TRUE (cpd.icv_sz > 0))
+      if (PREDICT_TRUE (sa0->integ_op_id != VNET_CRYPTO_OP_NONE))
 	{
 	  vnet_crypto_op_t *op;
 	  vec_add2_aligned (ptd->integ_ops, op, 1, CLIB_CACHE_LINE_BYTES);
@@ -233,6 +233,39 @@ esp_decrypt_inline (vlib_main_t * vm,
 	  vnet_crypto_op_init (op, sa0->crypto_dec_op_id);
 	  op->key = sa0->crypto_key.data;
 	  op->iv = payload;
+
+	  if (ipsec_sa_is_set_IS_AEAD (sa0))
+	    {
+	      esp_header_t *esp0;
+	      esp_aead_t *aad;
+	      u8 *scratch;
+	      u32 salt;
+
+	      /*
+	       * construct the AAD and the nonce (Salt || IV) in a scratch
+	       * space in front of the IP header.
+	       */
+	      scratch = payload - esp_sz;
+	      esp0 = (esp_header_t *) (scratch);
+
+	      scratch -= (sizeof (*aad) + pd->hdr_sz);
+	      op->aad = scratch;
+
+	      esp_aad_fill (op, esp0, sa0);
+
+	      /*
+	       * we don't need to refer to the ESP header anymore so we
+	       * can overwrite it with the salt and use the IV where it is
+	       * to form the nonce = (Salt + IV)
+	       */
+	      salt = clib_host_to_net_u32 (sa0->salt);
+	      op->iv -= sizeof (sa0->salt);
+	      clib_memcpy_fast (op->iv, &salt, sizeof (sa0->salt));
+	      op->iv_len = cpd.iv_sz + sizeof (sa0->salt);
+
+	      op->tag = payload + len;
+	      op->tag_len = 16;
+	    }
 	  op->src = op->dst = payload += cpd.iv_sz;
 	  op->len = len - cpd.iv_sz;
 	  op->user_data = b - bufs;

src/vnet/ipsec/esp_encrypt.c
@@ -436,6 +436,21 @@ esp_encrypt_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 	  op->len = payload_len - icv_sz;
 	  op->flags = VNET_CRYPTO_OP_FLAG_INIT_IV;
 	  op->user_data = b - bufs;
+	  op->salt = sa0->salt;
+
+	  if (ipsec_sa_is_set_IS_AEAD (sa0))
+	    {
+	      /*
+	       * construct the AAD in a scratch space in front
+	       * of the IP header.
+	       */
+	      op->aad = payload - hdr_len - sizeof (esp_aead_t);
+
+	      esp_aad_fill (op, esp, sa0);
+
+	      op->tag = payload + op->len;
+	      op->tag_len = 16;
+	    }
 	}
 
       if (sa0->integ_op_id)

src/vnet/ipsec/ipsec.c
@@ -38,13 +38,6 @@ ipsec_check_ah_support (ipsec_sa_t * sa)
 static clib_error_t *
 ipsec_check_esp_support (ipsec_sa_t * sa)
 {
-  if (sa->crypto_alg == IPSEC_CRYPTO_ALG_AES_GCM_128)
-    return clib_error_return (0, "unsupported aes-gcm-128 crypto-alg");
-  if (sa->crypto_alg == IPSEC_CRYPTO_ALG_AES_GCM_192)
-    return clib_error_return (0, "unsupported aes-gcm-192 crypto-alg");
-  if (sa->crypto_alg == IPSEC_CRYPTO_ALG_AES_GCM_256)
-    return clib_error_return (0, "unsupported aes-gcm-256 crypto-alg");
-
   return 0;
 }
 
@@ -292,6 +285,24 @@ ipsec_init (vlib_main_t * vm)
   a->enc_op_id = VNET_CRYPTO_OP_AES_256_CBC_ENC;
   a->dec_op_id = VNET_CRYPTO_OP_AES_256_CBC_DEC;
   a->iv_size = a->block_size = 16;
+
+  a = im->crypto_algs + IPSEC_CRYPTO_ALG_AES_GCM_128;
+  a->enc_op_id = VNET_CRYPTO_OP_AES_128_GCM_ENC;
+  a->dec_op_id = VNET_CRYPTO_OP_AES_128_GCM_DEC;
+  a->iv_size = a->block_size = 8;
+  a->icv_size = 16;
+
+  a = im->crypto_algs + IPSEC_CRYPTO_ALG_AES_GCM_192;
+  a->enc_op_id = VNET_CRYPTO_OP_AES_192_GCM_ENC;
+  a->dec_op_id = VNET_CRYPTO_OP_AES_192_GCM_DEC;
+  a->iv_size = a->block_size = 8;
+  a->icv_size = 16;
+
+  a = im->crypto_algs + IPSEC_CRYPTO_ALG_AES_GCM_256;
+  a->enc_op_id = VNET_CRYPTO_OP_AES_256_GCM_ENC;
+  a->dec_op_id = VNET_CRYPTO_OP_AES_256_GCM_DEC;
+  a->iv_size = a->block_size = 8;
+  a->icv_size = 16;
 
   vec_validate (im->integ_algs, IPSEC_INTEG_N_ALG - 1);
   ipsec_main_integ_alg_t *i;

src/vnet/ipsec/ipsec.h
@@ -70,6 +70,7 @@ typedef struct
   vnet_crypto_op_id_t dec_op_id;
   u8 iv_size;
   u8 block_size;
+  u8 icv_size;
 } ipsec_main_crypto_alg_t;
 
 typedef struct

src/vnet/ipsec/ipsec_api.c
@@ -390,7 +390,7 @@ static void vl_api_ipsec_sad_entry_add_del_t_handler
     rv = ipsec_sa_add (id, spi, proto,
 		       crypto_alg, &crypto_key,
 		       integ_alg, &integ_key, flags,
-		       0, &tun_src, &tun_dst, &sa_index);
+		       0, 0, &tun_src, &tun_dst, &sa_index);
   else
     rv = ipsec_sa_del (id);
 

commit 4b34ea41787e7812f15108243fa483b311f4eccf

ipsec: fix perf issue in esp_aad_fill
Type: fix
Fixes: 23747b4
Change-Id: I6b3b97cd361eef19c910c14fd06edb001a4c191b
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vnet/ipsec/esp.h
@@ -137,18 +137,21 @@ esp_aad_fill (vnet_crypto_op_t * op,
   esp_aead_t *aad;
 
   aad = (esp_aead_t *) op->aad;
-  clib_memcpy_fast (aad, esp, 8);
+  aad->data[0] = esp->spi;
 
   if (ipsec_sa_is_set_USE_ESN (sa))
     {
       /* SPI, seq-hi, seq-low */
-      aad->data[2] = aad->data[1];
       aad->data[1] = clib_host_to_net_u32 (sa->seq_hi);
+      aad->data[2] = esp->seq;
       op->aad_len = 12;
     }
   else
-    /* SPI, seq-low */
-    op->aad_len = 8;
+    {
+      /* SPI, seq-low */
+      aad->data[1] = esp->seq;
+      op->aad_len = 8;
+    }
 }
 #endif /* __ESP_H__ */
 