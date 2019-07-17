sollor525/vpp-ips

commit df28a58e09812d9c873defcca9724ee73d24a904

vppinfra: optimize elog_string
Add a string hash to make sure that strings in the string table are
unique. This optimization has been coded piecemeal in multiple places,
we should have made the underlying function do the work years ago.

Type: fix

Change-Id: I5010fd4926b9b80ce3a168748f6de64e333ef498
Signed-off-by: Dave Barach <dave@barachs.net>

src/vppinfra/elog.c
@@ -508,6 +508,7 @@ elog_init (elog_main_t * em, u32 n_events)
   elog_track_register (em, &em->default_track);
 
   elog_time_now (&em->init_time);
+  em->string_table_hash = hash_create_string (0, sizeof (uword));
 }
 
 /* Returns number of events in ring and start index. */
@@ -561,17 +562,39 @@ u32
 elog_string (elog_main_t * em, char *fmt, ...)
 {
   u32 offset;
+  uword *p;
+  uword len;
   va_list va;
 
   elog_lock (em);
+  vec_reset_length (em->string_table_tmp);
   va_start (va, fmt);
-  offset = vec_len (em->string_table);
-  em->string_table = (char *) va_format ((u8 *) em->string_table, fmt, &va);
+  em->string_table_tmp = va_format (em->string_table_tmp, fmt, &va);
   va_end (va);
 
-  /* Null terminate string if it is not already. */
-  if (vec_end (em->string_table)[-1] != 0)
-    vec_add1 (em->string_table, 0);
+  /* See if we already have this string in the string table */
+  p = hash_get_mem (em->string_table_hash, em->string_table_tmp);
+
+  /* We already have the string, so give the caller its offset */
+  if (p)
+    {
+      elog_unlock (em);
+      return (p[0]);
+    }
+
+  /* We don't, so add it. String table entries MUST be NULL terminated */
+  len = vec_len (em->string_table_tmp);
+  ASSERT (len > 0);
+  if (em->string_table_tmp[len - 1] != 0)
+    vec_add1 (em->string_table_tmp, 0);
+
+  offset = vec_len (em->string_table);
+  vec_append (em->string_table, em->string_table_tmp);
+
+  hash_set_mem (em->string_table_hash, em->string_table_tmp, offset);
+
+  /* We gave the key to the string table hash, so we can't reuse it! */
+  em->string_table_tmp = 0;
   elog_unlock (em);
 
   return offset;

src/vppinfra/elog.h
@@ -53,6 +53,7 @@
 #include <vppinfra/error.h>	/* for ASSERT */
 #include <vppinfra/serialize.h>
 #include <vppinfra/time.h>	/* for clib_cpu_time_now */
+#include <vppinfra/hash.h>
 #include <vppinfra/mhash.h>
 
 typedef struct
@@ -155,6 +156,8 @@ typedef struct
 
   /** Events may refer to strings in string table. */
   char *string_table;
+  uword *string_table_hash;
+  u8 *string_table_tmp;
 
   /** Vector of tracks. */
   elog_track_t *tracks;

commit df89c672ec3dc926092e6aa94e1c4fa324572d1e

vppinfra: elog: fix read overflow in string lookup
elog string hashtable use strlen() to determine string length for
hashing, strings must be NULL-terminated for both inserts and lookups.

Type: fix
Fixes: df28a58e09812d9c873defcca9724ee73d24a904

Change-Id: I0680d39a9b89411055fd6adc89c9f253adfae32c
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/vppinfra/elog.c
@@ -572,6 +572,12 @@ elog_string (elog_main_t * em, char *fmt, ...)
   em->string_table_tmp = va_format (em->string_table_tmp, fmt, &va);
   va_end (va);
 
+  /* String table entries MUST be NULL terminated */
+  len = vec_len (em->string_table_tmp);
+  ASSERT (len > 0);
+  if (em->string_table_tmp[len - 1] != 0)
+    vec_add1 (em->string_table_tmp, 0);
+
   /* See if we already have this string in the string table */
   p = hash_get_mem (em->string_table_hash, em->string_table_tmp);
 
@@ -582,11 +588,7 @@ elog_string (elog_main_t * em, char *fmt, ...)
       return (p[0]);
     }
 
-  /* We don't, so add it. String table entries MUST be NULL terminated */
-  len = vec_len (em->string_table_tmp);
-  ASSERT (len > 0);
-  if (em->string_table_tmp[len - 1] != 0)
-    vec_add1 (em->string_table_tmp, 0);
+  /* We don't, so add it. */
 
   offset = vec_len (em->string_table);
   vec_append (em->string_table, em->string_table_tmp);