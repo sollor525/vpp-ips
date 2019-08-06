sollor525/vpp-ips

commit c8d8d8df43ca9bcd6814276ca89965a3b4fe1ae4

QoS recording and marking
Change-Id: Ie5a50def4ec1e4a3b3404a8b6ab9ec248bc16744
Signed-off-by: Neale Ranns <nranns@cisco.com>

src/vnet/ethernet/packet.h
@@ -91,6 +91,15 @@ typedef struct
   u16 type;
 } ethernet_vlan_header_t;
 
+always_inline void
+ethernet_vlan_header_set_priority_net_order (ethernet_vlan_header_t * h,
+					     u8 prio)
+{
+  u8 *bytes = (u8 *) (&h->priority_cfi_and_id);
+
+  bytes[0] &= 0x1f;
+  bytes[0] |= (prio & 0x7) << 5;
+}
 
 /* VLAN with ethertype first and vlan id second */
 typedef struct

test/test_qos.py
@@ -1,0 +1,366 @@
+#!/usr/bin/env python
+
+import unittest
+import socket
+import struct
+
+from framework import VppTestCase, VppTestRunner
+from vpp_object import VppObject
+from vpp_papi_provider import QOS_SOURCE
+from vpp_ip_route import VppIpRoute, VppRoutePath, VppMplsRoute
+from vpp_sub_interface import VppSubInterface, VppDot1QSubint
+
+from scapy.packet import Raw
+from scapy.layers.l2 import Ether, Dot1Q
+from scapy.layers.inet import IP, UDP
+from scapy.layers.inet6 import IPv6
+from scapy.contrib.mpls import MPLS
+
+
+class TestQOS(VppTestCase):
+    """ QOS Test Case """
+
+    def setUp(self):
+        super(TestQOS, self).setUp()
+
+        self.create_pg_interfaces(range(5))
+
+        for i in self.pg_interfaces:
+            i.admin_up()
+            i.config_ip4()
+            i.resolve_arp()
+            i.config_ip6()
+            i.resolve_ndp()
+
+    def tearDown(self):
+        for i in self.pg_interfaces:
+            i.unconfig_ip4()
+            i.unconfig_ip6()
+
+        super(TestQOS, self).tearDown()
+
+    def test_qos_ip(self):
+        """ QoS Mark IP """
+
+        #
+        # for table 1 map the n=0xff possible values of input QoS mark,
+        # n to 1-n
+        #
+        output = [chr(0)] * 256
+        for i in range(0, 255):
+            output[i] = chr(255 - i)
+        os = ''.join(output)
+        rows = [{'outputs': os},
+                {'outputs': os},
+                {'outputs': os},
+                {'outputs': os}]
+
+        self.vapi.qos_egress_map_update(1, rows)
+
+        #
+        # For table 2 (and up) use the value n for everything
+        #
+        output = [chr(2)] * 256
+        os = ''.join(output)
+        rows = [{'outputs': os},
+                {'outputs': os},
+                {'outputs': os},
+                {'outputs': os}]
+
+        self.vapi.qos_egress_map_update(2, rows)
+
+        output = [chr(3)] * 256
+        os = ''.join(output)
+        rows = [{'outputs': os},
+                {'outputs': os},
+                {'outputs': os},
+                {'outputs': os}]
+
+        self.vapi.qos_egress_map_update(3, rows)
+
+        output = [chr(4)] * 256
+        os = ''.join(output)
+        rows = [{'outputs': os},
+                {'outputs': os},
+                {'outputs': os},
+                {'outputs': os}]
+        self.vapi.qos_egress_map_update(4, rows)
+        self.vapi.qos_egress_map_update(5, rows)
+        self.vapi.qos_egress_map_update(6, rows)
+        self.vapi.qos_egress_map_update(7, rows)
+
+        self.logger.info(self.vapi.cli("sh qos eg map"))
+
+        #
+        # Bind interface pgN to table n
+        #
+        self.vapi.qos_mark_enable_disable(self.pg1.sw_if_index,
+                                          QOS_SOURCE.IP,
+                                          1,
+                                          1)
+        self.vapi.qos_mark_enable_disable(self.pg2.sw_if_index,
+                                          QOS_SOURCE.IP,
+                                          2,
+                                          1)
+        self.vapi.qos_mark_enable_disable(self.pg3.sw_if_index,
+                                          QOS_SOURCE.IP,
+                                          3,
+                                          1)
+        self.vapi.qos_mark_enable_disable(self.pg4.sw_if_index,
+                                          QOS_SOURCE.IP,
+                                          4,
+                                          1)
+
+        #
+        # packets ingress on Pg0
+        #
+        p_v4 = (Ether(src=self.pg0.remote_mac, dst=self.pg0.local_mac) /
+                IP(src=self.pg0.remote_ip4, dst=self.pg1.remote_ip4, tos=1) /
+                UDP(sport=1234, dport=1234) /
+                Raw(chr(100) * 65))
+        p_v6 = (Ether(src=self.pg0.remote_mac, dst=self.pg0.local_mac) /
+                IPv6(src=self.pg0.remote_ip6, dst=self.pg1.remote_ip6,
+                     tc=1) /
+                UDP(sport=1234, dport=1234) /
+                Raw(chr(100) * 65))
+
+        #
+        # Since we have not yet enabled the recording of the input QoS
+        # from the input iP header, the egress packet's ToS will be unchanged
+        #
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 1)
+        rx = self.send_and_expect(self.pg0, p_v6 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IPv6].tc, 1)
+
+        #
+        # Enable QoS recrding on IP input for pg0
+        #
+        self.vapi.qos_record_enable_disable(self.pg0.sw_if_index,
+                                            QOS_SOURCE.IP,
+                                            1)
+
+        #
+        # send the same packets, this time expect the input TOS of 1
+        # to be mapped to pg1's egress value of 254
+        #
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 254)
+        rx = self.send_and_expect(self.pg0, p_v6 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IPv6].tc, 254)
+
+        #
+        # different input ToS to test the mapping
+        #
+        p_v4[IP].tos = 127
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 128)
+        p_v6[IPv6].tc = 127
+        rx = self.send_and_expect(self.pg0, p_v6 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IPv6].tc, 128)
+
+        p_v4[IP].tos = 254
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 1)
+        p_v6[IPv6].tc = 254
+        rx = self.send_and_expect(self.pg0, p_v6 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IPv6].tc, 1)
+
+        #
+        # send packets out the other interfaces to test the maps are
+        # correctly applied
+        #
+        p_v4[IP].dst = self.pg2.remote_ip4
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg2)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 2)
+
+        p_v4[IP].dst = self.pg3.remote_ip4
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg3)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 3)
+
+        p_v6[IPv6].dst = self.pg3.remote_ip6
+        rx = self.send_and_expect(self.pg0, p_v6 * 65, self.pg3)
+        for p in rx:
+            self.assertEqual(p[IPv6].tc, 3)
+
+        #
+        # remove the map on pg2 and pg3, now expect an unchanged IP tos
+        #
+        self.vapi.qos_mark_enable_disable(self.pg2.sw_if_index,
+                                          QOS_SOURCE.IP,
+                                          2,
+                                          0)
+        self.vapi.qos_mark_enable_disable(self.pg3.sw_if_index,
+                                          QOS_SOURCE.IP,
+                                          3,
+                                          0)
+        self.logger.info(self.vapi.cli("sh int feat pg2"))
+
+        p_v4[IP].dst = self.pg2.remote_ip4
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg2)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 254)
+
+        p_v4[IP].dst = self.pg3.remote_ip4
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg3)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 254)
+
+        #
+        # still mapping out of pg1
+        #
+        p_v4[IP].dst = self.pg1.remote_ip4
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 1)
+
+        #
+        # disable the input recording on pg0
+        #
+        self.vapi.qos_record_enable_disable(self.pg0.sw_if_index,
+                                            QOS_SOURCE.IP,
+                                            0)
+
+        #
+        # back to an unchanged TOS value
+        #
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 254)
+
+        #
+        # disable the egress map on pg1 and pg4
+        #
+        self.vapi.qos_mark_enable_disable(self.pg1.sw_if_index,
+                                          QOS_SOURCE.IP,
+                                          1,
+                                          0)
+        self.vapi.qos_mark_enable_disable(self.pg4.sw_if_index,
+                                          QOS_SOURCE.IP,
+                                          4,
+                                          0)
+
+        #
+        # unchanged Tos on pg1
+        #
+        rx = self.send_and_expect(self.pg0, p_v4 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[IP].tos, 254)
+
+        #
+        # clean-up the masp
+        #
+        self.vapi.qos_egress_map_delete(1)
+        self.vapi.qos_egress_map_delete(4)
+        self.vapi.qos_egress_map_delete(2)
+        self.vapi.qos_egress_map_delete(3)
+        self.vapi.qos_egress_map_delete(5)
+        self.vapi.qos_egress_map_delete(6)
+        self.vapi.qos_egress_map_delete(7)
+
+    def test_qos_mpls(self):
+        """ QoS Mark MPLS """
+
+        #
+        # 255 QoS for all input values
+        #
+        output = [chr(255)] * 256
+        os = ''.join(output)
+        rows = [{'outputs': os},
+                {'outputs': os},
+                {'outputs': os},
+                {'outputs': os}]
+
+        self.vapi.qos_egress_map_update(1, rows)
+
+        #
+        # a route with 1 MPLS label
+        #
+        route_10_0_0_1 = VppIpRoute(self, "10.0.0.1", 32,
+                                    [VppRoutePath(self.pg1.remote_ip4,
+                                                  self.pg1.sw_if_index,
+                                                  labels=[32])])
+        route_10_0_0_1.add_vpp_config()
+
+        #
+        # a route with 3 MPLS labels
+        #
+        route_10_0_0_3 = VppIpRoute(self, "10.0.0.3", 32,
+                                    [VppRoutePath(self.pg1.remote_ip4,
+                                                  self.pg1.sw_if_index,
+                                                  labels=[63, 33, 34])])
+        route_10_0_0_3.add_vpp_config()
+
+        #
+        # enable IP QoS recording on the input Pg0 and MPLS egress marking
+        # on Pg1
+        #
+        self.vapi.qos_record_enable_disable(self.pg0.sw_if_index,
+                                            QOS_SOURCE.IP,
+                                            1)
+        self.vapi.qos_mark_enable_disable(self.pg1.sw_if_index,
+                                          QOS_SOURCE.MPLS,
+                                          1,
+                                          1)
+
+        #
+        # packet that will get one label added and 3 labels added resp.
+        #
+        p_1 = (Ether(src=self.pg0.remote_mac, dst=self.pg0.local_mac) /
+               IP(src=self.pg0.remote_ip4, dst="10.0.0.1", tos=1) /
+               UDP(sport=1234, dport=1234) /
+               Raw(chr(100) * 65))
+        p_3 = (Ether(src=self.pg0.remote_mac, dst=self.pg0.local_mac) /
+               IP(src=self.pg0.remote_ip4, dst="10.0.0.3", tos=1) /
+               UDP(sport=1234, dport=1234) /
+               Raw(chr(100) * 65))
+
+        rx = self.send_and_expect(self.pg0, p_1 * 65, self.pg1)
+
+        #
+        # only 3 bits of ToS value in MPLS make sure tos is correct
+        # and the label and EOS bit have not been corrupted
+        #
+        for p in rx:
+            self.assertEqual(p[MPLS].cos, 7)
+            self.assertEqual(p[MPLS].label, 32)
+            self.assertEqual(p[MPLS].s, 1)
+        rx = self.send_and_expect(self.pg0, p_3 * 65, self.pg1)
+        for p in rx:
+            self.assertEqual(p[MPLS].cos, 7)
+            self.assertEqual(p[MPLS].label, 63)
+            self.assertEqual(p[MPLS].s, 0)
+            h = p[MPLS].payload
+            self.assertEqual(h[MPLS].cos, 7)
+            self.assertEqual(h[MPLS].label, 33)
+            self.assertEqual(h[MPLS].s, 0)
+            h = h[MPLS].payload
+            self.assertEqual(h[MPLS].cos, 7)
+            self.assertEqual(h[MPLS].label, 34)
+            self.assertEqual(h[MPLS].s, 1)
+
+        #
+        # cleanup
+        #
+        self.vapi.qos_record_enable_disable(self.pg0.sw_if_index,
+                                            QOS_SOURCE.IP,
+                                            0)
+        self.vapi.qos_mark_enable_disable(self.pg1.sw_if_index,
+                                          QOS_SOURCE.MPLS,
+                                          1,
+                                          0)
+        self.vapi.qos_egress_map_delete(1)
+
+
+if __name__ == '__main__':
+    unittest.main(testRunner=VppTestRunner)

src/plugins/nat/nat64.h
@@ -377,9 +377,6 @@ u32 nat64_get_worker_in2out (ip6_address_t * addr);
  */
 u32 nat64_get_worker_out2in (ip4_header_t * ip);
 
-#define u8_ptr_add(ptr, index) (((u8 *)ptr) + index)
-#define u16_net_add(u, val) clib_host_to_net_u16(clib_net_to_host_u16(u) + (val))
-
 #endif /* __included_nat64_h__ */
 
 /*

src/plugins/nat/nat66.h
@@ -75,9 +75,6 @@ int nat66_static_mapping_add_del (ip6_address_t * l_addr,
 				  ip6_address_t * e_addr, u32 vrf_id,
 				  u8 is_add);
 
-#define u8_ptr_add(ptr, index) (((u8 *)ptr) + index)
-#define u16_net_add(u, val) clib_host_to_net_u16(clib_net_to_host_u16(u) + (val))
-
 #endif /* __included_nat66_h__ */
 
 /*

src/vnet.am
@@ -348,7 +348,7 @@ libvnet_la_SOURCES +=				\
  vnet/ip/ip_frag.c				\
  vnet/ip/ip.c					\
  vnet/ip/ip_init.c				\
- vnet/ip/ip_in_out_acl.c				\
+ vnet/ip/ip_in_out_acl.c			\
  vnet/ip/lookup.c				\
  vnet/ip/ping.c					\
  vnet/ip/punt_api.c				\
@@ -1196,6 +1196,19 @@ libvnet_la_SOURCES +=                    \
   vnet/util/radix.c			     \
   vnet/util/refcount.c			 \
   vnet/util/trajectory.c
+
+########################################
+# QoS
+########################################
+
+libvnet_la_SOURCES +=      	                \
+  vnet/qos/qos_types.c				\
+  vnet/qos/qos_api.c				\
+  vnet/qos/qos_egress_map.c			\
+  vnet/qos/qos_record.c				\
+  vnet/qos/qos_mark.c
+
+API_FILES += vnet/qos/qos.api
 
 ########################################
 # BIER

src/vnet/buffer.c
@@ -38,6 +38,10 @@ format_vnet_buffer (u8 * s, va_list * args)
   if (b->flags & VNET_BUFFER_F_L4_HDR_OFFSET_VALID)
     a = format (a, "l4-hdr-offset %d ", vnet_buffer (b)->l4_hdr_offset);
 
+  if (b->flags & VNET_BUFFER_F_QOS_DATA_VALID)
+    a = format (a, "qos %d.%d ",
+		vnet_buffer2 (b)->qos.bits, vnet_buffer2 (b)->qos.source);
+
   s = format (s, "%U", format_vlib_buffer, b);
   if (a)
     s = format (s, "\n%U%v", format_white_space, indent, a);

src/vnet/buffer.h
@@ -63,7 +63,8 @@
   _(15, L3_HDR_OFFSET_VALID, 0)				\
   _(16, L4_HDR_OFFSET_VALID, 0)				\
   _(17, FLOW_REPORT, "flow-report")			\
-  _(18, IS_DVR, "dvr")
+  _(18, IS_DVR, "dvr")                                  \
+  _(19, QOS_DATA_VALID, 0)
 
 #define VNET_BUFFER_FLAGS_VLAN_BITS \
   (VNET_BUFFER_F_VLAN_1_DEEP | VNET_BUFFER_F_VLAN_2_DEEP)
@@ -186,6 +187,8 @@ typedef struct
       u8 ttl;
       u8 exp;
       u8 first;
+      /* Rewrite length */
+      u32 save_rewrite_length;
       /*
        * BIER - the nubmer of bytes in the header.
        *  the len field inthe header is not authoritative. It's the
@@ -362,16 +365,29 @@ STATIC_ASSERT (sizeof (vnet_buffer_opaque_t) <=
 /* Full cache line (64 bytes) of additional space */
 typedef struct
 {
+  /**
+   * QoS marking data that needs to persist from the recording nodes
+   * (nominally in the ingress path) to the marking node (in the
+   * egress path)
+   */
+  struct
+  {
+    u8 bits;
+    u8 source;
+  } qos;
+
+  u8 __unused[2];
+
   union
   {
-#if VLIB_BUFFER_TRACE_TRAJECTORY > 0
-    /* buffer trajectory tracing */
     struct
     {
+#if VLIB_BUFFER_TRACE_TRAJECTORY > 0
+      /* buffer trajectory tracing */
       u16 *trajectory_trace;
-    };
 #endif
-    u32 unused[12];
+    };
+    u32 unused[11];
   };
 } vnet_buffer_opaque2_t;
 

src/vnet/ip/ip.h
@@ -65,6 +65,9 @@
 #include <vnet/ip/icmp6.h>
 #include <vnet/classify/vnet_classify.h>
 
+#define u8_ptr_add(ptr, index) (((u8 *)ptr) + index)
+#define u16_net_add(u, val) clib_host_to_net_u16(clib_net_to_host_u16(u) + (val))
+
 /* Per protocol info. */
 typedef struct
 {

commit a5d7ced474568e8b94323f611d174b83c7126356

ethernet: change to mark the CFI bit in the L2 header.
Type: fix
Fixes: c8d8d8df

Signed-off-by: Prashant Maheshwari <pmahesh2@cisco.com>
Change-Id: Idca91c73758824688dd6ce61df994be66753d838

src/vnet/ethernet/packet.h
@@ -142,8 +142,8 @@ ethernet_vlan_header_set_priority_net_order (ethernet_vlan_header_t * h,
 {
   u8 *bytes = (u8 *) (&h->priority_cfi_and_id);
 
-  bytes[0] &= 0x1f;
-  bytes[0] |= (prio & 0x7) << 5;
+  bytes[0] &= 0x0f;
+  bytes[0] |= (prio & 0xf) << 4;
 }
 
 always_inline u8
@@ -151,7 +151,7 @@ ethernet_vlan_header_get_priority_net_order (ethernet_vlan_header_t * h)
 {
   u8 *bytes = (u8 *) (&h->priority_cfi_and_id);
 
-  return (bytes[0] >> 5);
+  return (bytes[0] >> 4);
 }
 
 /* VLAN with ethertype first and vlan id second */

test/test_dhcp.py
@@ -1653,7 +1653,7 @@ class TestDHCP(VppTestCase):
         rx = self.pg3.get_capture(1)
 
         self.assertEqual(rx[0][Dot1Q].vlan, 100)
-        self.assertEqual(rx[0][Dot1Q].prio, 4)
+        self.assertEqual(rx[0][Dot1Q].prio, 2)
 
         self.verify_orig_dhcp_discover(rx[0], self.pg3, hostname,
                                        dscp=vdscp.IP_API_DSCP_EF)

test/test_qos.py
@@ -515,15 +515,27 @@ class TestQOS(VppTestCase):
                 UDP(sport=1234, dport=1234) /
                 Raw(scapy.compat.chb(100) * NUM_PKTS))
 
+        p_v3 = (Ether(src=self.pg0.remote_mac, dst=self.pg0.local_mac) /
+                Dot1Q(vlan=11, prio=1, id=1) /
+                IP(src="1.1.1.1", dst="10.0.0.2", tos=2) /
+                UDP(sport=1234, dport=1234) /
+                Raw(scapy.compat.chb(100) * NUM_PKTS))
+
         rx = self.send_and_expect(self.pg1, p_v2 * NUM_PKTS, self.pg0)
 
         for p in rx:
-            self.assertEqual(p[Dot1Q].prio, 6)
+            self.assertEqual(p[Dot1Q].prio, 7)
+            self.assertEqual(p[Dot1Q].id, 0)
+
+        rx = self.send_and_expect(self.pg0, p_v3 * NUM_PKTS, self.pg1)
+
+        for p in rx:
+            self.assertEqual(p[IP].tos, 252)
 
         rx = self.send_and_expect(self.pg0, p_v1 * NUM_PKTS, self.pg1)
 
         for p in rx:
-            self.assertEqual(p[IP].tos, 254)
+            self.assertEqual(p[IP].tos, 253)
 
         p_v1 = (Ether(src=self.pg0.remote_mac, dst=self.pg0.local_mac) /
                 Dot1Q(vlan=11, prio=2) /
@@ -539,12 +551,13 @@ class TestQOS(VppTestCase):
         rx = self.send_and_expect(self.pg1, p_v2 * NUM_PKTS, self.pg0)
 
         for p in rx:
-            self.assertEqual(p[Dot1Q].prio, 6)
+            self.assertEqual(p[Dot1Q].prio, 7)
+            self.assertEqual(p[Dot1Q].id, 0)
 
         rx = self.send_and_expect(self.pg0, p_v1 * NUM_PKTS, self.pg1)
 
         for p in rx:
-            self.assertEqual(p[IPv6].tc, 253)
+            self.assertEqual(p[IPv6].tc, 251)
 
         #
         # cleanup