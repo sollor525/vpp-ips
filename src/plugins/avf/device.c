sollor525/vpp-ips

commit a16215d79572e63128df86c28d615b894bc888d1

avf: request queues support
Change-Id: I3a4529ff8dae70da99ec95c39ddf788c9498f478
Signed-off-by: Jakub Grajciar <jgrajcia@cisco.com>

src/plugins/avf/device.c
@@ -661,6 +661,46 @@ retry:
 }
 
 clib_error_t *
+avf_request_queues (vlib_main_t * vm, avf_device_t * ad, u16 num_queue_pairs)
+{
+  virtchnl_vf_res_request_t res_req = { 0 };
+  clib_error_t *error;
+  u32 rstat;
+  int n_retry = 20;
+
+  res_req.num_queue_pairs = num_queue_pairs;
+
+  error = avf_send_to_pf (vm, ad, VIRTCHNL_OP_REQUEST_QUEUES, &res_req,
+			  sizeof (virtchnl_vf_res_request_t), &res_req,
+			  sizeof (virtchnl_vf_res_request_t));
+
+  /*
+   * if PF respondes, the request failed
+   * else PF initializes restart and avf_send_to_pf returns an error
+   */
+  if (!error)
+    {
+      return clib_error_return (0, "requested more than %u queue pairs",
+				res_req.num_queue_pairs);
+    }
+
+retry:
+  vlib_process_suspend (vm, 10e-3);
+  rstat = avf_get_u32 (ad->bar0, AVFGEN_RSTAT);
+
+  if ((rstat == VIRTCHNL_VFR_COMPLETED) || (rstat == VIRTCHNL_VFR_VFACTIVE))
+    goto done;
+
+  if (--n_retry == 0)
+    return clib_error_return (0, "reset failed (timeout)");
+
+  goto retry;
+
+done:
+  return NULL;
+}
+
+clib_error_t *
 avf_device_init (vlib_main_t * vm, avf_device_t * ad,
 		 avf_create_if_args_t * args)
 {
@@ -672,8 +712,15 @@ avf_device_init (vlib_main_t * vm, avf_device_t * ad,
 
   avf_adminq_init (vm, ad);
 
-  if ((error = avf_device_reset (vm, ad)))
-    return error;
+  /* request more queues only if we need them */
+  if ((error = avf_request_queues (vm, ad, tm->n_vlib_mains)))
+    {
+      /* we failed to get more queues, but still we want to proceed */
+      clib_error_free (error);
+
+      if ((error = avf_device_reset (vm, ad)))
+	return error;
+    }
 
   avf_adminq_init (vm, ad);
 
@@ -767,6 +814,11 @@ avf_process_one_device (vlib_main_t * vm, avf_device_t * ad, int is_irq)
     return;
 
   ASSERT (ad->error == 0);
+
+  /* do not process device in reset state */
+  r = avf_get_u32 (ad->bar0, AVFGEN_RSTAT);
+  if (r != VIRTCHNL_VFR_VFACTIVE)
+    return;
 
   r = avf_get_u32 (ad->bar0, AVF_ARQLEN);
   if ((r & 0xf0000000) != (1ULL << 31))

src/plugins/avf/cli.c
@@ -104,7 +104,7 @@ avf_delete_command_fn (vlib_main_t * vm, unformat_input_t * input,
 
   hw = vnet_get_sup_hw_interface (vnm, sw_if_index);
   if (hw == NULL || avf_device_class.index != hw->dev_class_index)
-    return clib_error_return (0, "not a AVF interface");
+    return clib_error_return (0, "not an AVF interface");
 
   ad = pool_elt_at_index (am->devices, hw->dev_instance);
 

src/plugins/avf/virtchnl.h
@@ -132,6 +132,13 @@ typedef enum
   VIRTCHNL_VSI_SRIOV = 6,
 } virtchnl_vsi_type_t;
 
+typedef enum
+{
+  VIRTCHNL_VFR_INPROGRESS = 0,
+  VIRTCHNL_VFR_COMPLETED,
+  VIRTCHNL_VFR_VFACTIVE,
+} virtchnl_vfr_states_t;
+
 typedef struct
 {
   u16 vsi_id;
@@ -354,6 +361,12 @@ typedef struct
 } virtchnl_rss_lut_t;
 
 STATIC_ASSERT_SIZEOF (virtchnl_rss_lut_t, 6);
+
+/* VIRTCHNL_OP_REQUEST_QUEUES */
+typedef struct
+{
+  u16 num_queue_pairs;
+} virtchnl_vf_res_request_t;
 
 #endif /* AVF_VIRTCHNL_H */
 

commit da3ae7513b17abb781fcfc243d490b8dd3cf9cb3

avf: fix init when num_rx_queues > num_tx_queues
Type: fix
Fixes: a16215d

Change-Id: Id93b0f4d23ad7022e341e305bbd93ef2fff5787d
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/avf/device.c
@@ -661,11 +661,11 @@ avf_op_config_vsi_queues (vlib_main_t * vm, avf_device_t * ad)
 		     i, rxq->max_pkt_size, rxq->ring_len,
 		     rxq->databuffer_size, rxq->dma_ring_addr);
 
-      avf_txq_t *q = vec_elt_at_index (ad->txqs, i);
       txq->vsi_id = ad->vsi_id;
+      txq->queue_id = i;
       if (i < vec_len (ad->txqs))
 	{
-	  txq->queue_id = i;
+	  avf_txq_t *q = vec_elt_at_index (ad->txqs, i);
 	  txq->ring_len = q->size;
 	  txq->dma_ring_addr = avf_dma_addr (vm, ad, (void *) q->descs);
 	}
@@ -856,8 +856,8 @@ avf_device_init (vlib_main_t * vm, avf_main_t * am, avf_device_t * ad,
 
   avf_adminq_init (vm, ad);
 
-  /* request more queues only if we need them */
-  if ((error = avf_request_queues (vm, ad, tm->n_vlib_mains)))
+  if ((error = avf_request_queues (vm, ad, clib_max (tm->n_vlib_mains,
+						     args->rxq_num))))
     {
       /* we failed to get more queues, but still we want to proceed */
       clib_error_free (error);