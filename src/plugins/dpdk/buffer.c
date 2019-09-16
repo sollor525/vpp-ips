sollor525/vpp-ips

commit a4502ff1506db165a0d3b4ed2fdfdf408395faa4

buffers: major cleanup and improvements
This patch introduces following changes:
- deprecated free lists which are not used and not compatible
  with external buffer managers (i.e. DPDK)
- introduces native support for per-numa buffer pools
- significantly improves performance of buffer alloc and free

Change-Id: I4a8e723ae47056717afd6cac0efe87cb731b5be7
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vppinfra/error_bootstrap.h
@@ -91,6 +91,10 @@ do {							\
 #define STATIC_ASSERT_OFFSET_OF(s, e, o) \
   STATIC_ASSERT (STRUCT_OFFSET_OF(s,e) == o, "Offset of " #s "." #e " must be " # o)
 
+#define STATIC_ASSERT_FITS_IN(s, e, o) \
+  STATIC_ASSERT (STRUCT_OFFSET_OF(s,e) <= (o - sizeof(((s *)0)->e)), \
+  #s "." #e " does not fit into " # o " bytes")
+
 /* Assert without allocating memory. */
 #define ASSERT_AND_PANIC(truth)			\
 do {						\

src/plugins/avf/avf.h
@@ -102,6 +102,7 @@ typedef struct
   u32 *bufs;
   u16 n_enqueued;
   u8 int_mode;
+  u8 buffer_pool_index;
 } avf_rxq_t;
 
 typedef struct

src/plugins/avf/device.c
@@ -229,6 +229,9 @@ avf_rxq_init (vlib_main_t * vm, avf_device_t * ad, u16 qid, u16 rxq_size)
 						   2 * CLIB_CACHE_LINE_BYTES,
 						   ad->numa_node);
 
+  rxq->buffer_pool_index =
+    vlib_buffer_pool_get_default_for_numa (vm, ad->numa_node);
+
   if (rxq->descs == 0)
     return vlib_physmem_last_error (vm);
 
@@ -239,7 +242,8 @@ avf_rxq_init (vlib_main_t * vm, avf_device_t * ad, u16 qid, u16 rxq_size)
   vec_validate_aligned (rxq->bufs, rxq->size, CLIB_CACHE_LINE_BYTES);
   rxq->qrx_tail = ad->bar0 + AVF_QRX_TAIL (qid);
 
-  n_alloc = vlib_buffer_alloc (vm, rxq->bufs, rxq->size - 8);
+  n_alloc = vlib_buffer_alloc_from_pool (vm, rxq->bufs, rxq->size - 8,
+					 rxq->buffer_pool_index);
 
   if (n_alloc == 0)
     return clib_error_return (0, "buffer allocation error");

src/plugins/avf/input.c
@@ -72,7 +72,9 @@ avf_rxq_refill (vlib_main_t * vm, vlib_node_runtime_t * node, avf_rxq_t * rxq,
   slot = (rxq->next - n_refill - 1) & mask;
 
   n_refill &= ~7;		/* round to 8 */
-  n_alloc = vlib_buffer_alloc_to_ring (vm, rxq->bufs, slot, size, n_refill);
+  n_alloc =
+    vlib_buffer_alloc_to_ring_from_pool (vm, rxq->bufs, slot, size, n_refill,
+					 rxq->buffer_pool_index);
 
   if (PREDICT_FALSE (n_alloc != n_refill))
     {
@@ -368,6 +370,7 @@ no_more_desc:
 
   vnet_buffer (bt)->sw_if_index[VLIB_RX] = ad->sw_if_index;
   vnet_buffer (bt)->sw_if_index[VLIB_TX] = ~0;
+  bt->buffer_pool_index = rxq->buffer_pool_index;
 
   if (n_tail_desc)
     n_rx_bytes = avf_process_rx_burst (vm, node, ptd, n_rx_packets, 1);

src/plugins/dpdk/buffer.h
@@ -19,8 +19,10 @@
 #define rte_mbuf_from_vlib_buffer(x) (((struct rte_mbuf *)x) - 1)
 #define vlib_buffer_from_rte_mbuf(x) ((vlib_buffer_t *)(x+1))
 
-clib_error_t *dpdk_buffer_pool_create (vlib_main_t * vm, unsigned num_mbufs,
-				       unsigned socket_id);
+extern struct rte_mempool **dpdk_mempool_by_buffer_pool_index;
+extern struct rte_mempool **dpdk_no_cache_mempool_by_buffer_pool_index;
+
+clib_error_t *dpdk_buffer_pools_create (vlib_main_t * vm);
 
 #endif /* include_dpdk_buffer_h */
 

src/plugins/dpdk/device/cli.c
@@ -382,27 +382,27 @@ static clib_error_t *
 show_dpdk_buffer (vlib_main_t * vm, unformat_input_t * input,
 		  vlib_cli_command_t * cmd)
 {
-  struct rte_mempool *rmp;
-  int i;
+  vlib_buffer_main_t *bm = vm->buffer_main;
+  vlib_buffer_pool_t *bp;
 
-  for (i = 0; i < vec_len (dpdk_main.pktmbuf_pools); i++)
-    {
-      rmp = dpdk_main.pktmbuf_pools[i];
-      if (rmp)
-	{
-	  unsigned count = rte_mempool_avail_count (rmp);
-	  unsigned free_count = rte_mempool_in_use_count (rmp);
+  vec_foreach (bp, bm->buffer_pools)
+  {
+    struct rte_mempool *rmp = dpdk_mempool_by_buffer_pool_index[bp->index];
+    if (rmp)
+      {
+	unsigned count = rte_mempool_avail_count (rmp);
+	unsigned free_count = rte_mempool_in_use_count (rmp);
 
-	  vlib_cli_output (vm,
-			   "name=\"%s\"  available = %7d allocated = %7d total = %7d\n",
-			   rmp->name, (u32) count, (u32) free_count,
-			   (u32) (count + free_count));
-	}
-      else
-	{
-	  vlib_cli_output (vm, "rte_mempool is NULL (!)\n");
-	}
-    }
+	vlib_cli_output (vm,
+			 "name=\"%s\"  available = %7d allocated = %7d total = %7d\n",
+			 rmp->name, (u32) count, (u32) free_count,
+			 (u32) (count + free_count));
+      }
+    else
+      {
+	vlib_cli_output (vm, "rte_mempool is NULL (!)\n");
+      }
+  }
   return 0;
 }
 
@@ -2017,59 +2017,6 @@ VLIB_CLI_COMMAND (show_vpe_version_command, static) = {
   .function = show_dpdk_version_command_fn,
 };
 /* *INDENT-ON* */
-
-#if CLI_DEBUG
-
-static clib_error_t *
-dpdk_validate_buffers_fn (vlib_main_t * vm, unformat_input_t * input,
-			  vlib_cli_command_t * cmd_arg)
-{
-  u32 n_invalid_bufs = 0, uninitialized = 0;
-  u32 is_poison = 0, is_test = 0;
-  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
-    {
-      if (unformat (input, "poison"))
-	is_poison = 1;
-      else if (unformat (input, "trajectory"))
-	is_test = 1;
-      else
-	return clib_error_return (0, "unknown input `%U'",
-				  format_unformat_error, input);
-    }
-
-  if (VLIB_BUFFER_TRACE_TRAJECTORY == 0)
-    {
-      vlib_cli_output (vm, "Trajectory not enabled. Recompile with "
-		       "VLIB_BUFFER_TRACE_TRAJECTORY 1");
-      return 0;
-    }
-  if (is_poison)
-    {
-      dpdk_buffer_poison_trajectory_all ();
-    }
-  if (is_test)
-    {
-      n_invalid_bufs = dpdk_buffer_validate_trajectory_all (&uninitialized);
-      if (!n_invalid_bufs)
-	vlib_cli_output (vm, "All buffers are valid %d uninitialized",
-			 uninitialized);
-      else
-	vlib_cli_output (vm, "Found %d invalid buffers and %d uninitialized",
-			 n_invalid_bufs, uninitialized);
-    }
-  return 0;
-}
-
-/* *INDENT-OFF* */
-VLIB_CLI_COMMAND (test_dpdk_buffers_command, static) =
-{
-  .path = "test dpdk buffers",
-  .short_help = "test dpdk buffers [poison] [trajectory]",
-  .function = dpdk_validate_buffers_fn,
-};
-/* *INDENT-ON* */
-
-#endif
 
 clib_error_t *
 dpdk_cli_init (vlib_main_t * vm)

commit 7f0853908a43ae6792b8281fa67b106a9c996158

dpdk: initialize rte_mbuf during mempool dequeue
In some cases it may happen that buffer is allocated by DPDK, and freed
by VPP native code. In such cases dpdk metadata is not reset, so we need
to do that during mempool dequeue. Template approach is taken to reduce
cost of that operation.

Type: fix
Fixes: a4502ff

Change-Id: Ic239007cfc8fbceb965021c56963cda9d53f63be
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/dpdk/buffer.c
@@ -27,9 +27,11 @@
 STATIC_ASSERT (VLIB_BUFFER_PRE_DATA_SIZE == RTE_PKTMBUF_HEADROOM,
 	       "VLIB_BUFFER_PRE_DATA_SIZE must be equal to RTE_PKTMBUF_HEADROOM");
 
+extern struct rte_mbuf *dpdk_mbuf_template_by_pool_index;
 #ifndef CLIB_MARCH_VARIANT
 struct rte_mempool **dpdk_mempool_by_buffer_pool_index = 0;
 struct rte_mempool **dpdk_no_cache_mempool_by_buffer_pool_index = 0;
+struct rte_mbuf *dpdk_mbuf_template_by_pool_index = 0;
 
 clib_error_t *
 dpdk_buffer_pool_init (vlib_main_t * vm, vlib_buffer_pool_t * bp)
@@ -116,6 +118,14 @@ dpdk_buffer_pool_init (vlib_main_t * vm, vlib_buffer_pool_t * bp)
 
   /* call the object initializers */
   rte_mempool_obj_iter (mp, rte_pktmbuf_init, 0);
+
+  /* create mbuf header tempate from the first buffer in the pool */
+  vec_validate_aligned (dpdk_mbuf_template_by_pool_index, bp->index,
+			CLIB_CACHE_LINE_BYTES);
+  clib_memcpy (vec_elt_at_index (dpdk_mbuf_template_by_pool_index, bp->index),
+	       rte_mbuf_from_vlib_buffer (vlib_buffer_ptr_from_index
+					  (buffer_mem_start, *bp->buffers,
+					   0)), sizeof (struct rte_mbuf));
 
   /* *INDENT-OFF* */
   vec_foreach (bi, bp->buffers)
@@ -256,7 +266,6 @@ dpdk_ops_vpp_enqueue_no_cache_one (vlib_main_t * vm, struct rte_mempool *old,
   if (clib_atomic_sub_fetch (&b->ref_count, 1) == 0)
     {
       u32 bi = vlib_get_buffer_index (vm, b);
-      mb->pool = new;
       vlib_buffer_copy_template (b, bt);
       vlib_buffer_pool_put (vm, bt->buffer_pool_index, &bi, 1);
       return;
@@ -298,6 +307,37 @@ CLIB_MULTIARCH_FN (dpdk_ops_vpp_enqueue_no_cache) (struct rte_mempool * cmp,
 
 CLIB_MARCH_FN_REGISTRATION (dpdk_ops_vpp_enqueue_no_cache);
 
+static_always_inline void
+dpdk_mbuf_init_from_template (struct rte_mbuf **mba, struct rte_mbuf *mt,
+			      int count)
+{
+  /* Assumptions about rte_mbuf layout */
+  STATIC_ASSERT_OFFSET_OF (struct rte_mbuf, buf_addr, 0);
+  STATIC_ASSERT_OFFSET_OF (struct rte_mbuf, buf_iova, 8);
+  STATIC_ASSERT_SIZEOF_ELT (struct rte_mbuf, buf_iova, 8);
+  STATIC_ASSERT_SIZEOF_ELT (struct rte_mbuf, buf_iova, 8);
+  STATIC_ASSERT_SIZEOF (struct rte_mbuf, 128);
+
+  while (count--)
+    {
+      struct rte_mbuf *mb = mba[0];
+      int i;
+      /* bytes 0 .. 15 hold buf_addr and buf_iova which we need to preserve */
+      /* copy bytes 16 .. 31 */
+      *((u8x16 *) mb + 1) = *((u8x16 *) mt + 1);
+
+      /* copy bytes 32 .. 127 */
+#ifdef CLIB_HAVE_VEC256
+      for (i = 1; i < 4; i++)
+	*((u8x32 *) mb + i) = *((u8x32 *) mt + i);
+#else
+      for (i = 2; i < 8; i++)
+	*((u8x16 *) mb + i) = *((u8x16 *) mt + i);
+#endif
+      mba++;
+    }
+}
+
 int
 CLIB_MULTIARCH_FN (dpdk_ops_vpp_dequeue) (struct rte_mempool * mp,
 					  void **obj_table, unsigned n)
@@ -307,6 +347,7 @@ CLIB_MULTIARCH_FN (dpdk_ops_vpp_dequeue) (struct rte_mempool * mp,
   u32 bufs[batch_size], total = 0, n_alloc = 0;
   u8 buffer_pool_index = mp->pool_id;
   void **obj = obj_table;
+  struct rte_mbuf t = dpdk_mbuf_template_by_pool_index[buffer_pool_index];
 
   while (n >= batch_size)
     {
@@ -317,6 +358,7 @@ CLIB_MULTIARCH_FN (dpdk_ops_vpp_dequeue) (struct rte_mempool * mp,
 
       vlib_get_buffers_with_offset (vm, bufs, obj, batch_size,
 				    -(i32) sizeof (struct rte_mbuf));
+      dpdk_mbuf_init_from_template ((struct rte_mbuf **) obj, &t, batch_size);
       total += batch_size;
       obj += batch_size;
       n -= batch_size;
@@ -331,6 +373,7 @@ CLIB_MULTIARCH_FN (dpdk_ops_vpp_dequeue) (struct rte_mempool * mp,
 
       vlib_get_buffers_with_offset (vm, bufs, obj, n,
 				    -(i32) sizeof (struct rte_mbuf));
+      dpdk_mbuf_init_from_template ((struct rte_mbuf **) obj, &t, n);
     }
 
   return 0;

src/vppinfra/error_bootstrap.h
@@ -88,6 +88,9 @@ do {							\
 #define STATIC_ASSERT_SIZEOF(d, s) \
   STATIC_ASSERT (sizeof (d) == s, "Size of " #d " must be " # s " bytes")
 
+#define STATIC_ASSERT_SIZEOF_ELT(d, e, s) \
+  STATIC_ASSERT (sizeof (((d *)0)->e) == s, "Size of " #d "." #e " must be " # s " bytes")
+
 #define STATIC_ASSERT_OFFSET_OF(s, e, o) \
   STATIC_ASSERT (STRUCT_OFFSET_OF(s,e) == o, "Offset of " #s "." #e " must be " # o)
 