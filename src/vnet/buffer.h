sollor525/vpp-ips

commit 32a2ef2de5d2e4448916f450f8512f1b0897baef

ip: add shallow virtual reassembly functionality
Type: feature

Change-Id: Ibc8334e26c7e6f6120696c3e313b6e11d73dab99
Signed-off-by: Klement Sekera <ksekera@cisco.com>

src/vnet/buffer.h
@@ -185,13 +185,40 @@ typedef struct
 	/* reassembly */
 	union
 	{
-	  /* in/out variables */
+	  /* group input/handoff as handoff is done before input is consumed,
+	   * this way we can handoff while keeping input variables intact */
 	  struct
 	  {
-	    u32 next_index;	/* index of next node - used by custom apps */
-	    u32 error_next_index;	/* index of next node if error - used by custom apps */
-	    u16 estimated_mtu;	/* estimated MTU calculated during reassembly */
-	    u16 owner_thread_index;
+	    /* input variables */
+	    struct
+	    {
+	      u32 next_index;	/* index of next node - used by custom apps */
+	      u32 error_next_index;	/* index of next node if error - used by custom apps */
+	    };
+	    /* handoff variables */
+	    struct
+	    {
+	      u16 owner_thread_index;
+	    };
+	  };
+	  /* output variables */
+	  struct
+	  {
+	    union
+	    {
+	      /* shallow virtual reassembly output variables */
+	      struct
+	      {
+		u8 ip_proto;	/* protocol in ip header */
+		u16 l4_src_port;	/* tcp/udp/icmp src port */
+		u16 l4_dst_port;	/* tcp/udp/icmp dst port */
+	      };
+	      /* full reassembly output variables */
+	      struct
+	      {
+		u16 estimated_mtu;	/* estimated MTU calculated during reassembly */
+	      };
+	    };
 	  };
 	  /* internal variables used during reassembly */
 	  struct
@@ -202,11 +229,9 @@ typedef struct
 	    u16 range_last;
 	    u32 next_range_bi;
 	    u16 ip6_frag_hdr_offset;
-	    u16 owner_feature_thread_index;
 	  };
 	} reass;
       };
-
     } ip;
 
     /*

src/vnet/CMakeLists.txt
@@ -432,6 +432,7 @@ list(APPEND VNET_SOURCES
   ip/ip4_source_and_port_range_check.c
   ip/ip4_source_check.c
   ip/reass/ip4_full_reass.c
+  ip/reass/ip4_sv_reass.c
   ip/ip6_format.c
   ip/ip6_forward.c
   ip/ip6_ll_table.c
@@ -442,6 +443,7 @@ list(APPEND VNET_SOURCES
   ip/ip6_neighbor.c
   ip/ip6_pg.c
   ip/reass/ip6_full_reass.c
+  ip/reass/ip6_sv_reass.c
   ip/rd_cp.c
   ip/ip_neighbor.c
   ip/ip_api.c
@@ -464,6 +466,10 @@ list(APPEND VNET_MULTIARCH_SOURCES
   ip/reass/ip4_full_reass.c
   ip/ip6_hop_by_hop.c
   ip/reass/ip6_full_reass.c
+  ip/reass/ip4_sv_reass.c
+  ip/ip6_hop_by_hop.c
+  ip/reass/ip6_full_reass.c
+  ip/reass/ip6_sv_reass.c
   ip/ip6_input.c
   ip/ip6_punt_drop.c
   ip/punt_node.c

src/vnet/ip/ip.api
@@ -1016,6 +1016,12 @@ autoreply define ioam_disable
   u16 id;
 };
 
+enum ip_reass_type
+{
+  IP_REASS_TYPE_FULL = 0,
+  IP_REASS_TYPE_SHALLOW_VIRTUAL = 0x1,
+};
+
 autoreply define ip_reassembly_set
 {
   u32 client_index;
@@ -1025,6 +1031,7 @@ autoreply define ip_reassembly_set
   u32 max_reassembly_length;
   u32 expire_walk_interval_ms;
   u8 is_ip6;
+  vl_api_ip_reass_type_t type;
 };
 
 define ip_reassembly_get
@@ -1032,6 +1039,7 @@ define ip_reassembly_get
   u32 client_index;
   u32 context;
   u8 is_ip6;
+  vl_api_ip_reass_type_t type;
 };
 
 define ip_reassembly_get_reply
@@ -1059,6 +1067,7 @@ autoreply define ip_reassembly_enable_disable
   u32 sw_if_index;
   u8 enable_ip4;
   u8 enable_ip6;
+  vl_api_ip_reass_type_t type;
 };
 
 /*

src/vnet/ip/ip4_error.h
@@ -89,7 +89,8 @@
   _ (REASS_FRAGMENT_CHAIN_TOO_LONG, "fragment chain too long (drop)")   \
   _ (REASS_NO_BUF, "out of buffers (drop)")                             \
   _ (REASS_MALFORMED_PACKET, "malformed packets")                       \
-  _ (REASS_INTERNAL_ERROR, "drops due to internal reassembly error")
+  _ (REASS_INTERNAL_ERROR, "drops due to internal reassembly error")    \
+  _ (REASS_UNSUPP_IP_PROT, "unsupported ip protocol")
 
 typedef enum
 {

src/vnet/ip/ip6_error.h
@@ -84,7 +84,8 @@
   _ (REASS_FRAGMENT_CHAIN_TOO_LONG, "fragment chain too long (drop)")   \
   _ (REASS_NO_BUF, "out of buffers (drop)")                             \
   _ (REASS_TIMEOUT, "fragments dropped due to reassembly timeout")      \
-  _ (REASS_INTERNAL_ERROR, "drops due to internal reassembly error")
+  _ (REASS_INTERNAL_ERROR, "drops due to internal reassembly error")    \
+  _ (REASS_UNSUPP_IP_PROTO, "unsupported ip protocol")
 
 typedef enum
 {

src/vnet/ip/ip_api.c
@@ -43,7 +43,9 @@
 #include <vnet/fib/ip6_fib.h>
 #include <vnet/fib/fib_path_list.h>
 #include <vnet/ip/ip6_hop_by_hop.h>
+#include <vnet/ip/reass/ip4_sv_reass.h>
 #include <vnet/ip/reass/ip4_full_reass.h>
+#include <vnet/ip/reass/ip6_sv_reass.h>
 #include <vnet/ip/reass/ip6_full_reass.h>
 #include <vnet/ethernet/arp.h>
 #include <vnet/ip/ip_types_api.h>
@@ -2736,23 +2738,51 @@ vl_api_ip_reassembly_set_t_handler (vl_api_ip_reassembly_set_t * mp)
 {
   vl_api_ip_reassembly_set_reply_t *rmp;
   int rv = 0;
-  if (mp->is_ip6)
+  switch ((vl_api_ip_reass_type_t) clib_net_to_host_u32 (mp->type))
     {
-      rv = ip6_full_reass_set (clib_net_to_host_u32 (mp->timeout_ms),
-			       clib_net_to_host_u32 (mp->max_reassemblies),
-			       clib_net_to_host_u32
-			       (mp->max_reassembly_length),
-			       clib_net_to_host_u32
-			       (mp->expire_walk_interval_ms));
-    }
-  else
-    {
-      rv = ip4_full_reass_set (clib_net_to_host_u32 (mp->timeout_ms),
-			       clib_net_to_host_u32 (mp->max_reassemblies),
-			       clib_net_to_host_u32
-			       (mp->max_reassembly_length),
-			       clib_net_to_host_u32
-			       (mp->expire_walk_interval_ms));
+    case IP_REASS_TYPE_FULL:
+      if (mp->is_ip6)
+	{
+	  rv = ip6_full_reass_set (clib_net_to_host_u32 (mp->timeout_ms),
+				   clib_net_to_host_u32
+				   (mp->max_reassemblies),
+				   clib_net_to_host_u32
+				   (mp->max_reassembly_length),
+				   clib_net_to_host_u32
+				   (mp->expire_walk_interval_ms));
+	}
+      else
+	{
+	  rv = ip4_full_reass_set (clib_net_to_host_u32 (mp->timeout_ms),
+				   clib_net_to_host_u32
+				   (mp->max_reassemblies),
+				   clib_net_to_host_u32
+				   (mp->max_reassembly_length),
+				   clib_net_to_host_u32
+				   (mp->expire_walk_interval_ms));
+	}
+      break;
+    case IP_REASS_TYPE_SHALLOW_VIRTUAL:
+      if (mp->is_ip6)
+	{
+	  rv =
+	    ip6_sv_reass_set (clib_net_to_host_u32 (mp->timeout_ms),
+			      clib_net_to_host_u32 (mp->max_reassemblies),
+			      clib_net_to_host_u32
+			      (mp->max_reassembly_length),
+			      clib_net_to_host_u32
+			      (mp->expire_walk_interval_ms));
+	}
+      else
+	{
+	  rv = ip4_sv_reass_set (clib_net_to_host_u32 (mp->timeout_ms),
+				 clib_net_to_host_u32 (mp->max_reassemblies),
+				 clib_net_to_host_u32
+				 (mp->max_reassembly_length),
+				 clib_net_to_host_u32
+				 (mp->expire_walk_interval_ms));
+	}
+      break;
     }
 
   REPLY_MACRO (VL_API_IP_REASSEMBLY_SET_REPLY);
@@ -2772,19 +2802,42 @@ vl_api_ip_reassembly_get_t_handler (vl_api_ip_reassembly_get_t * mp)
   rmp->_vl_msg_id = ntohs (VL_API_IP_REASSEMBLY_GET_REPLY);
   rmp->context = mp->context;
   rmp->retval = 0;
-  if (mp->is_ip6)
+  u32 timeout_ms;
+  u32 max_reassemblies;
+  u32 max_reassembly_length;
+  u32 expire_walk_interval_ms;
+  switch ((vl_api_ip_reass_type_t) clib_net_to_host_u32 (mp->type))
     {
-      rmp->is_ip6 = 1;
-      ip6_full_reass_get (&rmp->timeout_ms, &rmp->max_reassemblies,
-			  &rmp->max_reassembly_length,
-			  &rmp->expire_walk_interval_ms);
-    }
-  else
-    {
-      rmp->is_ip6 = 0;
-      ip4_full_reass_get (&rmp->timeout_ms, &rmp->max_reassemblies,
-			  &rmp->max_reassembly_length,
-			  &rmp->expire_walk_interval_ms);
+    case IP_REASS_TYPE_FULL:
+      if (mp->is_ip6)
+	{
+	  rmp->is_ip6 = 1;
+	  ip6_full_reass_get (&timeout_ms, &max_reassemblies,
+			      &max_reassembly_length,
+			      &expire_walk_interval_ms);
+	}
+      else
+	{
+	  rmp->is_ip6 = 0;
+	  ip4_full_reass_get (&timeout_ms, &max_reassemblies,
+			      &max_reassembly_length,
+			      &expire_walk_interval_ms);
+	}
+      break;
+    case IP_REASS_TYPE_SHALLOW_VIRTUAL:
+      if (mp->is_ip6)
+	{
+	  rmp->is_ip6 = 1;
+	  ip6_sv_reass_get (&timeout_ms, &max_reassemblies,
+			    &max_reassembly_length, &expire_walk_interval_ms);
+	}
+      else
+	{
+	  rmp->is_ip6 = 0;
+	  ip4_sv_reass_get (&timeout_ms, &max_reassemblies,
+			    &max_reassembly_length, &expire_walk_interval_ms);
+	}
+      break;
     }
   rmp->timeout_ms = clib_host_to_net_u32 (rmp->timeout_ms);
   rmp->max_reassemblies = clib_host_to_net_u32 (rmp->max_reassemblies);
@@ -2799,13 +2852,28 @@ void
 {
   vl_api_ip_reassembly_enable_disable_reply_t *rmp;
   int rv = 0;
-  rv = ip4_full_reass_enable_disable (clib_net_to_host_u32 (mp->sw_if_index),
-				      mp->enable_ip4);
-  if (0 == rv)
+  switch ((vl_api_ip_reass_type_t) clib_net_to_host_u32 (mp->type))
     {
+    case IP_REASS_TYPE_FULL:
       rv =
-	ip6_full_reass_enable_disable (clib_net_to_host_u32 (mp->sw_if_index),
-				       mp->enable_ip6);
+	ip4_full_reass_enable_disable (clib_net_to_host_u32 (mp->sw_if_index),
+				       mp->enable_ip4);
+      if (0 == rv)
+	rv =
+	  ip6_full_reass_enable_disable (clib_net_to_host_u32
+					 (mp->sw_if_index), mp->enable_ip6);
+      break;
+    case IP_REASS_TYPE_SHALLOW_VIRTUAL:
+      rv =
+	ip4_sv_reass_enable_disable (clib_net_to_host_u32 (mp->sw_if_index),
+				     mp->enable_ip4);
+      if (0 == rv)
+	{
+	  rv =
+	    ip6_sv_reass_enable_disable (clib_net_to_host_u32
+					 (mp->sw_if_index), mp->enable_ip6);
+	}
+      break;
     }
 
   REPLY_MACRO (VL_API_IP_REASSEMBLY_ENABLE_DISABLE_REPLY);

src/vnet/ip/reass/ip4_full_reass.c
@@ -1146,12 +1146,8 @@ ip4_full_reass_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 	  if (PREDICT_FALSE (do_handoff))
 	    {
 	      next0 = IP4_FULL_REASS_NEXT_HANDOFF;
-	      if (is_feature)
-		vnet_buffer (b0)->ip.reass.owner_feature_thread_index =
-		  kv.v.memory_owner_thread_index;
-	      else
-		vnet_buffer (b0)->ip.reass.owner_thread_index =
-		  kv.v.memory_owner_thread_index;
+	      vnet_buffer (b0)->ip.reass.owner_thread_index =
+		kv.v.memory_owner_thread_index;
 	    }
 	  else if (reass)
 	    {
@@ -1166,12 +1162,8 @@ ip4_full_reass_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 		case IP4_REASS_RC_HANDOFF:
 		  next0 = IP4_FULL_REASS_NEXT_HANDOFF;
 		  b0 = vlib_get_buffer (vm, bi0);
-		  if (is_feature)
-		    vnet_buffer (b0)->ip.reass.owner_feature_thread_index =
-		      handoff_thread_idx;
-		  else
-		    vnet_buffer (b0)->ip.reass.owner_thread_index =
-		      handoff_thread_idx;
+		  vnet_buffer (b0)->ip.reass.owner_thread_index =
+		    handoff_thread_idx;
 		  break;
 		case IP4_REASS_RC_TOO_MANY_FRAGMENTS:
 		  vlib_node_increment_counter (vm, node->node_index,
@@ -1218,16 +1210,10 @@ ip4_full_reass_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
 		{
 		  if (PREDICT_FALSE (b0->flags & VLIB_BUFFER_IS_TRACED))
 		    {
-		      if (is_feature)
-			ip4_full_reass_add_trace (vm, node, rm, NULL,
-						  bi0, HANDOFF, 0,
-						  vnet_buffer (b0)->ip.
-						  reass.owner_feature_thread_index);
-		      else
-			ip4_full_reass_add_trace (vm, node, rm, NULL,
-						  bi0, HANDOFF, 0,
-						  vnet_buffer (b0)->ip.
-						  reass.owner_thread_index);
+		      ip4_full_reass_add_trace (vm, node, rm, NULL, bi0,
+						HANDOFF, 0,
+						vnet_buffer (b0)->ip.
+						reass.owner_thread_index);
 		    }
 		}
 	      else if (is_feature && IP4_ERROR_NONE == error0)
@@ -1726,10 +1712,7 @@ ip4_full_reass_handoff_node_inline (vlib_main_t * vm,
 
   while (n_left_from > 0)
     {
-      ti[0] =
-	(is_feature) ? vnet_buffer (b[0])->ip.
-	reass.owner_feature_thread_index : vnet_buffer (b[0])->ip.
-	reass.owner_thread_index;
+      ti[0] = vnet_buffer (b[0])->ip.reass.owner_thread_index;
 
       if (PREDICT_FALSE
 	  ((node->flags & VLIB_NODE_FLAG_TRACE)

commit 05c1cce0cd066e358b0931804a66408b773c7674

ip: reassembly: avoid corruption in vnet buffer
Avoid corrupt next_index in vnet_buffer by moving input and output
variables into different memory places instead of sharing a common
space.

Type: fix
Fixes: 32a2ef2de5d2e4448916f450f8512f1b0897baef

Change-Id: I34471fc6d0c8487535fac21349e688f398934f6d
Signed-off-by: Klement Sekera <ksekera@cisco.com>

src/vnet/buffer.h
@@ -185,8 +185,9 @@ typedef struct
 	/* reassembly */
 	union
 	{
-	  /* group input/handoff as handoff is done before input is consumed,
-	   * this way we can handoff while keeping input variables intact */
+	  /* group input/output/handoff to simplify the code, this way:
+	   * we can handoff while keeping input variables intact
+	   * and also we can write the output and still use next_index later */
 	  struct
 	  {
 	    /* input variables */
@@ -200,23 +201,23 @@ typedef struct
 	    {
 	      u16 owner_thread_index;
 	    };
-	  };
-	  /* output variables */
-	  struct
-	  {
-	    union
+	    /* output variables */
+	    struct
 	    {
-	      /* shallow virtual reassembly output variables */
-	      struct
+	      union
 	      {
-		u8 ip_proto;	/* protocol in ip header */
-		u16 l4_src_port;	/* tcp/udp/icmp src port */
-		u16 l4_dst_port;	/* tcp/udp/icmp dst port */
-	      };
-	      /* full reassembly output variables */
-	      struct
-	      {
-		u16 estimated_mtu;	/* estimated MTU calculated during reassembly */
+		/* shallow virtual reassembly output variables */
+		struct
+		{
+		  u8 ip_proto;	/* protocol in ip header */
+		  u16 l4_src_port;	/* tcp/udp/icmp src port */
+		  u16 l4_dst_port;	/* tcp/udp/icmp dst port */
+		};
+		/* full reassembly output variables */
+		struct
+		{
+		  u16 estimated_mtu;	/* estimated MTU calculated during reassembly */
+		};
 	      };
 	    };
 	  };