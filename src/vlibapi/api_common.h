FDio/vpp

api: Use vl_msg_push/pop_heap
Type: refactor

Change-Id: I0eb46676fc22ce6825b2d879498df344b5a855e8
Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

src/vlibapi/api_common.h
@@ -201,7 +201,9 @@ void vl_msg_api_set_first_available_msg_id (u16 first_avail);
 u16 vl_msg_api_get_msg_ids (const char *name, int n);
 u32 vl_msg_api_get_msg_index (u8 * name_and_crc);
 void *vl_msg_push_heap (void);
+void *vl_msg_push_heap_w_vrp (svm_region_t * vlib_rp);
 void vl_msg_pop_heap (void *oldheap);
+void vl_msg_pop_heap_w_vrp (svm_region_t * vlib_rp, void *oldheap);
 
 typedef clib_error_t *(vl_msg_api_init_function_t) (u32 client_index);
 

src/vlibmemory/memory_shared.c
@@ -162,15 +162,13 @@ vl_msg_api_alloc_internal (svm_region_t * vlib_rp, int nbytes, int pool,
    */
   am->ring_misses++;
 
-  pthread_mutex_lock (&vlib_rp->mutex);
-  oldheap = svm_push_data_heap (vlib_rp);
+  oldheap = vl_msg_push_heap_w_vrp (vlib_rp);
   if (may_return_null)
     {
       rv = clib_mem_alloc_or_null (nbytes);
       if (PREDICT_FALSE (rv == 0))
 	{
-	  svm_pop_heap (oldheap);
-	  pthread_mutex_unlock (&vlib_rp->mutex);
+	  vl_msg_pop_heap_w_vrp (vlib_rp, oldheap);
 	  return 0;
 	}
     }
@@ -179,8 +177,7 @@ vl_msg_api_alloc_internal (svm_region_t * vlib_rp, int nbytes, int pool,
 
   rv->q = 0;
   rv->gc_mark_timestamp = 0;
-  svm_pop_heap (oldheap);
-  pthread_mutex_unlock (&vlib_rp->mutex);
+  vl_msg_pop_heap_w_vrp (vlib_rp, oldheap);
 
 out:
 #if DEBUG_MESSAGE_BUFFER_OVERRUN > 0
@@ -293,8 +290,7 @@ vl_msg_api_free_w_vrp (svm_region_t * vlib_rp, void *a)
       return;
     }
 
-  pthread_mutex_lock (&vlib_rp->mutex);
-  oldheap = svm_push_data_heap (vlib_rp);
+  oldheap = vl_msg_push_heap_w_vrp (vlib_rp);
 
 #if DEBUG_MESSAGE_BUFFER_OVERRUN > 0
   {
@@ -305,8 +301,7 @@ vl_msg_api_free_w_vrp (svm_region_t * vlib_rp, void *a)
 #endif
 
   clib_mem_free (rv);
-  svm_pop_heap (oldheap);
-  pthread_mutex_unlock (&vlib_rp->mutex);
+  vl_msg_pop_heap_w_vrp (vlib_rp, oldheap);
 }
 
 void

sollor525/vpp-ips

src/vlibapi/api_common.h
@@ -201,7 +201,9 @@ void vl_msg_api_set_first_available_msg_id (u16 first_avail);
 u16 vl_msg_api_get_msg_ids (const char *name, int n);
 u32 vl_msg_api_get_msg_index (u8 * name_and_crc);
 void *vl_msg_push_heap (void);
+void *vl_msg_push_heap_w_region (svm_region_t * vlib_rp);
 void vl_msg_pop_heap (void *oldheap);
+void vl_msg_pop_heap_w_region (svm_region_t * vlib_rp, void *oldheap);
 
 typedef clib_error_t *(vl_msg_api_init_function_t) (u32 client_index);
 

src/vlibmemory/memory_shared.c
@@ -163,15 +163,13 @@ vl_msg_api_alloc_internal (svm_region_t * vlib_rp, int nbytes, int pool,
    */
   am->ring_misses++;
 
-  pthread_mutex_lock (&vlib_rp->mutex);
-  oldheap = svm_push_data_heap (vlib_rp);
+  oldheap = vl_msg_push_heap_w_region (vlib_rp);
   if (may_return_null)
     {
       rv = clib_mem_alloc_or_null (nbytes);
       if (PREDICT_FALSE (rv == 0))
 	{
-	  svm_pop_heap (oldheap);
-	  pthread_mutex_unlock (&vlib_rp->mutex);
+	  vl_msg_pop_heap_w_region (vlib_rp, oldheap);
 	  return 0;
 	}
     }
@@ -180,8 +178,7 @@ vl_msg_api_alloc_internal (svm_region_t * vlib_rp, int nbytes, int pool,
 
   rv->q = 0;
   rv->gc_mark_timestamp = 0;
-  svm_pop_heap (oldheap);
-  pthread_mutex_unlock (&vlib_rp->mutex);
+  vl_msg_pop_heap_w_region (vlib_rp, oldheap);
 
 out:
 #if DEBUG_MESSAGE_BUFFER_OVERRUN > 0
@@ -296,8 +293,7 @@ vl_msg_api_free_w_region (svm_region_t * vlib_rp, void *a)
       return;
     }
 
-  pthread_mutex_lock (&vlib_rp->mutex);
-  oldheap = svm_push_data_heap (vlib_rp);
+  oldheap = vl_msg_push_heap_w_region (vlib_rp);
 
 #if DEBUG_MESSAGE_BUFFER_OVERRUN > 0
   {
@@ -308,8 +304,7 @@ vl_msg_api_free_w_region (svm_region_t * vlib_rp, void *a)
 #endif
 
   clib_mem_free (rv);
-  svm_pop_heap (oldheap);
-  pthread_mutex_unlock (&vlib_rp->mutex);
+  vl_msg_pop_heap_w_region (vlib_rp, oldheap);
 }
 
 void