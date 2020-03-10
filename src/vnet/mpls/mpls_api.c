FDio/vpp

mpls: add user defined name tag to mpls tunnels
This allows a user creating MPLS tunnel through the bin_api to add a name tag. This is useful
to correlate the Tunnel with its use-case. Also useful if the user needs to recover the MPLS
Tunnel after a restart (mark-sweep).

Type: feature

Change-Id:

Signed-off-by: IJsbrand Wijnands <ice@cisco.com>
Change-Id: Idc080a63810a176ab090a2678a73d2cf9f7b523f

src/vnet/mpls/mpls_api.c
@@ -283,7 +283,8 @@ vl_api_mpls_tunnel_add_del_t_handler (vl_api_mpls_tunnel_add_del_t * mp)
       if (~0 == tunnel_sw_if_index)
 	tunnel_sw_if_index =
 	  vnet_mpls_tunnel_create (mp->mt_tunnel.mt_l2_only,
-				   mp->mt_tunnel.mt_is_multicast);
+				   mp->mt_tunnel.mt_is_multicast,
+				   mp->mt_tunnel.tag);
       vnet_mpls_tunnel_path_add (tunnel_sw_if_index, rpaths);
 
       tunnel_index = vnet_mpls_tunnel_get_index (tunnel_sw_if_index);
@@ -365,6 +366,7 @@ send_mpls_tunnel_entry (u32 mti, void *arg)
   mp->mt_tunnel.mt_tunnel_index = ntohl (mti);
   mp->mt_tunnel.mt_l2_only = ! !(MPLS_TUNNEL_FLAG_L2 & mt->mt_flags);
   mp->mt_tunnel.mt_is_multicast = ! !(MPLS_TUNNEL_FLAG_MCAST & mt->mt_flags);
+  memcpy (mp->mt_tunnel.tag, mt->tag, sizeof (mp->mt_tunnel.tag));
 
   fib_path_list_walk_w_ext (mt->mt_path_list,
 			    &mt->mt_path_exts, fib_path_encode, &path_ctx);

src/vnet/mpls/mpls_tunnel.c
@@ -606,7 +606,8 @@ vnet_mpls_tunnel_del (u32 sw_if_index)
 
 u32
 vnet_mpls_tunnel_create (u8 l2_only,
-                         u8 is_multicast)
+                         u8 is_multicast,
+                         u8 *tag)
 {
     vnet_hw_interface_t * hi;
     mpls_tunnel_t *mt;
@@ -625,6 +626,10 @@ vnet_mpls_tunnel_create (u8 l2_only,
         mt->mt_flags |= MPLS_TUNNEL_FLAG_MCAST;
     if (l2_only)
         mt->mt_flags |= MPLS_TUNNEL_FLAG_L2;
+    if (tag)
+        memcpy(mt->tag, tag, sizeof(mt->tag));
+    else
+        mt->tag[0] = '\0';
 
     /*
      * Create a new tunnel HW interface
@@ -858,7 +863,7 @@ vnet_create_mpls_tunnel_command_fn (vlib_main_t * vm,
 
         if (~0 == sw_if_index)
         {
-            sw_if_index = vnet_mpls_tunnel_create(l2_only, is_multicast);
+            sw_if_index = vnet_mpls_tunnel_create(l2_only, is_multicast, NULL);
         }
         vnet_mpls_tunnel_path_add(sw_if_index, rpaths);
     }

sollor525/vpp-ips

src/vnet/mpls/mpls_api.c
@@ -283,7 +283,8 @@ vl_api_mpls_tunnel_add_del_t_handler (vl_api_mpls_tunnel_add_del_t * mp)
       if (~0 == tunnel_sw_if_index)
 	tunnel_sw_if_index =
 	  vnet_mpls_tunnel_create (mp->mt_tunnel.mt_l2_only,
-				   mp->mt_tunnel.mt_is_multicast);
+				   mp->mt_tunnel.mt_is_multicast,
+				   mp->mt_tunnel.mt_tag);
       vnet_mpls_tunnel_path_add (tunnel_sw_if_index, rpaths);
 
       tunnel_index = vnet_mpls_tunnel_get_index (tunnel_sw_if_index);
@@ -365,6 +366,7 @@ send_mpls_tunnel_entry (u32 mti, void *arg)
   mp->mt_tunnel.mt_tunnel_index = ntohl (mti);
   mp->mt_tunnel.mt_l2_only = ! !(MPLS_TUNNEL_FLAG_L2 & mt->mt_flags);
   mp->mt_tunnel.mt_is_multicast = ! !(MPLS_TUNNEL_FLAG_MCAST & mt->mt_flags);
+  memcpy (mp->mt_tunnel.mt_tag, mt->mt_tag, sizeof (mp->mt_tunnel.mt_tag));
 
   fib_path_list_walk_w_ext (mt->mt_path_list,
 			    &mt->mt_path_exts, fib_path_encode, &path_ctx);

src/vnet/mpls/mpls_tunnel.c
@@ -606,7 +606,8 @@ vnet_mpls_tunnel_del (u32 sw_if_index)
 
 u32
 vnet_mpls_tunnel_create (u8 l2_only,
-                         u8 is_multicast)
+                         u8 is_multicast,
+                         u8 *tag)
 {
     vnet_hw_interface_t * hi;
     mpls_tunnel_t *mt;
@@ -625,6 +626,10 @@ vnet_mpls_tunnel_create (u8 l2_only,
         mt->mt_flags |= MPLS_TUNNEL_FLAG_MCAST;
     if (l2_only)
         mt->mt_flags |= MPLS_TUNNEL_FLAG_L2;
+    if (tag)
+        memcpy(mt->mt_tag, tag, sizeof(mt->mt_tag));
+    else
+        mt->mt_tag[0] = '\0';
 
     /*
      * Create a new tunnel HW interface
@@ -858,7 +863,7 @@ vnet_create_mpls_tunnel_command_fn (vlib_main_t * vm,
 
         if (~0 == sw_if_index)
         {
-            sw_if_index = vnet_mpls_tunnel_create(l2_only, is_multicast);
+            sw_if_index = vnet_mpls_tunnel_create(l2_only, is_multicast, NULL);
         }
         vnet_mpls_tunnel_path_add(sw_if_index, rpaths);
     }