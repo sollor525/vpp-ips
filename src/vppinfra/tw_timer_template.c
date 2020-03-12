sollor525/vpp-ips

commit 3ba4c59c3150164ca50fa9e2c384e05df91ac40f

vppinfra: fix corner cases in tw_timer_expire
Type: fix

Signed-off-by: Dave Barach <dave@barachs.net>
Change-Id: I4b3ff6e9c8e1d76037b168aeab36dcb5b4482260

src/vppinfra/tw_timer_template.c
@@ -513,7 +513,7 @@ static inline
   u32 slow_wheel_index __attribute__ ((unused));
   u32 glacier_wheel_index __attribute__ ((unused));
 
-  /* Shouldn't happen */
+  /* Called too soon to process new timer expirations? */
   if (PREDICT_FALSE (now < tw->next_run_time))
     return callback_vector_arg;
 
@@ -524,6 +524,27 @@ static inline
 
   /* Remember when we ran, compute next runtime */
   tw->next_run_time = (now + tw->timer_interval);
+
+  /* First call, or time jumped backwards? */
+  if (PREDICT_FALSE
+      ((tw->last_run_time == 0.0) || (now <= tw->last_run_time)))
+    {
+      tw->last_run_time = now;
+      return callback_vector_arg;
+    }
+
+  /*
+   * Refuse to do anything if we're about to process way too many slots.
+   * Should never come anywhere close to happening, with the possible exception
+   * of cases involving a large forward jump in the timebase.
+   */
+  if (nticks > (1 << (TW_RING_SHIFT + 1)))
+    {
+      clib_warning ("Excessive nticks %u at %.6f last run %.6f",
+		    nticks, now, tw->last_run_time);
+      tw->last_run_time = now;
+      return callback_vector_arg;
+    }
 
   if (callback_vector_arg == 0)
     {

commit ca3e0d43d5dac1cd62a5513f99042a0f89afd624

vppinfra: remove time jump workaround
A partial revert of gerrit 25729. The last_run_time == 0.0 check is
necessary and remains in place.

Type: fix
Fixes: 3ba4c59

Signed-off-by: Dave Barach <dave@barachs.net>
Change-Id: I3d2c9f90b2bc867f02c4749a5b19f997b84185b9

src/vppinfra/tw_timer_template.c
@@ -533,20 +533,6 @@ static inline
       return callback_vector_arg;
     }
 
-  /*
-   * Refuse to do anything if we're about to process way too many slots.
-   * Should never come anywhere close to happening, with the possible exception
-   * of cases involving a large forward jump in the timebase.
-   */
-  if (nticks > (1 << (TW_RING_SHIFT + 1)))
-    {
-      if (nticks / tw->ticks_per_second > 100e-3)
-	clib_warning ("Excessive nticks %u at %.6f last run %.6f",
-		      nticks, now, tw->last_run_time);
-      tw->last_run_time = now;
-      return callback_vector_arg;
-    }
-
   if (callback_vector_arg == 0)
     {
       _vec_len (tw->expired_timer_handles) = 0;