sollor525/vpp-ips

commit 0be7febbe339a29a551c1cda0286eab10a65c842

docs: clean up make docs job
Type: docs

Change-Id: I9b5e5137eb4c1e89f6e8d7a278cd11a0fd496471
Signed-off-by: Paul Vinciguerra <pvinci@vinciconsulting.com>

src/plugins/nat/nat.h
@@ -13,7 +13,8 @@
  * limitations under the License.
  */
 /**
- * @file NAT plugin global declarations
+ * @file nat.c
+ * NAT plugin global declarations
  */
 #ifndef __included_nat_h__
 #define __included_nat_h__
@@ -512,7 +513,7 @@ typedef u32 (snat_get_worker_out2in_function_t) (vlib_buffer_t * b,
 						 u32 rx_fib_index,
 						 u8 is_output);
 
-/* NAT address and port allacotaion function */
+/* NAT address and port allocation function */
 typedef int (nat_alloc_out_addr_and_port_function_t) (snat_address_t *
 						      addresses,
 						      u32 fib_index,
@@ -570,7 +571,7 @@ typedef struct snat_main_s
   /* vector of outside fibs */
   nat_outside_fib_t *outside_fibs;
 
-  /* Vector of twice NAT addresses for extenal hosts */
+  /* Vector of twice NAT addresses for external hosts */
   snat_address_t *twice_nat_addresses;
 
   /* sw_if_indices whose intfc addresses should be auto-added */
@@ -770,13 +771,13 @@ unformat_function_t unformat_snat_protocol;
 #define is_affinity_sessions(s) (s->flags & SNAT_SESSION_FLAG_AFFINITY)
 
 /** \brief Check if NAT interface is inside.
-    @param i NAT interfce
+    @param i NAT interface
     @return 1 if inside interface
 */
 #define nat_interface_is_inside(i) i->flags & NAT_INTERFACE_FLAG_IS_INSIDE
 
 /** \brief Check if NAT interface is outside.
-    @param i NAT interfce
+    @param i NAT interface
     @return 1 if outside interface
 */
 #define nat_interface_is_outside(i) i->flags & NAT_INTERFACE_FLAG_IS_OUTSIDE
@@ -1088,6 +1089,7 @@ int nat44_o2i_is_idle_session_cb (clib_bihash_kv_8_8_t * kv, void *arg);
 /**
  * @brief Add external address to NAT44 pool
  *
+ * @param sm        snat global configuration data
  * @param addr      IPv4 address
  * @param vrf_id    VRF id of tenant, ~0 means independent of VRF
  * @param twice_nat 1 if twice NAT address
@@ -1100,6 +1102,7 @@ int snat_add_address (snat_main_t * sm, ip4_address_t * addr, u32 vrf_id,
 /**
  * @brief Delete external address from NAT44 pool
  *
+ * @param sm        snat global configuration data
  * @param addr      IPv4 address
  * @param delete_sm 1 if delete static mapping using address
  * @param twice_nat 1 if twice NAT address
@@ -1206,11 +1209,11 @@ int snat_interface_add_del_output_feature (u32 sw_if_index, u8 is_inside,
 					   int is_del);
 
 /**
- * @brief Add/delete NAT44 pool address from specific interfce
+ * @brief Add/delete NAT44 pool address from specific interface
  *
  * @param sw_if_index software index of the interface
  * @param is_del      1 = delete, 0 = add
- * @param twice_nat   1 = twice NAT address for extenal hosts
+ * @param twice_nat   1 = twice NAT address for external hosts
  *
  * @return 0 on success, non-zero value otherwise
  */
@@ -1234,6 +1237,7 @@ int nat44_del_session (snat_main_t * sm, ip4_address_t * addr, u16 port,
 /**
  * @brief Delete NAT44 endpoint-dependent session
  *
+ * @param sm     snat global configuration data
  * @param addr   IPv4 address
  * @param port   L4 port number
  * @param proto  L4 protocol
@@ -1247,8 +1251,9 @@ int nat44_del_ed_session (snat_main_t * sm, ip4_address_t * addr, u16 port,
 			  u32 vrf_id, int is_in);
 
 /**
- * @brief Free NAT44 session data (lookup keys, external addrres port)
+ * @brief Free NAT44 session data (lookup keys, external address port)
  *
+ * @param sm           snat global configuration data
  * @param s            NAT session
  * @param thread_index thread index
  * @param is_ha        is HA event
@@ -1257,6 +1262,7 @@ void nat_free_session_data (snat_main_t * sm, snat_session_t * s,
 			    u32 thread_index, u8 is_ha);
 
 /**
+<<<<<<< bdfe5955f59a735fd8d70e9026f8c1867a4c8cc6
  * @brief Set NAT44 session limit (session limit, vrf id)
  *
  * @param session_limit Session limit
@@ -1267,6 +1273,9 @@ int nat44_set_session_limit (u32 session_limit, u32 vrf_id);
 
 /**
  * @brief Free NAT44 ED session data (lookup keys, external addrres port)
+=======
+ * @brief Free NAT44 ED session data (lookup keys, external address port)
+>>>>>>> docs: clean up make docs job
  *
  * @param s            NAT session
  * @param thread_index thread index
@@ -1293,6 +1302,7 @@ void nat44_db_free (snat_main_per_thread_data_t * tsm);
 /**
  * @brief Find or create NAT user
  *
+ * @param sm           snat global configuration data
  * @param addr         IPv4 address
  * @param fib_index    FIB table index
  * @param thread_index thread index
@@ -1306,8 +1316,10 @@ snat_user_t *nat_user_get_or_create (snat_main_t * sm,
 /**
  * @brief Allocate new NAT session or recycle last used
  *
+ * @param sm           snat global configuration data
  * @param u            NAT user
  * @param thread_index thread index
+ * @param now          time now
  *
  * @return session data structure on success otherwise zero value
  */
@@ -1318,7 +1330,9 @@ snat_session_t *nat_session_alloc_or_recycle (snat_main_t * sm,
 /**
  * @brief Allocate NAT endpoint-dependent session
  *
+ * @param sm           snat global configuration data
  * @param thread_index thread index
+ * @param now          time now
  *
  * @return session data structure on success otherwise zero value
  */
@@ -1366,7 +1380,7 @@ void snat_free_outside_address_and_port (snat_address_t * addresses,
  * @param fib_index         FIB table index
  * @param thread_index      thread index
  * @param k                 allocated address and port pair
- * @param port_per_thread   number of ports per threead
+ * @param port_per_thread   number of ports per thread
  * @param snat_thread_index NAT thread index
  *
  * @return 0 on success, non-zero value otherwise

extras/libmemif/doxygen.conf
@@ -2376,7 +2376,7 @@ PLANTUML_INCLUDE_PATH  =
 # Minimum value: 0, maximum value: 10000, default value: 50.
 # This tag requires that the tag HAVE_DOT is set to YES.
 
-DOT_GRAPH_MAX_NODES    = 50
+DOT_GRAPH_MAX_NODES    = 150
 
 # The MAX_DOT_GRAPH_DEPTH tag can be used to set the maximum depth of the graphs
 # generated by dot. A depth value of 3 means that only nodes reachable from the

extras/libmemif/src/libmemif.h
@@ -16,7 +16,7 @@
  */
 
 /** @file
- *  @defgroup libmemif
+ *  @defgroup libmemif Example libmemif App
  */
 
 #ifndef _LIBMEMIF_H_

src/plugins/flowprobe/node.c
@@ -26,7 +26,8 @@
 static void flowprobe_export_entry (vlib_main_t * vm, flowprobe_entry_t * e);
 
 /**
- * @file flow record generator graph node
+ * @file node.c
+ * flow record generator graph node
  */
 
 typedef struct

src/plugins/http_static/http_static.h
@@ -31,8 +31,9 @@
 #include <vppinfra/tw_timer_2t_1w_2048sl.h>
 #include <vppinfra/bihash_vec8_8.h>
 
-/** @file Static http server definitions
-*/
+/** @file http_static.h
+ * Static http server definitions
+ */
 
 typedef struct
 {

src/plugins/http_static/static_server.c
@@ -25,9 +25,10 @@
 
 #include <vppinfra/bihash_template.c>
 
-/** @file Static http server, sufficient to
-    serve .html / .css / .js content.
-*/
+/** @file static_server.c
+ *  Static http server, sufficient to
+ *  serve .html / .css / .js content.
+ */
 /*? %%clicmd:group_label Static HTTP Server %% ?*/
 
 http_static_server_main_t http_static_server_main;
@@ -299,6 +300,7 @@ static const char *http_response_template =
 /** \brief send http data
     @param hs - http session
     @param data - the data vector to transmit
+    @param length - length of data
     @param offset - transmit offset for this operation
     @return offset for next transmit operation, may be unchanged w/ full fifo
 */

src/plugins/igmp/igmp.c
@@ -303,13 +303,6 @@ error:
   return (rv);
 }
 
-/** \brief igmp hardware interface link up down
-    @param vnm - vnet main
-    @param hw_if_index - interface hw_if_index
-    @param flags - hw interface flags
-
-    If an interface goes down, remove its (S,G)s.
-*/
 static walk_rc_t
 igmp_sw_if_down (vnet_main_t * vnm, u32 sw_if_index, void *ctx)
 {
@@ -325,6 +318,13 @@ igmp_sw_if_down (vnet_main_t * vnm, u32 sw_if_index, void *ctx)
   return (WALK_CONTINUE);
 }
 
+/** \brief igmp hardware interface link up down
+    @param vnm - vnet main
+    @param hw_if_index - interface hw_if_index
+    @param flags - hw interface flags
+
+    If an interface goes down, remove its (S,G)s.
+*/
 static clib_error_t *
 igmp_hw_interface_link_up_down (vnet_main_t * vnm, u32 hw_if_index, u32 flags)
 {

src/plugins/igmp/igmp_api.h
@@ -33,11 +33,10 @@ int igmp_enable_disable (u32 sw_if_index, u8 enable, u8 mode);
 /**
  * @brief igmp listen (RFC3376 Section 2).
  * @param vm - vlib main
- * @param enable - 0 == remove (S,G), else add (S,G), aka. include/exclue
+ * @param enable - 0 == remove (S,G), else add (S,G), aka. include/exclude
  * @param sw_if_index - interface sw_if_index
  * @param saddr - source address
  * @param gaddr - group address
- * @param cli_api_configured - if zero, an igmp report has been received on interface
  *
  *   Add/del (S,G) on an interface. If user configured,
  *   send a status change report from the interface.

commit 4ed270723c143e1d00517c7a4fa5eac26db9c41f

docs: fix merge damage in nat.h
Type: fix
Fixes: 0be7febbe

Signed-off-by: Dave Wallace <dwallacelf@gmail.com>
Change-Id: I22d35159a923075243df9760f1a74e2ca74e30e1

src/plugins/nat/nat.h
@@ -1261,7 +1261,6 @@ void nat_free_session_data (snat_main_t * sm, snat_session_t * s,
 			    u32 thread_index, u8 is_ha);
 
 /**
-<<<<<<< bdfe5955f59a735fd8d70e9026f8c1867a4c8cc6
  * @brief Set NAT44 session limit (session limit, vrf id)
  *
  * @param session_limit Session limit
@@ -1271,10 +1270,7 @@ void nat_free_session_data (snat_main_t * sm, snat_session_t * s,
 int nat44_set_session_limit (u32 session_limit, u32 vrf_id);
 
 /**
- * @brief Free NAT44 ED session data (lookup keys, external addrres port)
-=======
  * @brief Free NAT44 ED session data (lookup keys, external address port)
->>>>>>> docs: clean up make docs job
  *
  * @param s            NAT session
  * @param thread_index thread index