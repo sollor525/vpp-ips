FDio/vpp

stats: counters data model
This adds a new data model for counters.
Specifying the errors severity and unit.
A later patch will update vpp_get_stats to take advantage of this.
Only the map plugin is updates as an example.

New .api language:
A new "counters" keyword to define counter sets.

counters map {
  none {
    severity info;
    type counter64;
    units "packets";
    description "valid MAP packets";
  };
  bad_protocol {
    severity error;
    type counter64;
    units "packets";
    description "bad protocol";
  };
};

Each counter has 4 keywords. severity, which is one of error, info or warn.
A type, which is one of counter64 or gauge64.
units, which is a text field using units from YANG.

paths {
  "/err/ip4-map" "map";
  "/err/ip6-map" "map";
  "/err/ip4-t-map" "map";
  "/err/ip6-t-map" "map";
};

A new paths keyword that maps the counter-set to a path in the stats segment KV store.

Updated VPP CLI to include severity so user can see error counter severity.
DBGvpp# show errors
   Count               Node                    Reason        Severity
        13        ethernet-input              no error         error

Type: feature
Signed-off-by: Ole Troan <ot@cisco.com>
Change-Id: Ib2177543f49d4c3aef4d7fa72476cff2068f7771

src/tools/vppapigen/vppapigen_c.py
@@ -516,6 +516,22 @@ def generate_include_enum(s, module, stream):
         write('}} vl_api_{}_enum_t;\n'.format(module))
 
 
+def generate_include_counters(s, module, stream):
+    write = stream.write
+
+    for counters in s:
+        csetname = counters.name
+        write('typedef enum {\n')
+        for c in counters.block:
+            write('   {}_ERROR_{},\n'
+                  .format(csetname.upper(), c['name'].upper()))
+        write('   {}_N_ERROR\n'.format(csetname.upper()))
+        write('}} vl_counter_{}_enum_t;\n'.format(csetname))
+
+        # write('extern char *{}_error_strings[];\n'.format(csetname))
+        # write('extern char *{}_description_strings[];\n'.format(csetname))
+        write('extern vl_counter_t {}_counters[];\n'.format(csetname))
+
 #
 # Generate separate API _types file.
 #
@@ -603,9 +619,10 @@ def generate_include_types(s, module, stream):
     write("\n#endif\n")
 
 
-def generate_c_boilerplate(services, defines, file_crc, module, stream):
+def generate_c_boilerplate(services, defines, counters, file_crc,
+                           module, stream):
     write = stream.write
-    define_hash = {d.name:d for d in defines}
+    define_hash = {d.name: d for d in defines}
 
     hdr = '''\
 #define vl_endianfun		/* define message structures */
@@ -661,6 +678,30 @@ def generate_c_boilerplate(services, defines, file_crc, module, stream):
     write('   return msg_id_base;\n')
     write('}\n')
 
+    severity = {'error': 'VL_COUNTER_SEVERITY_ERROR',
+                'info': 'VL_COUNTER_SEVERITY_INFO',
+                'warn': 'VL_COUNTER_SEVERITY_WARN'}
+
+    for cnt in counters:
+        csetname = cnt.name
+        '''
+        write('char *{}_error_strings[] = {{\n'.format(csetname))
+        for c in cnt.block:
+            write('   "{}",\n'.format(c['name']))
+        write('};\n')
+        write('char *{}_description_strings[] = {{\n'.format(csetname))
+        for c in cnt.block:
+            write('   "{}",\n'.format(c['description']))
+        write('};\n')
+        '''
+        write('vl_counter_t {}_counters[] = {{\n'.format(csetname))
+        for c in cnt.block:
+            write('  {\n')
+            write('   .name = "{}",\n'.format(c['name']))
+            write('   .desc = "{}",\n'.format(c['description']))
+            write('   .severity = {},\n'.format(severity[c['severity']]))
+            write('  },\n')
+        write('};\n')
 
 def generate_c_test_boilerplate(services, defines, file_crc, module, plugin, stream):
     write = stream.write
@@ -788,7 +829,11 @@ def run(args, input_filename, s):
 
     # Generate separate enum file
     st = StringIO()
+    st.write('#ifndef included_{}_api_enum_h\n'.format(modulename))
+    st.write('#define included_{}_api_enum_h\n'.format(modulename))
     generate_include_enum(s, modulename, st)
+    generate_include_counters(s['Counters'], modulename, st)
+    st.write('#endif\n')
     with open (filename_enum, 'w') as fd:
         st.seek (0)
         shutil.copyfileobj (st, fd)
@@ -796,8 +841,8 @@ def run(args, input_filename, s):
 
     # Generate separate C file
     st = StringIO()
-    generate_c_boilerplate(s['Service'], s['Define'], s['file_crc'],
-                           modulename, st)
+    generate_c_boilerplate(s['Service'], s['Define'], s['Counters'],
+                           s['file_crc'], modulename, st)
     with open (filename_c, 'w') as fd:
         st.seek (0)
         shutil.copyfileobj(st, fd)

src/vlib/error.c
@@ -115,7 +115,8 @@ vlib_error_drop_buffers (vlib_main_t * vm,
 /* Reserves given number of error codes for given node. */
 void
 vlib_register_errors (vlib_main_t * vm,
-		      u32 node_index, u32 n_errors, char *error_strings[])
+		      u32 node_index, u32 n_errors, char *error_strings[],
+		      vl_counter_t counters[])
 {
   vlib_error_main_t *em = &vm->error_main;
   vlib_node_main_t *nm = &vm->node_main;
@@ -128,21 +129,33 @@ vlib_register_errors (vlib_main_t * vm,
 
   /* Free up any previous error strings. */
   if (n->n_errors > 0)
-    heap_dealloc (em->error_strings_heap, n->error_heap_handle);
-
-  n->n_errors = n_errors;
-  n->error_strings = error_strings;
+    heap_dealloc (em->counters_heap, n->error_heap_handle);
 
   if (n_errors == 0)
     return;
 
+  n->n_errors = n_errors;
+
+  /*  Legacy node */
+  if (!counters)
+    {
+      counters = clib_mem_alloc (sizeof (counters[0]) * n_errors);
+      int i;
+      for (i = 0; i < n_errors; i++)
+	{
+	  counters[i].name = error_strings[i];	// XXX Make name saner
+	  counters[i].desc = error_strings[i];
+	  counters[i].severity = VL_COUNTER_SEVERITY_ERROR;
+	}
+    }
+
+  n->counters = counters;
+
   n->error_heap_index =
-    heap_alloc (em->error_strings_heap, n_errors, n->error_heap_handle);
-
-  l = vec_len (em->error_strings_heap);
-
-  clib_memcpy (vec_elt_at_index (em->error_strings_heap, n->error_heap_index),
-	       error_strings, n_errors * sizeof (error_strings[0]));
+    heap_alloc (em->counters_heap, n_errors, n->error_heap_handle);
+  l = vec_len (em->counters_heap);
+  clib_memcpy (vec_elt_at_index (em->counters_heap, n->error_heap_index),
+	       counters, n_errors * sizeof (counters[0]));
 
   vec_validate (vm->error_elog_event_types, l - 1);
 
@@ -170,7 +183,7 @@ vlib_register_errors (vlib_main_t * vm,
       {
 	vec_reset_length (error_name);
 	error_name =
-	  format (error_name, "/err/%v/%s%c", n->name, error_strings[i], 0);
+	  format (error_name, "/err/%v/%s%c", n->name, counters[i].name, 0);
 	vlib_stats_register_error_index (oldheap, error_name, em->counters,
 					 n->error_heap_index + i);
       }
@@ -191,11 +204,27 @@ vlib_register_errors (vlib_main_t * vm,
     for (i = 0; i < n_errors; i++)
       {
 	t.format = (char *) format (0, "%v %s: %%d",
-				    n->name, error_strings[i]);
+				    n->name, counters[i].name);
 	vm->error_elog_event_types[n->error_heap_index + i] = t;
 	nm->node_by_error[n->error_heap_index + i] = n->index;
       }
   }
+}
+
+static char *
+sev2str (enum vl_counter_severity_e s)
+{
+  switch (s)
+    {
+    case VL_COUNTER_SEVERITY_ERROR:
+      return "error";
+    case VL_COUNTER_SEVERITY_WARN:
+      return "warn";
+    case VL_COUNTER_SEVERITY_INFO:
+      return "info";
+    default:
+      return "unknown";
+    }
 }
 
 static clib_error_t *
@@ -218,10 +247,11 @@ show_errors (vlib_main_t * vm,
   vec_validate (sums, vec_len (em->counters));
 
   if (verbose)
-    vlib_cli_output (vm, "%=10s%=40s%=20s%=6s", "Count", "Node", "Reason",
-		     "Index");
+    vlib_cli_output (vm, "%=10s%=30s%=20s%=10s%=6s", "Count", "Node",
+		     "Reason", "Severity", "Index");
   else
-    vlib_cli_output (vm, "%=10s%=40s%=6s", "Count", "Node", "Reason");
+    vlib_cli_output (vm, "%=10s%=30s%=20s%=10s", "Count", "Node", "Reason",
+		     "Severity");
 
 
   /* *INDENT-OFF* */
@@ -247,11 +277,13 @@ show_errors (vlib_main_t * vm,
 	      continue;
 
             if (verbose)
-              vlib_cli_output (vm, "%10lu%=40v%=20s%=6d", c, n->name,
-                               em->error_strings_heap[i], i);
+              vlib_cli_output (vm, "%10lu%=30v%=20s%=10s%=6d", c, n->name,
+                               em->counters_heap[i].name,
+                               sev2str(em->counters_heap[i].severity), i);
             else
-              vlib_cli_output (vm, "%10lu%=40v%s", c, n->name,
-                               em->error_strings_heap[i]);
+              vlib_cli_output (vm, "%10lu%=30v%=20s%=10s", c, n->name,
+                               em->counters_heap[i].name,
+                               sev2str(em->counters_heap[i].severity));
 	  }
       }
     index++;
@@ -271,7 +303,7 @@ show_errors (vlib_main_t * vm,
 	    {
 	      if (verbose)
 		vlib_cli_output (vm, "%10lu%=40v%=20s%=10d", sums[i], n->name,
-				 em->error_strings_heap[i], i);
+				 em->counters_heap[i].name, i);
 	    }
 	}
     }

src/vlib/node.c
@@ -380,7 +380,8 @@ register_node (vlib_main_t * vm, vlib_node_registration_t * r)
   _(validate_frame);
 
   /* Register error counters. */
-  vlib_register_errors (vm, n->index, r->n_errors, r->error_strings);
+  vlib_register_errors (vm, n->index, r->n_errors, r->error_strings,
+			r->counters);
   node_elog_init (vm, n->index);
 
   _(runtime_data_bytes);

src/vlib/node.h
@@ -116,6 +116,7 @@ typedef struct _vlib_node_registration
 
   /* Error strings indexed by error code for this node. */
   char **error_strings;
+  vl_counter_t *counters;
 
   /* Buffer format/unformat for this node. */
   format_function_t *format_buffer;
@@ -323,8 +324,8 @@ typedef struct vlib_node_t
   u32 error_heap_handle;
   u32 error_heap_index;
 
-  /* Error strings indexed by error code for this node. */
-  char **error_strings;
+  /* Counter structures indexed by counter code for this node. */
+  vl_counter_t *counters;
 
   /* Vector of next node names.
      Only used before next_nodes array is initialized. */

sollor525/vpp-ips

src/tools/vppapigen/vppapigen_c.py
@@ -516,6 +516,22 @@ def generate_include_enum(s, module, stream):
         write('}} vl_api_{}_enum_t;\n'.format(module))
 
 
+def generate_include_counters(s, module, stream):
+    write = stream.write
+
+    for counters in s:
+        csetname = counters.name
+        write('typedef enum {\n')
+        for c in counters.block:
+            write('   {}_ERROR_{},\n'
+                  .format(csetname.upper(), c['name'].upper()))
+        write('   {}_N_ERROR\n'.format(csetname.upper()))
+        write('}} vl_counter_{}_enum_t;\n'.format(csetname))
+
+        # write('extern char *{}_error_strings[];\n'.format(csetname))
+        # write('extern char *{}_description_strings[];\n'.format(csetname))
+        write('extern vl_counter_t {}_error_counters[];\n'.format(csetname))
+
 #
 # Generate separate API _types file.
 #
@@ -603,9 +619,10 @@ def generate_include_types(s, module, stream):
     write("\n#endif\n")
 
 
-def generate_c_boilerplate(services, defines, file_crc, module, stream):
+def generate_c_boilerplate(services, defines, counters, file_crc,
+                           module, stream):
     write = stream.write
-    define_hash = {d.name:d for d in defines}
+    define_hash = {d.name: d for d in defines}
 
     hdr = '''\
 #define vl_endianfun		/* define message structures */
@@ -661,6 +678,30 @@ def generate_c_boilerplate(services, defines, file_crc, module, stream):
     write('   return msg_id_base;\n')
     write('}\n')
 
+    severity = {'error': 'VL_COUNTER_SEVERITY_ERROR',
+                'info': 'VL_COUNTER_SEVERITY_INFO',
+                'warn': 'VL_COUNTER_SEVERITY_WARN'}
+
+    for cnt in counters:
+        csetname = cnt.name
+        '''
+        write('char *{}_error_strings[] = {{\n'.format(csetname))
+        for c in cnt.block:
+            write('   "{}",\n'.format(c['name']))
+        write('};\n')
+        write('char *{}_description_strings[] = {{\n'.format(csetname))
+        for c in cnt.block:
+            write('   "{}",\n'.format(c['description']))
+        write('};\n')
+        '''
+        write('vl_counter_t {}_error_counters[] = {{\n'.format(csetname))
+        for c in cnt.block:
+            write('  {\n')
+            write('   .name = "{}",\n'.format(c['name']))
+            write('   .desc = "{}",\n'.format(c['description']))
+            write('   .severity = {},\n'.format(severity[c['severity']]))
+            write('  },\n')
+        write('};\n')
 
 def generate_c_test_boilerplate(services, defines, file_crc, module, plugin, stream):
     write = stream.write
@@ -788,7 +829,11 @@ def run(args, input_filename, s):
 
     # Generate separate enum file
     st = StringIO()
+    st.write('#ifndef included_{}_api_enum_h\n'.format(modulename))
+    st.write('#define included_{}_api_enum_h\n'.format(modulename))
     generate_include_enum(s, modulename, st)
+    generate_include_counters(s['Counters'], modulename, st)
+    st.write('#endif\n')
     with open (filename_enum, 'w') as fd:
         st.seek (0)
         shutil.copyfileobj (st, fd)
@@ -796,8 +841,8 @@ def run(args, input_filename, s):
 
     # Generate separate C file
     st = StringIO()
-    generate_c_boilerplate(s['Service'], s['Define'], s['file_crc'],
-                           modulename, st)
+    generate_c_boilerplate(s['Service'], s['Define'], s['Counters'],
+                           s['file_crc'], modulename, st)
     with open (filename_c, 'w') as fd:
         st.seek (0)
         shutil.copyfileobj(st, fd)

src/vlib/error.c
@@ -115,7 +115,8 @@ vlib_error_drop_buffers (vlib_main_t * vm,
 /* Reserves given number of error codes for given node. */
 void
 vlib_register_errors (vlib_main_t * vm,
-		      u32 node_index, u32 n_errors, char *error_strings[])
+		      u32 node_index, u32 n_errors, char *error_strings[],
+		      vl_counter_t counters[])
 {
   vlib_error_main_t *em = &vm->error_main;
   vlib_node_main_t *nm = &vm->node_main;
@@ -128,21 +129,33 @@ vlib_register_errors (vlib_main_t * vm,
 
   /* Free up any previous error strings. */
   if (n->n_errors > 0)
-    heap_dealloc (em->error_strings_heap, n->error_heap_handle);
-
-  n->n_errors = n_errors;
-  n->error_strings = error_strings;
+    heap_dealloc (em->counters_heap, n->error_heap_handle);
 
   if (n_errors == 0)
     return;
 
+  n->n_errors = n_errors;
+
+  /*  Legacy node */
+  if (!counters)
+    {
+      counters = clib_mem_alloc (sizeof (counters[0]) * n_errors);
+      int i;
+      for (i = 0; i < n_errors; i++)
+	{
+	  counters[i].name = error_strings[i];	// XXX Make name saner
+	  counters[i].desc = error_strings[i];
+	  counters[i].severity = VL_COUNTER_SEVERITY_ERROR;
+	}
+    }
+
+  n->error_counters = counters;
+
   n->error_heap_index =
-    heap_alloc (em->error_strings_heap, n_errors, n->error_heap_handle);
-
-  l = vec_len (em->error_strings_heap);
-
-  clib_memcpy (vec_elt_at_index (em->error_strings_heap, n->error_heap_index),
-	       error_strings, n_errors * sizeof (error_strings[0]));
+    heap_alloc (em->counters_heap, n_errors, n->error_heap_handle);
+  l = vec_len (em->counters_heap);
+  clib_memcpy (vec_elt_at_index (em->counters_heap, n->error_heap_index),
+	       counters, n_errors * sizeof (counters[0]));
 
   vec_validate (vm->error_elog_event_types, l - 1);
 
@@ -170,7 +183,7 @@ vlib_register_errors (vlib_main_t * vm,
       {
 	vec_reset_length (error_name);
 	error_name =
-	  format (error_name, "/err/%v/%s%c", n->name, error_strings[i], 0);
+	  format (error_name, "/err/%v/%s%c", n->name, counters[i].name, 0);
 	vlib_stats_register_error_index (oldheap, error_name, em->counters,
 					 n->error_heap_index + i);
       }
@@ -191,11 +204,27 @@ vlib_register_errors (vlib_main_t * vm,
     for (i = 0; i < n_errors; i++)
       {
 	t.format = (char *) format (0, "%v %s: %%d",
-				    n->name, error_strings[i]);
+				    n->name, counters[i].name);
 	vm->error_elog_event_types[n->error_heap_index + i] = t;
 	nm->node_by_error[n->error_heap_index + i] = n->index;
       }
   }
+}
+
+static char *
+sev2str (enum vl_counter_severity_e s)
+{
+  switch (s)
+    {
+    case VL_COUNTER_SEVERITY_ERROR:
+      return "error";
+    case VL_COUNTER_SEVERITY_WARN:
+      return "warn";
+    case VL_COUNTER_SEVERITY_INFO:
+      return "info";
+    default:
+      return "unknown";
+    }
 }
 
 static clib_error_t *
@@ -218,10 +247,11 @@ show_errors (vlib_main_t * vm,
   vec_validate (sums, vec_len (em->counters));
 
   if (verbose)
-    vlib_cli_output (vm, "%=10s%=40s%=20s%=6s", "Count", "Node", "Reason",
-		     "Index");
+    vlib_cli_output (vm, "%=10s%=30s%=20s%=10s%=6s", "Count", "Node",
+		     "Reason", "Severity", "Index");
   else
-    vlib_cli_output (vm, "%=10s%=40s%=6s", "Count", "Node", "Reason");
+    vlib_cli_output (vm, "%=10s%=30s%=20s%=10s", "Count", "Node", "Reason",
+		     "Severity");
 
 
   /* *INDENT-OFF* */
@@ -247,11 +277,13 @@ show_errors (vlib_main_t * vm,
 	      continue;
 
             if (verbose)
-              vlib_cli_output (vm, "%10lu%=40v%=20s%=6d", c, n->name,
-                               em->error_strings_heap[i], i);
+              vlib_cli_output (vm, "%10lu%=30v%=20s%=10s%=6d", c, n->name,
+                               em->counters_heap[i].name,
+                               sev2str(em->counters_heap[i].severity), i);
             else
-              vlib_cli_output (vm, "%10lu%=40v%s", c, n->name,
-                               em->error_strings_heap[i]);
+              vlib_cli_output (vm, "%10lu%=30v%=20s%=10s", c, n->name,
+                               em->counters_heap[i].name,
+                               sev2str(em->counters_heap[i].severity));
 	  }
       }
     index++;
@@ -271,7 +303,7 @@ show_errors (vlib_main_t * vm,
 	    {
 	      if (verbose)
 		vlib_cli_output (vm, "%10lu%=40v%=20s%=10d", sums[i], n->name,
-				 em->error_strings_heap[i], i);
+				 em->counters_heap[i].name, i);
 	    }
 	}
     }

src/vlib/node.c
@@ -380,7 +380,8 @@ register_node (vlib_main_t * vm, vlib_node_registration_t * r)
   _(validate_frame);
 
   /* Register error counters. */
-  vlib_register_errors (vm, n->index, r->n_errors, r->error_strings);
+  vlib_register_errors (vm, n->index, r->n_errors, r->error_strings,
+			r->error_counters);
   node_elog_init (vm, n->index);
 
   _(runtime_data_bytes);

src/vlib/node.h
@@ -116,6 +116,7 @@ typedef struct _vlib_node_registration
 
   /* Error strings indexed by error code for this node. */
   char **error_strings;
+  vl_counter_t *error_counters;
 
   /* Buffer format/unformat for this node. */
   format_function_t *format_buffer;
@@ -323,8 +324,8 @@ typedef struct vlib_node_t
   u32 error_heap_handle;
   u32 error_heap_index;
 
-  /* Error strings indexed by error code for this node. */
-  char **error_strings;
+  /* Counter structures indexed by counter code for this node. */
+  vl_counter_t *error_counters;
 
   /* Vector of next node names.
      Only used before next_nodes array is initialized. */