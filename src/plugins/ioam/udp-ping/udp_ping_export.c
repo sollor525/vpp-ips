FDio/vpp

misc: minimize dependencies on udp.h
Type: improvement

Signed-off-by: Florin Coras <fcoras@cisco.com>
Change-Id: Id13f33843b230a1d169560742c4f7b2dc17d8718

src/plugins/ioam/udp-ping/udp_ping_export.c
@@ -16,6 +16,7 @@
 #include <vnet/ipfix-export/flow_report.h>
 #include <ioam/analyse/ioam_summary_export.h>
 #include <vnet/api_errno.h>
+#include <vnet/udp/udp.h>
 #include <ioam/udp-ping/udp_ping.h>
 
 #define UDP_PING_EXPORT_RECORD_SIZE 400

src/plugins/lisp/lisp-gpe/lisp_gpe_adjacency.c
@@ -26,6 +26,7 @@
 #include <lisp/lisp-gpe/lisp_gpe_tunnel.h>
 #include <vnet/fib/fib_entry.h>
 #include <vnet/adj/adj_midchain.h>
+#include <vnet/udp/udp.h>
 #include <vppinfra/bihash_24_8.h>
 #include <vppinfra/bihash_template.h>
 

src/plugins/nat/lib/ipfix_logging.c
@@ -16,6 +16,7 @@
  */
 
 #include <vnet/ipfix-export/flow_report.h>
+#include <vnet/udp/udp.h>
 #include <vlibmemory/api.h>
 #include <vppinfra/atomics.h>
 #include <nat/lib/ipfix_logging.h>

src/vnet/ipfix-export/flow_report_classify.c
@@ -16,6 +16,7 @@
 #include <vnet/ipfix-export/flow_report_classify.h>
 #include <vnet/api_errno.h>
 #include <vnet/classify/vnet_classify.h>
+#include <vnet/udp/udp.h>
 
 /* Common prefix of tcp and udp headers
  * containing only source and destination port fields */

src/vnet/vxlan-gpe/encap.c
@@ -22,6 +22,7 @@
 #include <vnet/vnet.h>
 #include <vnet/ip/ip.h>
 #include <vnet/ethernet/ethernet.h>
+#include <vnet/udp/udp.h>
 #include <vnet/vxlan-gpe/vxlan_gpe.h>
 
 /** Statistics (not really errors) */

sollor525/vpp-ips

src/plugins/ioam/udp-ping/udp_ping_export.c
@@ -16,6 +16,8 @@
 #include <vnet/ipfix-export/flow_report.h>
 #include <ioam/analyse/ioam_summary_export.h>
 #include <vnet/api_errno.h>
+#include <vnet/ip/ip4.h>
+#include <vnet/udp/udp_local.h>
 #include <ioam/udp-ping/udp_ping.h>
 
 #define UDP_PING_EXPORT_RECORD_SIZE 400

src/plugins/lisp/lisp-gpe/lisp_gpe_adjacency.c
@@ -26,6 +26,7 @@
 #include <lisp/lisp-gpe/lisp_gpe_tunnel.h>
 #include <vnet/fib/fib_entry.h>
 #include <vnet/adj/adj_midchain.h>
+#include <vnet/udp/udp_inlines.h>
 #include <vppinfra/bihash_24_8.h>
 #include <vppinfra/bihash_template.h>
 

src/plugins/nat/lib/ipfix_logging.c
@@ -16,6 +16,8 @@
  */
 
 #include <vnet/ipfix-export/flow_report.h>
+#include <vnet/ip/ip4.h>
+#include <vnet/udp/udp_local.h>
 #include <vlibmemory/api.h>
 #include <vppinfra/atomics.h>
 #include <nat/lib/ipfix_logging.h>

src/vnet/ipfix-export/flow_report_classify.c
@@ -16,6 +16,8 @@
 #include <vnet/ipfix-export/flow_report_classify.h>
 #include <vnet/api_errno.h>
 #include <vnet/classify/vnet_classify.h>
+#include <vnet/ip/ip4.h>
+#include <vnet/udp/udp_local.h>
 
 /* Common prefix of tcp and udp headers
  * containing only source and destination port fields */

src/vnet/vxlan-gpe/encap.c
@@ -22,6 +22,7 @@
 #include <vnet/vnet.h>
 #include <vnet/ip/ip.h>
 #include <vnet/ethernet/ethernet.h>
+#include <vnet/udp/udp_inlines.h>
 #include <vnet/vxlan-gpe/vxlan_gpe.h>
 
 /** Statistics (not really errors) */