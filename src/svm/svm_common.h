sollor525/vpp-ips

commit 05a314ccfaf51d4cb03ed44e96dba6ac6ffeaf3f

svm: change high_seg_baseva
Type: fix

It appears that the session layer baseva is conflicting
with DPDK. 0x2000000000 seem to be working experimentally
Configurable with :

session {
  segment-baseva 0x2000000000
}

Change-Id: Ie93b9b9eb56a796877d614f78eb5801ea8e5ab31
Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

src/svm/svm_common.h
@@ -82,9 +82,9 @@ typedef struct svm_map_region_args_
  */
 #if __WORDSIZE == 64
 #ifdef CLIB_SANITIZE_ADDR
-#define HIGH_SEGMENT_BASEVA 0x300000000000
+#define HIGH_SEGMENT_BASEVA 0x2000000000
 #else /* CLIB_SANITIZE_ADDR */
-#define HIGH_SEGMENT_BASEVA (8ULL   << 30)	/* 8GB */
+#define HIGH_SEGMENT_BASEVA (128ULL << 30)	/* 128GB */
 #endif /* CLIB_SANITIZE_ADDR */
 #elif __WORDSIZE == 32
 #define HIGH_SEGMENT_BASEVA (3584UL << 20)	/* 3.5GB */

commit 540c52c20375a7d08d4fb29b4ea78be3897e5d15

svm: fix high segment base address for ASan
AddressSanitizer has specific requirements on memory map. In particular,
the range [0x00007fff8000, 0x10007fff7fff] is reserved for ASan use.

Type: fix
Fixes: 05a314ccfaf51d4cb03ed44e96dba6ac6ffeaf3f

Change-Id: Ie96c4dd88b4f02d7fc5c24464572ff72cb6fd96b
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/svm/svm_common.h
@@ -82,7 +82,7 @@ typedef struct svm_map_region_args_
  */
 #if __WORDSIZE == 64
 #ifdef CLIB_SANITIZE_ADDR
-#define HIGH_SEGMENT_BASEVA 0x2000000000
+#define HIGH_SEGMENT_BASEVA 0x300000000000	/* DO NOT CHANGE THIS: YOU'LL BREAK ASAN */
 #else /* CLIB_SANITIZE_ADDR */
 #define HIGH_SEGMENT_BASEVA (128ULL << 30)	/* 128GB */
 #endif /* CLIB_SANITIZE_ADDR */