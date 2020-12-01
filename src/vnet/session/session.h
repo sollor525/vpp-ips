FDio/vpp

session: make pacer worker load aware
Type: improvement

Signed-off-by: Florin Coras <fcoras@cisco.com>
Change-Id: I71b9d54311fcad808fcdaad0df2dca8c161d580e

src/vnet/session/session.h
@@ -549,6 +549,12 @@ transport_us_time_now (u32 thread_index)
   return session_main.wrk[thread_index].last_vlib_us_time;
 }
 
+always_inline clib_us_time_t
+transport_seconds_per_loop (u32 thread_index)
+{
+  return session_main.wrk[thread_index].vm->seconds_per_loop;
+}
+
 always_inline void
 transport_add_tx_event (transport_connection_t * tc)
 {

sollor525/vpp-ips

src/vnet/session/session.h
@@ -552,6 +552,12 @@ transport_us_time_now (u32 thread_index)
   return session_main.wrk[thread_index].last_vlib_us_time;
 }
 
+always_inline clib_time_type_t
+transport_seconds_per_loop (u32 thread_index)
+{
+  return session_main.wrk[thread_index].vm->seconds_per_loop;
+}
+
 always_inline void
 transport_add_tx_event (transport_connection_t * tc)
 {