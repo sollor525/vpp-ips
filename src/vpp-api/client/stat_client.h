sollor525/vpp-ips

commit d3785f91203cfbd40c97a4200a9aa5ffca967a4a

stats: void-pointer arithmetic fix
Cast to (char *) to avoid pointer arithmetic for (void *) pointers.

Type: fix

Signed-off-by: Elias Rudberg <elias.rudberg@bahnhof.net>
Change-Id: I62607b4cbc553449e2c60d514b4b17dbb4f88216

src/vpp-api/client/stat_client.h
@@ -103,8 +103,9 @@ stat_segment_adjust (stat_client_main_t * sm, void *data)
 {
   void *p = (void *) ((char *) sm->shared_header +
 		      ((char *) data - (char *) sm->shared_header->base));
-  if (p > (void *) sm->shared_header &&
-      ((p + sizeof (p)) < ((void *) sm->shared_header + sm->memory_size)))
+  if ((char *) p > (char *) sm->shared_header &&
+      (((char *) p + sizeof (p)) <
+       ((char *) sm->shared_header + sm->memory_size)))
     return p;
   return 0;
 }

commit 25803838f4a89096b8ae7e120957c7e238bd4b23

stats: char-pointer arithmetic style-up
Char* typed shared_header value appears multiple times,
so store it is a variable.
Pointer "p" value is used more frequently casted to char*,
so use that type for it, and convert to void* only at return.

Type: style
Fixes: d3785f91203cfbd40c97a4200a9aa5ffca967a4a

Change-Id: I7b3f1e6a43c020acd7ae561f87d12237b07fcd66
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/vpp-api/client/stat_client.h
@@ -101,12 +101,10 @@ _time_now_nsec (void)
 static inline void *
 stat_segment_adjust (stat_client_main_t * sm, void *data)
 {
-  void *p = (void *) ((char *) sm->shared_header +
-		      ((char *) data - (char *) sm->shared_header->base));
-  if ((char *) p > (char *) sm->shared_header &&
-      (((char *) p + sizeof (p)) <
-       ((char *) sm->shared_header + sm->memory_size)))
-    return p;
+  char *csh = (char *) sm->shared_header;
+  char *p = csh + ((char *) data - (char *) sm->shared_header->base);
+  if (p > csh && p + sizeof (p) < csh + sm->memory_size)
+    return (void *) p;
   return 0;
 }
 