FDio/vpp

svm: split fifo into private and shared structs
Type: improvement

Signed-off-by: Florin Coras <fcoras@cisco.com>
Change-Id: Id8e77e8b2623be719fd43a95e181eaa5b7df2b6e

src/plugins/hs_apps/proxy.c
@@ -309,7 +309,7 @@ proxy_rx_callback (session_t * s)
       if (svm_fifo_set_event (ao_tx_fifo))
 	{
 	  u32 ao_thread_index = ao_tx_fifo->master_thread_index;
-	  u32 ao_session_index = ao_tx_fifo->master_session_index;
+	  u32 ao_session_index = ao_tx_fifo->f_shr->master_session_index;
 	  if (session_send_io_evt_to_thread_custom (&ao_session_index,
 						    ao_thread_index,
 						    SESSION_IO_EVT_TX))
@@ -475,7 +475,7 @@ active_open_connected_callback (u32 app_index, u32 opaque,
    * Reset the active-open tx-fifo master indices so the active-open session
    * will receive data, etc.
    */
-  s->tx_fifo->master_session_index = s->session_index;
+  s->tx_fifo->f_shr->master_session_index = s->session_index;
   s->tx_fifo->master_thread_index = s->thread_index;
 
   /*
@@ -535,7 +535,7 @@ active_open_rx_callback (session_t * s)
   if (svm_fifo_set_event (proxy_tx_fifo))
     {
       u8 thread_index = proxy_tx_fifo->master_thread_index;
-      u32 session_index = proxy_tx_fifo->master_session_index;
+      u32 session_index = proxy_tx_fifo->f_shr->master_session_index;
       return session_send_io_evt_to_thread_custom (&session_index,
 						   thread_index,
 						   SESSION_IO_EVT_TX);

src/vnet/session/session.c
@@ -78,7 +78,7 @@ session_send_evt_to_thread (void *data, void *args, u32 thread_index,
 int
 session_send_io_evt_to_thread (svm_fifo_t * f, session_evt_type_t evt_type)
 {
-  return session_send_evt_to_thread (&f->master_session_index, 0,
+  return session_send_evt_to_thread (&f->f_shr->master_session_index, 0,
 				     f->master_thread_index, evt_type);
 }
 
@@ -436,8 +436,8 @@ session_fifo_tuning (session_t * s, svm_fifo_t * f,
 	{
 	  segment_manager_t *sm;
 	  sm = segment_manager_get (f->segment_manager);
-	  ASSERT (f->size >= 4096);
-	  ASSERT (f->size <= sm->max_fifo_size);
+	  ASSERT (f->f_shr->size >= 4096);
+	  ASSERT (f->f_shr->size <= sm->max_fifo_size);
 	}
     }
 }
@@ -611,9 +611,9 @@ session_notify_subscribers (u32 app_index, session_t * s,
   if (!app)
     return -1;
 
-  for (i = 0; i < f->n_subscribers; i++)
+  for (i = 0; i < f->f_shr->n_subscribers; i++)
     {
-      app_wrk = application_get_worker (app, f->subscribers[i]);
+      app_wrk = application_get_worker (app, f->f_shr->subscribers[i]);
       if (!app_wrk)
 	continue;
       if (app_worker_lock_and_send_event (app_wrk, s, evt_type))
@@ -723,7 +723,7 @@ session_dequeue_notify (session_t * s)
 						     SESSION_IO_EVT_TX)))
     return -1;
 
-  if (PREDICT_FALSE (s->tx_fifo->n_subscribers))
+  if (PREDICT_FALSE (s->tx_fifo->f_shr->n_subscribers))
     return session_notify_subscribers (app_wrk->app_index, s,
 				       s->tx_fifo, SESSION_IO_EVT_TX);
 

src/vnet/session/session_debug.c
@@ -187,7 +187,7 @@ session_node_cmp_event (session_event_t * e, svm_fifo_t * f)
     case SESSION_IO_EVT_BUILTIN_RX:
     case SESSION_IO_EVT_BUILTIN_TX:
     case SESSION_IO_EVT_TX_FLUSH:
-      if (e->session_index == f->master_session_index)
+      if (e->session_index == f->f_shr->master_session_index)
 	return 1;
       break;
     case SESSION_CTRL_EVT_CLOSE:

sollor525/vpp-ips

src/plugins/hs_apps/proxy.c
@@ -309,7 +309,7 @@ proxy_rx_callback (session_t * s)
       if (svm_fifo_set_event (ao_tx_fifo))
 	{
 	  u32 ao_thread_index = ao_tx_fifo->master_thread_index;
-	  u32 ao_session_index = ao_tx_fifo->master_session_index;
+	  u32 ao_session_index = ao_tx_fifo->shr->master_session_index;
 	  if (session_send_io_evt_to_thread_custom (&ao_session_index,
 						    ao_thread_index,
 						    SESSION_IO_EVT_TX))
@@ -475,7 +475,7 @@ active_open_connected_callback (u32 app_index, u32 opaque,
    * Reset the active-open tx-fifo master indices so the active-open session
    * will receive data, etc.
    */
-  s->tx_fifo->master_session_index = s->session_index;
+  s->tx_fifo->shr->master_session_index = s->session_index;
   s->tx_fifo->master_thread_index = s->thread_index;
 
   /*
@@ -532,7 +532,7 @@ active_open_rx_callback (session_t * s)
   if (svm_fifo_set_event (proxy_tx_fifo))
     {
       u8 thread_index = proxy_tx_fifo->master_thread_index;
-      u32 session_index = proxy_tx_fifo->master_session_index;
+      u32 session_index = proxy_tx_fifo->shr->master_session_index;
       return session_send_io_evt_to_thread_custom (&session_index,
 						   thread_index,
 						   SESSION_IO_EVT_TX);

src/vnet/session/session.c
@@ -78,7 +78,7 @@ session_send_evt_to_thread (void *data, void *args, u32 thread_index,
 int
 session_send_io_evt_to_thread (svm_fifo_t * f, session_evt_type_t evt_type)
 {
-  return session_send_evt_to_thread (&f->master_session_index, 0,
+  return session_send_evt_to_thread (&f->shr->master_session_index, 0,
 				     f->master_thread_index, evt_type);
 }
 
@@ -436,8 +436,8 @@ session_fifo_tuning (session_t * s, svm_fifo_t * f,
 	{
 	  segment_manager_t *sm;
 	  sm = segment_manager_get (f->segment_manager);
-	  ASSERT (f->size >= 4096);
-	  ASSERT (f->size <= sm->max_fifo_size);
+	  ASSERT (f->shr->size >= 4096);
+	  ASSERT (f->shr->size <= sm->max_fifo_size);
 	}
     }
 }
@@ -611,9 +611,9 @@ session_notify_subscribers (u32 app_index, session_t * s,
   if (!app)
     return -1;
 
-  for (i = 0; i < f->n_subscribers; i++)
+  for (i = 0; i < f->shr->n_subscribers; i++)
     {
-      app_wrk = application_get_worker (app, f->subscribers[i]);
+      app_wrk = application_get_worker (app, f->shr->subscribers[i]);
       if (!app_wrk)
 	continue;
       if (app_worker_lock_and_send_event (app_wrk, s, evt_type))
@@ -723,7 +723,7 @@ session_dequeue_notify (session_t * s)
 						     SESSION_IO_EVT_TX)))
     return -1;
 
-  if (PREDICT_FALSE (s->tx_fifo->n_subscribers))
+  if (PREDICT_FALSE (s->tx_fifo->shr->n_subscribers))
     return session_notify_subscribers (app_wrk->app_index, s,
 				       s->tx_fifo, SESSION_IO_EVT_TX);
 

src/vnet/session/session_debug.c
@@ -187,7 +187,7 @@ session_node_cmp_event (session_event_t * e, svm_fifo_t * f)
     case SESSION_IO_EVT_BUILTIN_RX:
     case SESSION_IO_EVT_BUILTIN_TX:
     case SESSION_IO_EVT_TX_FLUSH:
-      if (e->session_index == f->master_session_index)
+      if (e->session_index == f->shr->master_session_index)
 	return 1;
       break;
     case SESSION_CTRL_EVT_CLOSE: