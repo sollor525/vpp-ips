sollor525/vpp-ips

commit 0120b10151962caae985d8b8bc5c8faa49352b78

virtio: move retry logic to outer function
Improves compile time....

Type: improvement
Change-Id: I310a2e55e5e488a953d65fd065f7795a43d2e9a7
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vnet/devices/virtio/device.c
@@ -552,11 +552,6 @@ virtio_interface_tx_gso_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
   u16 sz = vring->size;
   u16 mask = sz - 1;
   u16 n_vectors = n_left;
-  u16 retry_count = 2;
-
-retry:
-  /* free consumed buffers */
-  virtio_free_used_device_desc (vm, vring, node->node_index);
 
   used = vring->desc_in_use;
   next = vring->desc_next;
@@ -645,9 +640,6 @@ retry:
 	virtio_kick (vm, vring, vif);
     }
 
-  if (n_left && retry_count--)
-    goto retry;
-
   return n_left;
 }
 
@@ -701,6 +693,12 @@ VNET_DEVICE_CLASS_TX_FN (virtio_device_class) (vlib_main_t * vm,
       buffers = to;
     }
 
+  u16 retry_count = 2;
+
+retry:
+  /* free consumed buffers */
+  virtio_free_used_device_desc (vm, vring, node->node_index);
+
   if (vif->type == VIRTIO_IF_TYPE_TAP)
     n_left = virtio_interface_tx_inline (vm, node, vif, vring,
 					 VIRTIO_IF_TYPE_TAP, buffers, n_left);
@@ -712,6 +710,9 @@ VNET_DEVICE_CLASS_TX_FN (virtio_device_class) (vlib_main_t * vm,
 					 VIRTIO_IF_TYPE_TUN, buffers, n_left);
   else
     ASSERT (0);
+
+  if (n_left && retry_count--)
+    goto retry;
 
   if (vif->packet_buffering && n_left)
     {

commit 1b77f7184affc031339b22b7891c92342ce53ac3

tap: fix the buffering index for gro
Type: fix

Fixes: 0120b10151962caae985d8b8bc5c8faa49352b78

Change-Id: Ia1739fad6a36fa658aece157d7adea8bbaa751d2
Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

src/vnet/devices/virtio/device.c
@@ -993,6 +993,7 @@ VNET_DEVICE_CLASS_TX_FN (virtio_device_class) (vlib_main_t * vm,
   u32 *buffers = vlib_frame_vector_args (frame);
   u32 to[GRO_TO_VECTOR_SIZE (n_left)];
   int packed = vif->is_packed;
+  u16 n_vectors = frame->n_vectors;
 
   clib_spinlock_lock_if_init (&vring->lockp);
 
@@ -1004,7 +1005,8 @@ VNET_DEVICE_CLASS_TX_FN (virtio_device_class) (vlib_main_t * vm,
 
   if (vif->packet_coalesce)
     {
-      n_left = vnet_gro_inline (vm, vring->flow_table, buffers, n_left, to);
+      n_vectors = n_left =
+	vnet_gro_inline (vm, vring->flow_table, buffers, n_left, to);
       buffers = to;
     }
 
@@ -1017,17 +1019,17 @@ retry:
   if (vif->type == VIRTIO_IF_TYPE_TAP)
     n_left = virtio_interface_tx_inline (vm, node, vif, vring,
 					 VIRTIO_IF_TYPE_TAP,
-					 &buffers[frame->n_vectors - n_left],
+					 &buffers[n_vectors - n_left],
 					 n_left, packed);
   else if (vif->type == VIRTIO_IF_TYPE_PCI)
     n_left = virtio_interface_tx_inline (vm, node, vif, vring,
 					 VIRTIO_IF_TYPE_PCI,
-					 &buffers[frame->n_vectors - n_left],
+					 &buffers[n_vectors - n_left],
 					 n_left, packed);
   else if (vif->type == VIRTIO_IF_TYPE_TUN)
     n_left = virtio_interface_tx_inline (vm, node, vif, vring,
 					 VIRTIO_IF_TYPE_TUN,
-					 &buffers[frame->n_vectors - n_left],
+					 &buffers[n_vectors - n_left],
 					 n_left, packed);
   else
     ASSERT (0);
@@ -1039,14 +1041,14 @@ retry:
     {
       u16 n_buffered = virtio_vring_buffering_store_packets (vring->buffering,
 							     &buffers
-							     [frame->n_vectors
+							     [n_vectors
 							      - n_left],
 							     n_left);
       n_left -= n_buffered;
     }
   if (n_left)
     virtio_interface_drop_inline (vm, node->node_index,
-				  &buffers[frame->n_vectors - n_left], n_left,
+				  &buffers[n_vectors - n_left], n_left,
 				  VIRTIO_TX_ERROR_NO_FREE_SLOTS);
 
   clib_spinlock_unlock_if_init (&vring->lockp);