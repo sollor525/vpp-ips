sollor525/vpp-ips

commit 5ce9a70d84598e9df7a902317e74fe51b13630cd

vlib: refactor checksum offload support
Type: refactor

This patch refactors the offload flags in vlib_buffer_t.
There are two main reasons behind this refactoring.
First, offload flags are insufficient to represent outer
and inner headers offloads. Second, room for these flags
in first cacheline of vlib_buffer_t is also limited.

This patch introduces a generic offload flag in first
cacheline. And detailed offload flags in 2nd cacheline
of the structure for performance optimization.

Change-Id: Icc363a142fb9208ec7113ab5bbfc8230181f6004
Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>

src/vnet/buffer.h
@@ -46,34 +46,34 @@
  * Flags that are set in the high order bits of ((vlib_buffer*)b)->flags
  *
  */
-#define foreach_vnet_buffer_flag                        \
-  _( 1, L4_CHECKSUM_COMPUTED, "l4-cksum-computed", 1)	\
-  _( 2, L4_CHECKSUM_CORRECT, "l4-cksum-correct", 1)	\
-  _( 3, VLAN_2_DEEP, "vlan-2-deep", 1)			\
-  _( 4, VLAN_1_DEEP, "vlan-1-deep", 1)			\
-  _( 5, SPAN_CLONE, "span-clone", 1)                    \
-  _( 6, LOOP_COUNTER_VALID, "loop-counter-valid", 0)    \
-  _( 7, LOCALLY_ORIGINATED, "local", 1)                 \
-  _( 8, IS_IP4, "ip4", 1)                               \
-  _( 9, IS_IP6, "ip6", 1)                               \
-  _(10, OFFLOAD_IP_CKSUM, "offload-ip-cksum", 1)        \
-  _(11, OFFLOAD_TCP_CKSUM, "offload-tcp-cksum", 1)      \
-  _(12, OFFLOAD_UDP_CKSUM, "offload-udp-cksum", 1)      \
-  _(13, IS_NATED, "natted", 1)                          \
-  _(14, L2_HDR_OFFSET_VALID, "l2_hdr_offset_valid", 0)  \
-  _(15, L3_HDR_OFFSET_VALID, "l3_hdr_offset_valid", 0)  \
-  _(16, L4_HDR_OFFSET_VALID, "l4_hdr_offset_valid", 0)  \
-  _(17, FLOW_REPORT, "flow-report", 1)                  \
-  _(18, IS_DVR, "dvr", 1)                               \
-  _(19, QOS_DATA_VALID, "qos-data-valid", 0)            \
-  _(20, GSO, "gso", 0)                                  \
-  _(21, AVAIL1, "avail1", 1)                            \
-  _(22, AVAIL2, "avail2", 1)                            \
-  _(23, AVAIL3, "avail3", 1)                            \
-  _(24, AVAIL4, "avail4", 1)                            \
-  _(25, AVAIL5, "avail5", 1)                            \
-  _(26, AVAIL6, "avail6", 1)                            \
-  _(27, AVAIL7, "avail7", 1)
+#define foreach_vnet_buffer_flag                                              \
+  _ (1, L4_CHECKSUM_COMPUTED, "l4-cksum-computed", 1)                         \
+  _ (2, L4_CHECKSUM_CORRECT, "l4-cksum-correct", 1)                           \
+  _ (3, VLAN_2_DEEP, "vlan-2-deep", 1)                                        \
+  _ (4, VLAN_1_DEEP, "vlan-1-deep", 1)                                        \
+  _ (5, SPAN_CLONE, "span-clone", 1)                                          \
+  _ (6, LOOP_COUNTER_VALID, "loop-counter-valid", 0)                          \
+  _ (7, LOCALLY_ORIGINATED, "local", 1)                                       \
+  _ (8, IS_IP4, "ip4", 1)                                                     \
+  _ (9, IS_IP6, "ip6", 1)                                                     \
+  _ (10, OFFLOAD, "offload", 0)                                               \
+  _ (11, IS_NATED, "natted", 1)                                               \
+  _ (12, L2_HDR_OFFSET_VALID, "l2_hdr_offset_valid", 0)                       \
+  _ (13, L3_HDR_OFFSET_VALID, "l3_hdr_offset_valid", 0)                       \
+  _ (14, L4_HDR_OFFSET_VALID, "l4_hdr_offset_valid", 0)                       \
+  _ (15, FLOW_REPORT, "flow-report", 1)                                       \
+  _ (16, IS_DVR, "dvr", 1)                                                    \
+  _ (17, QOS_DATA_VALID, "qos-data-valid", 0)                                 \
+  _ (18, GSO, "gso", 0)                                                       \
+  _ (19, AVAIL1, "avail1", 1)                                                 \
+  _ (20, AVAIL2, "avail2", 1)                                                 \
+  _ (21, AVAIL3, "avail3", 1)                                                 \
+  _ (22, AVAIL4, "avail4", 1)                                                 \
+  _ (23, AVAIL5, "avail5", 1)                                                 \
+  _ (24, AVAIL6, "avail6", 1)                                                 \
+  _ (25, AVAIL7, "avail7", 1)                                                 \
+  _ (26, AVAIL8, "avail8", 1)                                                 \
+  _ (27, AVAIL9, "avail9", 1)
 
 /*
  * Please allocate the FIRST available bit, redefine
@@ -81,10 +81,10 @@
  * VNET_BUFFER_FLAGS_ALL_AVAIL definition.
  */
 
-#define VNET_BUFFER_FLAGS_ALL_AVAIL                                     \
-  (VNET_BUFFER_F_AVAIL1 | VNET_BUFFER_F_AVAIL2 | VNET_BUFFER_F_AVAIL3 | \
-   VNET_BUFFER_F_AVAIL4 | VNET_BUFFER_F_AVAIL5 | VNET_BUFFER_F_AVAIL6 | \
-   VNET_BUFFER_F_AVAIL7)
+#define VNET_BUFFER_FLAGS_ALL_AVAIL                                           \
+  (VNET_BUFFER_F_AVAIL1 | VNET_BUFFER_F_AVAIL2 | VNET_BUFFER_F_AVAIL3 |       \
+   VNET_BUFFER_F_AVAIL4 | VNET_BUFFER_F_AVAIL5 | VNET_BUFFER_F_AVAIL6 |       \
+   VNET_BUFFER_F_AVAIL7 | VNET_BUFFER_F_AVAIL8 | VNET_BUFFER_F_AVAIL9)
 
 #define VNET_BUFFER_FLAGS_VLAN_BITS \
   (VNET_BUFFER_F_VLAN_1_DEEP | VNET_BUFFER_F_VLAN_2_DEEP)
@@ -420,6 +420,21 @@ STATIC_ASSERT (sizeof (vnet_buffer_opaque_t) <=
 
 #define vnet_buffer(b) ((vnet_buffer_opaque_t *) (b)->opaque)
 
+#define foreach_vnet_buffer_offload_flag                                      \
+  _ (0, IP_CKSUM, "offload-ip-cksum", 1)                                      \
+  _ (1, TCP_CKSUM, "offload-tcp-cksum", 1)                                    \
+  _ (2, UDP_CKSUM, "offload-udp-cksum", 1)                                    \
+  _ (3, OUTER_IP_CKSUM, "offload-outer-ip-cksum", 1)                          \
+  _ (4, OUTER_TCP_CKSUM, "offload-outer-tcp-cksum", 1)                        \
+  _ (5, OUTER_UDP_CKSUM, "offload-outer-udp-cksum", 1)
+
+enum
+{
+#define _(bit, name, s, v) VNET_BUFFER_OFFLOAD_F_##name = (1 << bit),
+  foreach_vnet_buffer_offload_flag
+#undef _
+};
+
 /* Full cache line (64 bytes) of additional space */
 typedef struct
 {
@@ -452,12 +467,15 @@ typedef struct
    * in case the egress interface is not GSO-enabled - then we need to perform
    * the segmentation, and use this value to cut the payload appropriately.
    */
-  u16 gso_size;
-  /* size of L4 prototol header */
-  u16 gso_l4_hdr_sz;
+  struct
+  {
+    u16 gso_size;
+    /* size of L4 prototol header */
+    u16 gso_l4_hdr_sz;
 
-  /* The union below has a u64 alignment, so this space is unused */
-  u32 __unused2[1];
+    /* offload flags */
+    u32 oflags;
+  };
 
   struct
   {
@@ -501,6 +519,22 @@ STATIC_ASSERT (sizeof (vnet_buffer_opaque2_t) <=
 
 
 format_function_t format_vnet_buffer;
+format_function_t format_vnet_buffer_offload;
+
+static_always_inline void
+vnet_buffer_offload_flags_set (vlib_buffer_t *b, u32 oflags)
+{
+  vnet_buffer2 (b)->oflags |= oflags;
+  b->flags |= VNET_BUFFER_F_OFFLOAD;
+}
+
+static_always_inline void
+vnet_buffer_offload_flags_clear (vlib_buffer_t *b, u32 oflags)
+{
+  vnet_buffer2 (b)->oflags &= ~oflags;
+  if (0 == vnet_buffer2 (b)->oflags)
+    b->flags &= ~VNET_BUFFER_F_OFFLOAD;
+}
 
 #endif /* included_vnet_buffer_h */
 

extras/pg/checksum_offload.pg
@@ -15,7 +15,8 @@ packet-generator new {
     interface loop0
     tx-interface loop1
     node loop1-output
-    buffer-flags ip4 offload-ip-cksum offload-udp-cksum
+    buffer-flags ip4 offload
+    buffer-offload-flags offload-ip-cksum offload-udp-cksum
     data { IP4: 1.2.3 -> 4.5.6
            UDP: 11.22.33.44 -> 11.22.34.44
                 ttl 2 checksum 13

src/plugins/avf/output.c
@@ -57,12 +57,12 @@ static_always_inline u64
 avf_tx_prepare_cksum (vlib_buffer_t * b, u8 is_tso)
 {
   u64 flags = 0;
-  if (!is_tso && !(b->flags & ((VNET_BUFFER_F_OFFLOAD_IP_CKSUM |
-				VNET_BUFFER_F_OFFLOAD_TCP_CKSUM |
-				VNET_BUFFER_F_OFFLOAD_UDP_CKSUM))))
+  if (!is_tso && !(b->flags & VNET_BUFFER_F_OFFLOAD))
     return 0;
-  u32 is_tcp = is_tso || b->flags & VNET_BUFFER_F_OFFLOAD_TCP_CKSUM;
-  u32 is_udp = !is_tso && b->flags & VNET_BUFFER_F_OFFLOAD_UDP_CKSUM;
+
+  u32 oflags = vnet_buffer2 (b)->oflags;
+  u32 is_tcp = is_tso || oflags & VNET_BUFFER_OFFLOAD_F_TCP_CKSUM;
+  u32 is_udp = !is_tso && oflags & VNET_BUFFER_OFFLOAD_F_UDP_CKSUM;
   u32 is_ip4 = b->flags & VNET_BUFFER_F_IS_IP4;
   u32 is_ip6 = b->flags & VNET_BUFFER_F_IS_IP6;
   ASSERT (!is_tcp || !is_udp);
@@ -173,9 +173,7 @@ avf_tx_enqueue (vlib_main_t * vm, vlib_node_runtime_t * node, avf_txq_t * txq,
 {
   u16 next = txq->next;
   u64 bits = AVF_TXD_CMD_EOP | AVF_TXD_CMD_RSV;
-  const u32 offload_mask = VNET_BUFFER_F_OFFLOAD_IP_CKSUM |
-    VNET_BUFFER_F_OFFLOAD_TCP_CKSUM | VNET_BUFFER_F_OFFLOAD_UDP_CKSUM |
-    VNET_BUFFER_F_GSO;
+  const u32 offload_mask = VNET_BUFFER_F_OFFLOAD | VNET_BUFFER_F_GSO;
   u64 one_by_one_offload_flags = 0;
   int is_tso;
   u16 n_desc = 0;

src/plugins/dpdk/device/device.c
@@ -218,16 +218,19 @@ static_always_inline void
 dpdk_buffer_tx_offload (dpdk_device_t * xd, vlib_buffer_t * b,
 			struct rte_mbuf *mb)
 {
-  u32 ip_cksum = b->flags & VNET_BUFFER_F_OFFLOAD_IP_CKSUM;
-  u32 tcp_cksum = b->flags & VNET_BUFFER_F_OFFLOAD_TCP_CKSUM;
-  u32 udp_cksum = b->flags & VNET_BUFFER_F_OFFLOAD_UDP_CKSUM;
   int is_ip4 = b->flags & VNET_BUFFER_F_IS_IP4;
   u32 tso = b->flags & VNET_BUFFER_F_GSO, max_pkt_len;
+  u32 oflags, ip_cksum, tcp_cksum, udp_cksum;
   u64 ol_flags;
 
   /* Is there any work for us? */
-  if (PREDICT_TRUE ((ip_cksum | tcp_cksum | udp_cksum | tso) == 0))
+  if (PREDICT_TRUE (((b->flags & VNET_BUFFER_F_OFFLOAD) | tso) == 0))
     return;
+
+  oflags = vnet_buffer2 (b)->oflags;
+  ip_cksum = oflags & VNET_BUFFER_OFFLOAD_F_IP_CKSUM;
+  tcp_cksum = oflags & VNET_BUFFER_OFFLOAD_F_TCP_CKSUM;
+  udp_cksum = oflags & VNET_BUFFER_OFFLOAD_F_UDP_CKSUM;
 
   mb->l2_len = vnet_buffer (b)->l3_hdr_offset - b->current_data;
   mb->l3_len = vnet_buffer (b)->l4_hdr_offset -
@@ -328,10 +331,7 @@ VNET_DEVICE_CLASS_TX_FN (dpdk_device_class) (vlib_main_t * vm,
 	}
 
       if (PREDICT_FALSE ((xd->flags & DPDK_DEVICE_FLAG_TX_OFFLOAD) &&
-			 (or_flags &
-			  (VNET_BUFFER_F_OFFLOAD_TCP_CKSUM
-			   | VNET_BUFFER_F_OFFLOAD_IP_CKSUM
-			   | VNET_BUFFER_F_OFFLOAD_UDP_CKSUM))))
+			 (or_flags & VNET_BUFFER_F_OFFLOAD)))
 	{
 	  dpdk_buffer_tx_offload (xd, b[0], mb[0]);
 	  dpdk_buffer_tx_offload (xd, b[1], mb[1]);
@@ -388,10 +388,7 @@ VNET_DEVICE_CLASS_TX_FN (dpdk_device_class) (vlib_main_t * vm,
 	}
 
       if (PREDICT_FALSE ((xd->flags & DPDK_DEVICE_FLAG_TX_OFFLOAD) &&
-			 (or_flags &
-			  (VNET_BUFFER_F_OFFLOAD_TCP_CKSUM
-			   | VNET_BUFFER_F_OFFLOAD_IP_CKSUM
-			   | VNET_BUFFER_F_OFFLOAD_UDP_CKSUM))))
+			 (or_flags & VNET_BUFFER_F_OFFLOAD)))
 	{
 	  dpdk_buffer_tx_offload (xd, b[0], mb[0]);
 	  dpdk_buffer_tx_offload (xd, b[1], mb[1]);

src/plugins/gtpu/gtpu_decap.c
@@ -1257,13 +1257,15 @@ static char *gtpu_flow_error_strings[] = {
 
 };
 
-#define gtpu_local_need_csum_check(_b) 			\
-    (!(_b->flags & VNET_BUFFER_F_L4_CHECKSUM_COMPUTED 	\
-	|| _b->flags & VNET_BUFFER_F_OFFLOAD_UDP_CKSUM))
+#define gtpu_local_need_csum_check(_b)                                        \
+  (!(_b->flags & VNET_BUFFER_F_L4_CHECKSUM_COMPUTED ||                        \
+     (_b->flags & VNET_BUFFER_F_OFFLOAD &&                                    \
+      vnet_buffer2 (_b)->oflags & VNET_BUFFER_OFFLOAD_F_UDP_CKSUM)))
 
-#define gtpu_local_csum_is_valid(_b)  \
-    ((_b->flags & VNET_BUFFER_F_L4_CHECKSUM_CORRECT \
-	|| _b->flags & VNET_BUFFER_F_OFFLOAD_UDP_CKSUM) != 0)
+#define gtpu_local_csum_is_valid(_b)                                          \
+  ((_b->flags & VNET_BUFFER_F_L4_CHECKSUM_CORRECT ||                          \
+    (_b->flags & VNET_BUFFER_F_OFFLOAD &&                                     \
+     vnet_buffer2 (_b)->oflags & VNET_BUFFER_OFFLOAD_F_UDP_CKSUM)) != 0)
 
 static_always_inline u8
 gtpu_validate_udp_csum (vlib_main_t * vm, vlib_buffer_t *b)

src/plugins/lisp/lisp-cp/packets.c
@@ -182,7 +182,7 @@ pkt_push_udp_and_ip (vlib_main_t * vm, vlib_buffer_t * b, u16 sp, u16 dp,
   if (csum_offload)
     {
       ih = pkt_push_ip (vm, b, sip, dip, IP_PROTOCOL_UDP, 1);
-      b->flags |= VNET_BUFFER_F_OFFLOAD_UDP_CKSUM;
+      vnet_buffer_offload_flags_set (b, VNET_BUFFER_OFFLOAD_F_UDP_CKSUM);
       vnet_buffer (b)->l3_hdr_offset = (u8 *) ih - b->data;
       vnet_buffer (b)->l4_hdr_offset = (u8 *) uh - b->data;
       uh->checksum = 0;

src/plugins/vmxnet3/input.c
@@ -79,6 +79,7 @@ vmxnet3_handle_offload (vmxnet3_rx_comp * rx_comp, vlib_buffer_t * hb,
 			u16 gso_size)
 {
   u8 l4_hdr_sz = 0;
+  u32 oflags = 0;
 
   if (rx_comp->flags & VMXNET3_RXCF_IP4)
     {
@@ -90,15 +91,15 @@ vmxnet3_handle_offload (vmxnet3_rx_comp * rx_comp, vlib_buffer_t * hb,
       vnet_buffer (hb)->l4_hdr_offset = sizeof (ethernet_header_t) +
 	ip4_header_bytes (ip4);
       hb->flags |= VNET_BUFFER_F_L2_HDR_OFFSET_VALID |
-	VNET_BUFFER_F_L3_HDR_OFFSET_VALID |
-	VNET_BUFFER_F_L4_HDR_OFFSET_VALID | VNET_BUFFER_F_IS_IP4;
+		   VNET_BUFFER_F_L3_HDR_OFFSET_VALID |
+		   VNET_BUFFER_F_L4_HDR_OFFSET_VALID | VNET_BUFFER_F_IS_IP4;
 
       /* checksum offload */
       if (!(rx_comp->index & VMXNET3_RXCI_CNC))
 	{
 	  if (!(rx_comp->flags & VMXNET3_RXCF_IPC))
 	    {
-	      hb->flags |= VNET_BUFFER_F_OFFLOAD_IP_CKSUM;
+	      oflags |= VNET_BUFFER_OFFLOAD_F_IP_CKSUM;
 	      ip4->checksum = 0;
 	    }
 	  if (!(rx_comp->flags & VMXNET3_RXCF_TUC))
@@ -108,7 +109,7 @@ vmxnet3_handle_offload (vmxnet3_rx_comp * rx_comp, vlib_buffer_t * hb,
 		  tcp_header_t *tcp =
 		    (tcp_header_t *) (hb->data +
 				      vnet_buffer (hb)->l4_hdr_offset);
-		  hb->flags |= VNET_BUFFER_F_OFFLOAD_TCP_CKSUM;
+		  oflags |= VNET_BUFFER_OFFLOAD_F_TCP_CKSUM;
 		  tcp->checksum = 0;
 		}
 	      else if (rx_comp->flags & VMXNET3_RXCF_UDP)
@@ -116,7 +117,7 @@ vmxnet3_handle_offload (vmxnet3_rx_comp * rx_comp, vlib_buffer_t * hb,
 		  udp_header_t *udp =
 		    (udp_header_t *) (hb->data +
 				      vnet_buffer (hb)->l4_hdr_offset);
-		  hb->flags |= VNET_BUFFER_F_OFFLOAD_UDP_CKSUM;
+		  oflags |= VNET_BUFFER_OFFLOAD_F_UDP_CKSUM;
 		  udp->checksum = 0;
 		}
 	    }
@@ -148,8 +149,8 @@ vmxnet3_handle_offload (vmxnet3_rx_comp * rx_comp, vlib_buffer_t * hb,
       vnet_buffer (hb)->l4_hdr_offset = sizeof (ethernet_header_t) +
 	sizeof (ip6_header_t);
       hb->flags |= VNET_BUFFER_F_L2_HDR_OFFSET_VALID |
-	VNET_BUFFER_F_L3_HDR_OFFSET_VALID |
-	VNET_BUFFER_F_L4_HDR_OFFSET_VALID | VNET_BUFFER_F_IS_IP6;
+		   VNET_BUFFER_F_L3_HDR_OFFSET_VALID |
+		   VNET_BUFFER_F_L4_HDR_OFFSET_VALID | VNET_BUFFER_F_IS_IP6;
 
       /* checksum offload */
       if (!(rx_comp->index & VMXNET3_RXCI_CNC))
@@ -161,7 +162,7 @@ vmxnet3_handle_offload (vmxnet3_rx_comp * rx_comp, vlib_buffer_t * hb,
 		  tcp_header_t *tcp =
 		    (tcp_header_t *) (hb->data +
 				      vnet_buffer (hb)->l4_hdr_offset);
-		  hb->flags |= VNET_BUFFER_F_OFFLOAD_TCP_CKSUM;
+		  oflags |= VNET_BUFFER_OFFLOAD_F_TCP_CKSUM;
 		  tcp->checksum = 0;
 		}
 	      else if (rx_comp->flags & VMXNET3_RXCF_UDP)
@@ -169,7 +170,7 @@ vmxnet3_handle_offload (vmxnet3_rx_comp * rx_comp, vlib_buffer_t * hb,
 		  udp_header_t *udp =
 		    (udp_header_t *) (hb->data +
 				      vnet_buffer (hb)->l4_hdr_offset);
-		  hb->flags |= VNET_BUFFER_F_OFFLOAD_UDP_CKSUM;
+		  oflags |= VNET_BUFFER_OFFLOAD_F_UDP_CKSUM;
 		  udp->checksum = 0;
 		}
 	    }
@@ -194,6 +195,8 @@ vmxnet3_handle_offload (vmxnet3_rx_comp * rx_comp, vlib_buffer_t * hb,
 	  hb->flags |= VNET_BUFFER_F_GSO;
 	}
     }
+  if (oflags)
+    vnet_buffer_offload_flags_set (hb, oflags);
 }
 
 static_always_inline uword

src/plugins/wireguard/wireguard_input.c
@@ -368,7 +368,8 @@ VLIB_NODE_FN (wg_input_node) (vlib_main_t * vm,
 
 	  clib_memcpy (vlib_buffer_get_current (b[0]), decr_data, decr_len);
 	  b[0]->current_length = decr_len;
-	  b[0]->flags &= ~VNET_BUFFER_F_OFFLOAD_UDP_CKSUM;
+	  vnet_buffer_offload_flags_clear (b[0],
+					   VNET_BUFFER_OFFLOAD_F_UDP_CKSUM);
 
 	  wg_timers_any_authenticated_packet_received (peer);
 	  wg_timers_any_authenticated_packet_traversal (peer);

commit a36a2ee9473e0bc113377a37253e6d556a92a5dd

vlib: fix offload flags value reset
When a buffer is freed and re-allocated for a new packet, opaque2 is
not reset, so the offload flags can be set to a stale value.
Make sure the offload flags are reset to the current value on 1st set.

Type: fix
Fixes: 5ce9a70d84598e9df7a902317e74fe51b13630cd

Change-Id: I4048febedf25b9995dbd080a11495ee7dbe59153
Signed-off-by: Benoît Ganne <bganne@cisco.com>

src/vnet/buffer.h
@@ -524,8 +524,17 @@ format_function_t format_vnet_buffer_offload;
 static_always_inline void
 vnet_buffer_offload_flags_set (vlib_buffer_t *b, u32 oflags)
 {
-  vnet_buffer2 (b)->oflags |= oflags;
-  b->flags |= VNET_BUFFER_F_OFFLOAD;
+  if (b->flags & VNET_BUFFER_F_OFFLOAD)
+    {
+      /* add a flag to existing offload */
+      vnet_buffer2 (b)->oflags |= oflags;
+    }
+  else
+    {
+      /* no offload yet: reset offload flags to new value */
+      vnet_buffer2 (b)->oflags = oflags;
+      b->flags |= VNET_BUFFER_F_OFFLOAD;
+    }
 }
 
 static_always_inline void