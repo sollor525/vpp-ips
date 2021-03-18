sollor525/vpp-ips

commit 4c802f27569bef97dfcceb79b7d3589a66ff2ae8

dpdk: implement interrupt mode
Change-Id: I6ababc99ecf559327a4370914580c98d32680175
Type: feature
Signed-off-by: Mohammed Hawari <mohammed@hawari.fr>

src/plugins/dpdk/device/common.c
@@ -16,6 +16,8 @@
 #include <vnet/vnet.h>
 #include <vppinfra/vec.h>
 #include <vppinfra/format.h>
+#include <vppinfra/file.h>
+#include <vlib/unix/unix.h>
 #include <assert.h>
 
 #include <vnet/ip/ip.h>
@@ -160,6 +162,79 @@ error:
   sw->flags |= VNET_SW_INTERFACE_FLAG_ERROR;
 }
 
+static clib_error_t *
+dpdk_rx_read_ready (clib_file_t *uf)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+  dpdk_main_t *dm = &dpdk_main;
+  u32 qidx = uf->private_data;
+  vnet_hw_if_rx_queue_t *rxq = vnet_hw_if_get_rx_queue (vnm, qidx);
+  dpdk_device_t *xd = vec_elt_at_index (dm->devices, rxq->dev_instance);
+
+  u64 b;
+  CLIB_UNUSED (ssize_t size) = read (uf->file_descriptor, &b, sizeof (b));
+  if (rxq->mode != VNET_HW_IF_RX_MODE_POLLING)
+    {
+      vnet_hw_if_rx_queue_set_int_pending (vnm, uf->private_data);
+      rte_eth_dev_rx_intr_enable (xd->port_id, rxq->queue_id);
+    }
+
+  return 0;
+}
+
+static void
+dpdk_setup_interrupts (dpdk_device_t *xd)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+  vnet_hw_interface_t *hi = vnet_get_hw_interface (vnm, xd->hw_if_index);
+  if (!hi)
+    return;
+
+  if (!xd->port_conf.intr_conf.rxq)
+    return;
+
+  /* Probe for interrupt support */
+  if (rte_eth_dev_rx_intr_enable (xd->port_id, 0))
+    {
+      dpdk_log_info ("probe for interrupt mode for device %U. Failed.\n",
+		     format_dpdk_device_name, xd->port_id);
+    }
+  else
+    {
+      xd->flags |= DPDK_DEVICE_FLAG_INT_SUPPORTED;
+      rte_eth_dev_rx_intr_disable (xd->port_id, 0);
+      dpdk_log_info ("Probe for interrupt mode for device %U. Success.\n",
+		     format_dpdk_device_name, xd->port_id);
+    }
+
+  if (xd->flags & DPDK_DEVICE_FLAG_INT_SUPPORTED)
+    {
+      hi->flags |= VNET_HW_INTERFACE_FLAG_SUPPORTS_INT_MODE;
+      for (int q = 0; q < xd->rx_q_used; q++)
+	{
+	  dpdk_rx_queue_t *rxq = vec_elt_at_index (xd->rx_queues, q);
+	  clib_file_t f = { 0 };
+	  rxq->efd = rte_eth_dev_rx_intr_ctl_q_get_fd (xd->port_id, q);
+	  if (rxq->efd < 0)
+	    {
+	      xd->flags &= ~DPDK_DEVICE_FLAG_INT_SUPPORTED;
+	      hi->flags &= ~VNET_HW_INTERFACE_FLAG_SUPPORTS_INT_MODE;
+	      break;
+	    }
+	  f.read_function = dpdk_rx_read_ready;
+	  f.flags = UNIX_FILE_EVENT_EDGE_TRIGGERED;
+	  f.file_descriptor = rxq->efd;
+	  f.private_data = rxq->queue_index;
+	  f.description =
+	    format (0, "%U queue %u", format_dpdk_device_name, xd->port_id, q);
+	  rxq->clib_file_index = clib_file_add (&file_main, &f);
+	  vnet_hw_if_set_rx_queue_file_index (vnm, rxq->queue_index,
+					      rxq->clib_file_index);
+	}
+    }
+  vnet_hw_if_update_runtime_data (vnm, xd->hw_if_index);
+}
+
 void
 dpdk_device_start (dpdk_device_t * xd)
 {
@@ -175,6 +250,8 @@ dpdk_device_start (dpdk_device_t * xd)
       dpdk_device_error (xd, "rte_eth_dev_start", rv);
       return;
     }
+
+  dpdk_setup_interrupts (xd);
 
   if (xd->default_mac_address)
     rv = rte_eth_dev_default_mac_addr_set (xd->port_id,

src/plugins/dpdk/device/device.c
@@ -694,6 +694,25 @@ done:
   return err;
 }
 
+static clib_error_t *
+dpdk_interface_rx_mode_change (vnet_main_t *vnm, u32 hw_if_index, u32 qid,
+			       vnet_hw_if_rx_mode mode)
+{
+  dpdk_main_t *xm = &dpdk_main;
+  vnet_hw_interface_t *hw = vnet_get_hw_interface (vnm, hw_if_index);
+  dpdk_device_t *xd = vec_elt_at_index (xm->devices, hw->dev_instance);
+  int rv = 0;
+  if (!(xd->flags & DPDK_DEVICE_FLAG_INT_SUPPORTED))
+    return clib_error_return (0, "unsupported op (is the interface up?)", rv);
+  if (mode == VNET_HW_IF_RX_MODE_POLLING)
+    rv = rte_eth_dev_rx_intr_disable (xd->port_id, qid);
+  else
+    rv = rte_eth_dev_rx_intr_enable (xd->port_id, qid);
+  if (rv)
+    return clib_error_return (0, "dpdk_interface_rx_mode_change err %d", rv);
+  return 0;
+}
+
 /* *INDENT-OFF* */
 VNET_DEVICE_CLASS (dpdk_device_class) = {
   .name = "dpdk",
@@ -711,6 +730,7 @@ VNET_DEVICE_CLASS (dpdk_device_class) = {
   .format_flow = format_dpdk_flow,
   .flow_ops_function = dpdk_flow_ops_fn,
   .set_rss_queues_function = dpdk_interface_set_rss_queues,
+  .rx_mode_change_function = dpdk_interface_rx_mode_change,
 };
 /* *INDENT-ON* */
 

src/plugins/dpdk/device/dpdk.h
@@ -128,17 +128,18 @@ typedef enum
 
 typedef uint16_t dpdk_portid_t;
 
-#define foreach_dpdk_device_flags \
-  _( 0, ADMIN_UP, "admin-up") \
-  _( 1, PROMISC, "promisc") \
-  _( 2, PMD, "pmd") \
-  _( 3, PMD_INIT_FAIL, "pmd-init-fail") \
-  _( 4, MAYBE_MULTISEG, "maybe-multiseg") \
-  _( 5, HAVE_SUBIF, "subif") \
-  _( 9, TX_OFFLOAD, "tx-offload") \
-  _(10, INTEL_PHDR_CKSUM, "intel-phdr-cksum") \
-  _(11, RX_FLOW_OFFLOAD, "rx-flow-offload") \
-  _(12, RX_IP4_CKSUM, "rx-ip4-cksum")
+#define foreach_dpdk_device_flags                                             \
+  _ (0, ADMIN_UP, "admin-up")                                                 \
+  _ (1, PROMISC, "promisc")                                                   \
+  _ (2, PMD, "pmd")                                                           \
+  _ (3, PMD_INIT_FAIL, "pmd-init-fail")                                       \
+  _ (4, MAYBE_MULTISEG, "maybe-multiseg")                                     \
+  _ (5, HAVE_SUBIF, "subif")                                                  \
+  _ (9, TX_OFFLOAD, "tx-offload")                                             \
+  _ (10, INTEL_PHDR_CKSUM, "intel-phdr-cksum")                                \
+  _ (11, RX_FLOW_OFFLOAD, "rx-flow-offload")                                  \
+  _ (12, RX_IP4_CKSUM, "rx-ip4-cksum")                                        \
+  _ (13, INT_SUPPORTED, "int-supported")
 
 enum
 {
@@ -166,6 +167,8 @@ typedef struct
   CLIB_CACHE_LINE_ALIGN_MARK (cacheline0);
   u8 buffer_pool_index;
   u32 queue_index;
+  int efd;
+  uword clib_file_index;
 } dpdk_rx_queue_t;
 
 typedef struct

src/plugins/dpdk/device/init.c
@@ -470,6 +470,7 @@ dpdk_lib_init (dpdk_main_t * dm)
 		    DPDK_DEVICE_FLAG_INTEL_PHDR_CKSUM;
 		}
 
+	      xd->port_conf.intr_conf.rxq = 1;
 
 	      break;
 	    case VNET_DPDK_PMD_CXGBE:
@@ -493,6 +494,7 @@ dpdk_lib_init (dpdk_main_t * dm)
 		    DPDK_DEVICE_FLAG_TX_OFFLOAD |
 		    DPDK_DEVICE_FLAG_INTEL_PHDR_CKSUM;
 		}
+	      /*xd->port_conf.intr_conf.rxq = 1;*/
 	      break;
 
 	      /* iAVF */
@@ -512,7 +514,8 @@ dpdk_lib_init (dpdk_main_t * dm)
 		    DPDK_DEVICE_FLAG_TX_OFFLOAD |
 		    DPDK_DEVICE_FLAG_INTEL_PHDR_CKSUM;
 		}
-              break;
+	      xd->port_conf.intr_conf.rxq = 1;
+	      break;
 
 	    case VNET_DPDK_PMD_THUNDERX:
 	      xd->port_type = VNET_DPDK_PORT_TYPE_ETH_VF;
@@ -528,6 +531,7 @@ dpdk_lib_init (dpdk_main_t * dm)
 	    case VNET_DPDK_PMD_ENA:
 	      xd->port_type = VNET_DPDK_PORT_TYPE_ETH_VF;
 	      xd->port_conf.rxmode.offloads &= ~DEV_RX_OFFLOAD_SCATTER;
+	      xd->port_conf.intr_conf.rxq = 1;
 	      break;
 
 	    case VNET_DPDK_PMD_DPAA2:

commit 7fe989f795546bca299ce94ba4b70bf005a74ed0

dpdk: don't mask interrupts for some drivers
It is well known that for some NICs, masking
interrupts results in delaying desc writebacks,
which breaks poll mode. This fix introduces an
"int-unmaskable" dpdk device flag to identify such
devices (typically Intel FVL). For such devices,
interrupts are masked by a call to
file_update(...,UNIX_FILE_UPDATE_DELETE) instead
of rte_eth_dev_rx_intr_disable (...)

Change-Id: Ifbc701aebe8572319b7aae19382bd683a47fc3cf
Type: fix
Fixes: 4c802f27569bef97dfcceb79b7d3589a66ff2ae8
Signed-off-by: Mohammed Hawari <mohammed@hawari.fr>

src/plugins/dpdk/device/common.c
@@ -202,7 +202,8 @@ dpdk_setup_interrupts (dpdk_device_t *xd)
   else
     {
       xd->flags |= DPDK_DEVICE_FLAG_INT_SUPPORTED;
-      rte_eth_dev_rx_intr_disable (xd->port_id, 0);
+      if (!(xd->flags & DPDK_DEVICE_FLAG_INT_UNMASKABLE))
+	rte_eth_dev_rx_intr_disable (xd->port_id, 0);
       dpdk_log_info ("Probe for interrupt mode for device %U. Success.\n",
 		     format_dpdk_device_name, xd->port_id);
     }
@@ -230,6 +231,13 @@ dpdk_setup_interrupts (dpdk_device_t *xd)
 	  rxq->clib_file_index = clib_file_add (&file_main, &f);
 	  vnet_hw_if_set_rx_queue_file_index (vnm, rxq->queue_index,
 					      rxq->clib_file_index);
+	  if (xd->flags & DPDK_DEVICE_FLAG_INT_UNMASKABLE)
+	    {
+	      clib_file_main_t *fm = &file_main;
+	      clib_file_t *f =
+		pool_elt_at_index (fm->file_pool, rxq->clib_file_index);
+	      fm->file_update (f, UNIX_FILE_UPDATE_DELETE);
+	    }
 	}
     }
   vnet_hw_if_update_runtime_data (vnm, xd->hw_if_index);

src/plugins/dpdk/device/device.c
@@ -22,6 +22,7 @@
 #include <dpdk/device/dpdk.h>
 #include <dpdk/device/dpdk_priv.h>
 #include <vppinfra/error.h>
+#include <vlib/unix/unix.h>
 
 #define foreach_dpdk_tx_func_error			\
   _(BAD_RETVAL, "DPDK tx function returned an error")	\
@@ -701,13 +702,29 @@ dpdk_interface_rx_mode_change (vnet_main_t *vnm, u32 hw_if_index, u32 qid,
   dpdk_main_t *xm = &dpdk_main;
   vnet_hw_interface_t *hw = vnet_get_hw_interface (vnm, hw_if_index);
   dpdk_device_t *xd = vec_elt_at_index (xm->devices, hw->dev_instance);
+  clib_file_main_t *fm = &file_main;
+  dpdk_rx_queue_t *rxq;
+  clib_file_t *f;
   int rv = 0;
   if (!(xd->flags & DPDK_DEVICE_FLAG_INT_SUPPORTED))
     return clib_error_return (0, "unsupported op (is the interface up?)", rv);
-  if (mode == VNET_HW_IF_RX_MODE_POLLING)
+  if (mode == VNET_HW_IF_RX_MODE_POLLING &&
+      !(xd->flags & DPDK_DEVICE_FLAG_INT_UNMASKABLE))
     rv = rte_eth_dev_rx_intr_disable (xd->port_id, qid);
-  else
+  else if (mode == VNET_HW_IF_RX_MODE_POLLING)
+    {
+      rxq = vec_elt_at_index (xd->rx_queues, qid);
+      f = pool_elt_at_index (fm->file_pool, rxq->clib_file_index);
+      fm->file_update (f, UNIX_FILE_UPDATE_DELETE);
+    }
+  else if (!(xd->flags & DPDK_DEVICE_FLAG_INT_UNMASKABLE))
     rv = rte_eth_dev_rx_intr_enable (xd->port_id, qid);
+  else
+    {
+      rxq = vec_elt_at_index (xd->rx_queues, qid);
+      f = pool_elt_at_index (fm->file_pool, rxq->clib_file_index);
+      fm->file_update (f, UNIX_FILE_UPDATE_ADD);
+    }
   if (rv)
     return clib_error_return (0, "dpdk_interface_rx_mode_change err %d", rv);
   return 0;

src/plugins/dpdk/device/dpdk.h
@@ -139,7 +139,8 @@ typedef uint16_t dpdk_portid_t;
   _ (10, INTEL_PHDR_CKSUM, "intel-phdr-cksum")                                \
   _ (11, RX_FLOW_OFFLOAD, "rx-flow-offload")                                  \
   _ (12, RX_IP4_CKSUM, "rx-ip4-cksum")                                        \
-  _ (13, INT_SUPPORTED, "int-supported")
+  _ (13, INT_SUPPORTED, "int-supported")                                      \
+  _ (14, INT_UNMASKABLE, "int-unmaskable")
 
 enum
 {

src/plugins/dpdk/device/init.c
@@ -448,10 +448,11 @@ dpdk_lib_init (dpdk_main_t * dm)
 	  switch (xd->pmd)
 	    {
 	      /* Drivers with valid speed_capa set */
+	    case VNET_DPDK_PMD_I40E:
+	      xd->flags |= DPDK_DEVICE_FLAG_INT_UNMASKABLE;
 	    case VNET_DPDK_PMD_E1000EM:
 	    case VNET_DPDK_PMD_IGB:
 	    case VNET_DPDK_PMD_IXGBE:
-	    case VNET_DPDK_PMD_I40E:
 	    case VNET_DPDK_PMD_ICE:
 	      xd->port_type = port_type_from_speed_capa (&dev_info);
 	      xd->supported_flow_actions = VNET_FLOW_ACTION_MARK |
@@ -471,7 +472,6 @@ dpdk_lib_init (dpdk_main_t * dm)
 		}
 
 	      xd->port_conf.intr_conf.rxq = 1;
-
 	      break;
 	    case VNET_DPDK_PMD_CXGBE:
 	    case VNET_DPDK_PMD_MLX4:
@@ -482,9 +482,10 @@ dpdk_lib_init (dpdk_main_t * dm)
 	      break;
 
 	      /* SR-IOV VFs */
+	    case VNET_DPDK_PMD_I40EVF:
+	      xd->flags |= DPDK_DEVICE_FLAG_INT_UNMASKABLE;
 	    case VNET_DPDK_PMD_IGBVF:
 	    case VNET_DPDK_PMD_IXGBEVF:
-	    case VNET_DPDK_PMD_I40EVF:
 	      xd->port_type = VNET_DPDK_PORT_TYPE_ETH_VF;
 	      if (dm->conf->no_tx_checksum_offload == 0)
 		{
@@ -494,12 +495,14 @@ dpdk_lib_init (dpdk_main_t * dm)
 		    DPDK_DEVICE_FLAG_TX_OFFLOAD |
 		    DPDK_DEVICE_FLAG_INTEL_PHDR_CKSUM;
 		}
-	      /*xd->port_conf.intr_conf.rxq = 1;*/
+	      /* sDPDK bug in multiqueue... */
+	      /* xd->port_conf.intr_conf.rxq = 1; */
 	      break;
 
 	      /* iAVF */
 	    case VNET_DPDK_PMD_IAVF:
-        xd->port_type = VNET_DPDK_PORT_TYPE_ETH_VF;
+	      xd->flags |= DPDK_DEVICE_FLAG_INT_UNMASKABLE;
+	      xd->port_type = VNET_DPDK_PORT_TYPE_ETH_VF;
 	      xd->supported_flow_actions = VNET_FLOW_ACTION_MARK |
 		VNET_FLOW_ACTION_REDIRECT_TO_NODE |
 		VNET_FLOW_ACTION_REDIRECT_TO_QUEUE |