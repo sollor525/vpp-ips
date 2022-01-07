sollor525/vpp-ips

commit 8cc3f7aaa2a362f919cf2c9d1fb6c81966ae0a0d

vmxnet3 device driver
Implemented vmxnet3 deivice driver for VMWare ESXi. Tested with Ubuntu 18.04
connected to ESXi 6.0

Ubuntu-18.04 (VPP) --- ESXi-6.0

Change-Id: I85fbc86f2d8532b017bc4271612d17e24e498e4d
Signed-off-by: Steven Luong  <sluong@cisco.com>

src/plugins/vmxnet3/cli.c
@@ -1,0 +1,371 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+#include <stdint.h>
+#include <net/if.h>
+#include <sys/ioctl.h>
+#include <inttypes.h>
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vlib/pci/pci.h>
+#include <vnet/ethernet/ethernet.h>
+
+#include <vmxnet3/vmxnet3.h>
+
+static clib_error_t *
+vmxnet3_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
+			   vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  vmxnet3_create_if_args_t args = { 0 };
+  u32 tmp;
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "%U", unformat_vlib_pci_addr, &args.addr))
+	;
+      else if (unformat (line_input, "elog"))
+	args.enable_elog = 1;
+      else if (unformat (line_input, "rx-queue-size %u", &tmp))
+	args.rxq_size = tmp;
+      else if (unformat (line_input, "tx-queue-size %u", &tmp))
+	args.txq_size = tmp;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  unformat_free (line_input);
+
+
+  vmxnet3_create_if (vm, &args);
+
+  return args.error;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (vmxnet3_create_command, static) = {
+  .path = "create interface vmxnet3",
+  .short_help = "create interface vmxnet3 <pci-address>"
+                "[rx-queue-size <size>] [tx-queue-size <size>]",
+  .function = vmxnet3_create_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+vmxnet3_delete_command_fn (vlib_main_t * vm, unformat_input_t * input,
+			   vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  u32 sw_if_index = ~0;
+  vnet_hw_interface_t *hw;
+  vmxnet3_main_t *vmxm = &vmxnet3_main;
+  vmxnet3_device_t *vd;
+  vnet_main_t *vnm = vnet_get_main ();
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "sw_if_index %d", &sw_if_index))
+	;
+      else if (unformat (line_input, "%U", unformat_vnet_sw_interface,
+			 vnm, &sw_if_index))
+	;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  unformat_free (line_input);
+
+  if (sw_if_index == ~0)
+    return clib_error_return (0,
+			      "please specify interface name or sw_if_index");
+
+  hw = vnet_get_sup_hw_interface (vnm, sw_if_index);
+  if (hw == NULL || vmxnet3_device_class.index != hw->dev_class_index)
+    return clib_error_return (0, "not a vmxnet3 interface");
+
+  vd = pool_elt_at_index (vmxm->devices, hw->dev_instance);
+
+  vmxnet3_delete_if (vm, vd);
+
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (vmxnet3_delete_command, static) = {
+  .path = "delete interface vmxnet3",
+  .short_help = "delete interface vmxnet3 "
+    "{<interface> | sw_if_index <sw_idx>}",
+  .function = vmxnet3_delete_command_fn,
+};
+/* *INDENT-ON* */
+
+static clib_error_t *
+vmxnet3_test_command_fn (vlib_main_t * vm, unformat_input_t * input,
+			 vlib_cli_command_t * cmd)
+{
+  unformat_input_t _line_input, *line_input = &_line_input;
+  u32 sw_if_index = ~0;
+  vnet_hw_interface_t *hw;
+  vmxnet3_main_t *vmxm = &vmxnet3_main;
+  vmxnet3_device_t *vd;
+  vnet_main_t *vnm = vnet_get_main ();
+  int enable_elog = 0, disable_elog = 0;
+
+  /* Get a line of input. */
+  if (!unformat_user (input, unformat_line_input, line_input))
+    return 0;
+
+  while (unformat_check_input (line_input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat (line_input, "sw_if_index %d", &sw_if_index))
+	;
+      else if (unformat (line_input, "elog-on"))
+	enable_elog = 1;
+      else if (unformat (line_input, "elog-off"))
+	disable_elog = 1;
+      else if (unformat (line_input, "%U", unformat_vnet_sw_interface,
+			 vnm, &sw_if_index))
+	;
+      else
+	return clib_error_return (0, "unknown input `%U'",
+				  format_unformat_error, input);
+    }
+  unformat_free (line_input);
+
+  if (sw_if_index == ~0)
+    return clib_error_return (0,
+			      "please specify interface name or sw_if_index");
+
+  hw = vnet_get_sup_hw_interface (vnm, sw_if_index);
+  if (hw == NULL || vmxnet3_device_class.index != hw->dev_class_index)
+    return clib_error_return (0, "not a vmxnet3 interface");
+
+  vd = pool_elt_at_index (vmxm->devices, hw->dev_instance);
+
+  if (enable_elog)
+    vd->flags |= VMXNET3_DEVICE_F_ELOG;
+
+  if (disable_elog)
+    vd->flags &= ~VMXNET3_DEVICE_F_ELOG;
+
+  return 0;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (vmxnet3_test_command, static) = {
+  .path = "test vmxnet3",
+  .short_help = "test vmxnet3 <interface> | sw_if_index <sw_idx> [irq] "
+    "[elog-on] [elog-off]",
+  .function = vmxnet3_test_command_fn,
+};
+/* *INDENT-ON* */
+
+static void
+show_vmxnet3 (vlib_main_t * vm, u32 * hw_if_indices, u8 show_descr)
+{
+  u32 i, desc_idx;
+  vmxnet3_device_t *vd;
+  vnet_main_t *vnm = &vnet_main;
+  vmxnet3_main_t *vmxm = &vmxnet3_main;
+  vnet_hw_interface_t *hi;
+  vmxnet3_rxq_t *rxq;
+  vmxnet3_rx_desc *rxd;
+  vmxnet3_rx_comp *rx_comp;
+  vmxnet3_txq_t *txq;
+  vmxnet3_tx_desc *txd;
+  vmxnet3_tx_comp *tx_comp;
+  u16 qid;
+
+  if (!hw_if_indices)
+    return;
+
+  for (i = 0; i < vec_len (hw_if_indices); i++)
+    {
+      hi = vnet_get_hw_interface (vnm, hw_if_indices[i]);
+      vd = vec_elt_at_index (vmxm->devices, hi->dev_instance);
+      vlib_cli_output (vm, "Interface: %s (ifindex %d)",
+		       hi->name, hw_if_indices[i]);
+      vlib_cli_output (vm, "  Version: %u", vd->version);
+      vlib_cli_output (vm, "  PCI Address: %U", format_vlib_pci_addr,
+		       &vd->pci_addr);
+      vlib_cli_output (vm, "  Mac Address: %U", format_ethernet_address,
+		       vd->mac_addr);
+      vlib_cli_output (vm, "  hw if index: %u", vd->hw_if_index);
+      vlib_cli_output (vm, "  Device instance: %u", vd->dev_instance);
+      vlib_cli_output (vm, "  Number of interrupts: %u", vd->num_intrs);
+
+      vec_foreach_index (qid, vd->rxqs)
+      {
+	rxq = vec_elt_at_index (vd->rxqs, qid);
+	u16 rid;
+
+	vlib_cli_output (vm, "  Queue %u (RX)", qid);
+	vlib_cli_output (vm, "    RX completion next index %u",
+			 rxq->rx_comp_ring.next);
+	for (rid = 0; rid < VMXNET3_RX_RING_SIZE; rid++)
+	  {
+	    vmxnet3_rx_ring *ring = &rxq->rx_ring[rid];
+
+	    vlib_cli_output (vm,
+			     "    ring %u size %u fill %u "
+			     "consume %u produce %u", rid,
+			     rxq->size, ring->fill, ring->consume,
+			     ring->produce);
+	    if (show_descr)
+	      {
+		vlib_cli_output (vm, "RX descriptors table");
+		vlib_cli_output (vm, "  %5s  %18s  %10s",
+				 "slot", "address", "flags");
+		for (desc_idx = 0; desc_idx < rxq->size; desc_idx++)
+		  {
+		    rxd = &rxq->rx_desc[rid][desc_idx];
+		    vlib_cli_output (vm, "  %5u  0x%016llx  0x%08x",
+				     desc_idx, rxd->address, rxd->flags);
+		  }
+		vlib_cli_output (vm, "RX completion descriptors table");
+		vlib_cli_output (vm, "  %5s  %10s  %10s  %10s  %10s",
+				 "slot", "index", "rss", "len", "flags");
+		for (desc_idx = 0; desc_idx < rxq->size; desc_idx++)
+		  {
+		    rx_comp = &rxq->rx_comp[desc_idx];
+		    vlib_cli_output (vm, "  %5u  0x%08x  %10u  %10u  0x%08x",
+				     desc_idx, rx_comp->index, rx_comp->rss,
+				     rx_comp->len, rx_comp->flags);
+		  }
+	      }
+	  }
+      }
+
+      vec_foreach_index (qid, vd->rxqs)
+      {
+	txq = vec_elt_at_index (vd->txqs, 0);
+	vlib_cli_output (vm, "  Queue %u (TX)", qid);
+	vlib_cli_output (vm, "    TX completion next index %u",
+			 txq->tx_comp_ring.next);
+	vlib_cli_output (vm, "    size %u consume %u produce %u",
+			 txq->size, txq->tx_ring.consume,
+			 txq->tx_ring.produce);
+	if (show_descr)
+	  {
+	    vlib_cli_output (vm, "TX descriptors table");
+	    vlib_cli_output (vm, "  %5s  %18s  %10s  %10s",
+			     "slot", "address", "flags0", "flags1");
+	    for (desc_idx = 0; desc_idx < txq->size; desc_idx++)
+	      {
+		txd = &txq->tx_desc[desc_idx];
+		vlib_cli_output (vm, "  %5u  0x%016llx  0x%08x  0x%08x",
+				 desc_idx, txd->address, txd->flags[0],
+				 txd->flags[1]);
+	      }
+	    vlib_cli_output (vm, "TX completion descriptors table");
+	    vlib_cli_output (vm, "  %5s  %10s  %10s",
+			     "slot", "index", "flags");
+	    for (desc_idx = 0; desc_idx < txq->size; desc_idx++)
+	      {
+		tx_comp = &txq->tx_comp[desc_idx];
+		vlib_cli_output (vm, "  %5u  0x%08x  0x%08x",
+				 desc_idx, tx_comp->index, tx_comp->flags);
+	      }
+	  }
+      }
+    }
+}
+
+static clib_error_t *
+show_vmxnet3_fn (vlib_main_t * vm, unformat_input_t * input,
+		 vlib_cli_command_t * cmd)
+{
+  vmxnet3_main_t *vmxm = &vmxnet3_main;
+  vnet_main_t *vnm = &vnet_main;
+  vmxnet3_device_t *vd;
+  clib_error_t *error = 0;
+  u32 hw_if_index, *hw_if_indices = 0;
+  vnet_hw_interface_t *hi;
+  u8 show_descr = 0;
+
+  while (unformat_check_input (input) != UNFORMAT_END_OF_INPUT)
+    {
+      if (unformat
+	  (input, "%U", unformat_vnet_hw_interface, vnm, &hw_if_index))
+	{
+	  hi = vnet_get_hw_interface (vnm, hw_if_index);
+	  if (vmxnet3_device_class.index != hi->dev_class_index)
+	    {
+	      error = clib_error_return (0, "unknown input `%U'",
+					 format_unformat_error, input);
+	      goto done;
+	    }
+	  vec_add1 (hw_if_indices, hw_if_index);
+	}
+      else if (unformat (input, "descriptors") || unformat (input, "desc"))
+	show_descr = 1;
+      else
+	{
+	  error = clib_error_return (0, "unknown input `%U'",
+				     format_unformat_error, input);
+	  goto done;
+	}
+    }
+
+  if (vec_len (hw_if_indices) == 0)
+    {
+      pool_foreach (vd, vmxm->devices,
+		    vec_add1 (hw_if_indices, vd->hw_if_index);
+	);
+    }
+
+  show_vmxnet3 (vm, hw_if_indices, show_descr);
+
+done:
+  vec_free (hw_if_indices);
+  return error;
+}
+
+/* *INDENT-OFF* */
+VLIB_CLI_COMMAND (show_vmxnet3_command, static) = {
+  .path = "show vmxnet3",
+  .short_help = "show vmxnet3 [<interface>]",
+  .function = show_vmxnet3_fn,
+};
+/* *INDENT-ON* */
+
+clib_error_t *
+vmxnet3_cli_init (vlib_main_t * vm)
+{
+  /* initialize binary API */
+  vmxnet3_plugin_api_hookup (vm);
+
+  return 0;
+}
+
+VLIB_INIT_FUNCTION (vmxnet3_cli_init);
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/plugins/vmxnet3/CMakeLists.txt
@@ -1,0 +1,37 @@
+# Copyright (c) 2018 Cisco and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+add_vpp_plugin(vmxnet3
+  SOURCES
+  cli.c
+  format.c
+  input.c
+  output.c
+  plugin.c
+  vmxnet3.c
+  vmxnet3_api.c
+
+  MULTIARCH_SOURCES
+  input.c
+  output.c
+
+  API_FILES
+  vmxnet3.api
+
+  API_TEST_SOURCES
+  vmxnet3_test.c
+
+  INSTALL_HEADERS
+  vmxnet3_all_api_h.h
+  vmxnet3_msg_enum.h
+)

src/plugins/vmxnet3/format.c
@@ -1,0 +1,143 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vlib/pci/pci.h>
+#include <vnet/ethernet/ethernet.h>
+
+#include <vmxnet3/vmxnet3.h>
+
+u8 *
+format_vmxnet3_device_name (u8 * s, va_list * args)
+{
+  u32 i = va_arg (*args, u32);
+  vmxnet3_main_t *vmxm = &vmxnet3_main;
+  vmxnet3_device_t *vd = vec_elt_at_index (vmxm->devices, i);
+  vlib_pci_addr_t *addr = vlib_pci_get_addr (vd->pci_dev_handle);
+
+  s = format (s, "vmxnet3-%x/%x/%x/%x",
+	      addr->domain, addr->bus, addr->slot, addr->function);
+  return s;
+}
+
+u8 *
+format_vmxnet3_device_flags (u8 * s, va_list * args)
+{
+  vmxnet3_device_t *vd = va_arg (*args, vmxnet3_device_t *);
+  u8 *t = 0;
+
+#define _(a, b, c) if (vd->flags & (1 << a)) \
+    t = format (t, "%s%s", t ? " ":"", c);
+  foreach_vmxnet3_device_flags
+#undef _
+    s = format (s, "%v", t);
+  vec_free (t);
+  return s;
+}
+
+u8 *
+format_vmxnet3_device (u8 * s, va_list * args)
+{
+  u32 i = va_arg (*args, u32);
+  vmxnet3_main_t *vmxm = &vmxnet3_main;
+  vmxnet3_device_t *vd = vec_elt_at_index (vmxm->devices, i);
+  u32 indent = format_get_indent (s);
+  vmxnet3_queues *q = &vd->dma->queues;
+  vmxnet3_rxq_t *rxq = vec_elt_at_index (vd->rxqs, 0);
+  vmxnet3_txq_t *txq = vec_elt_at_index (vd->txqs, 0);
+
+  s = format (s, "flags: %U", format_vmxnet3_device_flags, vd);
+  s = format (s, "\n%Urx queues %u, rx desc %u, tx queues %u, tx desc %u",
+	      format_white_space, indent,
+	      vd->num_rx_queues, rxq->size, vd->num_tx_queues, txq->size);
+  if (vd->error)
+    s = format (s, "\n%Uerror %U", format_white_space, indent,
+		format_clib_error, vd->error);
+
+  vmxnet3_reg_write (vd, 1, VMXNET3_REG_CMD, VMXNET3_CMD_GET_STATS);
+
+  s = format (s, "\n%UTX:", format_white_space, indent);
+  s = format (s, "\n%U  TSO packets                         %llu",
+	      format_white_space, indent, q->tx.stats.tso_pkts);
+  s = format (s, "\n%U  TSO bytes                           %llu",
+	      format_white_space, indent, q->tx.stats.tso_bytes);
+  s = format (s, "\n%U  ucast packets                       %llu",
+	      format_white_space, indent, q->tx.stats.ucast_pkts);
+  s = format (s, "\n%U  ucast bytes                         %llu",
+	      format_white_space, indent, q->tx.stats.ucast_bytes);
+  s = format (s, "\n%U  mcast packets                       %llu",
+	      format_white_space, indent, q->tx.stats.mcast_pkts);
+  s = format (s, "\n%U  mcast bytes                         %llu",
+	      format_white_space, indent, q->tx.stats.mcast_bytes);
+  s = format (s, "\n%U  bcast packets                       %llu",
+	      format_white_space, indent, q->tx.stats.bcast_pkts);
+  s = format (s, "\n%U  bcast bytes                         %llu",
+	      format_white_space, indent, q->tx.stats.bcast_bytes);
+  s = format (s, "\n%U  Errors packets                      %llu",
+	      format_white_space, indent, q->tx.stats.error_pkts);
+  s = format (s, "\n%U  Discard packets                     %llu",
+	      format_white_space, indent, q->tx.stats.discard_pkts);
+
+  s = format (s, "\n%URX:", format_white_space, indent);
+  s = format (s, "\n%U  LRO packets                         %llu",
+	      format_white_space, indent, q->rx.stats.lro_pkts);
+  s = format (s, "\n%U  LRO bytes                           %llu",
+	      format_white_space, indent, q->rx.stats.lro_bytes);
+  s = format (s, "\n%U  ucast packets                       %llu",
+	      format_white_space, indent, q->rx.stats.ucast_pkts);
+  s = format (s, "\n%U  ucast bytes                         %llu",
+	      format_white_space, indent, q->rx.stats.ucast_bytes);
+  s = format (s, "\n%U  mcast packets                       %llu",
+	      format_white_space, indent, q->rx.stats.mcast_pkts);
+  s = format (s, "\n%U  mcast bytes                         %llu",
+	      format_white_space, indent, q->rx.stats.mcast_bytes);
+  s = format (s, "\n%U  bcast packets                       %llu",
+	      format_white_space, indent, q->rx.stats.bcast_pkts);
+  s = format (s, "\n%U  bcast bytes                         %llu",
+	      format_white_space, indent, q->rx.stats.bcast_bytes);
+  s = format (s, "\n%U  No Bufs                             %llu",
+	      format_white_space, indent, q->rx.stats.nobuf_pkts);
+  s = format (s, "\n%U  Error packets                       %llu",
+	      format_white_space, indent, q->rx.stats.error_pkts);
+  return s;
+}
+
+u8 *
+format_vmxnet3_input_trace (u8 * s, va_list * args)
+{
+  vlib_main_t *vm = va_arg (*args, vlib_main_t *);
+  vlib_node_t *node = va_arg (*args, vlib_node_t *);
+  vmxnet3_input_trace_t *t = va_arg (*args, vmxnet3_input_trace_t *);
+  vnet_main_t *vnm = vnet_get_main ();
+  vnet_hw_interface_t *hi = vnet_get_hw_interface (vnm, t->hw_if_index);
+
+  s = format (s, "vmxnet3: %v (%d) next-node %U",
+	      hi->name, t->hw_if_index, format_vlib_next_node_name, vm,
+	      node->index, t->next_index);
+  s = format (s, "\n  buffer %U", format_vlib_buffer, &t->buffer);
+
+  return s;
+}
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/plugins/vmxnet3/input.c
@@ -1,0 +1,295 @@
+/*
+ *------------------------------------------------------------------
+ * Copyright (c) 2018 Cisco and/or its affiliates.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *------------------------------------------------------------------
+ */
+
+#include <vlib/vlib.h>
+#include <vlib/unix/unix.h>
+#include <vlib/pci/pci.h>
+#include <vnet/ethernet/ethernet.h>
+#include <vnet/devices/devices.h>
+
+#include <vmxnet3/vmxnet3.h>
+
+#define foreach_vmxnet3_input_error \
+  _(BUFFER_ALLOC, "buffer alloc error") \
+  _(RX_PACKET, "Rx packet error") \
+  _(NO_BUFFER, "Rx no buffer error")
+
+typedef enum
+{
+#define _(f,s) VMXNET3_INPUT_ERROR_##f,
+  foreach_vmxnet3_input_error
+#undef _
+    VMXNET3_INPUT_N_ERROR,
+} vmxnet3_input_error_t;
+
+static __clib_unused char *vmxnet3_input_error_strings[] = {
+#define _(n,s) s,
+  foreach_vmxnet3_input_error
+#undef _
+};
+
+static_always_inline u16
+vmxnet3_find_rid (vmxnet3_device_t * vd, vmxnet3_rx_comp * rx_comp)
+{
+  u32 rid;
+
+  // rid is bits 16-25 (10 bits number)
+  rid = rx_comp->index & (0xffffffff >> 6);
+  rid >>= 16;
+  if ((rid >= vd->num_rx_queues) && (rid < (vd->num_rx_queues << 1)))
+    return 1;
+  else
+    return 0;
+}
+
+static_always_inline void
+vmxnet3_rx_comp_ring_advance_next (vmxnet3_rxq_t * rxq)
+{
+  vmxnet3_rx_comp_ring *comp_ring = &rxq->rx_comp_ring;
+
+  comp_ring->next++;
+  if (PREDICT_FALSE (comp_ring->next == rxq->size))
+    {
+      comp_ring->next = 0;
+      comp_ring->gen ^= VMXNET3_RXCF_GEN;
+    }
+}
+
+static_always_inline uword
+vmxnet3_device_input_inline (vlib_main_t * vm, vlib_node_runtime_t * node,
+			     vlib_frame_t * frame, vmxnet3_device_t * vd,
+			     u16 qid)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+  uword n_trace = vlib_get_trace_count (vm, node);
+  u32 n_rx_packets = 0, n_rx_bytes = 0;
+  vmxnet3_rx_comp *rx_comp;
+  u32 comp_idx;
+  u32 desc_idx;
+  vmxnet3_rxq_t *rxq;
+  u32 thread_index = vm->thread_index;
+  u32 buffer_indices[VLIB_FRAME_SIZE], *bi;
+  u16 nexts[VLIB_FRAME_SIZE], *next;
+  vmxnet3_rx_ring *ring;
+  vmxnet3_rx_comp_ring *comp_ring;
+  u16 rid;
+  vlib_buffer_t *prev_b0 = 0, *hb = 0;
+
+  rxq = vec_elt_at_index (vd->rxqs, qid);
+  comp_ring = &rxq->rx_comp_ring;
+  bi = buffer_indices;
+  next = nexts;
+  while (comp_ring->gen ==
+	 (rxq->rx_comp[comp_ring->next].flags & VMXNET3_RXCF_GEN))
+    {
+      vlib_buffer_t *b0;
+
+      comp_idx = comp_ring->next;
+      rx_comp = &rxq->rx_comp[comp_idx];
+
+      rid = vmxnet3_find_rid (vd, rx_comp);
+      ring = &rxq->rx_ring[rid];
+
+      if (PREDICT_TRUE (ring->fill >= 1))
+	ring->fill--;
+      else
+	{
+	  vlib_error_count (vm, node->node_index,
+			    VMXNET3_INPUT_ERROR_NO_BUFFER, 1);
+	  break;
+	}
+
+      vmxnet3_rx_comp_ring_advance_next (rxq);
+      desc_idx = rx_comp->index & VMXNET3_RXC_INDEX;
+      ring->consume = desc_idx;
+
+      bi[0] = ring->bufs[desc_idx];
+      ring->bufs[desc_idx] = ~0;
+
+      b0 = vlib_get_buffer (vm, bi[0]);
+      vnet_buffer (b0)->sw_if_index[VLIB_RX] = vd->sw_if_index;
+      vnet_buffer (b0)->sw_if_index[VLIB_TX] = (u32) ~ 0;
+      b0->current_length = rx_comp->len & VMXNET3_RXCL_LEN_MASK;
+      b0->current_data = 0;
+      b0->total_length_not_including_first_buffer = 0;
+      b0->next_buffer = 0;
+      b0->flags = 0;
+      b0->error = 0;
+      ASSERT (b0->current_length != 0);
+
+      if (rx_comp->index & VMXNET3_RXCI_SOP)
+	{
+	  /* start segment */
+	  hb = b0;
+	  if (!(rx_comp->index & VMXNET3_RXCI_EOP))
+	    {
+	      hb->flags = VLIB_BUFFER_TOTAL_LENGTH_VALID;
+	      b0->flags |= VLIB_BUFFER_NEXT_PRESENT;
+	      prev_b0 = b0;
+	    }
+	  else
+	    {
+	      /*
+	       * Both start and end of packet is set. It is a complete packet
+	       */
+	      prev_b0 = 0;
+	    }
+
+	}
+      else if (rx_comp->index & VMXNET3_RXCI_EOP)
+	{
+	  /* end of segment */
+	  if (prev_b0)
+	    {
+	      prev_b0->next_buffer = bi[0];
+	      prev_b0->flags |= VLIB_BUFFER_NEXT_PRESENT;
+	      hb->total_length_not_including_first_buffer +=
+		b0->current_length;
+	      prev_b0 = 0;	// Get next packet
+	    }
+	  else
+	    {
+	      /* EOP without SOP, error */
+	      hb = 0;
+	      bi++;
+	      vlib_error_count (vm, node->node_index,
+				VMXNET3_INPUT_ERROR_RX_PACKET, 1);
+	      vlib_buffer_free_one (vm, bi[0]);
+	      continue;
+	    }
+	}
+      else if (prev_b0)		// !sop && !eop
+	{
+	  /* mid chain */
+	  b0->flags |= VLIB_BUFFER_NEXT_PRESENT;
+	  prev_b0->next_buffer = bi[0];
+	  prev_b0 = b0;
+	  hb->total_length_not_including_first_buffer += b0->current_length;
+	}
+      else
+	{
+	  ASSERT (0);
+	}
+
+      bi++;
+      n_rx_bytes += b0->current_length;
+
+      if (!prev_b0)
+	{
+	  next[0] = VNET_DEVICE_INPUT_NEXT_ETHERNET_INPUT;
+	  n_rx_packets++;
+	  next++;
+	  hb = 0;
+	}
+    }
+
+  if (PREDICT_FALSE ((n_trace = vlib_get_trace_count (vm, node))))
+    {
+      u32 n_left = n_rx_packets;
+
+      bi = buffer_indices;
+      next = nexts;
+      while (n_trace && n_left)
+	{
+	  vlib_buffer_t *b;
+	  vmxnet3_input_trace_t *tr;
+
+	  b = vlib_get_buffer (vm, bi[0]);
+	  vlib_trace_buffer (vm, node, next[0], b, /* follow_chain */ 0);
+	  tr = vlib_add_trace (vm, node, b, sizeof (*tr));
+	  tr->next_index = next[0];
+	  tr->hw_if_index = vd->hw_if_index;
+	  tr->buffer = *b;
+
+	  n_trace--;
+	  n_left--;
+	  bi++;
+	  next++;
+	}
+      vlib_set_trace_count (vm, node, n_trace);
+    }
+
+  if (PREDICT_TRUE (n_rx_packets))
+    {
+      clib_error_t *error;
+
+      vlib_buffer_enqueue_to_next (vm, node, buffer_indices, nexts,
+				   n_rx_packets);
+      vlib_increment_combined_counter
+	(vnm->interface_main.combined_sw_if_counters +
+	 VNET_INTERFACE_COUNTER_RX, thread_index,
+	 vd->hw_if_index, n_rx_packets, n_rx_bytes);
+
+      error = vmxnet3_rxq_refill_ring0 (vm, vd, rxq);
+      if (PREDICT_FALSE (error != 0))
+	{
+	  vlib_error_count (vm, node->node_index,
+			    VMXNET3_INPUT_ERROR_BUFFER_ALLOC, 1);
+	}
+      error = vmxnet3_rxq_refill_ring1 (vm, vd, rxq);
+      if (PREDICT_FALSE (error != 0))
+	{
+	  vlib_error_count (vm, node->node_index,
+			    VMXNET3_INPUT_ERROR_BUFFER_ALLOC, 1);
+	}
+    }
+
+  return n_rx_packets;
+}
+
+VLIB_NODE_FN (vmxnet3_input_node) (vlib_main_t * vm,
+				   vlib_node_runtime_t * node,
+				   vlib_frame_t * frame)
+{
+  u32 n_rx = 0;
+  vmxnet3_main_t *vmxm = &vmxnet3_main;
+  vnet_device_input_runtime_t *rt = (void *) node->runtime_data;
+  vnet_device_and_queue_t *dq;
+
+  foreach_device_and_queue (dq, rt->devices_and_queues)
+  {
+    vmxnet3_device_t *vd;
+    vd = vec_elt_at_index (vmxm->devices, dq->dev_instance);
+    if ((vd->flags & VMXNET3_DEVICE_F_ADMIN_UP) == 0)
+      continue;
+    n_rx += vmxnet3_device_input_inline (vm, node, frame, vd, dq->queue_id);
+  }
+  return n_rx;
+}
+
+#ifndef CLIB_MARCH_VARIANT
+/* *INDENT-OFF* */
+VLIB_REGISTER_NODE (vmxnet3_input_node) = {
+  .name = "vmxnet3-input",
+  .sibling_of = "device-input",
+  .format_trace = format_vmxnet3_input_trace,
+  .type = VLIB_NODE_TYPE_INPUT,
+  .state = VLIB_NODE_STATE_DISABLED,
+  .n_errors = VMXNET3_INPUT_N_ERROR,
+  .error_strings = vmxnet3_input_error_strings,
+};
+#endif
+
+/* *INDENT-ON* */
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

commit cbbc764ce73b6131bb670cd6413660e5640f7cb0

vmxnet3: memory leak on parsing bad CLI command
When parsing bad "create interface vmxnet3" command, we should call
unformat_free prior to return

Type: fix
Fixes: 8cc3f7aaa2a362f919cf2c9d1fb6c81966ae0a0d

Signed-off-by: Steven Luong <sluong@cisco.com>
Change-Id: I2663894c6cb8066ca2abb1c56f0ca46d0fef19f7

src/plugins/vmxnet3/cli.c
@@ -58,11 +58,13 @@ vmxnet3_create_command_fn (vlib_main_t * vm, unformat_input_t * input,
       else if (unformat (line_input, "num-rx-queues %u", &size))
 	args.rxq_num = size;
       else
-	return clib_error_return (0, "unknown input `%U'",
-				  format_unformat_error, input);
+	{
+	  unformat_free (line_input);
+	  return clib_error_return (0, "unknown input `%U'",
+				    format_unformat_error, input);
+	}
     }
   unformat_free (line_input);
-
 
   vmxnet3_create_if (vm, &args);
   if (args.error == 0)