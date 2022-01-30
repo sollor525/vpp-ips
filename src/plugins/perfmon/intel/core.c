sollor525/vpp-ips

commit 11b4a60079b4976258d5de4aac60a2a847524fd7

perfmon: prune bundles by available pmu counters
Prune perfmon bundles that exceed the number of available pmu counters.

Type: improvement

Signed-off-by: Ray Kinsella <mdr@ashroe.eu>
Change-Id: I70fec26bb8ca915f4b980963e06c2e43dfde5a23

src/plugins/perfmon/intel/core.c
@@ -77,11 +77,21 @@ intel_core_init (vlib_main_t *vm, perfmon_source_t *src)
   return 0;
 }
 
+u8
+intel_core_is_fixed (u32 event)
+{
+  u64 config = events[event].config;
+  u8 eventcode = (config & 0xFF);
+
+  return !eventcode ? 1 : 0;
+}
+
 PERFMON_REGISTER_SOURCE (intel_core) = {
   .name = "intel-core",
   .description = "intel arch core events",
   .events = events,
   .n_events = ARRAY_LEN (events),
   .init_fn = intel_core_init,
+  .is_fixed = intel_core_is_fixed,
   .format_config = format_intel_core_config,
 };

src/plugins/perfmon/perfmon.c
@@ -325,9 +325,35 @@ perfmon_stop (vlib_main_t *vm)
 }
 
 static_always_inline u8
+is_enough_counters (perfmon_bundle_t *b)
+{
+  struct
+  {
+    u8 general;
+    u8 fixed;
+  } bl = { 0, 0 }, cpu = { 0, 0 };
+
+  /* how many does this uarch support */
+  if (!clib_get_pmu_counter_count (&cpu.fixed, &cpu.general))
+    return 0;
+
+  /* how many does the bundle require */
+  for (u16 i = 0; i < b->n_events; i++)
+    if (b->src->is_fixed && b->src->is_fixed (b->events[i]))
+      bl.fixed++;
+    else
+      bl.general++;
+
+  return cpu.general >= bl.general && cpu.fixed >= bl.fixed;
+}
+
+static_always_inline u8
 is_bundle_supported (perfmon_bundle_t *b)
 {
   perfmon_cpu_supports_t *supports = b->cpu_supports;
+
+  if (!is_enough_counters (b))
+    return 0;
 
   if (!b->cpu_supports)
     return 1;
@@ -372,13 +398,6 @@ perfmon_init (vlib_main_t *vm)
       clib_error_t *err;
       uword *p;
 
-      if (!is_bundle_supported (b))
-	{
-	  log_debug ("skipping bundle '%s' - not supported", b->name);
-	  b = b->next;
-	  continue;
-	}
-
       if (hash_get_mem (pm->bundle_by_name, b->name) != 0)
 	clib_panic ("duplicate bundle name '%s'", b->name);
 
@@ -391,6 +410,13 @@ perfmon_init (vlib_main_t *vm)
 	}
 
       b->src = (perfmon_source_t *) p[0];
+      if (!is_bundle_supported (b))
+	{
+	  log_debug ("skipping bundle '%s' - not supported", b->name);
+	  b = b->next;
+	  continue;
+	}
+
       if (b->init_fn && ((err = (b->init_fn) (vm, b))))
 	{
 	  log_warn ("skipping bundle '%s' - %U", b->name, format_clib_error,

src/plugins/perfmon/perfmon.h
@@ -82,6 +82,8 @@ extern vlib_node_function_t *perfmon_dispatch_wrappers[PERF_MAX_EVENTS + 1];
 
 typedef clib_error_t *(perfmon_source_init_fn_t) (vlib_main_t *vm,
 						  struct perfmon_source *);
+typedef u8 (perfmon_source_is_fixed) (u32 event);
+
 typedef struct perfmon_source
 {
   char *name;
@@ -91,6 +93,7 @@ typedef struct perfmon_source
   u32 n_events;
   perfmon_instance_type_t *instances_by_type;
   format_function_t *format_config;
+  perfmon_source_is_fixed *is_fixed;
   perfmon_source_init_fn_t *init_fn;
 } perfmon_source_t;
 

src/vppinfra/cpu.h
@@ -266,6 +266,24 @@ clib_cpu_march_priority_hsw ()
   return -1;
 }
 
+#define X86_CPU_ARCH_PERF_FUNC 0xA
+
+static inline int
+clib_get_pmu_counter_count (u8 *fixed, u8 *general)
+{
+#if defined(__x86_64__)
+  u32 __clib_unused eax = 0, ebx = 0, ecx = 0, edx = 0;
+  clib_get_cpuid (X86_CPU_ARCH_PERF_FUNC, &eax, &ebx, &ecx, &edx);
+
+  *general = (eax & 0xFF00) >> 8;
+  *fixed = (edx & 0xF);
+
+  return 1;
+#else
+  return 0;
+#endif
+}
+
 static inline u32
 clib_cpu_implementer ()
 {

commit 975adcf395645fb7422af40d14e26561c1e5014e

perfmon: fix init of bundles with pseudo events
Previously Linux pseudo events were being counted as multiple fixed
events, such that a bundle with pseudo events could exceed the number of
available fixed counters. Reworked to ignore pseudo events in the
accounting for the moment.

Type: fix
Fixes: 11b4a6007
Signed-off-by: Ray Kinsella <mdr@ashroe.eu>
Change-Id: Ic938f8266fd04d7731afbd02e261c61ef22a8522

src/plugins/perfmon/intel/core.c
@@ -77,13 +77,22 @@ intel_core_init (vlib_main_t *vm, perfmon_source_t *src)
   return 0;
 }
 
-u8
-intel_core_is_fixed (u32 event)
+perfmon_event_type_t
+intel_core_get_event_type (u32 event)
 {
   u64 config = events[event].config;
   u8 eventcode = (config & 0xFF);
+  u8 umask = ((config >> 8) & 0xFF);
 
-  return !eventcode ? 1 : 0;
+  if (!eventcode) /* is fixed or pseudo */
+    {
+      if (umask >= 0x80) /* is pseudo */
+	return PERFMON_EVENT_TYPE_PSEUDO;
+      else /* is fixed */
+	return PERFMON_EVENT_TYPE_FIXED;
+    }
+  else
+    return PERFMON_EVENT_TYPE_GENERAL;
 }
 
 PERFMON_REGISTER_SOURCE (intel_core) = {
@@ -92,6 +101,6 @@ PERFMON_REGISTER_SOURCE (intel_core) = {
   .events = events,
   .n_events = ARRAY_LEN (events),
   .init_fn = intel_core_init,
-  .is_fixed = intel_core_is_fixed,
+  .get_event_type = intel_core_get_event_type,
   .format_config = format_intel_core_config,
 };

src/plugins/perfmon/perfmon.c
@@ -327,24 +327,30 @@ perfmon_stop (vlib_main_t *vm)
 static_always_inline u8
 is_enough_counters (perfmon_bundle_t *b)
 {
-  struct
-  {
-    u8 general;
-    u8 fixed;
-  } bl = { 0, 0 }, cpu = { 0, 0 };
+  u8 bl[PERFMON_EVENT_TYPE_MAX];
+  u8 cpu[PERFMON_EVENT_TYPE_MAX];
+
+  clib_memset (&bl, 0, sizeof (bl));
+  clib_memset (&cpu, 0, sizeof (cpu));
 
   /* how many does this uarch support */
-  if (!clib_get_pmu_counter_count (&cpu.fixed, &cpu.general))
+  if (!clib_get_pmu_counter_count (&cpu[PERFMON_EVENT_TYPE_FIXED],
+				   &cpu[PERFMON_EVENT_TYPE_GENERAL]))
     return 0;
 
   /* how many does the bundle require */
   for (u16 i = 0; i < b->n_events; i++)
-    if (b->src->is_fixed && b->src->is_fixed (b->events[i]))
-      bl.fixed++;
-    else
-      bl.general++;
+    {
+      /* if source allows us to identify events, otherwise assume general */
+      if (b->src->get_event_type)
+	bl[b->src->get_event_type (b->events[i])]++;
+      else
+	bl[PERFMON_EVENT_TYPE_GENERAL]++;
+    }
 
-  return cpu.general >= bl.general && cpu.fixed >= bl.fixed;
+  /* consciously ignoring pseudo events here */
+  return cpu[PERFMON_EVENT_TYPE_GENERAL] >= bl[PERFMON_EVENT_TYPE_GENERAL] &&
+	 cpu[PERFMON_EVENT_TYPE_FIXED] >= bl[PERFMON_EVENT_TYPE_FIXED];
 }
 
 static_always_inline u8

src/plugins/perfmon/perfmon.h
@@ -27,6 +27,14 @@
 
 typedef enum
 {
+  PERFMON_EVENT_TYPE_GENERAL,
+  PERFMON_EVENT_TYPE_FIXED,
+  PERFMON_EVENT_TYPE_PSEUDO,
+  PERFMON_EVENT_TYPE_MAX,
+} perfmon_event_type_t;
+
+typedef enum
+{
   PERFMON_BUNDLE_TYPE_UNKNOWN,
   PERFMON_BUNDLE_TYPE_NODE,
   PERFMON_BUNDLE_TYPE_THREAD,
@@ -82,7 +90,7 @@ extern vlib_node_function_t *perfmon_dispatch_wrappers[PERF_MAX_EVENTS + 1];
 
 typedef clib_error_t *(perfmon_source_init_fn_t) (vlib_main_t *vm,
 						  struct perfmon_source *);
-typedef u8 (perfmon_source_is_fixed) (u32 event);
+typedef perfmon_event_type_t (perfmon_source_get_event_type) (u32 event);
 
 typedef struct perfmon_source
 {
@@ -93,7 +101,7 @@ typedef struct perfmon_source
   u32 n_events;
   perfmon_instance_type_t *instances_by_type;
   format_function_t *format_config;
-  perfmon_source_is_fixed *is_fixed;
+  perfmon_source_get_event_type *get_event_type;
   perfmon_source_init_fn_t *init_fn;
 } perfmon_source_t;
 