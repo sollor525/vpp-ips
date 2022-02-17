sollor525/vpp-ips

commit b9e76d84760ba1f98bad610c1e9359f2fb92513e

wireguard: initial implementation of wireguard protocol
Type: feature

The main information about plugin you can see in README.md

vpp# wireguard ?
  wireguard create                         wireguard create listen-port <port> private-key <key> src <IP> [generate-key]
  wireguard delete                         wireguard delete <interface>
  wireguard peer add                       wireguard peer add <wg_int> public-key <pub_key_other>endpoint <ip4_dst> allowed-ip <prefix>dst-port [port_dst] persistent-keepalive [keepalive_interval]
  wireguard peer remove                    wireguard peer remove <index>

Change-Id: I85eb0bfc033ccfb2045696398d8a108b1c64b8d9
Signed-off-by: Artem Glazychev <artem.glazychev@xored.com>
Signed-off-by: Damjan Marion <damarion@cisco.com>
Signed-off-by: Jim Thompson <jim@netgate.com>
Signed-off-by: Neale Ranns <nranns@cisco.com>
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/plugins/wireguard/wireguard_if.c
@@ -1,0 +1,422 @@
+
+#include <vnet/adj/adj_midchain.h>
+#include <vnet/udp/udp.h>
+
+#include <wireguard/wireguard_messages.h>
+#include <wireguard/wireguard_if.h>
+#include <wireguard/wireguard.h>
+
+/* pool of interfaces */
+wg_if_t *wg_if_pool;
+
+/* bitmap of Allocated WG_ITF instances */
+static uword *wg_if_instances;
+
+/* vector of interfaces key'd on their sw_if_index */
+static index_t *wg_if_index_by_sw_if_index;
+
+/* vector of interfaces key'd on their UDP port (in network order) */
+index_t *wg_if_index_by_port;
+
+static u8 *
+format_wg_if_name (u8 * s, va_list * args)
+{
+  u32 dev_instance = va_arg (*args, u32);
+  return format (s, "wg%d", dev_instance);
+}
+
+u8 *
+format_wg_if (u8 * s, va_list * args)
+{
+  index_t wgii = va_arg (*args, u32);
+  wg_if_t *wgi = wg_if_get (wgii);
+  u8 key[NOISE_KEY_LEN_BASE64];
+
+  key_to_base64 (wgi->local.l_private, NOISE_PUBLIC_KEY_LEN, key);
+
+  s = format (s, "[%d] %U src:%U port:%d",
+	      wgii,
+	      format_vnet_sw_if_index_name, vnet_get_main (),
+	      wgi->sw_if_index, format_ip_address, &wgi->src_ip, wgi->port);
+
+  key_to_base64 (wgi->local.l_private, NOISE_PUBLIC_KEY_LEN, key);
+
+  s = format (s, " private-key:%s", key);
+
+  key_to_base64 (wgi->local.l_public, NOISE_PUBLIC_KEY_LEN, key);
+
+  s = format (s, " public-key:%s", key);
+
+  return (s);
+}
+
+index_t
+wg_if_find_by_sw_if_index (u32 sw_if_index)
+{
+  if (vec_len (wg_if_index_by_sw_if_index) <= sw_if_index)
+    return INDEX_INVALID;
+  u32 ti = wg_if_index_by_sw_if_index[sw_if_index];
+  if (ti == ~0)
+    return INDEX_INVALID;
+
+  return (ti);
+}
+
+static noise_remote_t *
+wg_remote_get (uint8_t public[NOISE_PUBLIC_KEY_LEN])
+{
+  wg_main_t *wmp = &wg_main;
+  wg_peer_t *peer = NULL;
+  wg_peer_t *peer_iter;
+  /* *INDENT-OFF* */
+  pool_foreach (peer_iter, wmp->peers,
+  ({
+    if (!memcmp (peer_iter->remote.r_public, public, NOISE_PUBLIC_KEY_LEN))
+    {
+      peer = peer_iter;
+      break;
+    }
+  }));
+  /* *INDENT-ON* */
+  return peer ? &peer->remote : NULL;
+}
+
+static uint32_t
+wg_index_set (noise_remote_t * remote)
+{
+  wg_main_t *wmp = &wg_main;
+  u32 rnd_seed = (u32) (vlib_time_now (wmp->vlib_main) * 1e6);
+  u32 ret =
+    wg_index_table_add (&wmp->index_table, remote->r_peer_idx, rnd_seed);
+  return ret;
+}
+
+static void
+wg_index_drop (uint32_t key)
+{
+  wg_main_t *wmp = &wg_main;
+  wg_index_table_del (&wmp->index_table, key);
+}
+
+static clib_error_t *
+wg_if_admin_up_down (vnet_main_t * vnm, u32 hw_if_index, u32 flags)
+{
+  vnet_hw_interface_t *hi;
+  index_t wgii;
+  u32 hw_flags;
+
+  hi = vnet_get_hw_interface (vnm, hw_if_index);
+  hw_flags = (flags & VNET_SW_INTERFACE_FLAG_ADMIN_UP ?
+	      VNET_HW_INTERFACE_FLAG_LINK_UP : 0);
+  vnet_hw_interface_set_flags (vnm, hw_if_index, hw_flags);
+
+  wgii = wg_if_find_by_sw_if_index (hi->sw_if_index);
+
+  wg_if_peer_walk (wg_if_get (wgii), wg_peer_if_admin_state_change, NULL);
+
+  return (NULL);
+}
+
+void
+wg_if_update_adj (vnet_main_t * vnm, u32 sw_if_index, adj_index_t ai)
+{
+  /* The peers manage the adjacencies */
+}
+
+
+/* *INDENT-OFF* */
+VNET_DEVICE_CLASS (wg_if_device_class) = {
+  .name = "Wireguard Tunnel",
+  .format_device_name = format_wg_if_name,
+  .admin_up_down_function = wg_if_admin_up_down,
+};
+
+VNET_HW_INTERFACE_CLASS(wg_hw_interface_class) = {
+  .name = "Wireguard",
+  .update_adjacency = wg_if_update_adj,
+  .flags = VNET_HW_INTERFACE_CLASS_FLAG_NBMA,
+};
+/* *INDENT-ON* */
+
+/*
+ * Maintain a bitmap of allocated wg_if instance numbers.
+ */
+#define WG_ITF_MAX_INSTANCE		(16 * 1024)
+
+static u32
+wg_if_instance_alloc (u32 want)
+{
+  /*
+   * Check for dynamically allocated instance number.
+   */
+  if (~0 == want)
+    {
+      u32 bit;
+
+      bit = clib_bitmap_first_clear (wg_if_instances);
+      if (bit >= WG_ITF_MAX_INSTANCE)
+	{
+	  return ~0;
+	}
+      wg_if_instances = clib_bitmap_set (wg_if_instances, bit, 1);
+      return bit;
+    }
+
+  /*
+   * In range?
+   */
+  if (want >= WG_ITF_MAX_INSTANCE)
+    {
+      return ~0;
+    }
+
+  /*
+   * Already in use?
+   */
+  if (clib_bitmap_get (wg_if_instances, want))
+    {
+      return ~0;
+    }
+
+  /*
+   * Grant allocation request.
+   */
+  wg_if_instances = clib_bitmap_set (wg_if_instances, want, 1);
+
+  return want;
+}
+
+static int
+wg_if_instance_free (u32 instance)
+{
+  if (instance >= WG_ITF_MAX_INSTANCE)
+    {
+      return -1;
+    }
+
+  if (clib_bitmap_get (wg_if_instances, instance) == 0)
+    {
+      return -1;
+    }
+
+  wg_if_instances = clib_bitmap_set (wg_if_instances, instance, 0);
+  return 0;
+}
+
+
+int
+wg_if_create (u32 user_instance,
+	      const u8 private_key[NOISE_PUBLIC_KEY_LEN],
+	      u16 port, const ip_address_t * src_ip, u32 * sw_if_indexp)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+  u32 instance, hw_if_index;
+  vnet_hw_interface_t *hi;
+  wg_if_t *wg_if;
+
+  ASSERT (sw_if_indexp);
+
+  *sw_if_indexp = (u32) ~ 0;
+
+  /*
+   * Allocate a wg_if instance. Either select on dynamically
+   * or try to use the desired user_instance number.
+   */
+  instance = wg_if_instance_alloc (user_instance);
+  if (instance == ~0)
+    return VNET_API_ERROR_INVALID_REGISTRATION;
+
+  pool_get (wg_if_pool, wg_if);
+
+  /* tunnel index (or instance) */
+  u32 t_idx = wg_if - wg_if_pool;
+
+  wg_if->user_instance = instance;
+  if (~0 == wg_if->user_instance)
+    wg_if->user_instance = t_idx;
+
+  udp_dst_port_info_t *pi = udp_get_dst_port_info (&udp_main, port, UDP_IP4);
+  if (pi)
+    return (VNET_API_ERROR_VALUE_EXIST);
+  udp_register_dst_port (vlib_get_main (), port, wg_input_node.index, 1);
+
+  vec_validate_init_empty (wg_if_index_by_port, port, INDEX_INVALID);
+  wg_if_index_by_port[port] = wg_if - wg_if_pool;
+
+  wg_if->port = port;
+  struct noise_upcall upcall;
+  upcall.u_remote_get = wg_remote_get;
+  upcall.u_index_set = wg_index_set;
+  upcall.u_index_drop = wg_index_drop;
+
+  noise_local_init (&wg_if->local, &upcall);
+  noise_local_set_private (&wg_if->local, private_key);
+  cookie_checker_update (&wg_if->cookie_checker, wg_if->local.l_public);
+
+  hw_if_index = vnet_register_interface (vnm,
+					 wg_if_device_class.index,
+					 t_idx,
+					 wg_hw_interface_class.index, t_idx);
+
+  hi = vnet_get_hw_interface (vnm, hw_if_index);
+
+  vec_validate_init_empty (wg_if_index_by_sw_if_index, hi->sw_if_index,
+			   INDEX_INVALID);
+  wg_if_index_by_sw_if_index[hi->sw_if_index] = t_idx;
+
+  ip_address_copy (&wg_if->src_ip, src_ip);
+  wg_if->sw_if_index = *sw_if_indexp = hi->sw_if_index;
+
+  return 0;
+}
+
+int
+wg_if_delete (u32 sw_if_index)
+{
+  vnet_main_t *vnm = vnet_get_main ();
+
+  if (pool_is_free_index (vnm->interface_main.sw_interfaces, sw_if_index))
+    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
+
+  vnet_hw_interface_t *hw = vnet_get_sup_hw_interface (vnm, sw_if_index);
+  if (hw == 0 || hw->dev_class_index != wg_if_device_class.index)
+    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
+
+  wg_if_t *wg_if;
+  wg_if = wg_if_get (wg_if_find_by_sw_if_index (sw_if_index));
+  if (NULL == wg_if)
+    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
+
+  if (wg_if_instance_free (hw->dev_instance) < 0)
+    return VNET_API_ERROR_INVALID_SW_IF_INDEX;
+
+  wg_if_index_by_port[wg_if->port] = INDEX_INVALID;
+  vnet_delete_hw_interface (vnm, hw->hw_if_index);
+  pool_put (wg_if_pool, wg_if);
+
+  return 0;
+}
+
+void
+wg_if_peer_add (wg_if_t * wgi, index_t peeri)
+{
+  hash_set (wgi->peers, peeri, peeri);
+
+  if (1 == hash_elts (wgi->peers))
+    vnet_feature_enable_disable ("ip4-output", "wg-output-tun",
+				 wgi->sw_if_index, 1, 0, 0);
+}
+
+void
+wg_if_peer_remove (wg_if_t * wgi, index_t peeri)
+{
+  hash_unset (wgi->peers, peeri);
+
+  if (0 == hash_elts (wgi->peers))
+    vnet_feature_enable_disable ("ip4-output", "wg-output-tun",
+				 wgi->sw_if_index, 0, 0, 0);
+}
+
+void
+wg_if_walk (wg_if_walk_cb_t fn, void *data)
+{
+  index_t wgii;
+
+  /* *INDENT-OFF* */
+  pool_foreach_index (wgii, wg_if_pool,
+  {
+    if (WALK_STOP == fn(wgii, data))
+      break;
+  });
+  /* *INDENT-ON* */
+}
+
+void
+wg_if_peer_walk (wg_if_t * wgi, wg_if_peer_walk_cb_t fn, void *data)
+{
+  index_t peeri, val;
+
+  /* *INDENT-OFF* */
+  hash_foreach (peeri, val, wgi->peers,
+  {
+    if (WALK_STOP == fn(wgi, peeri, data))
+      break;
+  });
+  /* *INDENT-ON* */
+}
+
+
+static void
+wg_if_table_bind_v4 (ip4_main_t * im,
+		     uword opaque,
+		     u32 sw_if_index, u32 new_fib_index, u32 old_fib_index)
+{
+  wg_if_t *wg_if;
+
+  wg_if = wg_if_get (wg_if_find_by_sw_if_index (sw_if_index));
+  if (NULL == wg_if)
+    return;
+
+  wg_peer_table_bind_ctx_t ctx = {
+    .af = AF_IP4,
+    .old_fib_index = old_fib_index,
+    .new_fib_index = new_fib_index,
+  };
+
+  wg_if_peer_walk (wg_if, wg_peer_if_table_change, &ctx);
+}
+
+static void
+wg_if_table_bind_v6 (ip6_main_t * im,
+		     uword opaque,
+		     u32 sw_if_index, u32 new_fib_index, u32 old_fib_index)
+{
+  wg_if_t *wg_if;
+
+  wg_if = wg_if_get (wg_if_find_by_sw_if_index (sw_if_index));
+  if (NULL == wg_if)
+    return;
+
+  wg_peer_table_bind_ctx_t ctx = {
+    .af = AF_IP6,
+    .old_fib_index = old_fib_index,
+    .new_fib_index = new_fib_index,
+  };
+
+  wg_if_peer_walk (wg_if, wg_peer_if_table_change, &ctx);
+}
+
+static clib_error_t *
+wg_if_module_init (vlib_main_t * vm)
+{
+  {
+    ip4_table_bind_callback_t cb = {
+      .function = wg_if_table_bind_v4,
+    };
+    vec_add1 (ip4_main.table_bind_callbacks, cb);
+  }
+  {
+    ip6_table_bind_callback_t cb = {
+      .function = wg_if_table_bind_v6,
+    };
+    vec_add1 (ip6_main.table_bind_callbacks, cb);
+  }
+
+  return (NULL);
+}
+
+/* *INDENT-OFF* */
+VLIB_INIT_FUNCTION (wg_if_module_init) =
+{
+  .runs_after = VLIB_INITS("ip_main_init"),
+};
+/* *INDENT-ON* */
+
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

MAINTAINERS
@@ -688,6 +688,11 @@ M:	Nathan Skrzypczak <nathan.skrzypczak@gmail.com>
 M:	Neale Ranns <nranns@cisco.com>
 F:	src/plugins/cnat
 
+Plugin - Wireguard
+I:	wireguard
+M:	Artem Glazychev <artem.glazychev@xored.com>
+F:	src/plugins/wireguard
+
 VPP Config Tooling
 I:	vpp_config
 M:	John DeNisco <jdenisco@cisco.com>

src/plugins/wireguard/CMakeLists.txt
@@ -1,0 +1,54 @@
+
+# Copyright (c) 2020 Doc.ai and/or its affiliates.
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+if (OPENSSL_VERSION VERSION_LESS 1.1.0)
+  return()
+endif()
+
+list(APPEND WG_BLAKE_SOURCES
+  blake/blake2s.h
+  blake/blake2s.c
+)
+
+add_vpp_plugin(wireguard
+  SOURCES
+  ${WG_BLAKE_SOURCES}
+  wireguard.c
+  wireguard.h
+  wireguard_if.c
+  wireguard_if.h
+  wireguard_input.c
+  wireguard_output_tun.c
+  wireguard_key.c
+  wireguard_key.h
+  wireguard_cli.c
+  wireguard_messages.h
+  wireguard_noise.c
+  wireguard_noise.h
+  wireguard_send.c
+  wireguard_send.h
+  wireguard_cookie.c
+  wireguard_cookie.h
+  wireguard_peer.c
+  wireguard_peer.h
+  wireguard_timer.c
+  wireguard_timer.h
+  wireguard_index_table.c
+  wireguard_index_table.h
+  wireguard_api.c
+
+  API_FILES
+  wireguard.api
+
+)

src/plugins/wireguard/FEATURE.yaml
@@ -1,0 +1,12 @@
+---
+name: Wireguard protocol
+maintainer: Artem Glazychev <artem.glazychev@xored.com>
+features:
+  - "based on wireguard-openbsd implementation: https://git.zx2c4.com/wireguard-openbsd"
+  - creating secure VPN-tunnel
+description: "Wireguard protocol implementation"
+state: development
+properties: [API, CLI]
+missing:
+  - IPv6 support
+  - DoS protection as in the original protocol

src/plugins/wireguard/blake/blake2-impl.h
@@ -1,0 +1,187 @@
+/*
+ * Copyright (c) 2020 Doc.ai and/or its affiliates.
+ * Copyright (c) 2012 Samuel Neves <sneves@dei.uc.pt>.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+/*
+   More information about the BLAKE2 hash function can be found at
+   https://blake2.net.
+*/
+#ifndef __included_crypto_blake2_impl_h__
+#define __included_crypto_blake2_impl_h__
+
+#include <stdint.h>
+#include <string.h>
+#include <vppinfra/byte_order.h>
+
+#if defined(CLIB_ARCH_IS_LITTLE_ENDIAN)
+#define NATIVE_LITTLE_ENDIAN
+#endif
+
+#if !defined(__cplusplus) && (!defined(__STDC_VERSION__) || __STDC_VERSION__ < 199901L)
+#if   defined(_MSC_VER)
+#define BLAKE2_INLINE __inline
+#elif defined(__GNUC__)
+#define BLAKE2_INLINE __inline__
+#else
+#define BLAKE2_INLINE
+#endif
+#else
+#define BLAKE2_INLINE inline
+#endif
+
+static BLAKE2_INLINE uint32_t
+load32 (const void *src)
+{
+#if defined(NATIVE_LITTLE_ENDIAN)
+  uint32_t w;
+  memcpy (&w, src, sizeof w);
+  return w;
+#else
+  const uint8_t *p = (const uint8_t *) src;
+  return ((uint32_t) (p[0]) << 0) |
+    ((uint32_t) (p[1]) << 8) |
+    ((uint32_t) (p[2]) << 16) | ((uint32_t) (p[3]) << 24);
+#endif
+}
+
+static BLAKE2_INLINE uint64_t
+load64 (const void *src)
+{
+#if defined(NATIVE_LITTLE_ENDIAN)
+  uint64_t w;
+  memcpy (&w, src, sizeof w);
+  return w;
+#else
+  const uint8_t *p = (const uint8_t *) src;
+  return ((uint64_t) (p[0]) << 0) |
+    ((uint64_t) (p[1]) << 8) |
+    ((uint64_t) (p[2]) << 16) |
+    ((uint64_t) (p[3]) << 24) |
+    ((uint64_t) (p[4]) << 32) |
+    ((uint64_t) (p[5]) << 40) |
+    ((uint64_t) (p[6]) << 48) | ((uint64_t) (p[7]) << 56);
+#endif
+}
+
+static BLAKE2_INLINE uint16_t
+load16 (const void *src)
+{
+#if defined(NATIVE_LITTLE_ENDIAN)
+  uint16_t w;
+  memcpy (&w, src, sizeof w);
+  return w;
+#else
+  const uint8_t *p = (const uint8_t *) src;
+  return (uint16_t) (((uint32_t) (p[0]) << 0) | ((uint32_t) (p[1]) << 8));
+#endif
+}
+
+static BLAKE2_INLINE void
+store16 (void *dst, uint16_t w)
+{
+#if defined(NATIVE_LITTLE_ENDIAN)
+  memcpy (dst, &w, sizeof w);
+#else
+  uint8_t *p = (uint8_t *) dst;
+  *p++ = (uint8_t) w;
+  w >>= 8;
+  *p++ = (uint8_t) w;
+#endif
+}
+
+static BLAKE2_INLINE void
+store32 (void *dst, uint32_t w)
+{
+#if defined(NATIVE_LITTLE_ENDIAN)
+  memcpy (dst, &w, sizeof w);
+#else
+  uint8_t *p = (uint8_t *) dst;
+  p[0] = (uint8_t) (w >> 0);
+  p[1] = (uint8_t) (w >> 8);
+  p[2] = (uint8_t) (w >> 16);
+  p[3] = (uint8_t) (w >> 24);
+#endif
+}
+
+static BLAKE2_INLINE void
+store64 (void *dst, uint64_t w)
+{
+#if defined(NATIVE_LITTLE_ENDIAN)
+  memcpy (dst, &w, sizeof w);
+#else
+  uint8_t *p = (uint8_t *) dst;
+  p[0] = (uint8_t) (w >> 0);
+  p[1] = (uint8_t) (w >> 8);
+  p[2] = (uint8_t) (w >> 16);
+  p[3] = (uint8_t) (w >> 24);
+  p[4] = (uint8_t) (w >> 32);
+  p[5] = (uint8_t) (w >> 40);
+  p[6] = (uint8_t) (w >> 48);
+  p[7] = (uint8_t) (w >> 56);
+#endif
+}
+
+static BLAKE2_INLINE uint64_t
+load48 (const void *src)
+{
+  const uint8_t *p = (const uint8_t *) src;
+  return ((uint64_t) (p[0]) << 0) |
+    ((uint64_t) (p[1]) << 8) |
+    ((uint64_t) (p[2]) << 16) |
+    ((uint64_t) (p[3]) << 24) |
+    ((uint64_t) (p[4]) << 32) | ((uint64_t) (p[5]) << 40);
+}
+
+static BLAKE2_INLINE void
+store48 (void *dst, uint64_t w)
+{
+  uint8_t *p = (uint8_t *) dst;
+  p[0] = (uint8_t) (w >> 0);
+  p[1] = (uint8_t) (w >> 8);
+  p[2] = (uint8_t) (w >> 16);
+  p[3] = (uint8_t) (w >> 24);
+  p[4] = (uint8_t) (w >> 32);
+  p[5] = (uint8_t) (w >> 40);
+}
+
+static BLAKE2_INLINE uint32_t
+rotr32 (const uint32_t w, const unsigned c)
+{
+  return (w >> c) | (w << (32 - c));
+}
+
+static BLAKE2_INLINE uint64_t
+rotr64 (const uint64_t w, const unsigned c)
+{
+  return (w >> c) | (w << (64 - c));
+}
+
+/* prevents compiler optimizing out memset() */
+static BLAKE2_INLINE void
+secure_zero_memory (void *v, size_t n)
+{
+  static void *(*const volatile memset_v) (void *, int, size_t) = &memset;
+  memset_v (v, 0, n);
+}
+
+#endif //__included_crypto_blake2_impl_h__
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

src/plugins/wireguard/blake/blake2s.c
@@ -1,0 +1,335 @@
+/*
+ * Copyright (c) 2020 Doc.ai and/or its affiliates.
+ * Copyright (c) 2012 Samuel Neves <sneves@dei.uc.pt>.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at:
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+/*
+   More information about the BLAKE2 hash function can be found at
+   https://blake2.net.
+*/
+
+#include <stdint.h>
+#include <string.h>
+#include <stdio.h>
+
+#include <wireguard/blake/blake2s.h>
+#include "blake2-impl.h"
+
+static const uint32_t blake2s_IV[8] = {
+  0x6A09E667UL, 0xBB67AE85UL, 0x3C6EF372UL, 0xA54FF53AUL,
+  0x510E527FUL, 0x9B05688CUL, 0x1F83D9ABUL, 0x5BE0CD19UL
+};
+
+static const uint8_t blake2s_sigma[10][16] = {
+  {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15},
+  {14, 10, 4, 8, 9, 15, 13, 6, 1, 12, 0, 2, 11, 7, 5, 3},
+  {11, 8, 12, 0, 5, 2, 15, 13, 10, 14, 3, 6, 7, 1, 9, 4},
+  {7, 9, 3, 1, 13, 12, 11, 14, 2, 6, 5, 10, 4, 0, 15, 8},
+  {9, 0, 5, 7, 2, 4, 10, 15, 14, 1, 11, 12, 6, 8, 3, 13},
+  {2, 12, 6, 10, 0, 11, 8, 3, 4, 13, 7, 5, 15, 14, 1, 9},
+  {12, 5, 1, 15, 14, 13, 4, 10, 0, 7, 6, 3, 9, 2, 8, 11},
+  {13, 11, 7, 14, 12, 1, 3, 9, 5, 0, 15, 4, 8, 6, 2, 10},
+  {6, 15, 14, 9, 11, 3, 0, 8, 12, 2, 13, 7, 1, 4, 10, 5},
+  {10, 2, 8, 4, 7, 6, 1, 5, 15, 11, 9, 14, 3, 12, 13, 0},
+};
+
+static void
+blake2s_set_lastnode (blake2s_state_t * S)
+{
+  S->f[1] = (uint32_t) - 1;
+}
+
+/* Some helper functions, not necessarily useful */
+static int
+blake2s_is_lastblock (const blake2s_state_t * S)
+{
+  return S->f[0] != 0;
+}
+
+static void
+blake2s_set_lastblock (blake2s_state_t * S)
+{
+  if (S->last_node)
+    blake2s_set_lastnode (S);
+
+  S->f[0] = (uint32_t) - 1;
+}
+
+static void
+blake2s_increment_counter (blake2s_state_t * S, const uint32_t inc)
+{
+  S->t[0] += inc;
+  S->t[1] += (S->t[0] < inc);
+}
+
+static void
+blake2s_init0 (blake2s_state_t * S)
+{
+  size_t i;
+  memset (S, 0, sizeof (blake2s_state_t));
+
+  for (i = 0; i < 8; ++i)
+    S->h[i] = blake2s_IV[i];
+}
+
+/* init2 xors IV with input parameter block */
+int
+blake2s_init_param (blake2s_state_t * S, const blake2s_param_t * P)
+{
+  const unsigned char *p = (const unsigned char *) (P);
+  size_t i;
+
+  blake2s_init0 (S);
+
+  /* IV XOR ParamBlock */
+  for (i = 0; i < 8; ++i)
+    S->h[i] ^= load32 (&p[i * 4]);
+
+  S->outlen = P->digest_length;
+  return 0;
+}
+
+
+/* Sequential blake2s initialization */
+int
+blake2s_init (blake2s_state_t * S, size_t outlen)
+{
+  blake2s_param_t P[1];
+
+  /* Move interval verification here? */
+  if ((!outlen) || (outlen > BLAKE2S_OUT_BYTES))
+    return -1;
+
+  P->digest_length = (uint8_t) outlen;
+  P->key_length = 0;
+  P->fanout = 1;
+  P->depth = 1;
+  store32 (&P->leaf_length, 0);
+  store32 (&P->node_offset, 0);
+  store16 (&P->xof_length, 0);
+  P->node_depth = 0;
+  P->inner_length = 0;
+  /* memset(P->reserved, 0, sizeof(P->reserved) ); */
+  memset (P->salt, 0, sizeof (P->salt));
+  memset (P->personal, 0, sizeof (P->personal));
+  return blake2s_init_param (S, P);
+}
+
+int
+blake2s_init_key (blake2s_state_t * S, size_t outlen, const void *key,
+		  size_t keylen)
+{
+  blake2s_param_t P[1];
+
+  if ((!outlen) || (outlen > BLAKE2S_OUT_BYTES))
+    return -1;
+
+  if (!key || !keylen || keylen > BLAKE2S_KEY_BYTES)
+    return -1;
+
+  P->digest_length = (uint8_t) outlen;
+  P->key_length = (uint8_t) keylen;
+  P->fanout = 1;
+  P->depth = 1;
+  store32 (&P->leaf_length, 0);
+  store32 (&P->node_offset, 0);
+  store16 (&P->xof_length, 0);
+  P->node_depth = 0;
+  P->inner_length = 0;
+  /* memset(P->reserved, 0, sizeof(P->reserved) ); */
+  memset (P->salt, 0, sizeof (P->salt));
+  memset (P->personal, 0, sizeof (P->personal));
+
+  if (blake2s_init_param (S, P) < 0)
+    return -1;
+
+  {
+    uint8_t block[BLAKE2S_BLOCK_BYTES];
+    memset (block, 0, BLAKE2S_BLOCK_BYTES);
+    memcpy (block, key, keylen);
+    blake2s_update (S, block, BLAKE2S_BLOCK_BYTES);
+    secure_zero_memory (block, BLAKE2S_BLOCK_BYTES);	/* Burn the key from stack */
+  }
+  return 0;
+}
+
+#define G(r,i,a,b,c,d)                      \
+  do {                                      \
+    a = a + b + m[blake2s_sigma[r][2*i+0]]; \
+    d = rotr32(d ^ a, 16);                  \
+    c = c + d;                              \
+    b = rotr32(b ^ c, 12);                  \
+    a = a + b + m[blake2s_sigma[r][2*i+1]]; \
+    d = rotr32(d ^ a, 8);                   \
+    c = c + d;                              \
+    b = rotr32(b ^ c, 7);                   \
+  } while(0)
+
+#define ROUND(r)                    \
+  do {                              \
+    G(r,0,v[ 0],v[ 4],v[ 8],v[12]); \
+    G(r,1,v[ 1],v[ 5],v[ 9],v[13]); \
+    G(r,2,v[ 2],v[ 6],v[10],v[14]); \
+    G(r,3,v[ 3],v[ 7],v[11],v[15]); \
+    G(r,4,v[ 0],v[ 5],v[10],v[15]); \
+    G(r,5,v[ 1],v[ 6],v[11],v[12]); \
+    G(r,6,v[ 2],v[ 7],v[ 8],v[13]); \
+    G(r,7,v[ 3],v[ 4],v[ 9],v[14]); \
+  } while(0)
+
+static void
+blake2s_compress (blake2s_state_t * S, const uint8_t in[BLAKE2S_BLOCK_BYTES])
+{
+  uint32_t m[16];
+  uint32_t v[16];
+  size_t i;
+
+  for (i = 0; i < 16; ++i)
+    {
+      m[i] = load32 (in + i * sizeof (m[i]));
+    }
+
+  for (i = 0; i < 8; ++i)
+    {
+      v[i] = S->h[i];
+    }
+
+  v[8] = blake2s_IV[0];
+  v[9] = blake2s_IV[1];
+  v[10] = blake2s_IV[2];
+  v[11] = blake2s_IV[3];
+  v[12] = S->t[0] ^ blake2s_IV[4];
+  v[13] = S->t[1] ^ blake2s_IV[5];
+  v[14] = S->f[0] ^ blake2s_IV[6];
+  v[15] = S->f[1] ^ blake2s_IV[7];
+
+  ROUND (0);
+  ROUND (1);
+  ROUND (2);
+  ROUND (3);
+  ROUND (4);
+  ROUND (5);
+  ROUND (6);
+  ROUND (7);
+  ROUND (8);
+  ROUND (9);
+
+  for (i = 0; i < 8; ++i)
+    {
+      S->h[i] = S->h[i] ^ v[i] ^ v[i + 8];
+    }
+}
+
+#undef G
+#undef ROUND
+
+int
+blake2s_update (blake2s_state_t * S, const void *pin, size_t inlen)
+{
+  const unsigned char *in = (const unsigned char *) pin;
+  if (inlen > 0)
+    {
+      size_t left = S->buflen;
+      size_t fill = BLAKE2S_BLOCK_BYTES - left;
+      if (inlen > fill)
+	{
+	  S->buflen = 0;
+	  memcpy (S->buf + left, in, fill);	/* Fill buffer */
+	  blake2s_increment_counter (S, BLAKE2S_BLOCK_BYTES);
+	  blake2s_compress (S, S->buf);	/* Compress */
+	  in += fill;
+	  inlen -= fill;
+	  while (inlen > BLAKE2S_BLOCK_BYTES)
+	    {
+	      blake2s_increment_counter (S, BLAKE2S_BLOCK_BYTES);
+	      blake2s_compress (S, in);
+	      in += BLAKE2S_BLOCK_BYTES;
+	      inlen -= BLAKE2S_BLOCK_BYTES;
+	    }
+	}
+      memcpy (S->buf + S->buflen, in, inlen);
+      S->buflen += inlen;
+    }
+  return 0;
+}
+
+int
+blake2s_final (blake2s_state_t * S, void *out, size_t outlen)
+{
+  uint8_t buffer[BLAKE2S_OUT_BYTES] = { 0 };
+  size_t i;
+
+  if (out == NULL || outlen < S->outlen)
+    return -1;
+
+  if (blake2s_is_lastblock (S))
+    return -1;
+
+  blake2s_increment_counter (S, (uint32_t) S->buflen);
+  blake2s_set_lastblock (S);
+  memset (S->buf + S->buflen, 0, BLAKE2S_BLOCK_BYTES - S->buflen);	/* Padding */
+  blake2s_compress (S, S->buf);
+
+  for (i = 0; i < 8; ++i)	/* Output full hash to temp buffer */
+    store32 (buffer + sizeof (S->h[i]) * i, S->h[i]);
+
+  memcpy (out, buffer, outlen);
+  secure_zero_memory (buffer, sizeof (buffer));
+  return 0;
+}
+
+int
+blake2s (void *out, size_t outlen, const void *in, size_t inlen,
+	 const void *key, size_t keylen)
+{
+  blake2s_state_t S[1];
+
+  /* Verify parameters */
+  if (NULL == in && inlen > 0)
+    return -1;
+
+  if (NULL == out)
+    return -1;
+
+  if (NULL == key && keylen > 0)
+    return -1;
+
+  if (!outlen || outlen > BLAKE2S_OUT_BYTES)
+    return -1;
+
+  if (keylen > BLAKE2S_KEY_BYTES)
+    return -1;
+
+  if (keylen > 0)
+    {
+      if (blake2s_init_key (S, outlen, key, keylen) < 0)
+	return -1;
+    }
+  else
+    {
+      if (blake2s_init (S, outlen) < 0)
+	return -1;
+    }
+
+  blake2s_update (S, (const uint8_t *) in, inlen);
+  blake2s_final (S, out, outlen);
+  return 0;
+}
+
+/*
+ * fd.io coding-style-patch-verification: ON
+ *
+ * Local Variables:
+ * eval: (c-set-style "gnu")
+ * End:
+ */

commit 99c29818377572139a2c7cd1879ffa2b2f2452df

wireguard: Fix wireguard device name to use user-instance
If the user supplies an instance number when creating a wireguard
interface, it should be used in the device name.  If no user-instance
is given, the device instnce (tunnel number) is used.

For example:
    vpp# wireguard create instance 17 listen-port 23023 src 1.2.3.4
            private-key MDEwMjAzMDQwNTA2MDcwODA5MTAxMTEyMTMxNDE1MTY=
    wg17

Type: fix
Fixes: b9e76d84760ba1f98bad610c1e9359f2fb92513e
Signed-off-by: Jon Loeliger <jdl@netgate.com>
Change-Id: Ibf51868bf7b4b9e8a3f85557d05667207873bc91

src/plugins/wireguard/wireguard_if.c
@@ -38,7 +38,8 @@ static u8 *
 format_wg_if_name (u8 * s, va_list * args)
 {
   u32 dev_instance = va_arg (*args, u32);
-  return format (s, "wg%d", dev_instance);
+  wg_if_t *wgi = wg_if_get (dev_instance);
+  return format (s, "wg%d", wgi->user_instance);
 }
 
 u8 *