FDio/vpp

libmemif: fix chain buffer support
Type: fix

This patch fixes chain buffer support as transmit side
missing to set the flag to the descriptor in case of
chain buffers.

Signed-off-by: Mohsin Kazmi <sykazmi@cisco.com>
Change-Id: I73ff11be69a388f14fea39a19272d8eb76148fba

extras/libmemif/examples/common/packet_handler.c
@@ -47,17 +47,37 @@ icmp_packet_handler (memif_connection_t *c)
     {
       for (i = 0; i < c->tx_buf_num; i++)
 	{
-	  resolve_packet (c->rx_bufs[i].data, c->rx_bufs[i].len,
-			  c->tx_bufs[i].data, &c->tx_bufs[i].len, c->ip_addr,
-			  c->hw_addr);
+	  uint32_t len;
+	  void *packet = c->rx_bufs[i].data;
+
+	  memcpy (c->tx_bufs[i].data, c->rx_bufs[i].data, c->rx_bufs[i].len);
+	  c->tx_bufs[i].flags = c->rx_bufs[i].flags;
+	  len = c->tx_bufs[i].len = c->rx_bufs[i].len;
+
+	  while (c->rx_bufs[i].flags & MEMIF_BUFFER_FLAG_NEXT)
+	    {
+	      i++;
+	      memcpy (c->tx_bufs[i].data, c->rx_bufs[i].data,
+		      c->rx_bufs[i].len);
+	      c->tx_bufs[i].flags = c->rx_bufs[i].flags;
+	      len += c->tx_bufs[i].len = c->rx_bufs[i].len;
+	    }
+
+	  resolve_packet_zero_copy (packet, &len, c->ip_addr, c->hw_addr);
 	}
     }
   else
     {
       for (i = 0; i < c->rx_buf_num; i++)
 	{
-	  resolve_packet_zero_copy (c->rx_bufs[i].data, &c->rx_bufs[i].len,
-				    c->ip_addr, c->hw_addr);
+	  uint32_t len = c->rx_bufs[i].len;
+	  void *packet = c->rx_bufs[i].data;
+	  while (c->rx_bufs[i].flags & MEMIF_BUFFER_FLAG_NEXT)
+	    {
+	      i++;
+	      len += c->rx_bufs[i].len;
+	    }
+	  resolve_packet_zero_copy (packet, &len, c->ip_addr, c->hw_addr);
 	}
     }
 

sollor525/vpp-ips

extras/libmemif/examples/common/packet_handler.c
@@ -47,17 +47,37 @@ icmp_packet_handler (memif_connection_t *c)
     {
       for (i = 0; i < c->tx_buf_num; i++)
 	{
-	  resolve_packet (c->rx_bufs[i].data, c->rx_bufs[i].len,
-			  c->tx_bufs[i].data, &c->tx_bufs[i].len, c->ip_addr,
-			  c->hw_addr);
+	  uint32_t len;
+	  void *packet = c->rx_bufs[i].data;
+
+	  memcpy (c->tx_bufs[i].data, c->rx_bufs[i].data, c->rx_bufs[i].len);
+	  c->tx_bufs[i].flags = c->rx_bufs[i].flags;
+	  len = c->tx_bufs[i].len = c->rx_bufs[i].len;
+
+	  while (c->rx_bufs[i].flags & MEMIF_BUFFER_FLAG_NEXT)
+	    {
+	      i++;
+	      memcpy (c->tx_bufs[i].data, c->rx_bufs[i].data,
+		      c->rx_bufs[i].len);
+	      c->tx_bufs[i].flags = c->rx_bufs[i].flags;
+	      len += c->tx_bufs[i].len = c->rx_bufs[i].len;
+	    }
+
+	  resolve_packet (packet, &len, c->ip_addr, c->hw_addr);
 	}
     }
   else
     {
       for (i = 0; i < c->rx_buf_num; i++)
 	{
-	  resolve_packet_zero_copy (c->rx_bufs[i].data, &c->rx_bufs[i].len,
-				    c->ip_addr, c->hw_addr);
+	  uint32_t len = c->rx_bufs[i].len;
+	  void *packet = c->rx_bufs[i].data;
+	  while (c->rx_bufs[i].flags & MEMIF_BUFFER_FLAG_NEXT)
+	    {
+	      i++;
+	      len += c->rx_bufs[i].len;
+	    }
+	  resolve_packet (packet, &len, c->ip_addr, c->hw_addr);
 	}
     }
 