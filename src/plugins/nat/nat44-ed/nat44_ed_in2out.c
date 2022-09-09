FDio/vpp

nat: fix nat44-ed-in2out fast path next node
When a session is found expired, the next node of in2out fast path
should be in2out slow path instead of out2in slow path.

Type: fix
Signed-off-by: Jing Peng <jing@meter.com>
Change-Id: If1dd920502089c25b33bea5434823b0496a44499

src/plugins/nat/nat44-ed/nat44_ed_in2out.c
@@ -1193,7 +1193,7 @@ nat44_ed_in2out_fast_path_node_fn_inline (vlib_main_t *vm,
 	  // session is closed, go slow path
 	  nat44_ed_free_session_data (sm, s0, thread_index, 0);
 	  nat_ed_session_delete (sm, s0, thread_index, 1);
-	  next[0] = NAT_NEXT_OUT2IN_ED_SLOW_PATH;
+	  next[0] = NAT_NEXT_IN2OUT_ED_SLOW_PATH;
 	  goto trace0;
 	}
 

sollor525/vpp-ips

src/plugins/nat/nat44-ed/nat44_ed_in2out.c
@@ -1193,7 +1193,7 @@ nat44_ed_in2out_fast_path_node_fn_inline (vlib_main_t *vm,
 	  // session is closed, go slow path
 	  nat44_ed_free_session_data (sm, s0, thread_index, 0);
 	  nat_ed_session_delete (sm, s0, thread_index, 1);
-	  next[0] = NAT_NEXT_OUT2IN_ED_SLOW_PATH;
+	  next[0] = def_slow;
 	  goto trace0;
 	}
 