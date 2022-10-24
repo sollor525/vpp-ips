sollor525/vpp-ips

commit 668f733c4a6ca4fca3f4223ec77da103ef476f8e

vppinfra: send minimal needed mask to the set_mempolicy syscall
Type: fix
fixes: 711d3ec

Change-Id: I0d98f5b43bc9ab5d31463b285177a11a10b864d2
Signed-off-by: Damjan Marion <dmarion@me.com>

src/vppinfra/linux/mem.c
@@ -28,6 +28,7 @@
 #include <vppinfra/mem.h>
 #include <vppinfra/lock.h>
 #include <vppinfra/time.h>
+#include <vppinfra/bitmap.h>
 #include <vppinfra/format.h>
 #include <vppinfra/clib_error.h>
 #include <vppinfra/linux/sysfs.h>
@@ -608,8 +609,8 @@ __clib_export int
 clib_mem_set_numa_affinity (u8 numa_node, int force)
 {
   clib_mem_main_t *mm = &clib_mem_main;
-  long unsigned int mask[16] = { 0 };
-  int mask_len = sizeof (mask) * 8 + 1;
+  clib_bitmap_t *bmp = 0;
+  int rv;
 
   /* no numa support */
   if (mm->numa_node_bitmap == 0)
@@ -625,19 +626,21 @@ clib_mem_set_numa_affinity (u8 numa_node, int force)
 	return 0;
     }
 
-  mask[0] = 1 << numa_node;
+  bmp = clib_bitmap_set (bmp, numa_node, 1);
 
-  if (syscall (__NR_set_mempolicy, force ? MPOL_BIND : MPOL_PREFERRED, mask,
-	       mask_len))
-    goto error;
+  rv = syscall (__NR_set_mempolicy, force ? MPOL_BIND : MPOL_PREFERRED, bmp,
+		vec_len (bmp) * sizeof (bmp[0] * 8) + 1);
 
+  clib_bitmap_free (bmp);
   vec_reset_length (mm->error);
+
+  if (rv)
+    {
+      mm->error = clib_error_return_unix (mm->error, (char *) __func__);
+      return CLIB_MEM_ERROR;
+    }
+
   return 0;
-
-error:
-  vec_reset_length (mm->error);
-  mm->error = clib_error_return_unix (mm->error, (char *) __func__);
-  return CLIB_MEM_ERROR;
 }
 
 __clib_export int

commit 737e06af38dfab5ee03e27ca55454753380e4339

vppinfra: fix incorrect sizeof() argument due to typo
fixes coverity 282527

Type: fix
Fixes: 668f733c4a
Signed-off-by: Andrew Yourtchenko <ayourtch@gmail.com>
Change-Id: I9ac72c3802e66369a8f24c92451e33f22c058f24

src/vppinfra/linux/mem.c
@@ -629,7 +629,7 @@ clib_mem_set_numa_affinity (u8 numa_node, int force)
   bmp = clib_bitmap_set (bmp, numa_node, 1);
 
   rv = syscall (__NR_set_mempolicy, force ? MPOL_BIND : MPOL_PREFERRED, bmp,
-		vec_len (bmp) * sizeof (bmp[0] * 8) + 1);
+		vec_len (bmp) * sizeof (bmp[0]) * 8 + 1);
 
   clib_bitmap_free (bmp);
   vec_reset_length (mm->error);