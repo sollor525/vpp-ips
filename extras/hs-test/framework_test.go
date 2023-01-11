FDio/vpp

hs-test: use anchors in yaml config files
Volumes can be referenced with anchors to reduce text duplication
and to explicitly show which containers share a volume.

Type: test
Signed-off-by: Maros Ondrejicka <maros.ondrejicka@pantheon.tech>
Change-Id: Id408a78262573b3faf2257c32bfa569eca2e2049

extras/hs-test/framework_test.go
@@ -119,7 +119,9 @@ func (s *HstSuite) loadContainerTopology(topologyName string) {
 	}
 
 	for _, elem := range yamlTopo.Volumes {
-		s.volumes = append(s.volumes, elem)
+		volumeMap := elem["volume"].(ContainerConfig)
+		hostDir := volumeMap["host-dir"].(string)
+		s.volumes = append(s.volumes, hostDir)
 	}
 
 	s.containers = make(map[string]*Container)

sollor525/vpp-ips

extras/hs-test/framework_test.go
@@ -131,7 +131,9 @@ func (s *HstSuite) loadContainerTopology(topologyName string) {
 	}
 
 	for _, elem := range yamlTopo.Volumes {
-		s.volumes = append(s.volumes, elem)
+		volumeMap := elem["volume"].(VolumeConfig)
+		hostDir := volumeMap["host-dir"].(string)
+		s.volumes = append(s.volumes, hostDir)
 	}
 
 	s.containers = make(map[string]*Container)