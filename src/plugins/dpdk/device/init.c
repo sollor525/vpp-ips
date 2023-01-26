FDio/vpp

dpdk: add intf tag to dev{} subinput
This patch allows to pass a tag when specifying
the dpdk `dev {  }` interface configuration.

It allows a control plane generating a vpp.conf
file to retreive the resulting mapping between
dpdk interfaces & sw_if_indices in VPP without
having to change the interface name exposed
to the user.

Type: feature

Change-Id: I55907417de0083b82d4a127172816cec3459acf3
Signed-off-by: Nathan Skrzypczak <nathan.skrzypczak@gmail.com>

src/plugins/dpdk/device/init.c
@@ -443,6 +443,9 @@ dpdk_lib_init (dpdk_main_t * dm)
       xd->sw_if_index = sw->sw_if_index;
       dpdk_log_debug ("[%u] interface %s created", port_id, hi->name);
 
+      if (devconf->tag)
+	vnet_set_sw_interface_tag (vnm, devconf->tag, sw->sw_if_index);
+
       ethernet_set_flags (vnm, xd->hw_if_index,
 			  ETHERNET_INTERFACE_FLAG_DEFAULT_L3);
 
@@ -853,6 +856,8 @@ dpdk_device_config (dpdk_config_main_t *conf, void *addr,
       else if (unformat (input, "num-tx-desc %u", &devconf->num_tx_desc))
 	;
       else if (unformat (input, "name %v", &devconf->name))
+	;
+      else if (unformat (input, "tag %v", &devconf->tag))
 	;
       else if (unformat (input, "workers %U", unformat_bitmap_list,
 			 &devconf->workers))

sollor525/vpp-ips

src/plugins/dpdk/device/init.c
@@ -449,6 +449,9 @@ dpdk_lib_init (dpdk_main_t * dm)
       xd->sw_if_index = sw->sw_if_index;
       dpdk_log_debug ("[%u] interface %s created", port_id, hi->name);
 
+      if (devconf->tag)
+	vnet_set_sw_interface_tag (vnm, devconf->tag, sw->sw_if_index);
+
       ethernet_set_flags (vnm, xd->hw_if_index,
 			  ETHERNET_INTERFACE_FLAG_DEFAULT_L3);
 
@@ -876,6 +879,8 @@ dpdk_device_config (dpdk_config_main_t *conf, void *addr,
       else if (unformat (input, "num-tx-desc %u", &devconf->num_tx_desc))
 	;
       else if (unformat (input, "name %v", &devconf->name))
+	;
+      else if (unformat (input, "tag %s", &devconf->tag))
 	;
       else if (unformat (input, "workers %U", unformat_bitmap_list,
 			 &devconf->workers))