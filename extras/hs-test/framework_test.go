FDio/vpp

hs-test: test vpp+nginx mirroring with tap ifaces
Type: test
Signed-off-by: Maros Ondrejicka <mondreji@cisco.com>
Change-Id: I05bbed8fd9d40929f040574044aed5292a475e91

extras/hs-test/framework_test.go
@@ -25,3 +25,8 @@ func TestNoTopo(t *testing.T) {
 	var m NoTopoSuite
 	suite.Run(t, &m)
 }
+
+func TestMirroring(t *testing.T) {
+	var m MirroringSuite
+	suite.Run(t, &m)
+}

extras/hs-test/script/build.sh
@@ -61,3 +61,4 @@ docker_build () {
 
 docker_build hs-test/vpp vpp
 docker_build hs-test/nginx-ldp nginx
+docker_build hs-test/nginx-standalone nginx-standalone

sollor525/vpp-ips

extras/hs-test/framework_test.go
@@ -25,3 +25,8 @@ func TestNoTopo(t *testing.T) {
 	var m NoTopoSuite
 	suite.Run(t, &m)
 }
+
+func TestNginx(t *testing.T) {
+	var m NginxSuite
+	suite.Run(t, &m)
+}

extras/hs-test/script/build.sh
@@ -61,3 +61,4 @@ docker_build () {
 
 docker_build hs-test/vpp vpp
 docker_build hs-test/nginx-ldp nginx
+docker_build hs-test/nginx-server nginx-server