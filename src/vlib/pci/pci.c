sollor525/vpp-ips

commit efa7119f055fb13b3e0f60a7bd8f98b747ab20ff

vlib: fix gcc build breakage
Type: fix
Fixes: f98e71f0f

Change-Id: I4ac8c9c5902299d5f283ba4912d52d9bfcedef5c
Signed-off-by: Dave Wallace <dwallacelf@gmail.com>

src/vlib/pci/pci.c
@@ -176,6 +176,9 @@ vlib_pci_function_level_reset (vlib_main_t *vm, vlib_pci_dev_handle_t h)
     return err;
 
   offset = cfg.cap_ptr;
+  /* Make gcc happy, otherwise gcc fails build due to cap not set if offset ==
+   * 0 */
+  cap = (pci_capability_pcie_t *) (cfg.data + offset);
 
   while (offset)
     {
@@ -410,4 +413,3 @@ VLIB_CLI_COMMAND (show_pci_command, static) = {
   .short_help = "show pci [all]",
   .function = show_pci_fn,
 };
-

commit 9842eda6eacfec4b8a7ca84e44ae34ca98a8b083

vlib: deuglify the offset finding loop in pci.c
Type: style
Fixes: efa7119f055fb13b3e0f60a7bd8f98b747ab20ff

Change-Id: I7c3080db94566af249e5082d59a4e6373c616888
Signed-off-by: Vratko Polak <vrpolak@cisco.com>

src/vlib/pci/pci.c
@@ -176,11 +176,7 @@ vlib_pci_function_level_reset (vlib_main_t *vm, vlib_pci_dev_handle_t h)
     return err;
 
   offset = cfg.cap_ptr;
-  /* Make gcc happy, otherwise gcc fails build due to cap not set if offset ==
-   * 0 */
-  cap = (pci_capability_pcie_t *) (cfg.data + offset);
-
-  while (offset)
+  do
     {
       cap = (pci_capability_pcie_t *) (cfg.data + offset);
 
@@ -189,6 +185,7 @@ vlib_pci_function_level_reset (vlib_main_t *vm, vlib_pci_dev_handle_t h)
 
       offset = cap->next_offset;
     }
+  while (offset);
 
   if (cap->capability_id != PCI_CAP_ID_PCIE)
     return clib_error_return (0, "PCIe capability config not found");