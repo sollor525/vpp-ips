FDio/vpp

hsa: unify echo test setup
Type: test

Change-Id: I8665492c2f7755901a428aacdb27e98329ff557a
Signed-off-by: Filip Tehlar <ftehlar@cisco.com>

test/asf/test_tcp.py
@@ -73,14 +73,14 @@ class TestTCP(VppTestCase):
 
         # Start builtin server and client
         uri = "tcp://" + self.loop0.local_ip4 + "/1234"
-        error = self.vapi.cli("test echo server appns 0 fifo-size 4 uri " + uri)
+        error = self.vapi.cli("test echo server appns 0 fifo-size 4k uri " + uri)
         if error:
             self.logger.critical(error)
             self.assertNotIn("failed", error)
 
         error = self.vapi.cli(
             "test echo client mbytes 10 appns 1 "
-            + "fifo-size 4 no-output test-bytes "
+            + "fifo-size 4k no-output "
             + "syn-timeout 2 uri "
             + uri
         )

sollor525/vpp-ips

test/asf/test_tcp.py
@@ -73,14 +73,14 @@ class TestTCP(VppTestCase):
 
         # Start builtin server and client
         uri = "tcp://" + self.loop0.local_ip4 + "/1234"
-        error = self.vapi.cli("test echo server appns 0 fifo-size 4 uri " + uri)
+        error = self.vapi.cli("test echo server appns 0 fifo-size 4k uri " + uri)
         if error:
             self.logger.critical(error)
             self.assertNotIn("failed", error)
 
         error = self.vapi.cli(
             "test echo client mbytes 10 appns 1 "
-            + "fifo-size 4 no-output test-bytes "
+            + "fifo-size 4k test-bytes "
             + "syn-timeout 2 uri "
             + uri
         )