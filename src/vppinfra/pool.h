sollor525/vpp-ips

commit 4a0e586dcb1d3a8054c2fffd91f0b3384b9d493f

vppinfra: add vec_foreach_pointer macro
works with vectors of pointers...

Type: improvement
Change-Id: I530653978fcf981be299cf42a1133be000d74d0c
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vppinfra/vec_bootstrap.h
@@ -238,6 +238,10 @@ _vec_set_len (void *v, uword len, uword elt_sz)
   if (v)                                                                      \
     for ((var) = vec_len ((v)) - 1; (var) >= 0; (var)--)
 
+#define vec_foreach_pointer(e, v)                                             \
+  for (typeof (**v) **__ep = (v), *(e) = *__ep; __ep - (v) < vec_len (v);     \
+       __ep++, (e) = *__ep)
+
 #endif /* included_clib_vec_bootstrap_h */
 
 /*

.clang-format
@@ -18,6 +18,7 @@ ForEachMacros:
   - 'vec_foreach_backwards'
   - 'vec_foreach_index'
   - 'vec_foreach_index_backwards'
+  - 'vec_foreach_pointer'
   - 'vlib_foreach_rx_tx'
   - 'foreach_vlib_main'
   - 'foreach_set_bit_index'

commit 5cdc40ff37d83160b0b75b57b99b5e2f375ea908

vppinfra: fix ASAN issue in vec_foreach_pointer and pool_foreach_pointer
Change-Id: If9381ae7283488b352a3c22f85732cd56ac6bfd9
Type: fix
Fixes: 4a0e586, 5cef406
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vppinfra/pool.h
@@ -571,9 +571,10 @@ do {									\
 /* works only for pool of pointers, e is declared inside macro */
 #define pool_foreach_pointer(e, p)                                            \
   if (p)                                                                      \
-    for (typeof ((p)[0]) *_t = (p) + pool_get_first_index (p), (e) = *_t;     \
-	 _t < vec_end (p);                                                    \
-	 _t = (p) + pool_get_next_index (p, _t - (p)), (e) = *_t)
+    for (typeof ((p)[0]) *_t = (p) + pool_get_first_index (p), (e) = *_t,     \
+			 *_end = vec_end (p);                                 \
+	 _t < _end; _t = (p) + pool_get_next_index (p, _t - (p)),             \
+			 (e) = _t < _end ? *_t : (e))
 
 /**
  * @brief Remove all elements from a pool in a safe way

src/vppinfra/vec_bootstrap.h
@@ -240,8 +240,8 @@ _vec_set_len (void *v, uword len, uword elt_sz)
 
 #define vec_foreach_pointer(e, v)                                             \
   if (v)                                                                      \
-    for (typeof (**v) **__ep = (v), *(e) = *__ep; __ep - (v) < vec_len (v);   \
-	 __ep++, (e) = *__ep)
+    for (typeof (**v) **__ep = (v), **__end = vec_end (v), *(e) = *__ep;      \
+	 __ep < __end; __ep++, (e) = __ep < __end ? *__ep : (e))
 
 #endif /* included_clib_vec_bootstrap_h */
 