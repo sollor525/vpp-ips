FDio/vpp

vppinfra: enable AES tests on ARM
Type: improvement
Change-Id: I30f3b2924ab219a8bace87f358b1b823e8bc83bb
Signed-off-by: Damjan Marion <damarion@cisco.com>

src/vppinfra/test/aes_cbc.c
@@ -2,7 +2,7 @@
  * Copyright(c) 2021 Cisco Systems, Inc.
  */
 
-#ifdef __AES__
+#if defined(__AES__) || defined(__ARM_FEATURE_AES)
 #include <vppinfra/format.h>
 #include <vppinfra/test/test.h>
 #include <vppinfra/crypto/aes_cbc.h>

src/vppinfra/test/aes_ctr.c
@@ -2,7 +2,7 @@
  * Copyright(c) 2024 Cisco Systems, Inc.
  */
 
-#if defined(__AES__)
+#if defined(__AES__) || defined(__ARM_FEATURE_AES)
 #include <vppinfra/format.h>
 #include <vppinfra/test/test.h>
 #include <vppinfra/crypto/aes_ctr.h>

src/vppinfra/test/aes_gcm.c
@@ -2,7 +2,7 @@
  * Copyright(c) 2021 Cisco Systems, Inc.
  */
 
-#if defined(__AES__) && defined(__PCLMUL__)
+#if (defined(__AES__) && defined(__PCLMUL__)) || defined(__ARM_FEATURE_AES)
 #include <vppinfra/format.h>
 #include <vppinfra/test/test.h>
 #include <vppinfra/crypto/aes_gcm.h>

sollor525/vpp-ips

src/vppinfra/test/aes_cbc.c
@@ -2,7 +2,7 @@
  * Copyright(c) 2021 Cisco Systems, Inc.
  */
 
-#ifdef __AES__
+#if defined(__AES__) || defined(__ARM_FEATURE_CRYPTO)
 #include <vppinfra/format.h>
 #include <vppinfra/test/test.h>
 #include <vppinfra/crypto/aes_cbc.h>

src/vppinfra/test/aes_ctr.c
@@ -2,7 +2,7 @@
  * Copyright(c) 2024 Cisco Systems, Inc.
  */
 
-#if defined(__AES__)
+#if defined(__AES__) || defined(__ARM_FEATURE_CRYPTO)
 #include <vppinfra/format.h>
 #include <vppinfra/test/test.h>
 #include <vppinfra/crypto/aes_ctr.h>

src/vppinfra/test/aes_gcm.c
@@ -2,7 +2,7 @@
  * Copyright(c) 2021 Cisco Systems, Inc.
  */
 
-#if defined(__AES__) && defined(__PCLMUL__)
+#if (defined(__AES__) && defined(__PCLMUL__)) || defined(__ARM_FEATURE_CRYPTO)
 #include <vppinfra/format.h>
 #include <vppinfra/test/test.h>
 #include <vppinfra/crypto/aes_gcm.h>