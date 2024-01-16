sollor525/vpp-ips

commit 0c09a86c927b6f1314dbc599ef4cdc43c313eba1

build: Add Debian12 environment
Add clang-14 and libffi8 which ship with Debian Bookworm. The project
compiles cleanly with these versions.

Type: make
Change-Id: I17350aae30cec72987792d54d88231b3221b56b9
Signed-off-by: pim@ipng.nl

Makefile
@@ -106,6 +106,10 @@ else ifeq ($(OS_ID)-$(OS_VERSION_ID),debian-11)
 	DEB_DEPENDS += virtualenv
 	DEB_DEPENDS += clang clang-format-11
 	LIBFFI=libffi7
+else ifeq ($(OS_ID)-$(OS_VERSION_ID),debian-12)
+	DEB_DEPENDS += virtualenv
+	DEB_DEPENDS += clang-14 clang-format-14
+	LIBFFI=libffi8
 else
 	DEB_DEPENDS += clang-11 clang-format-11
 	LIBFFI=libffi7

commit 7269f30d3c4a94fae6e76b78b3f0d565e7aa4b52

build: Provide clang-14 for Debian12
checkstyle.sh assumes clang-format-11 but allows it to be overridden.
Debian12 ships with a minimal version of clang-14, so set the correct
version for checkstyle.

Before:
$ make checkstyle
extras/scripts/checkstyle.sh: line 41: --version: command not found
make: *** [Makefile:720: checkstyle] Error 127

After:
$ make checkstyle
Debian clang-format version 14.0.6
*******************************************************************
* CHECKSTYLE SUCCESSFULLY COMPLETED
*******************************************************************

Type: make
Fixes: 0c09a86c927b6f1314dbc599ef4cdc43c313eba1
Change-Id: I0c58456477011397115810dab825865b5850d10d
Signed-off-by: pim@ipng.nl

Makefile
@@ -109,6 +109,8 @@ else ifeq ($(OS_ID)-$(OS_VERSION_ID),debian-11)
 else ifeq ($(OS_ID)-$(OS_VERSION_ID),debian-12)
 	DEB_DEPENDS += virtualenv
 	DEB_DEPENDS += clang-14 clang-format-14
+	# for extras/scripts/checkstyle.sh
+	export CLANG_FORMAT_VER=14
 	LIBFFI=libffi8
 else
 	DEB_DEPENDS += clang-11 clang-format-11