FDio/vpp

session svm: track session indices in private struct
Type: improvement

Signed-off-by: Florin Coras <fcoras@cisco.com>
Change-Id: I898baf3e2a7586124f4678eaeaa4516db7186f8f

src/vnet/session/application_worker.c
@@ -175,9 +175,11 @@ app_worker_alloc_session_fifos (segment_manager_t * sm, session_t * s)
     return rv;
 
   rx_fifo->shr->master_session_index = s->session_index;
+  rx_fifo->vpp_session_index = s->session_index;
   rx_fifo->master_thread_index = s->thread_index;
 
   tx_fifo->shr->master_session_index = s->session_index;
+  tx_fifo->vpp_session_index = s->session_index;
   tx_fifo->master_thread_index = s->thread_index;
 
   s->rx_fifo = rx_fifo;
@@ -210,9 +212,11 @@ app_worker_alloc_wrk_cl_session (app_worker_t *app_wrk, session_t *ls)
 				       &tx_fifo);
 
   rx_fifo->shr->master_session_index = s->session_index;
+  rx_fifo->vpp_session_index = s->session_index;
   rx_fifo->master_thread_index = s->thread_index;
 
   tx_fifo->shr->master_session_index = s->session_index;
+  tx_fifo->vpp_session_index = s->session_index;
   tx_fifo->master_thread_index = s->thread_index;
 
   s->rx_fifo = rx_fifo;

src/vnet/session/segment_manager.c
@@ -625,7 +625,7 @@ segment_manager_del_sessions (segment_manager_t * sm)
          */
         while (f)
           {
-	    session = session_get_if_valid (f->shr->master_session_index,
+	    session = session_get_if_valid (f->vpp_session_index,
 					    f->master_thread_index);
 	    if (session)
 	      vec_add1 (handles, session_handle (session));
@@ -672,7 +672,7 @@ segment_manager_del_sessions_filter (segment_manager_t *sm,
 	  f = fifo_segment_get_slice_fifo_list (fs, slice_index);
 	  while (f)
 	    {
-	      session = session_get_if_valid (f->shr->master_session_index,
+	      session = session_get_if_valid (f->vpp_session_index,
 					      f->master_thread_index);
 	      if (session)
 		{
@@ -920,6 +920,7 @@ segment_manager_attach_fifo (segment_manager_t *sm, svm_fifo_t **f,
   segment_manager_segment_reader_unlock (sm);
 
   (*f)->shr->master_session_index = s->session_index;
+  (*f)->vpp_session_index = s->session_index;
   (*f)->master_thread_index = s->thread_index;
 }
 
@@ -1159,7 +1160,7 @@ segment_manager_format_sessions (segment_manager_t * sm, int verbose)
             u32 session_index, thread_index;
             session_t *session;
 
-	    session_index = f->shr->master_session_index;
+	    session_index = f->vpp_session_index;
 	    thread_index = f->master_thread_index;
 
 	    session = session_get (session_index, thread_index);

sollor525/vpp-ips

src/vnet/session/application_worker.c
@@ -175,10 +175,10 @@ app_worker_alloc_session_fifos (segment_manager_t * sm, session_t * s)
     return rv;
 
   rx_fifo->shr->master_session_index = s->session_index;
-  rx_fifo->master_thread_index = s->thread_index;
+  rx_fifo->vpp_sh = s->handle;
 
   tx_fifo->shr->master_session_index = s->session_index;
-  tx_fifo->master_thread_index = s->thread_index;
+  tx_fifo->vpp_sh = s->handle;
 
   s->rx_fifo = rx_fifo;
   s->tx_fifo = tx_fifo;
@@ -210,10 +210,10 @@ app_worker_alloc_wrk_cl_session (app_worker_t *app_wrk, session_t *ls)
 				       &tx_fifo);
 
   rx_fifo->shr->master_session_index = s->session_index;
-  rx_fifo->master_thread_index = s->thread_index;
+  rx_fifo->vpp_sh = s->handle;
 
   tx_fifo->shr->master_session_index = s->session_index;
-  tx_fifo->master_thread_index = s->thread_index;
+  tx_fifo->vpp_sh = s->handle;
 
   s->rx_fifo = rx_fifo;
   s->tx_fifo = tx_fifo;

src/vnet/session/segment_manager.c
@@ -625,7 +625,7 @@ segment_manager_del_sessions (segment_manager_t * sm)
          */
         while (f)
           {
-	    session = session_get_if_valid (f->shr->master_session_index,
+	    session = session_get_if_valid (f->vpp_session_index,
 					    f->master_thread_index);
 	    if (session)
 	      vec_add1 (handles, session_handle (session));
@@ -672,7 +672,7 @@ segment_manager_del_sessions_filter (segment_manager_t *sm,
 	  f = fifo_segment_get_slice_fifo_list (fs, slice_index);
 	  while (f)
 	    {
-	      session = session_get_if_valid (f->shr->master_session_index,
+	      session = session_get_if_valid (f->vpp_session_index,
 					      f->master_thread_index);
 	      if (session)
 		{
@@ -920,7 +920,7 @@ segment_manager_attach_fifo (segment_manager_t *sm, svm_fifo_t **f,
   segment_manager_segment_reader_unlock (sm);
 
   (*f)->shr->master_session_index = s->session_index;
-  (*f)->master_thread_index = s->thread_index;
+  (*f)->vpp_sh = s->handle;
 }
 
 u32
@@ -1195,7 +1195,7 @@ segment_manager_format_sessions (segment_manager_t * sm, int verbose)
             u32 session_index, thread_index;
             session_t *session;
 
-	    session_index = f->shr->master_session_index;
+	    session_index = f->vpp_session_index;
 	    thread_index = f->master_thread_index;
 
 	    session = session_get (session_index, thread_index);