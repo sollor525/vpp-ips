FDio/vpp

hsa: http client parallel sessions
- client is now able to use multiple workers to send requests
  (sometimes it uses multiple sessions on a single worker)

Type: feature

Change-Id: I2d83d47a9768011b3d8d05ed320852606841e4b8
Signed-off-by: Adrian Villin <avillin@cisco.com>

extras/hs-test/http_test.go
@@ -36,10 +36,10 @@ func init() {
 		HttpClientErrRespTest, HttpClientPostFormTest, HttpClientGet128kbResponseTest, HttpClientGetResponseBodyTest,
 		HttpClientGetNoResponseBodyTest, HttpClientPostFileTest, HttpClientPostFilePtrTest, HttpUnitTest,
 		HttpRequestLineTest, HttpClientGetTimeout, HttpStaticFileHandlerWrkTest, HttpStaticUrlHandlerWrkTest, HttpConnTimeoutTest,
-		HttpClientGetRepeat, HttpClientPostRepeat, HttpIgnoreH2UpgradeTest, HttpInvalidAuthorityFormUriTest, HttpHeaderErrorConnectionDropTest)
+		HttpClientGetRepeatTest, HttpClientPostRepeatTest, HttpIgnoreH2UpgradeTest, HttpInvalidAuthorityFormUriTest, HttpHeaderErrorConnectionDropTest)
 	RegisterNoTopoSoloTests(HttpStaticPromTest, HttpGetTpsTest, HttpGetTpsInterruptModeTest, PromConcurrentConnectionsTest,
 		PromMemLeakTest, HttpClientPostMemLeakTest, HttpInvalidClientRequestMemLeakTest, HttpPostTpsTest, HttpPostTpsInterruptModeTest,
-		PromConsecutiveConnectionsTest, HttpGetTpsTlsTest, HttpPostTpsTlsTest)
+		PromConsecutiveConnectionsTest, HttpGetTpsTlsTest, HttpPostTpsTlsTest, HttpClientGetRepeatMTTest)
 }
 
 const wwwRootPath = "/tmp/www_root"
@@ -396,22 +396,35 @@ func startSimpleServer(s *NoTopoSuite, replyCount *int, serverAddress string) (s
 	return server
 }
 
-func HttpClientGetRepeat(s *NoTopoSuite) {
-	httpClientRepeat(s, "")
+func HttpClientGetRepeatMTTest(s *NoTopoSuite) {
+	httpClientRepeat(s, "", "parallel")
 }
 
-func HttpClientPostRepeat(s *NoTopoSuite) {
-	httpClientRepeat(s, "post")
+func HttpClientGetRepeatTest(s *NoTopoSuite) {
+	httpClientRepeat(s, "", "")
 }
 
-func httpClientRepeat(s *NoTopoSuite, requestMethod string) {
-	replyCount := 0
+func HttpClientPostRepeatTest(s *NoTopoSuite) {
+	httpClientRepeat(s, "post", "")
+}
+
+func httpClientRepeat(s *NoTopoSuite, requestMethod string, parallel string) {
 	vpp := s.Containers.Vpp.VppInstance
-	serverAddress := s.HostAddr()
-	repeatAmount := 10000
-	server := startSimpleServer(s, &replyCount, serverAddress)
-	defer server.Close()
+	logPath := "/tmp/nginx/" + s.Containers.NginxServer.Name + "-access.log"
+	serverAddress := s.Interfaces.Server.Ip4AddressString()
+	replyCountInt := 0
+	repeatAmount := 20000
+	durationInSec := 10
+	var err error
 
+	// recreate interfaces with RX-queues
+	s.AssertNil(vpp.DeleteTap(s.Interfaces.Client))
+	s.AssertNil(vpp.CreateTap(s.Interfaces.Client, 2, 2))
+	s.AssertNil(vpp.CreateTap(s.Interfaces.Server, 2, 3))
+
+	s.CreateNginxServer()
+	s.AssertNil(s.Containers.NginxServer.Start())
+	
 	if requestMethod == "post" {
 		fileName := "/tmp/test_file.txt"
 		s.Log(vpp.Container.Exec(false, "fallocate -l 64 "+fileName))
@@ -419,41 +432,45 @@ func httpClientRepeat(s *NoTopoSuite, requestMethod string) {
 		requestMethod += " file /tmp/test_file.txt"
 	}
 
-	uri := "http://" + serverAddress + "/80"
-	cmd := fmt.Sprintf("http client %s use-ptr duration 10 header Hello:World uri %s target /index.html",
-		requestMethod, uri)
+	uri := "http://" + serverAddress + "/" + s.GetPortFromPpid()
+	cmd := fmt.Sprintf("http client %s use-ptr %s duration %d header Hello:World uri %s target /index.html",
+		requestMethod, parallel, durationInSec, uri)
 
-	s.Log("Duration 10s")
+	s.Log("Duration %ds", durationInSec)
 	o := vpp.Vppctl(cmd)
-	outputLen := len(o)
-	if outputLen > 500 {
-		s.Log(o[:500])
-		s.Log("* HST Framework: output limited to 500 chars to avoid flooding the console. Output length: " + fmt.Sprint(outputLen))
-	} else {
-		s.Log(o)
+	s.Log(o)
+
+	replyCount := s.Containers.NginxServer.Exec(false, "awk 'END { print NR }' "+logPath)
+	if replyCount != "" {
+		replyCountInt, err = strconv.Atoi(replyCount[:len(replyCount)-1])
+		s.AssertNil(err)
 	}
-	s.Log("Server response count: %d", replyCount)
+	// empty the log file
+	s.Containers.NginxServer.Exec(false, "truncate -s 0 "+logPath)
+
+	s.Log("Server response count: %d", replyCountInt)
 	s.AssertNotNil(o)
 	s.AssertNotContains(o, "error")
-	s.AssertGreaterThan(replyCount, 15000)
+	s.AssertGreaterThan(replyCountInt, 15000)
 
-	cmd = fmt.Sprintf("http client %s use-ptr repeat %d header Hello:World uri %s target /index.html",
-		requestMethod, repeatAmount, uri)
+	replyCount = ""
+	cmd = fmt.Sprintf("http client %s use-ptr %s repeat %d header Hello:World uri %s target /index.html",
+		requestMethod, parallel, repeatAmount, uri)
 
-	replyCount = 0
+	time.Sleep(time.Second * 2)
 	s.Log("Repeat %d", repeatAmount)
 	o = vpp.Vppctl(cmd)
-	outputLen = len(o)
-	if outputLen > 500 {
-		s.Log(o[:500])
-		s.Log("* HST Framework: output limited to 500 chars to avoid flooding the console. Output length: " + fmt.Sprint(outputLen))
-	} else {
-		s.Log(o)
+	s.Log(o)
+
+	replyCount = s.Containers.NginxServer.Exec(false, "awk 'END { print NR }' "+logPath)
+	if replyCount != "" {
+		replyCountInt, err = strconv.Atoi(replyCount[:len(replyCount)-1])
+		s.AssertNil(err)
 	}
-	s.Log("Server response count: %d", replyCount)
+	s.Log("Server response count: %d", replyCountInt)
 	s.AssertNotNil(o)
 	s.AssertNotContains(o, "error")
-	s.AssertEqual(repeatAmount, replyCount)
+	s.AssertEqual(repeatAmount, replyCountInt)
 }
 
 func HttpClientGetTimeout(s *NoTopoSuite) {
@@ -1093,7 +1110,7 @@ func HttpStaticMacTimeTest(s *NoTopoSuite) {
 	s.AssertNil(err, fmt.Sprint(err))
 	s.AssertContains(string(data), "mactime")
 	s.AssertContains(string(data), s.HostAddr())
-	s.AssertContains(string(data), s.Interfaces.Tap.HwAddress.String())
+	s.AssertContains(string(data), s.Interfaces.Client.HwAddress.String())
 	s.AssertHttpHeaderWithValue(resp, "Content-Type", "application/json")
 	parsedTime, err := time.Parse(time.RFC1123, resp.Header.Get("Date"))
 	s.AssertNil(err, fmt.Sprint(err))

extras/hs-test/infra/suite_no_topo.go
@@ -15,16 +15,19 @@ var noTopoSoloTests = map[string][]func(s *NoTopoSuite){}
 type NoTopoSuite struct {
 	HstSuite
 	Interfaces struct {
-		Tap *NetInterface
+		Client *NetInterface
+		Server *NetInterface
 	}
 	Containers struct {
-		Vpp        *Container
-		Nginx      *Container
-		NginxHttp3 *Container
-		Wrk        *Container
-		Curl       *Container
-		Ab         *Container
+		Vpp         *Container
+		Nginx       *Container
+		NginxHttp3  *Container
+		NginxServer *Container
+		Wrk         *Container
+		Curl        *Container
+		Ab          *Container
 	}
+	NginxServerPort string
 }
 
 func RegisterNoTopoTests(tests ...func(s *NoTopoSuite)) {
@@ -36,12 +39,14 @@ func RegisterNoTopoSoloTests(tests ...func(s *NoTopoSuite)) {
 
 func (s *NoTopoSuite) SetupSuite() {
 	s.HstSuite.SetupSuite()
-	s.LoadNetworkTopology("tap")
+	s.LoadNetworkTopology("2taps")
 	s.LoadContainerTopology("single")
-	s.Interfaces.Tap = s.GetInterfaceByName("htaphost")
+	s.Interfaces.Client = s.GetInterfaceByName("hstcln")
+	s.Interfaces.Server = s.GetInterfaceByName("hstsrv")
 	s.Containers.Vpp = s.GetContainerByName("vpp")
 	s.Containers.Nginx = s.GetContainerByName("nginx")
 	s.Containers.NginxHttp3 = s.GetContainerByName("nginx-http3")
+	s.Containers.NginxServer = s.GetTransientContainerByName("nginx-server")
 	s.Containers.Wrk = s.GetContainerByName("wrk")
 	s.Containers.Curl = s.GetContainerByName("curl")
 	s.Containers.Ab = s.GetContainerByName("ab")
@@ -67,7 +72,7 @@ func (s *NoTopoSuite) SetupTest() {
 	vpp, _ := s.Containers.Vpp.newVppInstance(s.Containers.Vpp.AllocatedCpus, sessionConfig)
 
 	s.AssertNil(vpp.Start())
-	s.AssertNil(vpp.CreateTap(s.Interfaces.Tap, 1, 1), "failed to create tap interface")
+	s.AssertNil(vpp.CreateTap(s.Interfaces.Client, 1, 1), "failed to create tap interface")
 
 	if *DryRun {
 		s.LogStartedContainers()
@@ -101,6 +106,28 @@ func (s *NoTopoSuite) CreateNginxConfig(container *Container, multiThreadWorkers
 	)
 }
 
+// Creates container and config.
+func (s *NoTopoSuite) CreateNginxServer() {
+	s.AssertNil(s.Containers.NginxServer.Create())
+	s.NginxServerPort = s.GetPortFromPpid()
+	nginxSettings := struct {
+		LogPrefix string
+		Address   string
+		Port      string
+		Timeout   int
+	}{
+		LogPrefix: s.Containers.NginxServer.Name,
+		Address:   s.Interfaces.Server.Ip4AddressString(),
+		Port:      s.NginxServerPort,
+		Timeout:   600,
+	}
+	s.Containers.NginxServer.CreateConfigFromTemplate(
+		"/nginx.conf",
+		"./resources/nginx/nginx_server.conf",
+		nginxSettings,
+	)
+}
+
 func (s *NoTopoSuite) AddNginxVclConfig(multiThreadWorkers bool) {
 	vclFileName := s.Containers.Nginx.GetHostWorkDir() + "/vcl.conf"
 	appSocketApi := fmt.Sprintf("app-socket-api %s/var/run/app_ns_sockets/default",
@@ -126,15 +153,15 @@ func (s *NoTopoSuite) AddNginxVclConfig(multiThreadWorkers bool) {
 }
 
 func (s *NoTopoSuite) VppAddr() string {
-	return s.Interfaces.Tap.Peer.Ip4AddressString()
+	return s.Interfaces.Client.Peer.Ip4AddressString()
 }
 
 func (s *NoTopoSuite) VppIfName() string {
-	return s.Interfaces.Tap.Peer.Name()
+	return s.Interfaces.Client.Peer.Name()
 }
 
 func (s *NoTopoSuite) HostAddr() string {
-	return s.Interfaces.Tap.Ip4AddressString()
+	return s.Interfaces.Client.Ip4AddressString()
 }
 
 func (s *NoTopoSuite) CreateNginxHttp3Config(container *Container) {

sollor525/vpp-ips

extras/hs-test/http_test.go
@@ -7,7 +7,6 @@ import (
 	"math/rand"
 	"net"
 	"net/http"
-	"net/http/httptest"
 	"net/http/httptrace"
 	"os"
 	"strconv"
@@ -36,10 +35,10 @@ func init() {
 		HttpClientErrRespTest, HttpClientPostFormTest, HttpClientGet128kbResponseTest, HttpClientGetResponseBodyTest,
 		HttpClientGetNoResponseBodyTest, HttpClientPostFileTest, HttpClientPostFilePtrTest, HttpUnitTest,
 		HttpRequestLineTest, HttpClientGetTimeout, HttpStaticFileHandlerWrkTest, HttpStaticUrlHandlerWrkTest, HttpConnTimeoutTest,
-		HttpClientGetRepeat, HttpClientPostRepeat, HttpIgnoreH2UpgradeTest, HttpInvalidAuthorityFormUriTest, HttpHeaderErrorConnectionDropTest)
+		HttpClientGetRepeatTest, HttpClientPostRepeatTest, HttpIgnoreH2UpgradeTest, HttpInvalidAuthorityFormUriTest, HttpHeaderErrorConnectionDropTest)
 	RegisterNoTopoSoloTests(HttpStaticPromTest, HttpGetTpsTest, HttpGetTpsInterruptModeTest, PromConcurrentConnectionsTest,
 		PromMemLeakTest, HttpClientPostMemLeakTest, HttpInvalidClientRequestMemLeakTest, HttpPostTpsTest, HttpPostTpsInterruptModeTest,
-		PromConsecutiveConnectionsTest, HttpGetTpsTlsTest, HttpPostTpsTlsTest)
+		PromConsecutiveConnectionsTest, HttpGetTpsTlsTest, HttpPostTpsTlsTest, HttpClientGetRepeatMTTest, HttpClientPtrGetRepeatMTTest)
 }
 
 const wwwRootPath = "/tmp/www_root"
@@ -382,35 +381,37 @@ func httpClientGet(s *NoTopoSuite, response string, size int) {
 	s.AssertContains(file_contents, response)
 }
 
-func startSimpleServer(s *NoTopoSuite, replyCount *int, serverAddress string) (server *httptest.Server) {
-	var err error
-	server = httptest.NewUnstartedServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello")
-		*replyCount++
-	}))
-	server.Listener, err = net.Listen("tcp", serverAddress+":80")
-	s.AssertNil(err, "Error while creating listener.")
-
-	server.Start()
-
-	return server
+func HttpClientGetRepeatMTTest(s *NoTopoSuite) {
+	httpClientRepeat(s, "", "sessions 2")
 }
 
-func HttpClientGetRepeat(s *NoTopoSuite) {
-	httpClientRepeat(s, "")
+func HttpClientPtrGetRepeatMTTest(s *NoTopoSuite) {
+	httpClientRepeat(s, "", "use-ptr sessions 2")
 }
 
-func HttpClientPostRepeat(s *NoTopoSuite) {
-	httpClientRepeat(s, "post")
+func HttpClientGetRepeatTest(s *NoTopoSuite) {
+	httpClientRepeat(s, "", "")
 }
 
-func httpClientRepeat(s *NoTopoSuite, requestMethod string) {
-	replyCount := 0
+func HttpClientPostRepeatTest(s *NoTopoSuite) {
+	httpClientRepeat(s, "post", "")
+}
+
+func httpClientRepeat(s *NoTopoSuite, requestMethod string, clientArgs string) {
 	vpp := s.Containers.Vpp.VppInstance
-	serverAddress := s.HostAddr()
+	logPath := s.Containers.NginxServer.GetContainerWorkDir() + "/" + s.Containers.NginxServer.Name + "-access.log"
+	serverAddress := s.Interfaces.Tap.Ip4AddressString()
+	replyCountInt := 0
 	repeatAmount := 10000
-	server := startSimpleServer(s, &replyCount, serverAddress)
-	defer server.Close()
+	durationInSec := 10
+	var err error
+
+	// recreate interfaces with RX-queues
+	s.AssertNil(vpp.DeleteTap(s.Interfaces.Tap))
+	s.AssertNil(vpp.CreateTap(s.Interfaces.Tap, 2, 2))
+
+	s.CreateNginxServer()
+	s.AssertNil(s.Containers.NginxServer.Start())
 
 	if requestMethod == "post" {
 		fileName := "/tmp/test_file.txt"
@@ -419,41 +420,45 @@ func httpClientRepeat(s *NoTopoSuite, requestMethod string) {
 		requestMethod += " file /tmp/test_file.txt"
 	}
 
-	uri := "http://" + serverAddress + "/80"
-	cmd := fmt.Sprintf("http client %s use-ptr duration 10 header Hello:World uri %s target /index.html",
-		requestMethod, uri)
+	uri := "http://" + serverAddress + "/" + s.GetPortFromPpid()
+	cmd := fmt.Sprintf("http client %s %s duration %d header Hello:World uri %s target /index.html",
+		requestMethod, clientArgs, durationInSec, uri)
 
-	s.Log("Duration 10s")
+	s.Log("Duration %ds", durationInSec)
 	o := vpp.Vppctl(cmd)
-	outputLen := len(o)
-	if outputLen > 500 {
-		s.Log(o[:500])
-		s.Log("* HST Framework: output limited to 500 chars to avoid flooding the console. Output length: " + fmt.Sprint(outputLen))
-	} else {
-		s.Log(o)
+	s.Log(o)
+
+	replyCount := s.Containers.NginxServer.Exec(false, "awk 'END { print NR }' "+logPath)
+	if replyCount != "" {
+		replyCountInt, err = strconv.Atoi(replyCount[:len(replyCount)-1])
+		s.AssertNil(err)
 	}
-	s.Log("Server response count: %d", replyCount)
+	// empty the log file
+	s.Containers.NginxServer.Exec(false, "truncate -s 0 "+logPath)
+
+	s.Log("Server response count: %d", replyCountInt)
 	s.AssertNotNil(o)
 	s.AssertNotContains(o, "error")
-	s.AssertGreaterThan(replyCount, 15000)
+	s.AssertGreaterThan(replyCountInt, 15000)
 
-	cmd = fmt.Sprintf("http client %s use-ptr repeat %d header Hello:World uri %s target /index.html",
-		requestMethod, repeatAmount, uri)
+	replyCount = ""
+	cmd = fmt.Sprintf("http client %s %s repeat %d header Hello:World uri %s target /index.html",
+		requestMethod, clientArgs, repeatAmount, uri)
 
-	replyCount = 0
+	s.AssertNil(err, fmt.Sprint(err))
 	s.Log("Repeat %d", repeatAmount)
 	o = vpp.Vppctl(cmd)
-	outputLen = len(o)
-	if outputLen > 500 {
-		s.Log(o[:500])
-		s.Log("* HST Framework: output limited to 500 chars to avoid flooding the console. Output length: " + fmt.Sprint(outputLen))
-	} else {
-		s.Log(o)
+	s.Log(o)
+
+	replyCount = s.Containers.NginxServer.Exec(false, "awk 'END { print NR }' "+logPath)
+	if replyCount != "" {
+		replyCountInt, err = strconv.Atoi(replyCount[:len(replyCount)-1])
+		s.AssertNil(err)
 	}
-	s.Log("Server response count: %d", replyCount)
+	s.Log("Server response count: %d", replyCountInt)
 	s.AssertNotNil(o)
 	s.AssertNotContains(o, "error")
-	s.AssertEqual(repeatAmount, replyCount)
+	s.AssertEqual(repeatAmount, replyCountInt)
 }
 
 func HttpClientGetTimeout(s *NoTopoSuite) {

extras/hs-test/infra/suite_no_topo.go
@@ -18,13 +18,15 @@ type NoTopoSuite struct {
 		Tap *NetInterface
 	}
 	Containers struct {
-		Vpp        *Container
-		Nginx      *Container
-		NginxHttp3 *Container
-		Wrk        *Container
-		Curl       *Container
-		Ab         *Container
+		Vpp         *Container
+		Nginx       *Container
+		NginxHttp3  *Container
+		NginxServer *Container
+		Wrk         *Container
+		Curl        *Container
+		Ab          *Container
 	}
+	NginxServerPort string
 }
 
 func RegisterNoTopoTests(tests ...func(s *NoTopoSuite)) {
@@ -42,6 +44,7 @@ func (s *NoTopoSuite) SetupSuite() {
 	s.Containers.Vpp = s.GetContainerByName("vpp")
 	s.Containers.Nginx = s.GetContainerByName("nginx")
 	s.Containers.NginxHttp3 = s.GetContainerByName("nginx-http3")
+	s.Containers.NginxServer = s.GetTransientContainerByName("nginx-server")
 	s.Containers.Wrk = s.GetContainerByName("wrk")
 	s.Containers.Curl = s.GetContainerByName("curl")
 	s.Containers.Ab = s.GetContainerByName("ab")
@@ -98,6 +101,28 @@ func (s *NoTopoSuite) CreateNginxConfig(container *Container, multiThreadWorkers
 		"/nginx.conf",
 		"./resources/nginx/nginx.conf",
 		values,
+	)
+}
+
+// Creates container and config.
+func (s *NoTopoSuite) CreateNginxServer() {
+	s.AssertNil(s.Containers.NginxServer.Create())
+	s.NginxServerPort = s.GetPortFromPpid()
+	nginxSettings := struct {
+		LogPrefix string
+		Address   string
+		Port      string
+		Timeout   int
+	}{
+		LogPrefix: s.Containers.NginxServer.Name,
+		Address:   s.Interfaces.Tap.Ip4AddressString(),
+		Port:      s.NginxServerPort,
+		Timeout:   600,
+	}
+	s.Containers.NginxServer.CreateConfigFromTemplate(
+		"/nginx.conf",
+		"./resources/nginx/nginx_server.conf",
+		nginxSettings,
 	)
 }
 