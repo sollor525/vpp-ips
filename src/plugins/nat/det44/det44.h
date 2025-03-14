sollor525/vpp-ips

commit 50a72ca4a9fb099010743a9ff08dd93071bf4b78

map: fix translation of icmp6 error messages
Translation of ICMPv6 error messages to ICMP error messages fails
because the sender port is not set that leads to securtiy check
failure.

With this commit, during ICMPv6 error messages translation, get the
sender port value from the inner packet.

Type: fix

Change-Id: I1ee295a3685fab4837172edfb629a699f49afbee
Signed-off-by: Alexander Chernavin <achernavin@netgate.com>

src/vnet/ip/ip6_to_ip4.h
@@ -170,6 +170,57 @@ ip6_get_port (vlib_main_t * vm, vlib_buffer_t * b, ip6_header_t * ip6,
 	  if (dst_port)
 	    *dst_port = ((u16 *) (icmp))[2];
 	}
+      else if (clib_net_to_host_u16 (ip6->payload_length) >= 64)
+	{
+	  u16 ip6_pay_len;
+	  ip6_header_t *inner_ip6;
+	  u8 inner_l4_protocol;
+	  u16 inner_l4_offset;
+	  u16 inner_frag_offset;
+	  u8 *inner_l4;
+
+	  ip6_pay_len = clib_net_to_host_u16 (ip6->payload_length);
+	  inner_ip6 = (ip6_header_t *) u8_ptr_add (icmp, 8);
+
+	  if (ip6_parse (vm, b, inner_ip6, ip6_pay_len - 8,
+			 &inner_l4_protocol, &inner_l4_offset,
+			 &inner_frag_offset))
+	    return 0;
+
+	  if (inner_frag_offset &&
+	      ip6_frag_hdr_offset (((ip6_frag_hdr_t *)
+				    u8_ptr_add (inner_ip6,
+						inner_frag_offset))))
+	    return 0;
+
+	  inner_l4 = u8_ptr_add (inner_ip6, inner_l4_offset);
+	  if (inner_l4_protocol == IP_PROTOCOL_TCP ||
+	      inner_l4_protocol == IP_PROTOCOL_UDP)
+	    {
+	      if (src_port)
+		*src_port = ((udp_header_t *) (inner_l4))->dst_port;
+	      if (dst_port)
+		*dst_port = ((udp_header_t *) (inner_l4))->src_port;
+	    }
+	  else if (inner_l4_protocol == IP_PROTOCOL_ICMP6)
+	    {
+	      icmp46_header_t *inner_icmp = (icmp46_header_t *) (inner_l4);
+	      if (inner_icmp->type == ICMP6_echo_request)
+		{
+		  if (src_port)
+		    *src_port = ((u16 *) (inner_icmp))[2];
+		  if (dst_port)
+		    *dst_port = ((u16 *) (inner_icmp))[2];
+		}
+	      else if (inner_icmp->type == ICMP6_echo_reply)
+		{
+		  if (src_port)
+		    *src_port = ((u16 *) (inner_icmp))[2];
+		  if (dst_port)
+		    *dst_port = ((u16 *) (inner_icmp))[2];
+		}
+	    }
+	}
     }
   return 1;
 }

src/plugins/map/test/test_map_br.py
@@ -12,7 +12,7 @@ import scapy.compat
 from scapy.layers.l2 import Ether
 from scapy.packet import Raw
 from scapy.layers.inet import IP, UDP, ICMP, TCP, IPerror, UDPerror
-from scapy.layers.inet6 import IPv6, ICMPv6TimeExceeded, IPv6ExtHdrFragment
+from scapy.layers.inet6 import IPv6, ICMPv6TimeExceeded, ICMPv6PacketTooBig
 from scapy.layers.inet6 import ICMPv6EchoRequest, ICMPv6EchoReply, IPerror6
 
 
@@ -425,6 +425,85 @@ class TestMAPBR(VppTestCase):
         self.assertEqual(rx_pkt[ICMP].type, ICMP(type="echo-reply").type)
         self.assertEqual(rx_pkt[ICMP].code, 0)
         self.assertEqual(rx_pkt[ICMP].id, self.ipv6_udp_or_tcp_map_port)
+
+    #
+    # Translation of ICMP Packet Too Big v6 -> v4 direction
+    # Received packet should be translated into an IPv4 Dest Unreachable.
+    #
+
+    def test_map_t_packet_too_big_ip6_to_ip4(self):
+        """ MAP-T packet too big IPv6 -> IPv4 """
+
+        eth = Ether(src=self.pg1.remote_mac,
+                    dst=self.pg1.local_mac)
+        ip = IPv6(src=self.ipv6_cpe_address,
+                  dst=self.ipv6_map_address)
+        icmp = ICMPv6PacketTooBig(mtu=1280)
+        ip_inner = IPv6(src=self.ipv6_map_address,
+                        dst=self.ipv6_cpe_address)
+        udp_inner = UDP(sport=self.ipv6_udp_or_tcp_internet_port,
+                        dport=self.ipv6_udp_or_tcp_map_port)
+        payload = "H" * 10
+        tx_pkt = eth / ip / icmp / ip_inner / udp_inner / payload
+
+        self.pg_send(self.pg1, tx_pkt * 1)
+
+        rx_pkts = self.pg0.get_capture(1)
+        rx_pkt = rx_pkts[0]
+
+        self.v4_address_check(rx_pkt)
+        self.assertEqual(rx_pkt[IP].proto, IP(proto="icmp").proto)
+        self.assertEqual(rx_pkt[ICMP].type, ICMP(type="dest-unreach").type)
+        self.assertEqual(rx_pkt[ICMP].code,
+                         ICMP(code="fragmentation-needed").code)
+        self.assertEqual(rx_pkt[ICMP].nexthopmtu,
+                         tx_pkt[ICMPv6PacketTooBig].mtu - 20)
+        self.assertTrue(rx_pkt.haslayer(IPerror))
+        self.assertTrue(rx_pkt.haslayer(UDPerror))
+        self.assertEqual(rx_pkt[IP].src, rx_pkt[IPerror].dst)
+        self.assertEqual(rx_pkt[IP].dst, rx_pkt[IPerror].src)
+        self.assertEqual(rx_pkt[UDPerror].sport,
+                         self.ipv4_udp_or_tcp_internet_port)
+        self.assertEqual(rx_pkt[UDPerror].dport, self.ipv4_udp_or_tcp_map_port)
+
+    #
+    # Translation of ICMP Time Exceeded v6 -> v4 direction
+    # Received packet should be translated into an IPv4 Time Exceeded.
+    #
+
+    def test_map_t_time_exceeded_ip6_to_ip4(self):
+        """ MAP-T time exceeded IPv6 -> IPv4 """
+
+        eth = Ether(src=self.pg1.remote_mac,
+                    dst=self.pg1.local_mac)
+        ip = IPv6(src=self.ipv6_cpe_address,
+                  dst=self.ipv6_map_address)
+        icmp = ICMPv6TimeExceeded()
+        ip_inner = IPv6(src=self.ipv6_map_address,
+                        dst=self.ipv6_cpe_address, hlim=1)
+        udp_inner = UDP(sport=self.ipv6_udp_or_tcp_internet_port,
+                        dport=self.ipv6_udp_or_tcp_map_port)
+        payload = "H" * 10
+        tx_pkt = eth / ip / icmp / ip_inner / udp_inner / payload
+
+        self.pg_send(self.pg1, tx_pkt * 1)
+
+        rx_pkts = self.pg0.get_capture(1)
+        rx_pkt = rx_pkts[0]
+
+        self.v4_address_check(rx_pkt)
+        self.assertEqual(rx_pkt[IP].proto, IP(proto="icmp").proto)
+        self.assertEqual(rx_pkt[ICMP].type, ICMP(type="time-exceeded").type)
+        self.assertEqual(rx_pkt[ICMP].code,
+                         ICMP(code="ttl-zero-during-transit").code)
+        self.assertEqual(rx_pkt[ICMP].ttl, tx_pkt[IPv6][1].hlim)
+        self.assertTrue(rx_pkt.haslayer(IPerror))
+        self.assertTrue(rx_pkt.haslayer(UDPerror))
+        self.assertEqual(rx_pkt[IP].src, rx_pkt[IPerror].dst)
+        self.assertEqual(rx_pkt[IP].dst, rx_pkt[IPerror].src)
+        self.assertEqual(rx_pkt[UDPerror].sport,
+                         self.ipv4_udp_or_tcp_internet_port)
+        self.assertEqual(rx_pkt[UDPerror].dport, self.ipv4_udp_or_tcp_map_port)
 
     #
     # Spoofed IPv4 Source Address v6 -> v4 direction

commit 6f9fd3e91dd5c547099741e8ffa8b65742b3722e

ip: fix ICMP inner payload parsing
Add a check so that ICMP type is verified to be an error before parsing
inner payload. If it's not an error, then the inner payload is not there.

Type: fix
Fixes: 50a72ca4a9
Change-Id: I5c7d8ddacb347ec030784f349064e66d63cd525e
Signed-off-by: Klement Sekera <klement.sekera@gmail.com>

src/plugins/nat/det44/det44.h
@@ -38,7 +38,6 @@
 #include <vnet/ip/reass/ip4_sv_reass.h>
 
 #include <nat/lib/lib.h>
-#include <nat/lib/inlines.h>
 #include <nat/lib/ipfix_logging.h>
 #include <nat/lib/nat_proto.h>
 

src/plugins/nat/det44/det44_in2out.c
@@ -21,6 +21,7 @@
 #include <vlib/vlib.h>
 #include <vnet/vnet.h>
 #include <vnet/ip/ip.h>
+#include <vnet/ip/ip4_to_ip6.h>
 #include <vnet/fib/ip4_fib.h>
 #include <vppinfra/error.h>
 #include <vppinfra/elog.h>
@@ -29,7 +30,6 @@
 #include <nat/det44/det44_inlines.h>
 
 #include <nat/lib/lib.h>
-#include <nat/lib/inlines.h>
 #include <nat/lib/nat_inlines.h>
 
 typedef enum

src/plugins/nat/det44/det44_out2in.c
@@ -21,6 +21,7 @@
 #include <vlib/vlib.h>
 #include <vnet/vnet.h>
 #include <vnet/ip/ip.h>
+#include <vnet/ip/ip4_to_ip6.h>
 #include <vnet/fib/ip4_fib.h>
 #include <vppinfra/error.h>
 #include <vppinfra/elog.h>
@@ -29,7 +30,6 @@
 #include <nat/det44/det44_inlines.h>
 
 #include <nat/lib/lib.h>
-#include <nat/lib/inlines.h>
 #include <nat/lib/nat_inlines.h>
 
 typedef enum

src/plugins/nat/dslite/dslite.h
@@ -22,7 +22,6 @@
 
 #include <nat/lib/lib.h>
 #include <nat/lib/alloc.h>
-#include <nat/lib/inlines.h>
 
 typedef struct
 {

src/plugins/nat/dslite/dslite_in2out.c
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <vnet/ip/ip4_to_ip6.h>
 #include <nat/dslite/dslite.h>
 #include <nat/lib/nat_syslog.h>
 

src/plugins/nat/dslite/dslite_out2in.c
@@ -12,6 +12,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+#include <vnet/ip/ip4_to_ip6.h>
 #include <nat/dslite/dslite.h>
 
 typedef enum

src/plugins/nat/lib/inlines.h
@@ -1,44 +1,0 @@
-/*
- * Copyright (c) 2020 Cisco and/or its affiliates.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at:
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-/**
- * @brief Common NAT inline functions
- */
-#ifndef included_nat_inlines_h__
-#define included_nat_inlines_h__
-
-#include <vnet/ip/icmp46_packet.h>
-
-static_always_inline u64
-icmp_type_is_error_message (u8 icmp_type)
-{
-  int bmp = 0;
-  bmp |= 1 << ICMP4_destination_unreachable;
-  bmp |= 1 << ICMP4_time_exceeded;
-  bmp |= 1 << ICMP4_parameter_problem;
-  bmp |= 1 << ICMP4_source_quench;
-  bmp |= 1 << ICMP4_redirect;
-  bmp |= 1 << ICMP4_alternate_host_address;
-
-  return (1ULL << icmp_type) & bmp;
-}
-
-#endif /* included_nat_inlines_h__ */
-/*
- * fd.io coding-style-patch-verification: ON
- *
- * Local Variables:
- * eval: (c-set-style "gnu")
- * End:
- */

src/plugins/nat/lib/ipfix_logging.c
@@ -22,7 +22,6 @@
 #include <vlibmemory/api.h>
 #include <vppinfra/atomics.h>
 #include <nat/lib/ipfix_logging.h>
-#include <nat/lib/inlines.h>
 
 vlib_node_registration_t nat_ipfix_flush_node;
 nat_ipfix_logging_main_t nat_ipfix_logging_main;

src/plugins/nat/lib/nat_syslog.c
@@ -21,7 +21,6 @@
 #include <vnet/syslog/syslog.h>
 
 #include <nat/lib/nat_syslog.h>
-#include <nat/lib/inlines.h>
 
 #include <nat/lib/nat_syslog_constants.h>
 

src/plugins/nat/nat44-ed/nat44_ed.h
@@ -31,7 +31,6 @@
 #include <vlibapi/api.h>
 
 #include <nat/lib/lib.h>
-#include <nat/lib/inlines.h>
 
 /* default number of worker handoff frame queue elements */
 #define NAT_FQ_NELTS_DEFAULT 64

src/plugins/nat/nat44-ed/nat44_ed_inlines.h
@@ -27,6 +27,7 @@
 #include <nat/lib/log.h>
 #include <nat/lib/ipfix_logging.h>
 #include <nat/nat44-ed/nat44_ed.h>
+#include <vnet/ip/ip4_to_ip6.h>
 
 always_inline void
 init_ed_k (clib_bihash_kv_16_8_t *kv, u32 l_addr, u16 l_port, u32 r_addr,

src/plugins/nat/nat44-ei/nat44_ei.c
@@ -21,6 +21,7 @@
 #include <vnet/vnet.h>
 #include <vnet/ip/ip.h>
 #include <vnet/ip/ip4.h>
+#include <vnet/ip/ip4_to_ip6.h>
 #include <vnet/ip/ip_table.h>
 #include <vnet/ip/reass/ip4_sv_reass.h>
 #include <vnet/fib/fib_table.h>

src/plugins/nat/nat44-ei/nat44_ei.h
@@ -35,7 +35,6 @@
 #include <vppinfra/hash.h>
 
 #include <nat/lib/lib.h>
-#include <nat/lib/inlines.h>
 #include <nat/lib/nat_proto.h>
 
 /* default number of worker handoff frame queue elements */

src/plugins/nat/nat44-ei/nat44_ei_in2out.c
@@ -21,6 +21,7 @@
 
 #include <vnet/vnet.h>
 #include <vnet/ip/ip.h>
+#include <vnet/ip/ip4_to_ip6.h>
 #include <vnet/ethernet/ethernet.h>
 #include <vnet/udp/udp_local.h>
 #include <vnet/fib/ip4_fib.h>

src/plugins/nat/nat44-ei/nat44_ei_out2in.c
@@ -21,6 +21,7 @@
 
 #include <vnet/vnet.h>
 #include <vnet/ip/ip.h>
+#include <vnet/ip/ip4_to_ip6.h>
 #include <vnet/ethernet/ethernet.h>
 #include <vnet/udp/udp_local.h>
 #include <vnet/fib/ip4_fib.h>

src/plugins/nat/nat64/nat64.c
@@ -15,6 +15,7 @@
 
 #include <vppinfra/crc32.h>
 #include <vnet/fib/ip4_fib.h>
+#include <vnet/ip/ip4_to_ip6.h>
 
 #include <vnet/ip/reass/ip4_sv_reass.h>
 #include <vnet/ip/reass/ip6_sv_reass.h>

src/plugins/nat/nat64/nat64.h
@@ -30,7 +30,6 @@
 #include <vnet/ip/reass/ip4_sv_reass.h>
 
 #include <nat/lib/lib.h>
-#include <nat/lib/inlines.h>
 #include <nat/lib/nat_inlines.h>
 
 #include <nat/nat64/nat64_db.h>

src/plugins/nat/nat64/nat64_db.c
@@ -16,7 +16,6 @@
 #include <vnet/fib/fib_table.h>
 #include <nat/lib/ipfix_logging.h>
 #include <nat/lib/nat_syslog.h>
-#include <nat/lib/inlines.h>
 #include <nat/nat64/nat64_db.h>
 
 int

src/vnet/ip/ip4_to_ip6.h
@@ -37,6 +37,20 @@ static u8 icmp_to_icmp6_updater_pointer_table[] =
 
 #define frag_id_4to6(id) (id)
 
+always_inline u64
+icmp_type_is_error_message (u8 icmp_type)
+{
+  int bmp = 0;
+  bmp |= 1 << ICMP4_destination_unreachable;
+  bmp |= 1 << ICMP4_time_exceeded;
+  bmp |= 1 << ICMP4_parameter_problem;
+  bmp |= 1 << ICMP4_source_quench;
+  bmp |= 1 << ICMP4_redirect;
+  bmp |= 1 << ICMP4_alternate_host_address;
+
+  return (1ULL << icmp_type) & bmp;
+}
+
 /**
  * @brief Get TCP/UDP port number or ICMP id from IPv4 packet.
  *
@@ -70,9 +84,14 @@ ip4_get_port (ip4_header_t *ip, u8 sender)
        *  - outer ICMP header length (2*sizeof (icmp46_header_t))
        *  - inner IP header length
        *  - first 8 bytes of payload of original packet in case of ICMP error
+       *
+       * Also make sure we only attempt to parse payload as IP packet if it's
+       * an ICMP error.
        */
       else if (clib_net_to_host_u16 (ip->length) >=
-	       2 * sizeof (ip4_header_t) + 2 * sizeof (icmp46_header_t) + 8)
+		 2 * sizeof (ip4_header_t) + 2 * sizeof (icmp46_header_t) +
+		   8 &&
+	       icmp_type_is_error_message (icmp->type))
 	{
 	  ip = (ip4_header_t *) (icmp + 2);
 	  if (PREDICT_TRUE ((ip->protocol == IP_PROTOCOL_TCP) ||

src/vnet/ip/ip6_to_ip4.h
@@ -168,7 +168,19 @@ ip6_get_port (vlib_main_t *vm, vlib_buffer_t *b, ip6_header_t *ip6,
 	  if (dst_port)
 	    *dst_port = ((u16 *) (icmp))[2];
 	}
-      else if (clib_net_to_host_u16 (ip6->payload_length) >= 64)
+      /*
+       * if there is enough data and ICMP type indicates ICMP error, then parse
+       * inner packet
+       *
+       * ICMP6 errors are:
+       *   1 - destination_unreachable
+       *   2 - packet_too_big
+       *   3 - time_exceeded
+       *   4 - parameter_problem
+       */
+      else if (clib_net_to_host_u16 (ip6->payload_length) >= 64 &&
+	       icmp->type >= ICMP6_destination_unreachable &&
+	       icmp->type <= ICMP6_parameter_problem)
 	{
 	  u16 ip6_pay_len;
 	  ip6_header_t *inner_ip6;