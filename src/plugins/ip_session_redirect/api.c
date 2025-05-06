FDio/vpp

ip_session_redirect: add dump api for session redirects
Type: improvement

Change-Id: Icd6e94535d388e4c2c08c8ab383fea8cac324d66
Signed-off-by: Hadi Rayan Al-Sandid <halsandi@cisco.com>

src/plugins/ip_session_redirect/api.c
@@ -12,8 +12,10 @@
  * limitations under the License. */
 
 #include <vlib/vlib.h>
+#include <vnet/fib/fib_path_list.h>
 #include <vnet/fib/fib_api.h>
 #include <vnet/ip/ip_format_fns.h>
+#include <vnet/classify/vnet_classify.h>
 #include <vlibmemory/api.h>
 #include <vlibapi/api.h>
 
@@ -103,6 +105,70 @@ vl_api_ip_session_redirect_del_t_handler (vl_api_ip_session_redirect_del_t *mp)
   vec_free (match);
 
   REPLY_MACRO (VL_API_IP_SESSION_REDIRECT_DEL_REPLY);
+}
+
+static void
+send_ip_session_redirect_details (vl_api_registration_t *reg, u32 table_index,
+				  u32 context)
+{
+  ip_session_redirect_main_t *im = &ip_session_redirect_main;
+  ip_session_redirect_t *ipr;
+  ip_session_redirect_t *iprs = im->pool;
+
+  pool_foreach (ipr, iprs)
+    {
+      if (~0 == table_index || ipr->table_index == table_index)
+	{
+	  vl_api_ip_session_redirect_details_t *rmp;
+	  vl_api_fib_path_t *fp;
+	  fib_route_path_t *rpath;
+	  fib_path_encode_ctx_t walk_ctx = {
+	    .rpaths = NULL,
+	  };
+	  u8 n_paths = fib_path_list_get_n_paths (ipr->pl);
+	  /* match_len is computed without table index at the end of the match
+	   * string */
+	  u32 match_len = vec_len (ipr->match_and_table_index) - 4;
+
+	  rmp = vl_msg_api_alloc (sizeof (*rmp) +
+				  sizeof (rmp->paths[0]) * n_paths);
+	  clib_memset (rmp, 0, sizeof (*rmp));
+	  rmp->_vl_msg_id =
+	    ntohs (REPLY_MSG_ID_BASE + VL_API_IP_SESSION_REDIRECT_DETAILS);
+	  rmp->context = context;
+	  rmp->opaque_index = htonl (ipr->opaque_index);
+	  rmp->table_index = htonl (ipr->table_index);
+	  rmp->match_length = htonl (match_len);
+	  rmp->is_punt = ipr->is_punt;
+	  rmp->is_ip6 = ipr->is_ip6;
+	  clib_memcpy (rmp->match, ipr->match_and_table_index, match_len);
+	  rmp->n_paths = n_paths;
+	  fp = rmp->paths;
+	  rmp->retval = 0;
+
+	  fib_path_list_walk_w_ext (ipr->pl, NULL, fib_path_encode, &walk_ctx);
+	  vec_foreach (rpath, walk_ctx.rpaths)
+	    {
+	      fib_api_path_encode (rpath, fp);
+	      fp++;
+	    }
+
+	  vl_api_send_msg (reg, (u8 *) rmp);
+	}
+    }
+}
+
+static void
+vl_api_ip_session_redirect_dump_t_handler (
+  vl_api_ip_session_redirect_dump_t *mp)
+{
+  vl_api_registration_t *reg;
+  u32 table_index = ntohl (mp->table_index);
+  reg = vl_api_client_index_to_registration (mp->client_index);
+  if (reg == 0)
+    return;
+
+  send_ip_session_redirect_details (reg, table_index, mp->context);
 }
 
 #include "ip_session_redirect.api.c"

sollor525/vpp-ips

src/plugins/ip_session_redirect/api.c
@@ -12,8 +12,10 @@
  * limitations under the License. */
 
 #include <vlib/vlib.h>
+#include <vnet/fib/fib_path_list.h>
 #include <vnet/fib/fib_api.h>
 #include <vnet/ip/ip_format_fns.h>
+#include <vnet/classify/vnet_classify.h>
 #include <vlibmemory/api.h>
 #include <vlibapi/api.h>
 
@@ -103,6 +105,68 @@ vl_api_ip_session_redirect_del_t_handler (vl_api_ip_session_redirect_del_t *mp)
   vec_free (match);
 
   REPLY_MACRO (VL_API_IP_SESSION_REDIRECT_DEL_REPLY);
+}
+
+static void
+send_ip_session_redirect_details (vl_api_registration_t *reg, u32 table_index,
+				  u32 context)
+{
+  ip_session_redirect_main_t *im = &ip_session_redirect_main;
+  ip_session_redirect_t *ipr;
+  ip_session_redirect_t *iprs = im->pool;
+
+  pool_foreach (ipr, iprs)
+    {
+      if (~0 == table_index || ipr->table_index == table_index)
+	{
+	  vl_api_ip_session_redirect_details_t *rmp;
+	  vl_api_fib_path_t *fp;
+	  fib_route_path_t *rpath;
+	  fib_path_encode_ctx_t walk_ctx = {
+	    .rpaths = NULL,
+	  };
+	  u8 n_paths = fib_path_list_get_n_paths (ipr->pl);
+	  /* match_len is computed without table index at the end of the match
+	   * string */
+	  u32 match_len = vec_len (ipr->match_and_table_index) - 4;
+
+	  rmp = vl_msg_api_alloc_zero (sizeof (*rmp) +
+				       sizeof (rmp->paths[0]) * n_paths);
+	  rmp->_vl_msg_id =
+	    ntohs (REPLY_MSG_ID_BASE + VL_API_IP_SESSION_REDIRECT_DETAILS);
+	  rmp->context = context;
+	  rmp->opaque_index = htonl (ipr->opaque_index);
+	  rmp->table_index = htonl (ipr->table_index);
+	  rmp->match_length = htonl (match_len);
+	  rmp->is_punt = ipr->is_punt;
+	  rmp->is_ip6 = ipr->is_ip6;
+	  clib_memcpy (rmp->match, ipr->match_and_table_index, match_len);
+	  rmp->n_paths = n_paths;
+	  fp = rmp->paths;
+	  rmp->retval = 0;
+	  fib_path_list_walk_w_ext (ipr->pl, NULL, fib_path_encode, &walk_ctx);
+	  vec_foreach (rpath, walk_ctx.rpaths)
+	    {
+	      fib_api_path_encode (rpath, fp);
+	      fp++;
+	    }
+
+	  vl_api_send_msg (reg, (u8 *) rmp);
+	}
+    }
+}
+
+static void
+vl_api_ip_session_redirect_dump_t_handler (
+  vl_api_ip_session_redirect_dump_t *mp)
+{
+  vl_api_registration_t *reg;
+  u32 table_index = ntohl (mp->table_index);
+  reg = vl_api_client_index_to_registration (mp->client_index);
+  if (reg == 0)
+    return;
+
+  send_ip_session_redirect_details (reg, table_index, mp->context);
 }
 
 #include "ip_session_redirect.api.c"