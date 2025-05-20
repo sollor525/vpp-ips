FDio/vpp

hs-test: further separated KinD from HST suites
- KinD suite was also improved and cleaned up a bit

Type: test

Change-Id: Iee1adb69f935b69b79d70a541fa40f5f5d73298d
Signed-off-by: Adrian Villin <avillin@cisco.com>

extras/hs-test/infra/container.go
@@ -13,20 +13,15 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/docker/go-units"
-
+	. "fd.io/hs-test/infra/infra_common"
 	"github.com/cilium/cilium/pkg/sysctl"
 	containerTypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/go-units"
 	"github.com/edwarnicke/exechelper"
-	. "github.com/onsi/ginkgo/v2"
-)
-
-const (
-	LogDir    string = "/tmp/hs-test/"
-	volumeDir string = "/volumes"
+	"github.com/onsi/ginkgo/v2"
 )
 
 var (
@@ -93,7 +88,7 @@ func newContainer(suite *HstSuite, yamlInput ContainerConfig) (*Container, error
 	}
 
 	if _, ok := yamlInput["volumes"]; ok {
-		workingVolumeDir := LogDir + suite.GetCurrentTestName() + volumeDir
+		workingVolumeDir := LogDir + suite.GetCurrentTestName() + VolumeDir
 		workDirReplacer := strings.NewReplacer("$HST_DIR", workDir)
 		volDirReplacer := strings.NewReplacer("$HST_VOLUME_DIR", workingVolumeDir)
 		for _, volu := range yamlInput["volumes"].([]interface{}) {
@@ -458,7 +453,7 @@ func (c *Container) ExecServer(useEnvVars bool, command string, arguments ...any
 		envVars = ""
 	}
 	containerExecCommand := fmt.Sprintf("docker exec -d %s %s %s", envVars, c.Name, serverCommand)
-	GinkgoHelper()
+	ginkgo.GinkgoHelper()
 	c.Suite.Log(containerExecCommand)
 	c.Suite.AssertNil(exechelper.Run(containerExecCommand))
 }
@@ -472,7 +467,7 @@ func (c *Container) Exec(useEnvVars bool, command string, arguments ...any) (str
 		envVars = ""
 	}
 	containerExecCommand := fmt.Sprintf("docker exec %s %s %s", envVars, c.Name, serverCommand)
-	GinkgoHelper()
+	ginkgo.GinkgoHelper()
 	c.Suite.Log(containerExecCommand)
 	byteOutput, err := exechelper.CombinedOutput(containerExecCommand)
 	return string(byteOutput), err

extras/hs-test/infra/cpu.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"strings"
 
+	. "fd.io/hs-test/infra/infra_common"
 	. "github.com/onsi/ginkgo/v2"
 )
 

extras/hs-test/infra/suite_envoy_proxy.go
@@ -12,6 +12,7 @@ import (
 	"strings"
 	"time"
 
+	. "fd.io/hs-test/infra/infra_common"
 	. "github.com/onsi/ginkgo/v2"
 )
 
@@ -142,12 +143,12 @@ func (s *EnvoyProxySuite) SetupTest() {
 	s.AssertNil(s.Containers.EnvoyProxy.Start())
 }
 
-func (s *EnvoyProxySuite) TearDownTest() {
+func (s *EnvoyProxySuite) TeardownTest() {
 	if CurrentSpecReport().Failed() {
 		s.CollectNginxLogs(s.Containers.NginxServerTransient)
 		s.CollectEnvoyLogs(s.Containers.EnvoyProxy)
 	}
-	s.HstSuite.TearDownTest()
+	s.HstSuite.TeardownTest()
 }
 
 func (s *EnvoyProxySuite) ProxyPort() uint16 {
@@ -182,10 +183,10 @@ var _ = Describe("EnvoyProxySuite", Ordered, ContinueOnFailure, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range envoyProxyTests {
@@ -211,10 +212,10 @@ var _ = Describe("EnvoyProxySuiteSolo", Ordered, ContinueOnFailure, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range envoyProxySoloTests {

extras/hs-test/infra/suite_iperf_linux.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"time"
 
+	. "fd.io/hs-test/infra/infra_common"
 	. "github.com/onsi/ginkgo/v2"
 )
 
@@ -51,10 +52,10 @@ var _ = Describe("IperfSuite", Ordered, ContinueOnFailure, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range iperfTests {
@@ -80,10 +81,10 @@ var _ = Describe("IperfSuiteSolo", Ordered, ContinueOnFailure, Serial, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range iperfSoloTests {

extras/hs-test/infra/suite_ldp.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	. "fd.io/hs-test/infra/infra_common"
 	. "github.com/onsi/ginkgo/v2"
 )
 
@@ -96,7 +97,7 @@ func (s *LdpSuite) SetupTest() {
 	}
 }
 
-func (s *LdpSuite) TearDownTest() {
+func (s *LdpSuite) TeardownTest() {
 	if CurrentSpecReport().Failed() {
 		s.CollectIperfLogs(s.Containers.ServerVpp)
 		s.CollectRedisServerLogs(s.Containers.ServerVpp)
@@ -106,7 +107,7 @@ func (s *LdpSuite) TearDownTest() {
 		delete(container.EnvVars, "LD_PRELOAD")
 		delete(container.EnvVars, "VCL_CONFIG")
 	}
-	s.HstSuite.TearDownTest()
+	s.HstSuite.TeardownTest()
 
 }
 
@@ -155,11 +156,11 @@ var _ = Describe("LdpSuite", Ordered, ContinueOnFailure, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	// https://onsi.github.io/ginkgo/#dynamically-generating-specs
@@ -186,10 +187,10 @@ var _ = Describe("LdpSuiteSolo", Ordered, ContinueOnFailure, Serial, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	// https://onsi.github.io/ginkgo/#dynamically-generating-specs

extras/hs-test/infra/suite_vpp_proxy.go
@@ -13,6 +13,7 @@ import (
 	"strconv"
 	"strings"
 
+	. "fd.io/hs-test/infra/infra_common"
 	. "github.com/onsi/ginkgo/v2"
 )
 
@@ -87,7 +88,7 @@ func (s *VppProxySuite) SetupTest() {
 	}
 }
 
-func (s *VppProxySuite) TearDownTest() {
+func (s *VppProxySuite) TeardownTest() {
 	vpp := s.Containers.VppProxy.VppInstance
 	if CurrentSpecReport().Failed() {
 		s.Log(vpp.Vppctl("show session verbose 2"))
@@ -95,7 +96,7 @@ func (s *VppProxySuite) TearDownTest() {
 		s.CollectNginxLogs(s.Containers.NginxServerTransient)
 		s.CollectIperfLogs(s.Containers.IperfS)
 	}
-	s.HstSuite.TearDownTest()
+	s.HstSuite.TeardownTest()
 }
 
 func (s *VppProxySuite) SetupNginxServer() {
@@ -221,10 +222,10 @@ var _ = Describe("VppProxySuite", Ordered, ContinueOnFailure, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range vppProxyTests {
@@ -250,10 +251,10 @@ var _ = Describe("VppProxySuiteSolo", Ordered, ContinueOnFailure, Serial, func()
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range vppProxySoloTests {

sollor525/vpp-ips

extras/hs-test/infra/container.go
@@ -13,20 +13,15 @@ import (
 	"text/template"
 	"time"
 
-	"github.com/docker/go-units"
-
+	. "fd.io/hs-test/infra/common"
 	"github.com/cilium/cilium/pkg/sysctl"
 	containerTypes "github.com/docker/docker/api/types/container"
 	"github.com/docker/docker/api/types/filters"
 	"github.com/docker/docker/api/types/image"
 	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/go-units"
 	"github.com/edwarnicke/exechelper"
-	. "github.com/onsi/ginkgo/v2"
-)
-
-const (
-	LogDir    string = "/tmp/hs-test/"
-	volumeDir string = "/volumes"
+	"github.com/onsi/ginkgo/v2"
 )
 
 var (
@@ -93,7 +88,7 @@ func newContainer(suite *HstSuite, yamlInput ContainerConfig) (*Container, error
 	}
 
 	if _, ok := yamlInput["volumes"]; ok {
-		workingVolumeDir := LogDir + suite.GetCurrentTestName() + volumeDir
+		workingVolumeDir := LogDir + suite.GetCurrentTestName() + VolumeDir
 		workDirReplacer := strings.NewReplacer("$HST_DIR", workDir)
 		volDirReplacer := strings.NewReplacer("$HST_VOLUME_DIR", workingVolumeDir)
 		for _, volu := range yamlInput["volumes"].([]interface{}) {
@@ -458,7 +453,7 @@ func (c *Container) ExecServer(useEnvVars bool, command string, arguments ...any
 		envVars = ""
 	}
 	containerExecCommand := fmt.Sprintf("docker exec -d %s %s %s", envVars, c.Name, serverCommand)
-	GinkgoHelper()
+	ginkgo.GinkgoHelper()
 	c.Suite.Log(containerExecCommand)
 	c.Suite.AssertNil(exechelper.Run(containerExecCommand))
 }
@@ -472,7 +467,7 @@ func (c *Container) Exec(useEnvVars bool, command string, arguments ...any) (str
 		envVars = ""
 	}
 	containerExecCommand := fmt.Sprintf("docker exec %s %s %s", envVars, c.Name, serverCommand)
-	GinkgoHelper()
+	ginkgo.GinkgoHelper()
 	c.Suite.Log(containerExecCommand)
 	byteOutput, err := exechelper.CombinedOutput(containerExecCommand)
 	return string(byteOutput), err

extras/hs-test/infra/cpu.go
@@ -8,6 +8,7 @@ import (
 	"os/exec"
 	"strings"
 
+	. "fd.io/hs-test/infra/common"
 	. "github.com/onsi/ginkgo/v2"
 )
 

extras/hs-test/infra/suite_envoy_proxy.go
@@ -12,6 +12,7 @@ import (
 	"strings"
 	"time"
 
+	. "fd.io/hs-test/infra/common"
 	. "github.com/onsi/ginkgo/v2"
 )
 
@@ -142,12 +143,12 @@ func (s *EnvoyProxySuite) SetupTest() {
 	s.AssertNil(s.Containers.EnvoyProxy.Start())
 }
 
-func (s *EnvoyProxySuite) TearDownTest() {
+func (s *EnvoyProxySuite) TeardownTest() {
 	if CurrentSpecReport().Failed() {
 		s.CollectNginxLogs(s.Containers.NginxServerTransient)
 		s.CollectEnvoyLogs(s.Containers.EnvoyProxy)
 	}
-	s.HstSuite.TearDownTest()
+	s.HstSuite.TeardownTest()
 }
 
 func (s *EnvoyProxySuite) ProxyPort() uint16 {
@@ -182,10 +183,10 @@ var _ = Describe("EnvoyProxySuite", Ordered, ContinueOnFailure, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range envoyProxyTests {
@@ -211,10 +212,10 @@ var _ = Describe("EnvoyProxySuiteSolo", Ordered, ContinueOnFailure, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range envoyProxySoloTests {

extras/hs-test/infra/suite_iperf_linux.go
@@ -6,6 +6,7 @@ import (
 	"strings"
 	"time"
 
+	. "fd.io/hs-test/infra/common"
 	. "github.com/onsi/ginkgo/v2"
 )
 
@@ -51,10 +52,10 @@ var _ = Describe("IperfSuite", Ordered, ContinueOnFailure, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range iperfTests {
@@ -80,10 +81,10 @@ var _ = Describe("IperfSuiteSolo", Ordered, ContinueOnFailure, Serial, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range iperfSoloTests {

extras/hs-test/infra/suite_ldp.go
@@ -7,6 +7,7 @@ import (
 	"strings"
 	"time"
 
+	. "fd.io/hs-test/infra/common"
 	. "github.com/onsi/ginkgo/v2"
 )
 
@@ -96,7 +97,7 @@ func (s *LdpSuite) SetupTest() {
 	}
 }
 
-func (s *LdpSuite) TearDownTest() {
+func (s *LdpSuite) TeardownTest() {
 	if CurrentSpecReport().Failed() {
 		s.CollectIperfLogs(s.Containers.ServerVpp)
 		s.CollectRedisServerLogs(s.Containers.ServerVpp)
@@ -106,7 +107,7 @@ func (s *LdpSuite) TearDownTest() {
 		delete(container.EnvVars, "LD_PRELOAD")
 		delete(container.EnvVars, "VCL_CONFIG")
 	}
-	s.HstSuite.TearDownTest()
+	s.HstSuite.TeardownTest()
 
 }
 
@@ -155,11 +156,11 @@ var _ = Describe("LdpSuite", Ordered, ContinueOnFailure, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	// https://onsi.github.io/ginkgo/#dynamically-generating-specs
@@ -186,10 +187,10 @@ var _ = Describe("LdpSuiteSolo", Ordered, ContinueOnFailure, Serial, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	// https://onsi.github.io/ginkgo/#dynamically-generating-specs

extras/hs-test/infra/suite_vpp_proxy.go
@@ -13,6 +13,7 @@ import (
 	"strconv"
 	"strings"
 
+	. "fd.io/hs-test/infra/common"
 	. "github.com/onsi/ginkgo/v2"
 )
 
@@ -87,7 +88,7 @@ func (s *VppProxySuite) SetupTest() {
 	}
 }
 
-func (s *VppProxySuite) TearDownTest() {
+func (s *VppProxySuite) TeardownTest() {
 	vpp := s.Containers.VppProxy.VppInstance
 	if CurrentSpecReport().Failed() {
 		s.Log(vpp.Vppctl("show session verbose 2"))
@@ -95,7 +96,7 @@ func (s *VppProxySuite) TearDownTest() {
 		s.CollectNginxLogs(s.Containers.NginxServerTransient)
 		s.CollectIperfLogs(s.Containers.IperfS)
 	}
-	s.HstSuite.TearDownTest()
+	s.HstSuite.TeardownTest()
 }
 
 func (s *VppProxySuite) SetupNginxServer() {
@@ -221,10 +222,10 @@ var _ = Describe("VppProxySuite", Ordered, ContinueOnFailure, func() {
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range vppProxyTests {
@@ -250,10 +251,10 @@ var _ = Describe("VppProxySuiteSolo", Ordered, ContinueOnFailure, Serial, func()
 		s.SetupTest()
 	})
 	AfterAll(func() {
-		s.TearDownSuite()
+		s.TeardownSuite()
 	})
 	AfterEach(func() {
-		s.TearDownTest()
+		s.TeardownTest()
 	})
 
 	for filename, tests := range vppProxySoloTests {